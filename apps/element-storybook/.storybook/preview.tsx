388044a313e898ef3e5a9271c9815bc484197f0a
L. Roxanne young <Roxanne.Young@availity.com>
2023-02-24 14:06:41 -0500

    feat(element-storybook): storybook styling and config

---
+++ apps/element-storybook/.storybook/preview.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ThemeProvider } from '@availity/theme-provider';
+import { themes } from '@storybook/theming';
+
+export const parameters = {
+  docs: {
+    theme: themes.light,
+  },
+  options: {
+    storySort: {
+      order: [
+        'Element',
+        'Contributing',
+        'Design System/*',
+        'Components/*'
+      ],
+    },
+  },
+};
+
+const withThemeProvider = (Story: any) => {
+    return (
+        <ThemeProvider>
+          <Story />
+        </ThemeProvider>
+      );
+  };
+
+export const decorators = [withThemeProvider];
