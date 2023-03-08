bf1142fbe2ca9838d7cf69a99f29cad0f71b05ff
Benjamin Newman <benjamin.newman@availity.com>
2023-03-08 16:35:08 -0500

    feat(element): move breadcrumbs to own package

--- packages/element/src/index.ts
+++ packages/element/src/index.ts
@@ -1,5 +1,6 @@
 export * from '@availity/mui-alert';
 export * from '@availity/mui-badge';
 export * from '@availity/mui-button';
+export * from '@availity/mui-breadcrumbs';
 export * from '@availity/mui-link';
 export * from '@availity/theme-provider';
