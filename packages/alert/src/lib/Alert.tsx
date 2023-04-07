0b332f535d28f31e4adb90a137205cf87f97660f
L. Roxanne young <Roxanne.Young@availity.com>
2023-04-07 09:06:41 -0400

    feat(mui-alert): update styling to match standards

--- packages/alert/src/lib/Alert.tsx
+++ packages/alert/src/lib/Alert.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Alert as MUIAlert } from '@mui/material';
 import type { AlertProps as MUIAlertProps } from '@mui/material';
-// import Icon from '@availity/mui-icon';
+import { InfoCircleIcon, WarningCircleIcon, SuccessCircleIcon } from '@availity/mui-icon';
 
-export type AlertProps = {
-  children: React.ReactNode;
-} & MUIAlertProps;
+export type AlertProps =  MUIAlertProps;
 
-export const Alert = ({ children, ...rest }: AlertProps): JSX.Element => (
-  <MUIAlert {...rest}>
-    {children}
-  </MUIAlert>
+const iconMapping =  {
+  error: <WarningCircleIcon aria-hidden={false} />,
+  info: <InfoCircleIcon aria-hidden={false} />,
+  success: <SuccessCircleIcon aria-hidden={false} />,
+  warning: <WarningCircleIcon aria-hidden={false} sx={{ color: 'warning.dark' }}/>
+}
+
+export const Alert = ({ ...args }: AlertProps): JSX.Element => (
+  <MUIAlert iconMapping={iconMapping} closeText="dismiss alert" {...args}/>
 );
