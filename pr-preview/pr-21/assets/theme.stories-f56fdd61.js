e68333fdc967fae9304a02f614e1a5380fa47e41
LauRoxx <LauRoxx@users.noreply.github.com>
2023-04-11 12:35:38 +0000

    Deploy preview for PR 21 🛫

---
+++ pr-preview/pr-21/assets/theme.stories-f56fdd61.js
@@ -0,0 +1,4 @@
+import{j as o}from"./jsx-runtime-670450c2.js";import{l as a}from"./index-010baa72.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";const e={render:()=>o("pre",{children:JSON.stringify(a,null,2)})},p={title:"Design System/Theme"};var r,t,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: () => <pre>{JSON.stringify(lightTheme, null, 2)}</pre>
+}`,...(s=(t=e.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,p as default};
+//# sourceMappingURL=theme.stories-f56fdd61.js.map
