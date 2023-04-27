d45d675b4440a82fb9e41d76857e20ffc52fac94
LauRoxx <LauRoxx@users.noreply.github.com>
2023-04-27 14:47:53 +0000

    Deploy preview for PR 22 🛫

---
+++ pr-preview/pr-22/assets/migration-e20115c5.js
@@ -0,0 +1,9 @@
+import{j as t,a as i,F as p}from"./jsx-runtime-670450c2.js";import{M as m}from"./chunk-PCJTTTQV-f7c5d3e0.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{u as a}from"./index-4fb8b842.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./preload-helper-41c905a7.js";import"./react-18-0a3f08e5.js";import"./_baseIsEqual-04a52134.js";import"./index-96c5f47c.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-34cf2b24.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-eaaa54fe.js";import"./memoizerific-a8c4a000.js";import"./index-abcfcb28.js";import"./chunk-RDJSMFWU-e11cec2c.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-Y5O7ZP4P-c6079acc.js";import"./extends-98964cd2.js";import"./inheritsLoose-d374ba88.js";import"./index-356e4a49.js";function B(o={}){const{wrapper:n}=Object.assign({},a(),o.components);return n?t(n,Object.assign({},o,{children:t(r,{})})):r();function r(){const e=Object.assign({h2:"h2",p:"p",code:"code",ul:"ul",li:"li"},a(),o.components);return i(p,{children:[t(m,{title:"BS4 Migration/Getting Started"}),`
+`,t(e.h2,{children:"First Steps"}),`
+`,i(e.p,{children:["Follow Usage instructions in ",t(e.code,{children:"@availity/element"})," ReadMe for installation and theme provider steps."]}),`
+`,t(e.h2,{children:"Reactstrap Component Equivalents"}),`
+`,t(e.p,{children:"Not all components from Reactstrap have a 1:1 equivalent in MUI. Listed below are some notable changes"}),`
+`,i(e.ul,{children:[`
+`,t(e.li,{children:"Badge -> Chip"}),`
+`]})]})}}export{B as default};
+//# sourceMappingURL=migration-e20115c5.js.map
