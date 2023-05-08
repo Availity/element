78ad1b9c21f821092524aa152d1aaa4f5c706b4e
L. Roxanne young <Roxanne.Young@availity.com>
2023-05-08 13:10:53 -0400

    chore(mui-button): loading button updates

--- packages/button/src/lib/LoadingButton.tsx
+++ packages/button/src/lib/LoadingButton.tsx
@@ -3,12 +3,24 @@ import MUILoadingButton, { LoadingButtonProps as MuiLoadingButtonProps} from '@m
 
 export type LoadingButtonProps = {
   children: React.ReactNode;
-} & MuiLoadingButtonProps;
+  /**
+   * The color of the component.
+   */
+  color?: "primary" | "secondary" | "error";
+} & Omit<MuiLoadingButtonProps, 'color' | 'centerRipple' | 'disableRipple' | 'disableTouchRipple' | 'focusRipple' | 'disableFocusRipple' | 'TouchRippleProps' | 'touchRippleRef'>;
 
-export const LoadingButton = ({ children, ...rest }: LoadingButtonProps): JSX.Element => (
-  <MUILoadingButton {...rest}>
-    {children}
+export const LoadingButton = ({ children, loading, ...rest }: LoadingButtonProps): JSX.Element => {
+  //changing default loading button behavior to add end loading icon instead of replacing contents completely with icon
+  const loadingPosition = rest.startIcon ? 'start' : 'end';
+  const nonLoadingStyling = (rest.startIcon || rest.endIcon || loading) ? {} : { padding: '0 .7rem' };
+  const endIcon = rest.startIcon ? undefined : <span style={ loading ? {padding: '0 .7rem'} : {}}></span>;
+
+  return (
+  <MUILoadingButton endIcon={endIcon} loadingPosition={loadingPosition} loading={loading} disableRipple {...rest}>
+    <span style={nonLoadingStyling} aria-hidden={!!loading}>
+      {children}
+    </span>
   </MUILoadingButton>
-);
+)};
 
 
