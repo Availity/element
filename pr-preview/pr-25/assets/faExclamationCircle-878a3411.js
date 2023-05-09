fd52c30dc28ad404dc42ae5798d4c0185aecda7e
LauRoxx <LauRoxx@users.noreply.github.com>
2023-05-09 13:28:47 +0000

    Deploy preview for PR 25 🛫

---
+++ pr-preview/pr-25/assets/faExclamationCircle-878a3411.js
@@ -0,0 +1,2 @@
+var a={},r={},c={};export{a,c as b,r as f};
+//# sourceMappingURL=faExclamationCircle-878a3411.js.map
---
+++ pr-preview/pr-25/assets/introduction-37638bd1.js
@@ -0,0 +1,24 @@
+import{j as t,a as r,F as o}from"./jsx-runtime-670450c2.js";import{M as a,a as m}from"./chunk-PCJTTTQV-f7c5d3e0.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{u as s}from"./index-4fb8b842.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./preload-helper-41c905a7.js";import"./react-18-0a3f08e5.js";import"./_baseIsEqual-04a52134.js";import"./index-96c5f47c.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-34cf2b24.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-eaaa54fe.js";import"./memoizerific-a8c4a000.js";import"./index-abcfcb28.js";import"./chunk-RDJSMFWU-e11cec2c.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-Y5O7ZP4P-c6079acc.js";import"./extends-98964cd2.js";import"./inheritsLoose-d374ba88.js";import"./index-356e4a49.js";const p=`# @availity/theme-provider
+
+> Theme Provider using MUI and @availity/design-tokens
+
+[![Version](https://img.shields.io/npm/v/@availity/theme-provider.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/theme-provider)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/theme-provider.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/theme-provider)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/theme-provider?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/theme-provider/package.json)
+
+## Installation
+
+### NPM
+
+\`\`\`bash
+npm install @availity/theme-provider
+\`\`\`
+
+### Yarn
+
+\`\`\`bash
+yarn add @availity/theme-provider
+\`\`\`
+`;function S(e={}){const{wrapper:i}=Object.assign({},s(),e.components);return i?t(i,Object.assign({},e,{children:t(n,{})})):n();function n(){return r(o,{children:[t(a,{title:"Design System/ThemeProvider/Introduction"}),`
+`,t(m,{children:p})]})}}export{S as default};
+//# sourceMappingURL=introduction-37638bd1.js.map
