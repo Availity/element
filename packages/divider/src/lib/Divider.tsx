1d0652bdc88cd0a7db437a327f5649a1a28d21fd
Joseph Spanbauer <joseph.spanbauer@availity.com>
2023-05-11 15:55:11 -0400

    feat(mui-divider): removing commented out code

--- packages/divider/src/lib/Divider.tsx
+++ packages/divider/src/lib/Divider.tsx
@@ -1,13 +1,3 @@
-// import { Card as MuiCard, CardProps as MuiCardProps } from '@mui/material';
-
-// export interface DividerProps extends MuiCardProps {
-//   children: React.ReactNode;
-// }
-
-// export const Divider = ({ children, ...rest }: DividerProps): JSX.Element => {
-//   return <MuiCard {...rest}>{children}</MuiCard>;
-// };
-
 import { Divider as MuiDivider, DividerProps as MuiDividerProps } from '@mui/material';
 
 export interface DividerProps extends MuiDividerProps {
