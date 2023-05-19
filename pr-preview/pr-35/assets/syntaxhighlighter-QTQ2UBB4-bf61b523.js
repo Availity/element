3dbb7d1831b6c906678e4372001ea58b1d246cae
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-05-19 18:14:19 +0000

    Remove preview for PR 35 🛬

--- pr-preview/pr-35/assets/syntaxhighlighter-QTQ2UBB4-bf61b523.js
+++
@@ -1,2 +0,0 @@
-import{S as n,c as d,s as f}from"./chunk-RDJSMFWU-e11cec2c.js";import"./preload-helper-41c905a7.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-34cf2b24.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-eaaa54fe.js";import"./memoizerific-a8c4a000.js";export{n as SyntaxHighlighter,d as createCopyToClipboardFunction,f as default};
-//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-bf61b523.js.map
--- pr-preview/pr-35/assets/theme-provider.stories-5ac93111.js
+++
@@ -1,9 +0,0 @@
-import{a as t,F as d,j as r}from"./jsx-runtime-670450c2.js";import{T as m}from"./index-125dd36a.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-010baa72.js";import"./extends-98964cd2.js";import"./emotion-react.browser.esm-5cb50a94.js";import"./emotion-element-6a883da9.browser.esm-1c5b50fe.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-eaaa54fe.js";const e={render:()=>t(d,{children:[r(m,{children:r("div",{children:"This component is inside the ThemeProvider"})}),r("div",{children:"This component is outside the ThemeProvider"})]})},u={title:"Design System/ThemeProvider"};var i,o,s;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
-  render: () => <>
-    <ThemeProvider>
-      <div>This component is inside the ThemeProvider</div>
-    </ThemeProvider>
-    <div>This component is outside the ThemeProvider</div>
-  </>
-}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const f=["Default"];export{e as Default,f as __namedExportsOrder,u as default};
-//# sourceMappingURL=theme-provider.stories-5ac93111.js.map
