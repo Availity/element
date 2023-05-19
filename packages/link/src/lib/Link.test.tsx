dbcf569191cd0525b2c474eab0d2b487feb473ee
L. Roxanne young <Roxanne.Young@availity.com>
2023-05-19 10:05:31 -0400

    feat(mui-link): add link package

---
+++ packages/link/src/lib/Link.test.tsx
@@ -0,0 +1,93 @@
+import { render, fireEvent } from '@testing-library/react';
+import { Link } from './Link';
+
+describe('Link', () => {
+  test('should render successfully', () => {
+    const { getByText } = render(<Link href="#">Test</Link>);
+
+    expect(getByText('Test')).toBeTruthy();
+  });
+
+  test('should render absolute url', () => {
+    const { getByRole } = render(<Link href="https://github.com/Availity">Test</Link>);
+
+    const link = getByRole('link');
+
+    expect(link.getAttribute('href')).toBe('https://github.com/Availity');
+  });
+
+  test('should render formatted url when url prop is relative', () => {
+    const { getByRole } = render(<Link href="/public/apps/my-app">My App</Link>);
+
+    const link = getByRole('link');
+    const expected = '/public/apps/home/#!/loadApp?appUrl=%2Fpublic%2Fapps%2Fmy-app';
+
+    expect(link.getAttribute('href')).toBe(expected);
+  });
+
+  test('should render url prop as is when loadApp is false', () => {
+    const { getByRole } = render(
+      <Link loadApp={false} href="/public/apps/my-app">
+        My App
+      </Link>
+    );
+
+    const link = getByRole('link');
+    const expected = '/public/apps/my-app';
+
+    expect(link.getAttribute('href')).toBe(expected);
+  });
+
+  test('should default rel prop if none provided when linking to external site', () => {
+    const { getByRole } = render(
+      <Link loadApp={false} href="https://github.com/Availity" target="_blank">
+        Github
+      </Link>
+    );
+
+    const link = getByRole('link');
+    const expected = 'noopener noreferrer';
+
+    expect(link.getAttribute('rel')).toBe(expected);
+  });
+
+  test('should respect rel prop if provided when linking to external site', () => {
+    const { getByRole } = render(
+      <Link loadApp={false} href="https://github.com/Availity" target="_blank" rel="nofollow">
+        Github
+      </Link>
+    );
+
+    const link = getByRole('link');
+    const expected = 'nofollow';
+
+    expect(link.getAttribute('rel')).toBe(expected);
+  });
+
+  test('should call onClick', () => {
+    const onClick = jest.fn();
+
+    const { getByRole } = render(
+      <Link loadApp={false} href="/public/apps/my-app" onClick={onClick}>
+        My App
+      </Link>
+    );
+
+    const link = getByRole('link');
+    fireEvent.click(link);
+
+    expect(onClick.mock.calls[0][1]).toBe('/public/apps/my-app');
+  });
+
+  test('should render link with proper styling', () => {
+    const { getByRole } = render(
+      <Link href="/public/apps/my-app" className="card-link">
+        My App
+      </Link>
+    );
+
+    const link = getByRole('link');
+
+    expect(link.getAttribute('class')).toContain('card-link');
+  });
+});
---
+++ packages/link/src/lib/Link.tsx
@@ -0,0 +1,81 @@
+import { ReactNode, forwardRef } from 'react';
+import { Link as MuiLink, LinkProps as MuiLinkProps } from '@mui/material';
+import { SystemProps } from '@mui/system';
+import { OpenInNewIcon } from '@availity/mui-icon';
+
+// Scheme: https://tools.ietf.org/html/rfc3986#section-3.1
+// Absolute URL: https://tools.ietf.org/html/rfc3986#section-4.3
+const ABSOLUTE_URL_REGEX = /^[a-zA-Z][a-zA-Z\d+\-.]*?:/;
+
+// Windows paths like `c:\`
+const WINDOWS_PATH_REGEX = /^[a-zA-Z]:\\/;
+
+function isAbsoluteUrl(url: string) {
+  if (WINDOWS_PATH_REGEX.test(url)) {
+    return false;
+  }
+
+  return ABSOLUTE_URL_REGEX.test(url);
+}
+
+// if absolute or loadApp is disabled, return url. otherwise loadappify the url
+const getUrl = (url = '') => {
+  return `/public/apps/home/#!/loadApp?appUrl=${encodeURIComponent(url)}`;
+};
+
+// takes href and transforms it so that we can compare hostnames and other properties
+const getLocation = (href: string) => {
+  const location = document.createElement('a');
+  location.href = href;
+  return location;
+};
+
+const setRel = (url: string, target: string, absolute: boolean) => {
+  if (target === '_blank' && absolute) {
+    const dest = getLocation(url);
+    if (dest.hostname !== window.location.hostname) {
+      // default rel when linking to external destinations for performance and security
+      return 'noopener noreferrer';
+    }
+  }
+  return undefined;
+};
+
+export type LinkProps = {
+  /** Url of the page the link goes to */
+  href: string;
+  /** format href to leverage loadApp within home iframe when relative url used.
+   *
+   * **Default:** `true` */
+  loadApp?: boolean;
+  /** Where to open the linked document. Adds `OpenInNewIcon` when target is `_blank` */
+  target?: string;
+  /** Function to run onClick of the link. The first argument passed to onClick is the event. The second argument is the processed url. */
+  onClick?: (event: React.MouseEvent, url: string) => void;
+  children?: ReactNode;
+  rel?: string;
+} & Omit<MuiLinkProps, 'underline' | 'noWrap' | 'variantMapping' | keyof SystemProps>;
+
+export const Link = forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => {
+  const { href, target = '_self', children, onClick, loadApp = true, rel, ...rest } = props;
+  const absolute = isAbsoluteUrl(href);
+  const encode = !(absolute || !loadApp);
+  const url = encode ? getUrl(href) : href;
+  const NewWindowIcon = target === '_blank' ? <OpenInNewIcon /> : null;
+
+  return (
+    <MuiLink
+      href={url}
+      target={target}
+      onClick={(event: React.MouseEvent) => onClick && onClick(event, url)}
+      rel={rel || setRel(url, target, absolute)}
+      underline="hover"
+      {...rest}
+      ref={ref}
+    >
+      <span>
+        {children} {NewWindowIcon}
+      </span>
+    </MuiLink>
+  );
+});
