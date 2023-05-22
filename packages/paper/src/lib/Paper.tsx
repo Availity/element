ecddf6908fdb69418705cb854aa84c419de3ee55
Jordan Young <jordan.young@availity.com>
2023-05-22 13:50:10 -0400

    feat(mui-paper): refactor how props are spread

--- packages/paper/src/lib/Paper.tsx
+++ packages/paper/src/lib/Paper.tsx
@@ -3,10 +3,6 @@ import { Paper as MuiPaper, PaperProps as MuiPaperProps } from '@mui/material';
 
 export type PaperProps = Omit<MuiPaperProps, 'classes' | 'style' | 'className' | 'sx'>;
 
-export const Paper = forwardRef<HTMLDivElement, PaperProps>(({ children, variant = 'outlined', ...rest }, ref) => {
-  return (
-    <MuiPaper variant={variant} {...rest} ref={ref}>
-      {children}
-    </MuiPaper>
-  );
+export const Paper = forwardRef<HTMLDivElement, PaperProps>((props, ref) => {
+  return <MuiPaper {...props} ref={ref} />;
 });
