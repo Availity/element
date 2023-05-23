de18c18a4932ba625a0f7efcf2c704278eb655b0
owolfhu1 <owolfhu1@users.noreply.github.com>
2023-05-23 12:59:33 +0000

    Deploy preview for PR 39 🛫

---
+++ pr-preview/pr-39/assets/introduction-ca754130.js
@@ -0,0 +1,60 @@
+import{j as t,a as i,F as o}from"./jsx-runtime-58698645.js";import{M as m,e as s}from"./chunk-PCJTTTQV-2679154a.js";import{u as r}from"./index-f2208173.js";import"./index-ccf6a75d.js";import"./iframe-6860623e.js";import"../sb-preview/runtime.js";import"./react-18-95b588b7.js";import"./index-8d7a8128.js";import"./chunk-4NMOSTKD-052c8761.js";import"./index-d475d2ea.js";import"./extends-79cb841a.js";import"./setPrototypeOf-8d862a7d.js";import"./inheritsLoose-a02b825e.js";import"./isNativeReflectConstruct-e378569d.js";import"./index-356e4a49.js";const p=`# @availity/mui-badge
+
+> Availity MUI Badge component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-badge.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-badge)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-badge.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-badge)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-badge?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/badge/package.json)
+
+## Documentation
+
+This package extends the MUI Badge component: [MUI Badge Docs](https://mui.com/components/badge/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-badge-introduction--docs)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/mui-badge
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-badge
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Badge } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Badge } from '@availity/mui-badge';
+\`\`\`
+`;function a(n){return i(o,{children:[t(m,{title:"Components/Badge/Introduction"}),`
+`,t(s,{children:p})]})}function k(n={}){const{wrapper:e}=Object.assign({},r(),n.components);return e?t(e,Object.assign({},n,{children:t(a,n)})):a()}export{k as default};
+//# sourceMappingURL=introduction-ca754130.js.map
