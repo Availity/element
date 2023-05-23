91a2eea248c63899da7c98f7c4214ade78fd8c95
owolfhu1 <owolfhu1@users.noreply.github.com>
2023-05-23 20:25:55 +0000

    Remove preview for PR 44 🛬

--- pr-preview/pr-44/assets/useId-fd294ee9.js
+++
@@ -1,2 +0,0 @@
-import{r as u,a as c}from"./index-8db94870.js";let n=0;function l(t){const[e,a]=u.useState(t),o=t||e;return u.useEffect(()=>{e==null&&(n+=1,a(`mui-${n}`))},[e]),o}const s=c["useId".toString()];function r(t){if(s!==void 0){const e=s();return t??e}return l(t)}export{r as u};
-//# sourceMappingURL=useId-fd294ee9.js.map
