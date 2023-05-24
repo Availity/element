299a5d613a527b5cb3d9c3ee9be225972e43a861
owolfhu1 <owolfhu1@users.noreply.github.com>
2023-05-24 18:43:20 +0000

    Deploy preview for PR 46 🛫

---
+++ pr-preview/pr-46/assets/inheritsLoose-a02b825e.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./setPrototypeOf-8d862a7d.js";function p(t,o){t.prototype=Object.create(o.prototype),t.prototype.constructor=t,e(t,o)}export{p as _};
+//# sourceMappingURL=inheritsLoose-a02b825e.js.map
