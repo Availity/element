72813c42c1550694841f4ec5ef650940406f0551
jordan-a-young <jordan-a-young@users.noreply.github.com>
2023-06-01 14:25:42 +0000

    Deploy preview for PR 47 🛫

---
+++ pr-preview/pr-47/assets/introduction-d3ada58e.js
@@ -0,0 +1,60 @@
+import{j as n,a as e,F as a}from"./jsx-runtime-91a467a5.js";import{M as m,e as s}from"./chunk-PCJTTTQV-d714e850.js";import{u as r}from"./index-1d576ef5.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-89f8c8f0.js";import"../sb-preview/runtime.js";import"./react-18-1b38c8e6.js";import"./index-8ce4a492.js";import"./chunk-4NMOSTKD-052c8761.js";import"./index-d475d2ea.js";import"./extends-bab83b91.js";import"./setPrototypeOf-8d862a7d.js";import"./inheritsLoose-a02b825e.js";import"./isNativeReflectConstruct-e378569d.js";import"./index-356e4a49.js";const l=`# @availity/mui-button
+
+> Availity MUI button component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-button.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-button)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-button.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-button)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-button?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/button/package.json)
+
+## Documentation
+
+This package extends the MUI button component: [MUI button Docs](https://mui.com/components/button/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-button-introduction--docs)
+
+## Installation
+
+### Install Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Install
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/mui-button
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-button
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element (Recommended)
+
+\`\`\`tsx
+import { Button } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import Button from '@availity/mui-button';
+\`\`\`
+`;function o(t){return e(a,{children:[n(m,{title:"Components/Button/Introduction"}),`
+`,n(s,{children:l})]})}function x(t={}){const{wrapper:i}=Object.assign({},r(),t.components);return i?n(i,Object.assign({},t,{children:n(o,t)})):o()}export{x as default};
+//# sourceMappingURL=introduction-d3ada58e.js.map
