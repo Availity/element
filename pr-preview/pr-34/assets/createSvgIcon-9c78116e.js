f998c2a868686e01bd8fd3fcaf7aaf7f95b98579
LauRoxx <LauRoxx@users.noreply.github.com>
2023-06-02 13:15:50 +0000

    Deploy preview for PR 34 🛫

---
+++ pr-preview/pr-34/assets/createSvgIcon-9c78116e.js
@@ -0,0 +1,2 @@
+import{r as o}from"./index-8db94870.js";import{j as c}from"./jsx-runtime-91a467a5.js";import{S as t}from"./SvgIcon-9883e960.js";function p(e,m){function r(n,a){return c(t,{"data-testid":`${m}Icon`,ref:a,...n,children:e})}return r.muiName=t.muiName,o.memo(o.forwardRef(r))}export{p as c};
+//# sourceMappingURL=createSvgIcon-9c78116e.js.map
--- pr-preview/pr-34/assets/faExternalLink-8ccf1391.js
+++
@@ -1,2 +0,0 @@
-var a={};export{a as f};
-//# sourceMappingURL=faExternalLink-8ccf1391.js.map
