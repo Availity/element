38d777b8a272110a3ab8989e89fe52d132fd5e07
LauRoxx <LauRoxx@users.noreply.github.com>
2023-06-15 12:54:42 +0000

    Deploy preview for PR 50 🛫

---
+++ pr-preview/pr-50/assets/introduction-8559d9f9.js
@@ -0,0 +1,24 @@
+import{j as e,a,F as s}from"./jsx-runtime-91a467a5.js";import{M as o,f as m}from"./index-5f0b42fd.js";import"./chunk-PCJTTTQV-49d7949a.js";import{u as r}from"./index-1d576ef5.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-094b828a.js";import"../sb-preview/runtime.js";import"./chunk-4NMOSTKD-052c8761.js";import"./index-d475d2ea.js";import"./extends-bab83b91.js";import"./index-8ce4a492.js";import"./setPrototypeOf-8d862a7d.js";import"./inheritsLoose-a02b825e.js";import"./isNativeReflectConstruct-e378569d.js";import"./index-f8236e9a.js";import"./index-356e4a49.js";import"./react-18-ff2c0a32.js";const p=`# @availity/theme
+
+> Theme using @availity/design-tokens
+
+[![Version](https://img.shields.io/npm/v/@availity/theme.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/theme)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/theme.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/theme)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/theme?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/theme/package.json)
+
+## Installation
+
+### NPM
+
+\`\`\`bash
+npm install @availity/theme
+\`\`\`
+
+### Yarn
+
+\`\`\`bash
+yarn add @availity/theme
+\`\`\`
+`;function i(t){return a(s,{children:[e(o,{title:"Design System/Theme/Introduction"}),`
+`,e(m,{children:p})]})}function F(t={}){const{wrapper:n}=Object.assign({},r(),t.components);return n?e(n,Object.assign({},t,{children:e(i,t)})):i()}export{F as default};
+//# sourceMappingURL=introduction-8559d9f9.js.map
