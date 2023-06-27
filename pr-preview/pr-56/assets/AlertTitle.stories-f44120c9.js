471f370398466a243636db4c6a36cdeb2b0de8b8
owolfhu1 <owolfhu1@users.noreply.github.com>
2023-06-27 16:05:56 +0000

    Deploy preview for PR 56 🛫

---
+++ pr-preview/pr-56/assets/AlertTitle.stories-f44120c9.js
@@ -0,0 +1,21 @@
+import{j as s,a as T}from"./jsx-runtime-91a467a5.js";import{r as A}from"./index-8db94870.js";import{A as h}from"./Alert-22d7f396.js";import{a as x,g as _,s as v,c as b,b as S}from"./styled-05cc2f2a.js";import{e as V}from"./useThemeProps-7db2adde.js";import{T as w}from"./Typography-0c7fab94.js";import{C as M}from"./Collapse-618aca38.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-e6ea1431.js";import"./faMagnifyingGlass-944a50c5.js";import"./faCircleArrowRight-d415119f.js";import"./SvgIcon-c4a75eda.js";import"./createSvgIcon-d0e166f9.js";import"./Paper-5dc87821.js";import"./IconButton-f5a91a1c.js";import"./ButtonBase-dac0a791.js";import"./emotion-react.browser.esm-84971059.js";import"./extends-bab83b91.js";import"./setPrototypeOf-8d862a7d.js";import"./inheritsLoose-a02b825e.js";import"./useForkRef-2674f3de.js";import"./useIsFocusVisible-bbf13f29.js";import"./extendSxProp-e468aa5c.js";import"./utils-e6237d7c.js";import"./index-8ce4a492.js";import"./useTheme-248aed36.js";function R(e){return x("MuiAlertTitle",e)}_("MuiAlertTitle",["root"]);const U=e=>{const{classes:t}=e;return S({root:["root"]},R,t)},q=v(w,{name:"MuiAlertTitle",slot:"Root",overridesResolver:(e,t)=>t.root})(({theme:e})=>({fontWeight:e.typography.fontWeightMedium,marginTop:-2})),I=A.forwardRef(function(t,r){const n=V({props:t,name:"MuiAlertTitle"}),{className:y,...g}=n,a=n,C=U(a);return s(q,{gutterBottom:!0,component:"div",ownerState:a,ref:r,className:b(C.root,y),...g})}),N=I,o=({children:e,...t})=>s(N,{...t,children:e});try{o.displayName="AlertTitle",o.__docgenInfo={description:"",displayName:"AlertTitle",props:{children:{defaultValue:null,description:"The content of the component.",name:"children",required:!1,type:{name:"any"}},classes:{defaultValue:null,description:"Override or extend the styles applied to the component.",name:"classes",required:!1,type:{name:"Partial<AlertTitleClasses>"}},sx:{defaultValue:null,description:"The system prop that allows defining system overrides as well as additional CSS styles.",name:"sx",required:!1,type:{name:"SxProps<Theme>"}},ref:{defaultValue:null,description:"",name:"ref",required:!1,type:{name:"Ref<unknown>"}}}}}catch{}const le={title:"Components/Alert/AlertTitle",component:o,tags:["autodocs"],args:{children:"This text is inside the AlertTitle"}},i={render:e=>T(h,{severity:"info",children:[s(o,{...e}),"This text is inside the Alert"]})},l={render:e=>{const[t,r]=A.useState(!0);return s(M,{in:t,children:T(h,{severity:"info",onClose:()=>{r(!1),setTimeout(()=>r(!0),1e3)},children:[s(o,{...e}),"This text is inside the Alert"]})})}};var p,m,c;i.parameters={...i.parameters,docs:{...(p=i.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: args => <Alert severity="info">
+      <AlertTitle {...args} />
+      This text is inside the Alert
+    </Alert>
+}`,...(c=(m=i.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};var d,u,f;l.parameters={...l.parameters,docs:{...(d=l.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: args => {
+    const [visible, setVisible] = useState(true);
+    const onClose = () => {
+      setVisible(false);
+      setTimeout(() => setVisible(true), 1000);
+    };
+    return <Collapse in={visible}>
+        <Alert severity="info" onClose={onClose}>
+          <AlertTitle {...args} />
+          This text is inside the Alert
+        </Alert>
+      </Collapse>;
+  }
+}`,...(f=(u=l.parameters)==null?void 0:u.docs)==null?void 0:f.source}}};const ne=["_AlertTitle","_InDismissableAlert"];export{i as _AlertTitle,l as _InDismissableAlert,ne as __namedExportsOrder,le as default};
+//# sourceMappingURL=AlertTitle.stories-f44120c9.js.map
