bb52b8531e21cc95610d0691d687393a0c1d71b1
owolfhu1 <owolfhu1@users.noreply.github.com>
2023-06-27 15:51:07 +0000

    Deploy preview for PR 56 🛫

---
+++ pr-preview/pr-56/assets/introduction-4f7e1150.js
@@ -0,0 +1,64 @@
+import{j as t,a,F as o}from"./jsx-runtime-91a467a5.js";import{M as m,f as s}from"./index-4dd72b85.js";import"./chunk-PCJTTTQV-a53fba37.js";import{u as r}from"./index-1d576ef5.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-b2195401.js";import"../sb-preview/runtime.js";import"./chunk-4NMOSTKD-052c8761.js";import"./index-d475d2ea.js";import"./extends-bab83b91.js";import"./index-8ce4a492.js";import"./setPrototypeOf-8d862a7d.js";import"./inheritsLoose-a02b825e.js";import"./isNativeReflectConstruct-e378569d.js";import"./index-f8236e9a.js";import"./index-356e4a49.js";import"./react-18-ff2c0a32.js";const p=`# @availity/mui-menu
+
+> Availity MUI Menu component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-menu.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-menu)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-menu.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-menu)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-menu?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-menu/package.json)
+
+## Documentation
+
+This package extends the MUI Menu component: [MUI Menu Docs](https://mui.com/components/menu/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-menu-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://zeroheight.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+_This package has a few peer dependencies. Add \`@mui/material\` & \`@emotion/react\` to your project if not already installed._
+
+\`\`\`bash
+npm install @availity/mui-menu
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-menu
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Menu } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Menu } from '@availity/mui-menu';
+\`\`\`
+`;function i(n){return a(o,{children:[t(m,{title:"Components/Menu/Introduction"}),`
+`,t(s,{children:p})]})}function C(n={}){const{wrapper:e}=Object.assign({},r(),n.components);return e?t(e,Object.assign({},n,{children:t(i,n)})):i()}export{C as default};
+//# sourceMappingURL=introduction-4f7e1150.js.map
