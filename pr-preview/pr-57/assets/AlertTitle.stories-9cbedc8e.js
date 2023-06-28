cad8ca034c23d07b6fe364dd6b4e68512626f1d9
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-06-28 19:12:39 +0000

    Deploy preview for PR 57 🛫

---
+++ pr-preview/pr-57/assets/AlertTitle.stories-9cbedc8e.js
@@ -0,0 +1,21 @@
+import{j as s,a as T}from"./jsx-runtime-91a467a5.js";import{r as A}from"./index-8db94870.js";import{A as h}from"./Alert-c4e87e4b.js";import{a as x,g as _,s as v,c as b,b as S}from"./styled-08029a61.js";import{b as V}from"./useThemeProps-2b3759a9.js";import{T as w}from"./Typography-f5775643.js";import{C as M}from"./Collapse-69ea4d7d.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-5f016480.js";import"./faMagnifyingGlass-944a50c5.js";import"./faCircleArrowRight-d415119f.js";import"./SvgIcon-0641d88f.js";import"./createSvgIcon-cf24bd3b.js";import"./Paper-05b8d5d7.js";import"./IconButton-6cabf22b.js";import"./ButtonBase-9665f477.js";import"./emotion-react.browser.esm-633bf1c8.js";import"./extends-bab83b91.js";import"./setPrototypeOf-8d862a7d.js";import"./inheritsLoose-a02b825e.js";import"./useForkRef-2674f3de.js";import"./useIsFocusVisible-bbf13f29.js";import"./extendSxProp-7e632729.js";import"./utils-3df70432.js";import"./index-8ce4a492.js";import"./useTheme-bf779243.js";function R(e){return x("MuiAlertTitle",e)}_("MuiAlertTitle",["root"]);const U=e=>{const{classes:t}=e;return S({root:["root"]},R,t)},q=v(w,{name:"MuiAlertTitle",slot:"Root",overridesResolver:(e,t)=>t.root})(({theme:e})=>({fontWeight:e.typography.fontWeightMedium,marginTop:-2})),I=A.forwardRef(function(t,r){const n=V({props:t,name:"MuiAlertTitle"}),{className:y,...g}=n,a=n,C=U(a);return s(q,{gutterBottom:!0,component:"div",ownerState:a,ref:r,className:b(C.root,y),...g})}),N=I,o=({children:e,...t})=>s(N,{...t,children:e});try{o.displayName="AlertTitle",o.__docgenInfo={description:"",displayName:"AlertTitle",props:{children:{defaultValue:null,description:"The content of the component.",name:"children",required:!1,type:{name:"any"}},classes:{defaultValue:null,description:"Override or extend the styles applied to the component.",name:"classes",required:!1,type:{name:"Partial<AlertTitleClasses>"}},sx:{defaultValue:null,description:"The system prop that allows defining system overrides as well as additional CSS styles.",name:"sx",required:!1,type:{name:"SxProps<Theme>"}},ref:{defaultValue:null,description:"",name:"ref",required:!1,type:{name:"Ref<unknown>"}}}}}catch{}const le={title:"Components/Alert/AlertTitle",component:o,tags:["autodocs"],args:{children:"This text is inside the AlertTitle"}},i={render:e=>T(h,{severity:"info",children:[s(o,{...e}),"This text is inside the Alert"]})},l={render:e=>{const[t,r]=A.useState(!0);return s(M,{in:t,children:T(h,{severity:"info",onClose:()=>{r(!1),setTimeout(()=>r(!0),1e3)},children:[s(o,{...e}),"This text is inside the Alert"]})})}};var p,m,c;i.parameters={...i.parameters,docs:{...(p=i.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: args => <Alert severity="info">
+      <AlertTitle {...args} />
+      This text is inside the Alert
+    </Alert>
+}`,...(c=(m=i.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};var d,u,f;l.parameters={...l.parameters,docs:{...(d=l.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: args => {
+    const [visible, setVisible] = useState(true);
+    const onClose = () => {
+      setVisible(false);
+      setTimeout(() => setVisible(true), 1000);
+    };
+    return <Collapse in={visible}>
+        <Alert severity="info" onClose={onClose}>
+          <AlertTitle {...args} />
+          This text is inside the Alert
+        </Alert>
+      </Collapse>;
+  }
+}`,...(f=(u=l.parameters)==null?void 0:u.docs)==null?void 0:f.source}}};const ne=["_AlertTitle","_InDismissableAlert"];export{i as _AlertTitle,l as _InDismissableAlert,ne as __namedExportsOrder,le as default};
+//# sourceMappingURL=AlertTitle.stories-9cbedc8e.js.map
---
+++ pr-preview/pr-57/assets/CardHeader.stories-2ef52cd8.js
@@ -0,0 +1,10 @@
+import{j as e}from"./jsx-runtime-91a467a5.js";import{C as m}from"./Card-2898e7a2.js";import{C as t}from"./CardHeader-d2d18b83.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./styled-08029a61.js";import"./useThemeProps-2b3759a9.js";import"./extends-bab83b91.js";import"./Paper-05b8d5d7.js";import"./Typography-f5775643.js";import"./extendSxProp-7e632729.js";const f={title:"Components/Card/CardHeader",component:t,tags:["autodocs"]},r={render:s=>e(m,{children:e(t,{...s})}),args:{title:"Card header",subheader:"Subheader"}};var a,d,o;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: (args: CardHeaderProps) => <Card>
+      <CardHeader {...args} />
+    </Card>,
+  args: {
+    title: 'Card header',
+    subheader: 'Subheader'
+  }
+}`,...(o=(d=r.parameters)==null?void 0:d.docs)==null?void 0:o.source}}};const _=["_CardHeader"];export{r as _CardHeader,_ as __namedExportsOrder,f as default};
+//# sourceMappingURL=CardHeader.stories-2ef52cd8.js.map
