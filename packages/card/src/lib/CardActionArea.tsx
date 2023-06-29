bb2ebd48105dbff3c0246c23fd5abba3aece831b
Greg Martin <greg.martin@availity.com>
2023-06-29 09:14:59 -0400

    refactor(mui-card): disable ripple on CardActionArea

--- packages/card/src/lib/CardActionArea.tsx
+++ packages/card/src/lib/CardActionArea.tsx
@@ -1,9 +1,17 @@
 import { CardActionArea as MuiCardActionArea, CardActionAreaProps as MuiCardActionAreaProps } from '@mui/material';
 
-export interface CardActionAreaProps extends MuiCardActionAreaProps {
+export interface CardActionAreaProps
+  extends Omit<
+    MuiCardActionAreaProps,
+    'centerRipple' | 'disableRipple' | 'disableTouchRipple' | 'focusRipple' | 'TouchRippleProps' | 'touchRippleRef'
+  > {
   children?: React.ReactNode;
 }
 
 export const CardActionArea = ({ children, ...rest }: CardActionAreaProps): JSX.Element => {
-  return <MuiCardActionArea {...rest}>{children}</MuiCardActionArea>;
+  return (
+    <MuiCardActionArea {...rest} disableRipple>
+      {children}
+    </MuiCardActionArea>
+  );
 };
