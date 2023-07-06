9b52724abcc70aff355377fff3b81177df271441
Greg Martin <greg.martin@availity.com>
2023-07-06 13:17:27 -0400

    feat(mui-tabs): tabcontext component initial commit

---
+++ packages/tabs/src/lib/TabContext.stories.tsx
@@ -0,0 +1,41 @@
+// Each exported component in the package should have its own stories file
+
+import { useState } from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { TabContext, TabContextProps } from './TabContext';
+import { Divider } from '../../../divider';
+import { TabList } from './TabList';
+import { Tab } from './Tab';
+import { TabPanel } from './TabPanel';
+
+const meta: Meta<typeof TabContext> = {
+  title: 'Components/Tabs/TabContext',
+  component: TabContext,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+export const _TabContext: StoryObj<typeof TabContext> = {
+  render: (args: TabContextProps) => {
+    const [value, setValue] = useState('1');
+
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+      setValue(newValue);
+    };
+    return (
+      <TabContext {...args} value={value}>
+        <TabList onChange={handleChange} aria-label="lab API tabs example">
+          <Tab label="Item One" value="1" />
+          <Tab label="Item Two" value="2" />
+          <Tab label="Item Three" value="3" />
+        </TabList>
+        <Divider />
+        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+      </TabContext>
+    );
+  },
+  args: {},
+};
