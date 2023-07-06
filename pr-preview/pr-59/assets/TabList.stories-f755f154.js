10a538ccca4fb975a4600213ac3fc3955ec055de
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-07-06 21:03:52 +0000

    Deploy preview for PR 59 🛫

---
+++ pr-preview/pr-59/assets/TabList.stories-f755f154.js
@@ -0,0 +1,17 @@
+import{j as e,a as b}from"./jsx-runtime-91a467a5.js";import{r as u}from"./index-8db94870.js";import{a as l,T}from"./TabList-d4a2ec23.js";import{T as t}from"./Tab-f482cf75.js";import"./_commonjsHelpers-042e6b4d.js";import"./ownerWindow-abbdbaf0.js";import"./ownerDocument-613eb639.js";import"./styled-cb03d4d5.js";import"./useThemeProps-911242ec.js";import"./extends-bab83b91.js";import"./useTheme-2f271e1c.js";import"./ButtonBase-3730792f.js";import"./emotion-react.browser.esm-ee37bc0f.js";import"./setPrototypeOf-8d862a7d.js";import"./inheritsLoose-a02b825e.js";import"./useForkRef-2674f3de.js";import"./useIsFocusVisible-bbf13f29.js";import"./createSvgIcon-bd229898.js";import"./SvgIcon-4c9e085d.js";import"./useSlotProps-fd9c0c61.js";const q={title:"Components/Tabs/TabList",component:l,tags:["autodocs"]},a={render:n=>{const[m,i]=u.useState("1");return e(T,{value:m,children:b(l,{...n,onChange:(d,p)=>{i(p)},"aria-label":"lab API tabs example",children:[e(t,{label:"Item One",value:"1"}),e(t,{label:"Item Two",value:"2"}),e(t,{label:"Item Three",value:"3"})]})})},args:{}};var r,o,s;a.parameters={...a.parameters,docs:{...(r=a.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: (args: TabListProps) => {
+    const [value, setValue] = useState('1');
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+      setValue(newValue);
+    };
+    return <TabContext value={value}>
+        <TabList {...args} onChange={handleChange} aria-label="lab API tabs example">
+          <Tab label="Item One" value="1" />
+          <Tab label="Item Two" value="2" />
+          <Tab label="Item Three" value="3" />
+        </TabList>
+      </TabContext>;
+  },
+  args: {}
+}`,...(s=(o=a.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const z=["_TabList"];export{a as _TabList,z as __namedExportsOrder,q as default};
+//# sourceMappingURL=TabList.stories-f755f154.js.map
---
+++ pr-preview/pr-59/assets/TabPanel.stories-9c263cf1.js
@@ -0,0 +1,15 @@
+import{a as s,j as a}from"./jsx-runtime-91a467a5.js";import{T as m,a as p}from"./TabList-d4a2ec23.js";import{D as l,T as n}from"./TabPanel-0a8a1a13.js";import{T as b}from"./Tab-f482cf75.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./Divider-9474b138.js";import"./styled-cb03d4d5.js";import"./useThemeProps-911242ec.js";import"./extends-bab83b91.js";import"./dividerClasses-d0d5581a.js";import"./ownerWindow-abbdbaf0.js";import"./ownerDocument-613eb639.js";import"./useTheme-2f271e1c.js";import"./ButtonBase-3730792f.js";import"./emotion-react.browser.esm-ee37bc0f.js";import"./setPrototypeOf-8d862a7d.js";import"./inheritsLoose-a02b825e.js";import"./useForkRef-2674f3de.js";import"./useIsFocusVisible-bbf13f29.js";import"./createSvgIcon-bd229898.js";import"./SvgIcon-4c9e085d.js";import"./useSlotProps-fd9c0c61.js";const y={title:"Components/Tabs/TabPanel",component:m,tags:["autodocs"]},r={render:i=>s(m,{value:"1",children:[a(p,{"aria-label":"lab API tabs example",children:a(b,{label:"Item One",value:"1"})}),a(l,{}),a(n,{...i,value:"1"})]}),args:{children:"Item One"}};var e,t,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: (args: TabPanelProps) => {
+    return <TabContext value="1">
+        <TabList aria-label="lab API tabs example">
+          <Tab label="Item One" value="1" />
+        </TabList>
+        <Divider />
+        <TabPanel {...args} value="1" />
+      </TabContext>;
+  },
+  args: {
+    children: 'Item One'
+  }
+}`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const z=["_TabPanel"];export{r as _TabPanel,z as __namedExportsOrder,y as default};
+//# sourceMappingURL=TabPanel.stories-9c263cf1.js.map
