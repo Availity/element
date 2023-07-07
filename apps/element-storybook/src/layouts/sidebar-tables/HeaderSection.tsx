665a157f6db45ef0d8967941b2486946cf23d59d
Greg Martin <greg.martin@availity.com>
2023-07-07 17:05:04 -0400

    feat(element-storybook): add breadcrumbs to sidebar-tables

--- apps/element-storybook/src/layouts/sidebar-tables/HeaderSection.tsx
+++ apps/element-storybook/src/layouts/sidebar-tables/HeaderSection.tsx
@@ -1,5 +1,15 @@
-import { Box } from '@mui/material';
+import { Box, Breadcrumbs, IconButton, Typography } from '@mui/material';
+import { HomeIcon, NavigateNextIcon } from '@availity/mui-icon';
 
 export const HeaderSection = (): JSX.Element => {
-  return <Box>Header</Box>;
+  return (
+    <Box>
+      <Breadcrumbs aria-label="breadcrumbs" separator={<NavigateNextIcon fontSize="small" />}>
+        <IconButton size="small">
+          <HomeIcon></HomeIcon>
+        </IconButton>
+        <Typography>yoooooo</Typography>
+      </Breadcrumbs>
+    </Box>
+  );
 };
