d9de82410f223bce56bfd50bf8ba7e3c48c8e92e
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-07-11 15:01:43 +0000

    Remove preview for PR 65 🛬

--- pr-preview/pr-65/assets/CardContent.stories-cd370224.js
+++
@@ -1,9 +0,0 @@
-import{j as t}from"./jsx-runtime-91a467a5.js";import{C as e}from"./CardContent-c32c4bef.js";import{C as d}from"./Card-f51e6c0f.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./styled-362768da.js";import"./useThemeProps-209b167f.js";import"./extends-bab83b91.js";import"./Paper-e8439940.js";const u={title:"Components/Card/CardContent",component:e,tags:["autodocs"]},r={render:s=>t(d,{children:t(e,{...s})}),args:{children:"This text is a child of CardContent"}};var o,n,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: (args: CardContentProps) => <Card>
-      <CardContent {...args} />
-    </Card>,
-  args: {
-    children: 'This text is a child of CardContent'
-  }
-}`,...(a=(n=r.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const x=["_CardContent"];export{r as _CardContent,x as __namedExportsOrder,u as default};
-//# sourceMappingURL=CardContent.stories-cd370224.js.map
--- pr-preview/pr-65/assets/faMagnifyingGlass-d219cdaa.js
+++
@@ -1,2 +0,0 @@
-var a={},s={},f={},r={},v={};export{r as a,v as b,s as c,f as d,a as f};
-//# sourceMappingURL=faMagnifyingGlass-d219cdaa.js.map
