f6a725d2cb8753c2055b8dc36c2f1413fdad6125
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-07-12 18:57:30 +0000

    Deploy preview for PR 67 🛫

---
+++ pr-preview/pr-67/assets/CardContent.stories-72d4b705.js
@@ -0,0 +1,9 @@
+import{j as t}from"./jsx-runtime-91a467a5.js";import{C as e}from"./CardContent-7223801f.js";import{C as d}from"./Card-84048188.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./styled-48536e78.js";import"./useThemeProps-4e8cc45e.js";import"./extends-bab83b91.js";import"./Paper-f9d84c2d.js";const u={title:"Components/Card/CardContent",component:e,tags:["autodocs"]},r={render:s=>t(d,{children:t(e,{...s})}),args:{children:"This text is a child of CardContent"}};var o,n,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: (args: CardContentProps) => <Card>
+      <CardContent {...args} />
+    </Card>,
+  args: {
+    children: 'This text is a child of CardContent'
+  }
+}`,...(a=(n=r.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const x=["_CardContent"];export{r as _CardContent,x as __namedExportsOrder,u as default};
+//# sourceMappingURL=CardContent.stories-72d4b705.js.map
