197afb57e04c35c35482b6a4f3aba80e486fd7a2
Greg Martin <greg.martin@availity.com>
2023-07-12 14:52:16 -0400

    refactor(mui-breadcrumbs): simplify renderBreadcrumb and add type defs

--- packages/breadcrumbs/src/lib/Breadcrumbs.tsx
+++ packages/breadcrumbs/src/lib/Breadcrumbs.tsx
@@ -3,32 +3,51 @@ import { NavigateNextIcon } from '@availity/mui-icon';
 import { Link } from '../../../link';
 
 interface Crumb {
+  /** The name of the ancestor page. */
   name: string;
+  /** The url for navigating to the ancestor page. */
   url: string;
 }
 
 export interface BreadcrumbsProps extends Omit<MuiBreadcrumbsProps, 'separator'> {
+  /** The name of the active page (the page the user is currently on). */
   active?: string;
-  children: React.ReactNode;
+  children?: React.ReactNode;
+  /** The ancestor pages. */
   crumbs?: Crumb[];
+  /** The value to display when the active page or an ancestor does not have a value.
+   * @default ... */
+  emptyState?: string;
+  /** Url for the Home route.
+   * @default /public/apps/dashboard */
+  homeUrl?: string;
 }
 
-const renderBreadCrumb = (crumb: Crumb) => {
-  if (crumb.name && crumb.url) {
-    return (
-      <Link aria-label={crumb.name} href={crumb.url}>
-        {crumb.name}
-      </Link>
-    );
-  }
-};
+export const Breadcrumbs = ({
+  active,
+  children,
+  crumbs,
+  emptyState = '...',
+  homeUrl = '/public/apps/dashboard',
+  ...rest
+}: BreadcrumbsProps): JSX.Element => {
+  const renderBreadCrumb = ({ name = emptyState, url }: Crumb) => {
+    const props = {
+      'aria-label': name,
+      children: name,
+    };
 
-export const Breadcrumbs = ({ active, children, crumbs, ...rest }: BreadcrumbsProps): JSX.Element => {
+    return url ? <Link {...props} href={url} /> : <Typography {...props} />;
+  };
+
   return (
     <MuiBreadcrumbs {...rest} separator={<NavigateNextIcon fontSize="small" />}>
+      <Link aria-label="Home" href={homeUrl}>
+        Home
+      </Link>
       {crumbs && crumbs.length > 0 && crumbs.map((crumb) => renderBreadCrumb(crumb))}
       {children}
-      {active && <Typography>{active}</Typography>}
+      {<Typography>{active || emptyState}</Typography>}
     </MuiBreadcrumbs>
   );
 };
