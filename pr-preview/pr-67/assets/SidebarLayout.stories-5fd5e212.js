63add43869367fee3cf7989b0dd4b6122766be7e
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-07-12 20:07:03 +0000

    Deploy preview for PR 67 🛫

---
+++ pr-preview/pr-67/assets/SidebarLayout.stories-5fd5e212.js
@@ -0,0 +1,36 @@
+import"./index-8db94870.js";import{M as d,U as s,b as c}from"./index-b36a2c8e.js";import"./index-05865f9f.js";import{j as r,a as o,F as l}from"./jsx-runtime-91a467a5.js";import"./index-872eb483.js";import"./index-0ea4270a.js";import"./index-fcd155c5.js";import{T as h}from"./index-14d5ee3b.js";import{C as m,H as u,S as g,a as x,T as b}from"./TablesSection-57491e9a.js";import{G as e}from"./Grid-dcc87a85.js";import{u as p}from"./index-1d576ef5.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-96b685d4.js";import"../sb-preview/runtime.js";import"./chunk-4NMOSTKD-052c8761.js";import"./index-d475d2ea.js";import"./extends-bab83b91.js";import"./index-8ce4a492.js";import"./setPrototypeOf-8d862a7d.js";import"./inheritsLoose-a02b825e.js";import"./isNativeReflectConstruct-e378569d.js";import"./index-f8236e9a.js";import"./index-356e4a49.js";import"./faMagnifyingGlass-d219cdaa.js";import"./faCircleArrowRight-c530ef6d.js";import"./SvgIcon-63a7a9ef.js";import"./styled-48536e78.js";import"./useThemeProps-4e8cc45e.js";import"./index-bd37c415.js";import"./Link-37452212.js";import"./Typography-c2535165.js";import"./extendSxProp-70e72958.js";import"./useIsFocusVisible-bbf13f29.js";import"./useForkRef-2674f3de.js";import"./Paper-f9d84c2d.js";import"./Button-0b23b338.js";import"./ButtonBase-71b73842.js";import"./emotion-react.browser.esm-94564128.js";import"./LoadingButton-eb9e2a59.js";import"./useId-fd294ee9.js";import"./Tooltip-e8b6626b.js";import"./useTheme-a8520578.js";import"./Grow-b15871a4.js";import"./useSlotProps-e755ff0f.js";import"./utils-e3b45a21.js";import"./IconButton-b3f5181d.js";import"./index-e7763b73.js";import"./GlobalStyles-2df33680.js";import"./Box-a370ec79.js";import"./Breadcrumbs-0bf4f691.js";import"./react-is.production.min-a192e302.js";import"./createSvgIcon-a18730f7.js";import"./TextField-a65d5831.js";import"./Menu-b45be0f8.js";import"./Close-0aea5f16.js";import"./Badge-fb5c3fb5.js";import"./Chip-8e419b25.js";import"./Divider-8b943f8f.js";import"./dividerClasses-34e8f2f7.js";import"./listItemTextClasses-9d46e741.js";function a(t){const i=Object.assign({h1:"h1",p:"p",em:"em"},p(),t.components);return o(l,{children:[r(d,{title:"Layouts/Sidebar/MDX"}),`
+`,r(i.h1,{id:"sidebar-layout",children:"Sidebar Layout"}),`
+`,r(i.p,{children:r(i.em,{children:"Work In Progress"})}),`
+`,r(s,{children:r(h,{children:r(m,{maxWidth:!1,sx:{bgcolor:"background.canvas",minHeight:"75%",maxHeight:"100%",padding:"1rem"},children:o(m,{fixed:!0,children:[r(u,{}),o(e,{container:!0,spacing:2,children:[r(e,{item:!0,xs:12,sm:"auto",children:r(g,{})}),o(e,{item:!0,xs:!0,children:[r("div",{role:"tabpanel",children:o(e,{container:!0,spacing:2,children:[r(e,{item:!0,xs:12,children:r(x,{})}),r(e,{item:!0,xs:12,children:r(b,{})})]})}),r("div",{role:"tabpanel",hidden:!0,children:r(i.p,{children:"future section"})}),r("div",{role:"tabpanel",hidden:!0,children:r(i.p,{children:"future section"})})]})]})]})})})}),`
+`,r(c,{code:`<ThemeProvider>
+  <Container maxWidth={false} sx={{bgcolor:'background.canvas', padding:'1rem'}}>
+    <Container fixed>
+      <HeaderSection/>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm='auto'>
+          <SideNavSection/>
+        </Grid>
+        <Grid item xs>
+          <div role="tabpanel">
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <SearchSection/>
+                </Grid>
+                <Grid item xs={12}>
+                  <TablesSection/>
+                </Grid>
+              </Grid>
+          </div>
+          <div role="tabpanel" hidden>
+            future section
+          </div>
+          <div role="tabpanel" hidden>
+            future section
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
+  </Container>
+</ThemeProvider>
+`})]})}function f(t={}){const{wrapper:i}=Object.assign({},p(),t.components);return i?r(i,{...t,children:r(a,{...t})}):a(t)}const S=()=>{throw new Error("Docs-only story")};S.parameters={docsOnly:!0};const n={title:"Layouts/Sidebar/MDX",tags:["stories-mdx"],includeStories:["__page"]};n.parameters=n.parameters||{};n.parameters.docs={...n.parameters.docs||{},page:f};const Tr=["__page"];export{Tr as __namedExportsOrder,S as __page,n as default};
+//# sourceMappingURL=SidebarLayout.stories-5fd5e212.js.map
