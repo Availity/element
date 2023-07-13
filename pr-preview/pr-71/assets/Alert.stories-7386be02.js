fb2f4ecbf7c5e56f66ae3e3e28c1ae670474b704
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-07-13 20:40:55 +0000

    Deploy preview for PR 71 🛫

---
+++ pr-preview/pr-71/assets/Alert.stories-7386be02.js
@@ -0,0 +1,24 @@
+import{j as s,F as a,a as m}from"./jsx-runtime-91a467a5.js";import{R as _}from"./index-8db94870.js";import{A as i}from"./Alert-da784e8c.js";import{C as y}from"./Collapse-a80ec344.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-1f2edb05.js";import"./faMagnifyingGlass-2576db7f.js";import"./faCircleArrowRight-c530ef6d.js";import"./SvgIcon-63a7a9ef.js";import"./styled-48536e78.js";import"./useThemeProps-4e8cc45e.js";import"./extends-bab83b91.js";import"./createSvgIcon-a18730f7.js";import"./Close-0aea5f16.js";import"./Paper-f9d84c2d.js";import"./IconButton-b3f5181d.js";import"./ButtonBase-71b73842.js";import"./emotion-react.browser.esm-94564128.js";import"./setPrototypeOf-8d862a7d.js";import"./inheritsLoose-a02b825e.js";import"./useForkRef-2674f3de.js";import"./useIsFocusVisible-bbf13f29.js";import"./utils-e3b45a21.js";import"./index-8ce4a492.js";import"./useTheme-a8520578.js";const X={title:"Components/Alert/Alert",component:i,tags:["autodocs"],args:{children:"This text is inside the alert"}},t={render:e=>s(i,{severity:"info",...e})},r={render:e=>{const[b,n]=_.useState(!0);return s(y,{in:b,children:s(i,{onClose:()=>{n(!1),setTimeout(()=>n(!0),1e3)},...e})})}},S=["info","success","warning","error"],o={render:()=>s(a,{children:S.map(e=>m(a,{children:[m(i,{severity:e,children:["This is a ",e," alert — check it out!"]},e),s("br",{})]}))})};var l,p,c;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: (args: AlertProps) => <Alert severity="info" {...args} />
+}`,...(c=(p=t.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};var d,u,h,A,f;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: (args: AlertProps) => {
+    const [visible, setVisible] = React.useState(true);
+    const onClose = () => {
+      setVisible(false);
+      setTimeout(() => setVisible(true), 1000);
+    };
+    return <Collapse in={visible}>
+        <Alert onClose={onClose} {...args} />
+      </Collapse>;
+  }
+}`,...(h=(u=r.parameters)==null?void 0:u.docs)==null?void 0:h.source},description:{story:"The `Collapse` component is a recommended way to handle visual dismissing of an alert.\nCheck out the Material UI docs for more information.",...(f=(A=r.parameters)==null?void 0:A.docs)==null?void 0:f.description}}};var v,g,C;o.parameters={...o.parameters,docs:{...(v=o.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: () => <>
+    {severities.map(severity => <>
+        <Alert severity={severity} key={severity}>
+          This is a {severity} alert — check it out!
+        </Alert>
+        <br />
+      </>)}
+  </>
+}`,...(C=(g=o.parameters)==null?void 0:g.docs)==null?void 0:C.source}}};const Y=["_Alert","_DismissableAlert","_Severities"];export{t as _Alert,r as _DismissableAlert,o as _Severities,Y as __namedExportsOrder,X as default};
+//# sourceMappingURL=Alert.stories-7386be02.js.map
---
+++ pr-preview/pr-71/assets/CardHeader.stories-bdb31987.js
@@ -0,0 +1,21 @@
+import{j as r}from"./jsx-runtime-91a467a5.js";import{C as c}from"./Card-84048188.js";import{C as t}from"./CardHeader-3795d081.js";import{I as C}from"./index-0ea4270a.js";import{M as u}from"./index-1f2edb05.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./styled-48536e78.js";import"./useThemeProps-4e8cc45e.js";import"./extends-bab83b91.js";import"./Paper-f9d84c2d.js";import"./Typography-c2535165.js";import"./extendSxProp-70e72958.js";import"./Button-0b23b338.js";import"./ButtonBase-71b73842.js";import"./emotion-react.browser.esm-94564128.js";import"./setPrototypeOf-8d862a7d.js";import"./inheritsLoose-a02b825e.js";import"./useForkRef-2674f3de.js";import"./useIsFocusVisible-bbf13f29.js";import"./LoadingButton-eb9e2a59.js";import"./useId-fd294ee9.js";import"./Tooltip-e8b6626b.js";import"./useTheme-a8520578.js";import"./Grow-b15871a4.js";import"./index-8ce4a492.js";import"./useSlotProps-e755ff0f.js";import"./utils-e3b45a21.js";import"./IconButton-b3f5181d.js";import"./faMagnifyingGlass-2576db7f.js";import"./faCircleArrowRight-c530ef6d.js";import"./SvgIcon-63a7a9ef.js";const R={title:"Components/Card/CardHeader",component:t,tags:["autodocs"]},e={render:a=>r(c,{children:r(t,{...a})}),args:{title:"Card header",subheader:"Subheader"}},o={render:a=>r(c,{children:r(t,{...a})}),args:{title:"Card header",action:r(C,{color:"primary",title:"More",size:"medium",children:r(u,{})})}};var d,i,m;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: (args: CardHeaderProps) => <Card>
+      <CardHeader {...args} />
+    </Card>,
+  args: {
+    title: 'Card header',
+    subheader: 'Subheader'
+  }
+}`,...(m=(i=e.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var p,s,n;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: (args: CardHeaderProps) => <Card>
+      <CardHeader {...args} />
+    </Card>,
+  args: {
+    title: 'Card header',
+    // subheader: 'Subheader',
+    action: <IconButton color="primary" title="More" size="medium">
+        <MoreVerticalIcon />
+      </IconButton>
+  }
+}`,...(n=(s=o.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const T=["_CardHeader","_CardHeaderAction"];export{e as _CardHeader,o as _CardHeaderAction,T as __namedExportsOrder,R as default};
+//# sourceMappingURL=CardHeader.stories-bdb31987.js.map
