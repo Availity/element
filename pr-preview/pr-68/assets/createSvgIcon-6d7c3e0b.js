5d347bae050ad3f9ac1069978a3c2677797fb139
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-07-13 15:04:01 +0000

    Remove preview for PR 68 🛬

--- pr-preview/pr-68/assets/createSvgIcon-6d7c3e0b.js
+++
@@ -1,2 +0,0 @@
-import{r as o}from"./index-8db94870.js";import{j as c}from"./jsx-runtime-91a467a5.js";import{S as t}from"./SvgIcon-ca7a5f5b.js";function p(e,m){function r(n,a){return c(t,{"data-testid":`${m}Icon`,ref:a,...n,children:e})}return r.muiName=t.muiName,o.memo(o.forwardRef(r))}export{p as c};
-//# sourceMappingURL=createSvgIcon-6d7c3e0b.js.map
