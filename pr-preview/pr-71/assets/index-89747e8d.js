1e2a48172778c0208014fd47e401750d15a5dcb5
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-07-13 20:58:27 +0000

    Remove preview for PR 71 🛬

--- pr-preview/pr-71/assets/index-89747e8d.js
+++
@@ -1,2 +0,0 @@
-import{t}from"./index-bd37c415.js";import{j as e}from"./jsx-runtime-91a467a5.js";import{r as f}from"./index-8db94870.js";import{M as n}from"./Paper-f9d84c2d.js";t.fontSizesCaption,t.fontSizesCaption,t.fontSizesH5,t.fontWeightsBold,t.fontSizesH3,t.fontWeightsBold,t.fontSizesH1,t.fontWeightsBold;var h=f.forwardRef((o,i)=>e(n,{...o,ref:i}));h.__docgenInfo={description:"",methods:[],displayName:"Paper"};
-//# sourceMappingURL=index-89747e8d.js.map
