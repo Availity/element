2f476fb2cf262723039c23a49acd302ff911a56f
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-07-26 17:30:06 +0000

    Remove preview for PR 72 🛬

--- pr-preview/pr-72/assets/listItemTextClasses-9d46e741.js
+++
@@ -1,2 +0,0 @@
-import{g as e,b as s}from"./styled-48536e78.js";function l(t){return s("MuiListItemText",t)}const i=e("MuiListItemText",["root","multiline","dense","inset","primary","secondary"]),n=i;export{l as g,n as l};
-//# sourceMappingURL=listItemTextClasses-9d46e741.js.map
