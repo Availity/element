b38400b3e5007b7f40aed07046dc4fb30017f9a5
dznjudo <dznjudo@users.noreply.github.com>
2023-08-02 18:11:30 +0000

    Deploy preview for PR 77 🛫

---
+++ pr-preview/pr-77/assets/createSvgIcon-bb9b4c74.js
@@ -0,0 +1,2 @@
+import{r as o}from"./index-8db94870.js";import{j as c}from"./jsx-runtime-91a467a5.js";import{S as t}from"./SvgIcon-6bda5215.js";function p(e,m){function r(n,a){return c(t,{"data-testid":`${m}Icon`,ref:a,...n,children:e})}return r.muiName=t.muiName,o.memo(o.forwardRef(r))}export{p as c};
+//# sourceMappingURL=createSvgIcon-bb9b4c74.js.map
---
+++ pr-preview/pr-77/assets/introduction-120a7a46.js
@@ -0,0 +1,65 @@
+import{j as t,a as o,F as a}from"./jsx-runtime-91a467a5.js";import{M as m,g as s}from"./index-0902fd07.js";import"./chunk-PCJTTTQV-86d2d9ed.js";import{u as r}from"./index-1d576ef5.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-027bcd20.js";import"../sb-preview/runtime.js";import"./chunk-4NMOSTKD-052c8761.js";import"./index-d475d2ea.js";import"./extends-bab83b91.js";import"./index-8ce4a492.js";import"./setPrototypeOf-8d862a7d.js";import"./inheritsLoose-a02b825e.js";import"./isNativeReflectConstruct-e378569d.js";import"./index-f8236e9a.js";import"./index-356e4a49.js";import"./react-18-ff2c0a32.js";const c=`# @availity/mui-icon
+
+> Availity MUI Icon component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-icon.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-icon)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-icon.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-icon)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-icon?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/icon/package.json)
+
+- ⚠️ _needs component theme added to @availity/theme_
+- ⚠️ _needs UI Team Verification (must conform to [Availity's Figma Design Standards](https://www.figma.com/file/bf6l79hehcKdRjb5Kzq1De/Element--MUI-%F0%9F%9A%A7?node-id=4662%3A14&t=QMqHeT6daeqXbjBR-1) - private link)_
+
+## Documentation
+
+This package extends the MUI SvgIcon component([MUI Icon Docs](https://mui.com/components/icons/)) to wrap UX Approved Font Awesome Icons.
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-icon-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://zeroheight.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/mui-icon
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-icon
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { HomeIcon } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { HomeIcon } from '@availity/mui-icon';
+\`\`\`
+`;function e(n){return o(a,{children:[t(m,{title:"Components/Icon/Introduction"}),`
+`,t(s,{children:c})]})}function U(n={}){const{wrapper:i}=Object.assign({},r(),n.components);return i?t(i,Object.assign({},n,{children:t(e,n)})):e()}export{U as default};
+//# sourceMappingURL=introduction-120a7a46.js.map
