c23e628679036d45464d4bbc2f27a3455d1f8281
jordan-a-young <jordan-a-young@users.noreply.github.com>
2023-08-03 12:58:58 +0000

    Remove preview for PR 76 🛬

--- pr-preview/pr-76/assets/CardContent.stories-542ead36.js
+++
@@ -1,9 +0,0 @@
-import{j as t}from"./jsx-runtime-91a467a5.js";import{C as e}from"./CardContent-62e4cfe7.js";import{C as d}from"./Card-810c769c.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./styled-e83a4d6b.js";import"./useThemeProps-2be0b178.js";import"./extends-bab83b91.js";import"./Paper-3e4d1c04.js";const u={title:"Components/Card/CardContent",component:e,tags:["autodocs"]},r={render:s=>t(d,{children:t(e,{...s})}),args:{children:"This text is a child of CardContent"}};var o,n,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: (args: CardContentProps) => <Card>
-      <CardContent {...args} />
-    </Card>,
-  args: {
-    children: 'This text is a child of CardContent'
-  }
-}`,...(a=(n=r.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const x=["_CardContent"];export{r as _CardContent,x as __namedExportsOrder,u as default};
-//# sourceMappingURL=CardContent.stories-542ead36.js.map
