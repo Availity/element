daba586826d8c53dc4ee8392e6ed0aa6e65acbfd
Jordan Young <jordan.young@availity.com>
2023-08-11 16:13:56 -0400

    feat(mui-datepicker): add datepicker component

--- packages/datepicker/src/lib/Datepicker.test.tsx
+++ packages/datepicker/src/lib/Datepicker.test.tsx
@@ -1,9 +1,14 @@
 import { render } from '@testing-library/react';
+import { ThemeProvider } from '@availity/theme-provider';
 import { Datepicker } from './Datepicker';
 
 describe('Datepicker', () => {
   test('should render successfully', () => {
-    const { getByText } = render(<Datepicker>Test</Datepicker>);
-    expect(getByText('Test')).toBeTruthy();
+    const { getAllByText } = render(
+      <ThemeProvider>
+        <Datepicker label="Test" onChange={jest.fn()} value={null} />
+      </ThemeProvider>
+    );
+    expect(getAllByText('Test')).toBeTruthy();
   });
 });
