3915e6dd6b238c502124c2046e4d7f81ed4cd281
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-08-16 15:00:13 +0000

    Deploy preview for PR 87 🛫

---
+++ pr-preview/pr-87/assets/CardHeader.stories-1cb5cec7.js
@@ -0,0 +1,21 @@
+import{j as r}from"./jsx-runtime-91a467a5.js";import{C as c}from"./Card-810c769c.js";import{C as t}from"./CardHeader-b47be9fa.js";import{I as C}from"./index-64a79fdb.js";import{b as u}from"./index-3ac6d6e8.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./styled-e83a4d6b.js";import"./useThemeProps-2be0b178.js";import"./extends-bab83b91.js";import"./Paper-3e4d1c04.js";import"./Typography-0affa63a.js";import"./extendSxProp-f7e67a44.js";import"./Button-91e81b21.js";import"./ButtonBase-43bbae14.js";import"./emotion-react.browser.esm-69c9b3f9.js";import"./setPrototypeOf-8d862a7d.js";import"./inheritsLoose-a02b825e.js";import"./useForkRef-2674f3de.js";import"./useIsFocusVisible-bbf13f29.js";import"./LoadingButton-968b367b.js";import"./useId-fd294ee9.js";import"./Tooltip-dcd87a2c.js";import"./useTheme-4b6e7437.js";import"./ownerDocument-613eb639.js";import"./index-8ce4a492.js";import"./useSlotProps-c61722cd.js";import"./utils-b43d3537.js";import"./IconButton-1375a629.js";import"./faCircleArrowRight-202cb008.js";import"./faMagnifyingGlass-2576db7f.js";import"./SvgIcon-6bda5215.js";const R={title:"Components/Card/CardHeader",component:t,tags:["autodocs"]},e={render:a=>r(c,{children:r(t,{...a})}),args:{title:"Card header",subheader:"Subheader"}},o={render:a=>r(c,{children:r(t,{...a})}),args:{title:"Card header",action:r(C,{color:"primary",title:"More",size:"medium",children:r(u,{})})}};var d,i,m;e.parameters={...e.parameters,docs:{...(d=e.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: (args: CardHeaderProps) => <Card>
+      <CardHeader {...args} />
+    </Card>,
+  args: {
+    title: 'Card header',
+    subheader: 'Subheader'
+  }
+}`,...(m=(i=e.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var p,s,n;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: (args: CardHeaderProps) => <Card>
+      <CardHeader {...args} />
+    </Card>,
+  args: {
+    title: 'Card header',
+    // subheader: 'Subheader',
+    action: <IconButton color="primary" title="More" size="medium">
+        <MoreVerticalIcon />
+      </IconButton>
+  }
+}`,...(n=(s=o.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const T=["_CardHeader","_CardHeaderAction"];export{e as _CardHeader,o as _CardHeaderAction,T as __namedExportsOrder,R as default};
+//# sourceMappingURL=CardHeader.stories-1cb5cec7.js.map
---
+++ pr-preview/pr-87/assets/Tab.stories-eb02739a.js
@@ -0,0 +1,39 @@
+import{a as s,F as v,j as e}from"./jsx-runtime-91a467a5.js";import{r as g}from"./index-8db94870.js";import{T as c}from"./Tabs-9f7faebb.js";import{T as i}from"./Tab-abc4c82b.js";import{B as P}from"./Box-21ea2d5d.js";import{T as f}from"./Typography-0affa63a.js";import"./_commonjsHelpers-042e6b4d.js";import"./react-is.production.min-a192e302.js";import"./styled-e83a4d6b.js";import"./useThemeProps-2be0b178.js";import"./extends-bab83b91.js";import"./useTheme-4b6e7437.js";import"./ownerWindow-698471fc.js";import"./ownerDocument-613eb639.js";import"./ButtonBase-43bbae14.js";import"./emotion-react.browser.esm-69c9b3f9.js";import"./setPrototypeOf-8d862a7d.js";import"./inheritsLoose-a02b825e.js";import"./useForkRef-2674f3de.js";import"./useIsFocusVisible-bbf13f29.js";import"./KeyboardArrowRight-1b89ff74.js";import"./createSvgIcon-bb9b4c74.js";import"./SvgIcon-6bda5215.js";import"./useSlotProps-c61722cd.js";import"./extendSxProp-f7e67a44.js";const M={title:"Components/Tabs/Tab",component:c,tags:["autodocs"]};function p(a){return{id:`simple-tab-${a}`,"aria-controls":`simple-tabpanel-${a}`}}const o={render:a=>{const[r,T]=g.useState(0),h=(m,t)=>{T(t)};function l(m){const{value:t,index:n,...x}=m;return e("div",{role:"tabpanel",hidden:t!==n,id:`simple-tabpanel-${n}`,"aria-labelledby":`simple-tab-${n}`,...x,children:t===n&&e(P,{sx:{p:3},children:s(f,{children:["Hello from panel ",n]})})})}return s(v,{children:[s(c,{value:r,onChange:h,children:[e(i,{label:"Item One",...a,...p(0)}),e(i,{label:"Item Two",...a,...p(1)}),e(i,{label:"Item Three",...a,...p(2),disabled:!0})]}),e(l,{value:r,index:0}),e(l,{value:r,index:1}),e(l,{value:r,index:2})]})},args:{}};var d,u,b;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: (args: TabProps) => {
+    interface TabPanelProps {
+      children?: React.ReactNode;
+      index: number;
+      value: number;
+    }
+    const [value, setValue] = useState(0);
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+      setValue(newValue);
+    };
+    function CustomTabPanel(props: TabPanelProps) {
+      const {
+        value,
+        index,
+        ...other
+      } = props;
+      return <div role="tabpanel" hidden={value !== index} id={\`simple-tabpanel-\${index}\`} aria-labelledby={\`simple-tab-\${index}\`} {...other}>
+          {value === index && <Box sx={{
+          p: 3
+        }}>
+              <Typography>Hello from panel {index}</Typography>
+            </Box>}
+        </div>;
+    }
+    return <>
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Item One" {...args} {...a11yProps(0)} />
+          <Tab label="Item Two" {...args} {...a11yProps(1)} />
+          <Tab label="Item Three" {...args} {...a11yProps(2)} disabled />
+        </Tabs>
+        <CustomTabPanel value={value} index={0} />
+        <CustomTabPanel value={value} index={1} />
+        <CustomTabPanel value={value} index={2} />
+      </>;
+  },
+  args: {}
+}`,...(b=(u=o.parameters)==null?void 0:u.docs)==null?void 0:b.source}}};const Q=["_Tab"];export{o as _Tab,Q as __namedExportsOrder,M as default};
+//# sourceMappingURL=Tab.stories-eb02739a.js.map
