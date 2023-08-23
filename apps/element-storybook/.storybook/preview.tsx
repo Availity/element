e806417ae0c499c6d1829490c4414e050991761a
L. Roxanne young <Roxanne.Young@availity.com>
2023-08-23 13:07:14 -0400

    feat(element-storybook): add theme switcher

--- apps/element-storybook/.storybook/preview.tsx
+++ apps/element-storybook/.storybook/preview.tsx
@@ -1,5 +1,9 @@
 import { themes } from '@storybook/theming';
+import { withThemeFromJSXProvider } from '@storybook/addon-styling';
+import type { DecoratorFunction, Renderer, Args } from '@storybook/types';
 import { ThemeProvider } from '@availity/theme-provider';
+import { ThemeOptions, createTheme } from '@mui/material';
+import { legacyTheme, lightTheme } from '@availity/theme';
 
 export const parameters = {
   docs: {
@@ -21,12 +25,16 @@ export const parameters = {
   },
 };
 
-const withThemeProvider = (Story: () => JSX.Element) => {
-  return (
-    <ThemeProvider>
-      <Story />
-    </ThemeProvider>
-  );
-};
+const LightTheme = createTheme(lightTheme as ThemeOptions);
+const LegacyTheme = createTheme(legacyTheme as ThemeOptions);
 
-export const decorators = [withThemeProvider];
+export const decorators: DecoratorFunction<Renderer, Args>[] = [
+  withThemeFromJSXProvider({
+    themes: {
+      Current: LightTheme,
+      Development: LegacyTheme,
+    },
+    defaultTheme: 'Current',
+    Provider: ThemeProvider,
+  }),
+];
