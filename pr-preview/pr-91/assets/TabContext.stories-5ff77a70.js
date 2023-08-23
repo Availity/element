17970e4c1293fc4d820b17aacc09f8fa06a3cab7
LauRoxx <LauRoxx@users.noreply.github.com>
2023-08-23 20:31:53 +0000

    Deploy preview for PR 91 🛫

---
+++ pr-preview/pr-91/assets/TabContext.stories-5ff77a70.js
@@ -0,0 +1,21 @@
+import{a as o,j as e}from"./jsx-runtime-91a467a5.js";import{r as u}from"./index-8db94870.js";import{T as s,a as c,b as t}from"./TabPanel-85b69c98.js";import{D as v}from"./index-47c0aa29.js";import{T as r}from"./Tab-2937fdbf.js";import"./_commonjsHelpers-042e6b4d.js";import"./styled-657e2aa7.js";import"./useThemeProps-ad7d4b8c.js";import"./extends-bab83b91.js";import"./Divider-c380d1fa.js";import"./dividerClasses-e8602bc7.js";import"./react-is.production.min-a192e302.js";import"./useTheme-92c76a56.js";import"./ownerWindow-698471fc.js";import"./ownerDocument-613eb639.js";import"./ButtonBase-920b2988.js";import"./emotion-react.browser.esm-a6af08ad.js";import"./setPrototypeOf-8d862a7d.js";import"./inheritsLoose-a02b825e.js";import"./useForkRef-2674f3de.js";import"./useIsFocusVisible-bbf13f29.js";import"./KeyboardArrowRight-a6d1a9a8.js";import"./createSvgIcon-fedf70c8.js";import"./SvgIcon-c3c5be0b.js";import"./useSlotProps-3883ee92.js";const J={title:"Components/Tabs/TabContext",component:s,tags:["autodocs"]},a={render:i=>{const[p,b]=u.useState("1");return o(s,{...i,value:p,children:[o(c,{onChange:(h,T)=>{b(T)},"aria-label":"lab API tabs example",children:[e(r,{label:"Item One",value:"1"}),e(r,{label:"Item Two",value:"2"}),e(r,{label:"Item Three",value:"3"})]}),e(v,{}),e(t,{value:"1",children:"Item One"}),e(t,{value:"2",children:"Item Two"}),e(t,{value:"3",children:"Item Three"})]})},args:{}};var l,n,m;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: (args: TabContextProps) => {
+    const [value, setValue] = useState('1');
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+      setValue(newValue);
+    };
+    return <TabContext {...args} value={value}>
+        <TabList onChange={handleChange} aria-label="lab API tabs example">
+          <Tab label="Item One" value="1" />
+          <Tab label="Item Two" value="2" />
+          <Tab label="Item Three" value="3" />
+        </TabList>
+        <Divider />
+        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+      </TabContext>;
+  },
+  args: {}
+}`,...(m=(n=a.parameters)==null?void 0:n.docs)==null?void 0:m.source}}};const K=["_TabContext"];export{a as _TabContext,K as __namedExportsOrder,J as default};
+//# sourceMappingURL=TabContext.stories-5ff77a70.js.map
