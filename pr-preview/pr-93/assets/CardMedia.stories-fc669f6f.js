1085c69687688da9bfb28d03199e086e332c39ab
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-08-23 15:10:12 +0000

    Deploy preview for PR 93 🛫

---
+++ pr-preview/pr-93/assets/CardMedia.stories-fc669f6f.js
@@ -0,0 +1,13 @@
+import{j as a}from"./jsx-runtime-91a467a5.js";import{C as s}from"./CardMedia-9cf7c08e.js";import{C as i}from"./Card-89f03616.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./styled-838f2411.js";import"./useThemeProps-ac6a164c.js";import"./extends-bab83b91.js";import"./Paper-25f4f657.js";const x={title:"Components/Card/CardMedia",component:s,tags:["autodocs"]},r={render:n=>a(i,{children:a(s,{...n})}),args:{image:"https://avatars.githubusercontent.com/u/329985?s=200&v=4",sx:{height:"100px"},"aria-label":"Orange rectangle with interlocking AV logo"}};var e,t,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: (args: CardMediaProps) => <Card>
+      <CardMedia {...args} />
+    </Card>,
+  args: {
+    image: 'https://avatars.githubusercontent.com/u/329985?s=200&v=4',
+    sx: {
+      height: '100px'
+    },
+    'aria-label': 'Orange rectangle with interlocking AV logo'
+  }
+}`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const M=["_CardMedia"];export{r as _CardMedia,M as __namedExportsOrder,x as default};
+//# sourceMappingURL=CardMedia.stories-fc669f6f.js.map
---
+++ pr-preview/pr-93/assets/TabPanel.stories-e73a32c0.js
@@ -0,0 +1,15 @@
+import{a as s,j as a}from"./jsx-runtime-91a467a5.js";import{T as m,a as p,b as l}from"./TabPanel-97f161c5.js";import{D as n}from"./index-3763e4c2.js";import{T as b}from"./Tab-c9ffd0be.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./styled-838f2411.js";import"./useThemeProps-ac6a164c.js";import"./extends-bab83b91.js";import"./Divider-3e9557ac.js";import"./dividerClasses-f59732d7.js";import"./react-is.production.min-a192e302.js";import"./useTheme-c349195b.js";import"./ownerWindow-698471fc.js";import"./ownerDocument-613eb639.js";import"./ButtonBase-246c583e.js";import"./emotion-react.browser.esm-63c9efdb.js";import"./setPrototypeOf-8d862a7d.js";import"./inheritsLoose-a02b825e.js";import"./useForkRef-2674f3de.js";import"./useIsFocusVisible-bbf13f29.js";import"./KeyboardArrowRight-c8f32650.js";import"./createSvgIcon-802be96d.js";import"./SvgIcon-247857dd.js";import"./useSlotProps-8f208bee.js";const B={title:"Components/Tabs/TabPanel",component:m,tags:["autodocs"]},r={render:i=>s(m,{value:"1",children:[a(p,{"aria-label":"lab API tabs example",children:a(b,{label:"Item One",value:"1"})}),a(n,{}),a(l,{...i,value:"1"})]}),args:{children:"Item One"}};var e,t,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: (args: TabPanelProps) => {
+    return <TabContext value="1">
+        <TabList aria-label="lab API tabs example">
+          <Tab label="Item One" value="1" />
+        </TabList>
+        <Divider />
+        <TabPanel {...args} value="1" />
+      </TabContext>;
+  },
+  args: {
+    children: 'Item One'
+  }
+}`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const F=["_TabPanel"];export{r as _TabPanel,F as __namedExportsOrder,B as default};
+//# sourceMappingURL=TabPanel.stories-e73a32c0.js.map
