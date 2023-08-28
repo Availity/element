3b391017945ee264cc11a9710643936c941aa7a2
LauRoxx <LauRoxx@users.noreply.github.com>
2023-08-28 13:01:29 +0000

    Deploy preview for PR 92 🛫

--- pr-preview/pr-92/assets/CardContent.stories-03f7deff.js
+++
@@ -1,9 +0,0 @@
-import{j as t}from"./jsx-runtime-390e5fc8.js";import{C as e}from"./CardContent-b15682c2.js";import{C as d}from"./Card-41a5b6cf.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./styled-46700abb.js";import"./useThemeProps-a0bcf1fa.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./Paper-9691a1f7.js";const u={title:"Components/Card/CardContent",component:e,tags:["autodocs"]},r={render:s=>t(d,{children:t(e,{...s})}),args:{children:"This text is a child of CardContent"}};var o,n,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: (args: CardContentProps) => <Card>
-      <CardContent {...args} />
-    </Card>,
-  args: {
-    children: 'This text is a child of CardContent'
-  }
-}`,...(a=(n=r.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const x=["_CardContent"];export{r as _CardContent,x as __namedExportsOrder,u as default};
-//# sourceMappingURL=CardContent.stories-03f7deff.js.map
