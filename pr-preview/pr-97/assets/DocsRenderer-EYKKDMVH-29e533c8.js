4facff6995f2d9c2027e84c7a0c897ac2751be9a
LauRoxx <LauRoxx@users.noreply.github.com>
2023-08-29 13:37:50 +0000

    Remove preview for PR 97 🛬

--- pr-preview/pr-97/assets/DocsRenderer-EYKKDMVH-29e533c8.js
+++
@@ -1,2 +0,0 @@
-import{D as l,d as u}from"./chunk-PCJTTTQV-b55509c3.js";import"./iframe-68c1defa.js";import"../sb-preview/runtime.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./react-18-b9b6ed02.js";import"./index-8a077077.js";import"./index-b36746bd.js";import"./chunk-4NMOSTKD-052c8761.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-f8236e9a.js";import"./index-356e4a49.js";export{l as DocsRenderer,u as defaultComponents};
-//# sourceMappingURL=DocsRenderer-EYKKDMVH-29e533c8.js.map
