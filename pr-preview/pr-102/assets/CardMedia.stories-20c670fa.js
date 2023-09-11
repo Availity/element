576a408cb65124ff718817d69ae45091deff29dd
LauRoxx <LauRoxx@users.noreply.github.com>
2023-09-11 22:52:51 +0000

    Deploy preview for PR 102 🛫

---
+++ pr-preview/pr-102/assets/CardMedia.stories-20c670fa.js
@@ -0,0 +1,13 @@
+import{j as a}from"./jsx-runtime-390e5fc8.js";import{C as s}from"./CardMedia-ba671ffd.js";import{C as i}from"./Card-1f65a195.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./styled-2191822d.js";import"./useThemeProps-66ab754c.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./Paper-576c7647.js";const x={title:"Components/Card/CardMedia",component:s,tags:["autodocs"]},r={render:n=>a(i,{children:a(s,{...n})}),args:{image:"https://avatars.githubusercontent.com/u/329985?s=200&v=4",sx:{height:"100px"},"aria-label":"Orange rectangle with interlocking AV logo"}};var e,t,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: (args: CardMediaProps) => <Card>
+      <CardMedia {...args} />
+    </Card>,
+  args: {
+    image: 'https://avatars.githubusercontent.com/u/329985?s=200&v=4',
+    sx: {
+      height: '100px'
+    },
+    'aria-label': 'Orange rectangle with interlocking AV logo'
+  }
+}`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const M=["_CardMedia"];export{r as _CardMedia,M as __namedExportsOrder,x as default};
+//# sourceMappingURL=CardMedia.stories-20c670fa.js.map
