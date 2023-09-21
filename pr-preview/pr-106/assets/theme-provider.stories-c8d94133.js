b16044d504ef6328350bffad18636004016add91
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-09-21 19:51:19 +0000

    Deploy preview for PR 106 🛫

---
+++ pr-preview/pr-106/assets/theme-provider.stories-c8d94133.js
@@ -0,0 +1,9 @@
+import{a as s,F as m,j as r}from"./jsx-runtime-390e5fc8.js";import{T as d}from"./index-e58c33f9.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-b564b747.js";import"./index-338918c6.js";import"./useThemeProps-66ab754c.js";import"./GlobalStyles-3f09bceb.js";import"./emotion-react.browser.esm-d7d6545d.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";const e={render:()=>s(m,{children:[r(d,{children:r("div",{children:"This component is inside the ThemeProvider"})}),r("div",{children:"This component is outside the ThemeProvider"})]})},f={title:"Design System/ThemeProvider"};var i,o,t;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: () => <>
+    <ThemeProvider>
+      <div>This component is inside the ThemeProvider</div>
+    </ThemeProvider>
+    <div>This component is outside the ThemeProvider</div>
+  </>
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const x=["Default"];export{e as Default,x as __namedExportsOrder,f as default};
+//# sourceMappingURL=theme-provider.stories-c8d94133.js.map
