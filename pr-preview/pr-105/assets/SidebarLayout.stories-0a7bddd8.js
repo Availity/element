b35c47a6d3be46f07c752c68b25f392f5442b714
LauRoxx <LauRoxx@users.noreply.github.com>
2023-09-25 12:47:46 +0000

    Deploy preview for PR 105 🛫

---
+++ pr-preview/pr-105/assets/SidebarLayout.stories-0a7bddd8.js
@@ -0,0 +1,39 @@
+import"./index-570b25c1.js";import{M as d,U as s,b as c}from"./index-4e12ebbc.js";import"./index-d51ee930.js";import"./index-91a95a3a.js";import{j as r,a as o,F as l}from"./jsx-runtime-390e5fc8.js";import"./index-ed4aff81.js";import"./index-3d8ed985.js";import"./index-e402bfbb.js";import{T as h}from"./index-35b94b83.js";import{H as u,S as x,a as g,T as b,D as S}from"./DataGridSection-d7851a38.js";import{C as n}from"./Container-a800528a.js";import{G as t}from"./Grid-e31ff0b5.js";import{u as p}from"./index-da4379a2.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-9c6be8b7.js";import"../sb-preview/runtime.js";import"./chunk-4NMOSTKD-052c8761.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-8a077077.js";import"./index-f8236e9a.js";import"./index-356e4a49.js";import"./Button-fce9389c.js";import"./styled-2191822d.js";import"./useThemeProps-66ab754c.js";import"./ButtonBase-121cbb7f.js";import"./emotion-react.browser.esm-d7d6545d.js";import"./useForkRef-153a0a89.js";import"./useIsFocusVisible-5e7b1ff1.js";import"./Tooltip-e09bffc1.js";import"./useTheme-c775f3da.js";import"./ownerDocument-613eb639.js";import"./useSlotProps-38dd38c0.js";import"./utils-aacfec48.js";import"./useControlled-9b1271e0.js";import"./useId-6f4bfce0.js";import"./IconButton-130e3844.js";import"./LoadingButton-25a551fc.js";import"./faCircleArrowRight-32490c46.js";import"./faMagnifyingGlass-2576db7f.js";import"./SvgIcon-644454bc.js";import"./index-338918c6.js";import"./Badge-4e5e02da.js";import"./Chip-17fa9859.js";import"./createSvgIcon-cafa5f0c.js";import"./index-9681c8ff.js";import"./Box-4c9f5af0.js";import"./extendSxProp-564f0963.js";import"./FormControlLabel-fd7fbd69.js";import"./FormLabel-f2bb53c2.js";import"./Typography-8314e3e3.js";import"./Paper-576c7647.js";import"./Link-6cc3d459.js";import"./index-20992310.js";import"./GlobalStyles-3f09bceb.js";import"./Breadcrumbs-09be348c.js";import"./react-is.production.min-a192e302.js";import"./TextField-1d5635ec.js";import"./FormControl-8b41a575.js";import"./Menu-23ccb5a4.js";import"./ownerWindow-698471fc.js";import"./createChainedFunction-0bab83cf.js";import"./OutlinedInput-be2438d3.js";import"./Divider-a85f70eb.js";import"./dividerClasses-cf8bd2cb.js";import"./visuallyHidden-14c3de6e.js";import"./index-1fc0ca9a.js";import"./Close-dea21bc6.js";import"./MenuItem-a91b8212.js";import"./Checkbox-a0186f17.js";import"./KeyboardArrowRight-56f43038.js";import"./LastPage-f057786b.js";function a(e){const i=Object.assign({h1:"h1",p:"p",em:"em"},p(),e.components);return o(l,{children:[r(d,{title:"Layouts/Sidebar/MDX"}),`
+`,r(i.h1,{id:"sidebar-layout",children:"Sidebar Layout"}),`
+`,r(i.p,{children:r(i.em,{children:"Work In Progress"})}),`
+`,r(s,{children:r(h,{children:r(n,{maxWidth:!1,sx:{bgcolor:"background.canvas",minHeight:"75%",maxHeight:"100%",padding:"1rem"},children:o(n,{fixed:!0,children:[r(u,{}),o(t,{container:!0,spacing:2,children:[r(t,{item:!0,xs:12,sm:"auto",children:r(x,{})}),o(t,{item:!0,xs:!0,children:[r("div",{role:"tabpanel",children:o(t,{container:!0,spacing:2,children:[r(t,{item:!0,xs:12,children:r(g,{})}),r(t,{item:!0,xs:12,children:r(b,{})}),r(t,{item:!0,xs:12,children:r(S,{})})]})}),r("div",{role:"tabpanel",hidden:!0,children:r(i.p,{children:"future section"})}),r("div",{role:"tabpanel",hidden:!0,children:r(i.p,{children:"future section"})})]})]})]})})})}),`
+`,r(c,{code:`<ThemeProvider>
+  <Container maxWidth={false} sx={{bgcolor:'background.canvas', padding:'1rem'}}>
+    <Container fixed>
+      <HeaderSection/>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm='auto'>
+          <SideNavSection/>
+        </Grid>
+        <Grid item xs>
+          <div role="tabpanel">
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <SearchSection/>
+                </Grid>
+                <Grid item xs={12}>
+                  <TablesSection/>
+                </Grid>
+                <Grid item xs={12}>
+                  <DataGridSection />
+                </Grid>
+              </Grid>
+          </div>
+          <div role="tabpanel" hidden>
+            future section
+          </div>
+          <div role="tabpanel" hidden>
+            future section
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
+  </Container>
+</ThemeProvider>
+`})]})}function f(e={}){const{wrapper:i}=Object.assign({},p(),e.components);return i?r(i,{...e,children:r(a,{...e})}):a(e)}const G=()=>{throw new Error("Docs-only story")};G.parameters={docsOnly:!0};const m={title:"Layouts/Sidebar/MDX",tags:["stories-mdx"],includeStories:["__page"]};m.parameters=m.parameters||{};m.parameters.docs={...m.parameters.docs||{},page:f};const Nr=["__page"];export{Nr as __namedExportsOrder,G as __page,m as default};
+//# sourceMappingURL=SidebarLayout.stories-0a7bddd8.js.map
