16a6b0f8b71c213919eec5ba21cb0465ca907123
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-09-27 19:23:49 +0000

    Deploy preview for PR 107 🛫

---
+++ pr-preview/pr-107/assets/introduction-01fd16aa.js
@@ -0,0 +1,65 @@
+import{j as a,a as e,F as o}from"./jsx-runtime-390e5fc8.js";import{M as m,g as r}from"./index-41e14725.js";import"./chunk-PCJTTTQV-2e195788.js";import{u as s}from"./index-da4379a2.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-b6db1f90.js";import"../sb-preview/runtime.js";import"./chunk-4NMOSTKD-052c8761.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-8a077077.js";import"./index-f8236e9a.js";import"./index-356e4a49.js";import"./react-18-b9b6ed02.js";const l=`# @availity/mui-avatar
+
+> Availity MUI Avatar component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-avatar.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-avatar)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-avatar.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-avatar)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-avatar?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-avatar/package.json)
+
+- ⚠️ _needs component theme added to @availity/theme_
+- ⚠️ _needs UI Team Verification (must conform to [Availity's Figma Design Standards](https://www.figma.com/file/bf6l79hehcKdRjb5Kzq1De/Element--MUI-%F0%9F%9A%A7?node-id=4662%3A14&t=QMqHeT6daeqXbjBR-1) - private link)_
+
+## Documentation
+
+This package extends the MUI Avatar component: [MUI Avatar Docs](https://mui.com/components/avatar/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-avatar-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://zeroheight.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/mui-avatar
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-avatar
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Avatar } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Avatar } from '@availity/mui-avatar';
+\`\`\`
+`;function i(t){return e(o,{children:[a(m,{title:"Components/Avatar/Introduction"}),`
+`,a(r,{children:l})]})}function I(t={}){const{wrapper:n}=Object.assign({},s(),t.components);return n?a(n,Object.assign({},t,{children:a(i,t)})):i()}export{I as default};
+//# sourceMappingURL=introduction-01fd16aa.js.map
