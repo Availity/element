bf64751413f39aa1434469ea8793c35e4855ac1b
Greg Martin <greg.martin@availity.com>
2023-09-29 16:42:24 -0400

    refactor(mui-chip): use availity avatar component

--- packages/chip/src/lib/Chip.stories.tsx
+++ packages/chip/src/lib/Chip.stories.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Avatar, Collapse } from '@mui/material';
+import { Avatar } from '@availity/mui-avatar';
+import { Collapse } from '@mui/material';
 import { Chip, ChipProps } from './Chip';
 
 const meta: Meta<typeof Chip> = {
