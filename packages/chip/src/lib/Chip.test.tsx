5abf287338227f732136cd8d317d7ab1f3d9dba0
Greg Martin <greg.martin@availity.com>
2023-09-29 16:46:50 -0400

    test(mui-chip): remove obsolete test

--- packages/chip/src/lib/Chip.test.tsx
+++ packages/chip/src/lib/Chip.test.tsx
@@ -6,12 +6,4 @@ describe('Chip', () => {
     const { getByText } = render(<Chip label="Test" />);
     expect(getByText('Test')).toBeTruthy();
   });
-  test('should have clickable styling when onDelete passed', () => {
-    const onDelete = () => {
-      // do nothing
-    };
-    const { getByRole } = render(<Chip label="Test" onDelete={onDelete} />);
-    expect(getByRole('button')).toBeTruthy();
-    expect(getByRole('button')).toHaveClass('MuiChip-clickable');
-  });
 });
