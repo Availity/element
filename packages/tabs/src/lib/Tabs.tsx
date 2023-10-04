ebc689b5b613fc38526957d889fe441f5656e823
Greg Martin <greg.martin@availity.com>
2023-10-04 16:29:52 -0400

    chore(mui-menu): upgrade mui to latest

--- packages/tabs/src/lib/Tabs.tsx
+++ packages/tabs/src/lib/Tabs.tsx
@@ -3,22 +3,14 @@ import { Tabs as MuiTabs, TabsProps as MuiTabsProps } from '@mui/material';
 export interface TabsProps
   extends Omit<
     MuiTabsProps,
-    | 'centered'
-    | 'centerRipple'
-    | 'disableFocusRipple'
-    | 'disableRipple'
-    | 'disableTouchRipple'
-    | 'focusRipple'
-    | 'orientation'
-    | 'TouchRippleProps'
-    | 'touchRippleRef'
+    'centered' | 'centerRipple' | 'focusRipple' | 'orientation' | 'TouchRippleProps' | 'touchRippleRef'
   > {
   children?: React.ReactNode;
 }
 
 export const Tabs = ({ children, ...rest }: TabsProps): JSX.Element => {
   return (
-    <MuiTabs {...rest} disableRipple disableTouchRipple orientation="horizontal" centered={false}>
+    <MuiTabs {...rest} orientation="horizontal" centered={false}>
       {children}
     </MuiTabs>
   );
