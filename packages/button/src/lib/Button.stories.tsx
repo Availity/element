e4b60a5329aaec3c5e21e16d2b5df4747b2c48be
L. Roxanne young <Roxanne.Young@availity.com>
2023-10-04 13:05:24 -0400

    docs: swap mui components for element

--- packages/button/src/lib/Button.stories.tsx
+++ packages/button/src/lib/Button.stories.tsx
@@ -1,5 +1,5 @@
 import { StoryObj, Meta } from '@storybook/react';
-import { Box, Grid } from '@mui/material';
+import { Box, Grid } from '@availity/mui-layout';
 import { ArrowCircleLeftIcon, ArrowCircleRightIcon } from '@availity/mui-icon';
 import { Button, ButtonProps } from './Button';
 import React from 'react';
@@ -27,24 +27,24 @@ export const _Button: Story = {
 export const _Colors: Story = {
   render: () => (
     <Grid container spacing={1}>
-      <Grid item xs={12}>
+      <Grid xs={12}>
         <Button color="primary">Primary</Button>
       </Grid>
-      <Grid item xs={12}>
+      <Grid xs={12}>
         <Button color="secondary">Secondary</Button>
       </Grid>
-      <Grid item xs={12}>
+      <Grid xs={12}>
         <Button color="tertiary">Tertiary</Button>
       </Grid>
-      <Grid item xs={12}>
+      <Grid xs={12}>
         <Button color="error">Error</Button>
       </Grid>
-      <Grid item xs={12}>
+      <Grid xs={12}>
         <Button color="primary" variant="outlined">
           Primary Outlined
         </Button>
       </Grid>
-      <Grid item xs={12}>
+      <Grid xs={12}>
         <Button color="secondary" variant="outlined">
           Secondary Outlined
         </Button>
@@ -56,17 +56,17 @@ export const _Colors: Story = {
 export const _ButtonSizes: Story = {
   render: () => (
     <Grid container spacing={1}>
-      <Grid item xs={12}>
+      <Grid xs={12}>
         <Button color="tertiary" size="small">
           Small Button
         </Button>
       </Grid>
-      <Grid item xs={12}>
+      <Grid xs={12}>
         <Button color="secondary" size="medium">
           Medium Button
         </Button>
       </Grid>
-      <Grid item xs={12}>
+      <Grid xs={12}>
         <Button color="primary" size="large">
           Large Button
         </Button>
@@ -78,38 +78,38 @@ export const _ButtonSizes: Story = {
 export const _ButtonWithIcon: Story = {
   render: () => (
     <Grid container spacing={1}>
-      <Grid item xs={3}>
+      <Grid xs={3}>
         <Button color="primary" size="large" startIcon={<ArrowCircleLeftIcon />}>
           Large
         </Button>
       </Grid>
-      <Grid item xs={3}>
+      <Grid xs={3}>
         <Button color="primary" size="medium" startIcon={<ArrowCircleLeftIcon />}>
           Medium
         </Button>
       </Grid>
-      <Grid item xs={3}>
+      <Grid xs={3}>
         <Button color="primary" size="small" startIcon={<ArrowCircleLeftIcon />}>
           Small
         </Button>
       </Grid>
-      <Grid item xs={3} />
-      <Grid item xs={3}>
+      <Grid xs={3} />
+      <Grid xs={3}>
         <Button color="primary" size="large" endIcon={<ArrowCircleRightIcon />}>
           Large
         </Button>
       </Grid>
-      <Grid item xs={3}>
+      <Grid xs={3}>
         <Button color="primary" size="medium" endIcon={<ArrowCircleRightIcon />}>
           Medium
         </Button>
       </Grid>
-      <Grid item xs={3}>
+      <Grid xs={3}>
         <Button color="primary" size="small" endIcon={<ArrowCircleRightIcon />}>
           Small
         </Button>
       </Grid>
-      <Grid item xs={3} />
+      <Grid xs={3} />
     </Grid>
   ),
 };
