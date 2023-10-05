c1e21620de25d41fd14ab065a362cbce1a9d0124
jordan-a-young <jordan-a-young@users.noreply.github.com>
2023-10-05 20:59:58 +0000

    Deploy preview for PR 114 🛫

---
+++ pr-preview/pr-114/assets/Icons.stories-60e94b7c.js
@@ -0,0 +1,37 @@
+import{j as e,a as x}from"./jsx-runtime-390e5fc8.js";import{H as i}from"./Common-7d6577cb.js";import{G as r}from"./Grid-0aa1fe5b.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./faMagnifyingGlass-2576db7f.js";import"./SvgIcon-102d705b.js";import"./clsx.m-fb33c42b.js";import"./createTheme-4dc6b23f.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./useThemeProps-5f33a28c.js";import"./styled-fc2b35f5.js";import"./generateUtilityClasses-8a827d83.js";import"./useTheme-117bd4f5.js";import"./extendSxProp-fa3ab054.js";const M={title:"Components/Icon/Named Icons",component:i,tags:["autodocs"]},n={render:u=>e(i,{...u})},t={render:()=>x(r,{container:!0,spacing:1,children:[e(r,{item:!0,xs:2,children:"Inherit (default)"}),e(r,{item:!0,xs:10,children:e(i,{fontSize:"inherit"})}),e(r,{item:!0,xs:2,children:"XSmall"}),e(r,{item:!0,xs:10,children:e(i,{fontSize:"xsmall"})}),e(r,{item:!0,xs:2,children:"Small"}),e(r,{item:!0,xs:10,children:e(i,{fontSize:"small"})}),e(r,{item:!0,xs:2,children:"Medium"}),e(r,{item:!0,xs:10,children:e(i,{fontSize:"medium"})}),e(r,{item:!0,xs:2,children:"Large"}),e(r,{item:!0,xs:10,children:e(i,{fontSize:"large"})})]})};var o,m,s,a,d;n.parameters={...n.parameters,docs:{...(o=n.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: (args: Omit<FaSvgIconProps, 'icon'>) => <HomeIcon {...args} />
+}`,...(s=(m=n.parameters)==null?void 0:m.docs)==null?void 0:s.source},description:{story:"Any named icon, like the `<HomeIcon />` below, extend mui's `SvgIcon` component and accept the same parameters.\n\nIcon is `aria-hidden` by default. If not accompanied with any on screen text then must pass `aria-hidden={false}`.",...(d=(a=n.parameters)==null?void 0:a.docs)==null?void 0:d.description}}};var c,l,p;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: () => <Grid container spacing={1}>
+      <Grid item xs={2}>
+        Inherit (default)
+      </Grid>
+      <Grid item xs={10}>
+        <HomeIcon fontSize="inherit" />
+      </Grid>
+      <Grid item xs={2}>
+        XSmall
+      </Grid>
+      <Grid item xs={10}>
+        <HomeIcon fontSize="xsmall" />
+      </Grid>
+      <Grid item xs={2}>
+        Small
+      </Grid>
+      <Grid item xs={10}>
+        <HomeIcon fontSize="small" />
+      </Grid>
+      <Grid item xs={2}>
+        Medium
+      </Grid>
+      <Grid item xs={10}>
+        <HomeIcon fontSize="medium" />
+      </Grid>
+      <Grid item xs={2}>
+        Large
+      </Grid>
+      <Grid item xs={10}>
+        <HomeIcon fontSize="large" />
+      </Grid>
+    </Grid>
+}`,...(p=(l=t.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};const O=["_HomeIcon","_Sizes"];export{n as _HomeIcon,t as _Sizes,O as __namedExportsOrder,M as default};
+//# sourceMappingURL=Icons.stories-60e94b7c.js.map
