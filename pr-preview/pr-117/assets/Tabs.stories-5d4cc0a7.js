e9bbe9f46e221925f948447c0c50777cae3aedb0
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-10-11 15:32:35 +0000

    Deploy preview for PR 117 🛫

---
+++ pr-preview/pr-117/assets/Tabs.stories-5d4cc0a7.js
@@ -0,0 +1,38 @@
+import{a as i,F as v,j as e}from"./jsx-runtime-390e5fc8.js";import{r as P}from"./index-570b25c1.js";import{T as c}from"./Tabs-d8765ea0.js";import{T as s}from"./Tab-dca237dc.js";import{B as f}from"./Box-ce79f7f7.js";import{M as g}from"./Typography-72ef0a36.js";import"./_commonjsHelpers-042e6b4d.js";import"./createTheme-b02e766d.js";import"./react-is.production.min-a192e302.js";import"./clsx-43049957.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./styled-69a32198.js";import"./useThemeProps-ac7867ed.js";import"./useTheme-a234fe30.js";import"./ownerWindow-698471fc.js";import"./ownerDocument-613eb639.js";import"./useEnhancedEffect-460150e6.js";import"./generateUtilityClasses-b06d554b.js";import"./KeyboardArrowRight-71a82ecc.js";import"./createSvgIcon-5dca4d92.js";import"./SvgIcon-667e6608.js";import"./ButtonBase-78eecb53.js";import"./emotion-react.browser.esm-dc887828.js";import"./useForkRef-153a0a89.js";import"./useIsFocusVisible-5e7b1ff1.js";import"./useSlotProps-c5009eb1.js";import"./objectWithoutPropertiesLoose-830c72af.js";import"./extendSxProp-b0ad88fc.js";const W={title:"Components/Tabs/Tabs",component:c,tags:["autodocs"]};function p(n){return{id:`simple-tab-${n}`,"aria-controls":`simple-tabpanel-${n}`}}const o={render:n=>{const[r,T]=P.useState(0),h=(m,t)=>{T(t)};function l(m){const{value:t,index:a,...x}=m;return e("div",{role:"tabpanel",hidden:t!==a,id:`simple-tabpanel-${a}`,"aria-labelledby":`simple-tab-${a}`,...x,children:t===a&&e(f,{sx:{p:3},children:i(g,{children:["Hello from panel ",a]})})})}return i(v,{children:[i(c,{value:r,onChange:h,...n,children:[e(s,{label:"Item One",...p(0)}),e(s,{label:"Item Two",...p(1)}),e(s,{label:"Item Three",...p(2),disabled:!0})]}),e(l,{value:r,index:0}),e(l,{value:r,index:1}),e(l,{value:r,index:2})]})}};var d,u,b;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: (args: TabsProps) => {
+    interface TabPanelProps {
+      children?: React.ReactNode;
+      index: number;
+      value: number;
+    }
+    const [value, setValue] = useState(0);
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+      setValue(newValue);
+    };
+    function CustomTabPanel(props: TabPanelProps) {
+      const {
+        value,
+        index,
+        ...other
+      } = props;
+      return <div role="tabpanel" hidden={value !== index} id={\`simple-tabpanel-\${index}\`} aria-labelledby={\`simple-tab-\${index}\`} {...other}>
+          {value === index && <Box sx={{
+          p: 3
+        }}>
+              <Typography>Hello from panel {index}</Typography>
+            </Box>}
+        </div>;
+    }
+    return <>
+        <Tabs value={value} onChange={handleChange} {...args}>
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} disabled />
+        </Tabs>
+        <CustomTabPanel value={value} index={0} />
+        <CustomTabPanel value={value} index={1} />
+        <CustomTabPanel value={value} index={2} />
+      </>;
+  }
+}`,...(b=(u=o.parameters)==null?void 0:u.docs)==null?void 0:b.source}}};const X=["_Tabs"];export{o as _Tabs,X as __namedExportsOrder,W as default};
+//# sourceMappingURL=Tabs.stories-5d4cc0a7.js.map
---
+++ pr-preview/pr-117/assets/styled-9972f020.js
@@ -0,0 +1,2 @@
+import{c as t}from"./styled-69a32198.js";const s=t(),o=s;export{o as s};
+//# sourceMappingURL=styled-9972f020.js.map
