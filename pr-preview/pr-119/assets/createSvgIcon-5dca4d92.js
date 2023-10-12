08c973702577b4f1ca65f5568c32610a948e2d03
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-10-12 15:16:12 +0000

    Remove preview for PR 119 🛬

--- pr-preview/pr-119/assets/createSvgIcon-5dca4d92.js
+++
@@ -1,2 +0,0 @@
-import{b as i}from"./createTheme-b02e766d.js";import{r as o}from"./index-570b25c1.js";import{j as c}from"./jsx-runtime-390e5fc8.js";import{S as t}from"./SvgIcon-667e6608.js";function d(e,m){function r(n,a){return c(t,i({"data-testid":`${m}Icon`,ref:a},n,{children:e}))}return r.muiName=t.muiName,o.memo(o.forwardRef(r))}export{d as c};
-//# sourceMappingURL=createSvgIcon-5dca4d92.js.map
