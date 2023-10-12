0c27bd0108e2835a2cff90d07d9f3500a7b63a49
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-10-12 14:56:54 +0000

    Deploy preview for PR 119 🛫

---
+++ pr-preview/pr-119/assets/TabPanel.stories-44076e85.js
@@ -0,0 +1,15 @@
+import{a as p,j as r}from"./jsx-runtime-390e5fc8.js";import{T as m,a as s,b as l}from"./TabPanel-896237fb.js";import{D as n}from"./index-69b098a0.js";import{T as b}from"./Tab-dca237dc.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./extends-90dea224.js";import"./clsx-43049957.js";import"./createTheme-b02e766d.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./styled-69a32198.js";import"./useThemeProps-ac7867ed.js";import"./generateUtilityClasses-b06d554b.js";import"./Divider-a67a617b.js";import"./dividerClasses-272b0ff4.js";import"./react-is.production.min-a192e302.js";import"./useTheme-a234fe30.js";import"./ownerWindow-698471fc.js";import"./ownerDocument-613eb639.js";import"./useEnhancedEffect-460150e6.js";import"./KeyboardArrowRight-71a82ecc.js";import"./createSvgIcon-5dca4d92.js";import"./SvgIcon-667e6608.js";import"./ButtonBase-78eecb53.js";import"./emotion-react.browser.esm-dc887828.js";import"./useForkRef-153a0a89.js";import"./useIsFocusVisible-5e7b1ff1.js";import"./useSlotProps-c5009eb1.js";import"./objectWithoutPropertiesLoose-830c72af.js";const J={title:"Components/Tabs/TabPanel",component:m,tags:["autodocs"]},a={render:i=>p(m,{value:"1",children:[r(s,{"aria-label":"lab API tabs example",children:r(b,{label:"Item One",value:"1"})}),r(n,{}),r(l,{...i,value:"1"})]}),args:{children:"Item One"}};var t,e,o;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: (args: TabPanelProps) => {
+    return <TabContext value="1">
+        <TabList aria-label="lab API tabs example">
+          <Tab label="Item One" value="1" />
+        </TabList>
+        <Divider />
+        <TabPanel {...args} value="1" />
+      </TabContext>;
+  },
+  args: {
+    children: 'Item One'
+  }
+}`,...(o=(e=a.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const K=["_TabPanel"];export{a as _TabPanel,K as __namedExportsOrder,J as default};
+//# sourceMappingURL=TabPanel.stories-44076e85.js.map
