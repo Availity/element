3cae3e8ae9e60eedf3572f9bbdd03b4f097f1d47
LauRoxx <LauRoxx@users.noreply.github.com>
2023-10-20 12:26:27 +0000

    Remove preview for PR 122 🛬

--- pr-preview/pr-122/assets/DocsRenderer-EYKKDMVH-9a5dfb04.js
+++
@@ -1,2 +0,0 @@
-import{D as l,d as u}from"./chunk-PCJTTTQV-54a2562d.js";import"./iframe-a1fca0a3.js";import"../sb-preview/runtime.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./react-18-b9b6ed02.js";import"./index-8a077077.js";import"./index-ebe5bc4e.js";import"./chunk-4NMOSTKD-052c8761.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-f8236e9a.js";import"./index-356e4a49.js";export{l as DocsRenderer,u as defaultComponents};
-//# sourceMappingURL=DocsRenderer-EYKKDMVH-9a5dfb04.js.map
--- pr-preview/pr-122/assets/useTheme-3a36d7a0.js
+++
@@ -1,2 +0,0 @@
-import"./index-570b25c1.js";import{d as m}from"./useThemeProps-db7202c2.js";import{u as t,T as o}from"./createTheme-e6b56ddb.js";function a(){const e=t(m);return e[o]||e}export{a as u};
-//# sourceMappingURL=useTheme-3a36d7a0.js.map
