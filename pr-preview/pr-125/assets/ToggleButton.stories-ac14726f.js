cbc401f2ece1d6e97cd3e0a5454a412768df1847
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-10-24 21:07:47 +0000

    Deploy preview for PR 125 🛫

---
+++ pr-preview/pr-125/assets/ToggleButton.stories-ac14726f.js
@@ -0,0 +1,26 @@
+import{j as e,a as B,F as h}from"./jsx-runtime-390e5fc8.js";import{b as T}from"./index-f35eb24e.js";import{T as s}from"./ToggleButton-2d4124ba.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./faCircleArrowRight-5066acd3.js";import"./faMagnifyingGlass-482fd726.js";import"./SvgIcon-ab7aa383.js";import"./createTheme-e6b56ddb.js";import"./clsx-8416b751.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./useThemeProps-db7202c2.js";import"./styled-188025da.js";import"./generateUtilityClasses-f776b170.js";import"./ButtonBase-6040f690.js";import"./emotion-react.browser.esm-69a15469.js";import"./useForkRef-153a0a89.js";import"./useIsFocusVisible-5e7b1ff1.js";import"./useEnhancedEffect-460150e6.js";const q={title:"Components/ToggleButton/ToggleButton",component:s,tags:["autodocs"],argTypes:{size:{options:["small","medium","large"]}}},r={render:o=>e(s,{...o}),args:{value:"test",children:"This text is a child of ToggleButton"}},t={render:o=>e(s,{...o}),args:{value:"1",children:e(T,{}),"aria-label":"Home"}},a={render:o=>e(s,{...o}),args:{value:"1",children:B(h,{children:[e(T,{fontSize:"small",sx:{paddingRight:".5rem"}}),"Home"]})}};var n,g,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: (args: ToggleButtonProps) => <ToggleButton {...args} />,
+  args: {
+    value: 'test',
+    children: 'This text is a child of ToggleButton'
+  }
+}`,...(m=(g=r.parameters)==null?void 0:g.docs)==null?void 0:m.source}}};var i,l,p;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: (args: ToggleButtonProps) => <ToggleButton {...args} />,
+  args: {
+    value: '1',
+    children: <HomeIcon />,
+    'aria-label': 'Home'
+  }
+}`,...(p=(l=t.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var c,u,d;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: (args: ToggleButtonProps) => <ToggleButton {...args} />,
+  args: {
+    value: '1',
+    children: <>
+        <HomeIcon fontSize="small" sx={{
+        paddingRight: '.5rem'
+      }} />
+        Home
+      </>
+  }
+}`,...(d=(u=a.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};const w=["_ToggleButton","_ToggleButtonIcon","_ToggleButtonIconLabel"];export{r as _ToggleButton,t as _ToggleButtonIcon,a as _ToggleButtonIconLabel,w as __namedExportsOrder,q as default};
+//# sourceMappingURL=ToggleButton.stories-ac14726f.js.map
