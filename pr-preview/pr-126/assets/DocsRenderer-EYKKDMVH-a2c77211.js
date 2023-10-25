8b9ea83805e1b4c2cb1bd9d54bb10a41781bb4c5
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-10-25 18:24:27 +0000

    Deploy preview for PR 126 🛫

---
+++ pr-preview/pr-126/assets/DocsRenderer-EYKKDMVH-a2c77211.js
@@ -0,0 +1,2 @@
+import{D as l,d as u}from"./chunk-PCJTTTQV-1e69c96d.js";import"./iframe-d9a70d37.js";import"../sb-preview/runtime.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./react-18-b9b6ed02.js";import"./index-8a077077.js";import"./index-775e790a.js";import"./chunk-4NMOSTKD-052c8761.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-f8236e9a.js";import"./index-356e4a49.js";export{l as DocsRenderer,u as defaultComponents};
+//# sourceMappingURL=DocsRenderer-EYKKDMVH-a2c77211.js.map
---
+++ pr-preview/pr-126/assets/Tabs.stories-85414441.js
@@ -0,0 +1,38 @@
+import{a as i,F as v,j as e}from"./jsx-runtime-390e5fc8.js";import{r as P}from"./index-570b25c1.js";import{T as c}from"./Tabs-157d036f.js";import{T as s}from"./Tab-5f8df101.js";import{B as f}from"./Box-7f261278.js";import{M as g}from"./Typography-a571125c.js";import"./_commonjsHelpers-042e6b4d.js";import"./createTheme-e6b56ddb.js";import"./react-is.production.min-a192e302.js";import"./clsx-8416b751.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./styled-188025da.js";import"./useThemeProps-db7202c2.js";import"./useTheme-3a36d7a0.js";import"./ownerWindow-698471fc.js";import"./ownerDocument-613eb639.js";import"./useEnhancedEffect-460150e6.js";import"./generateUtilityClasses-f776b170.js";import"./KeyboardArrowRight-5ae0643c.js";import"./createSvgIcon-e4b87ef5.js";import"./SvgIcon-ab7aa383.js";import"./ButtonBase-6040f690.js";import"./emotion-react.browser.esm-69a15469.js";import"./useForkRef-153a0a89.js";import"./useIsFocusVisible-5e7b1ff1.js";import"./useSlotProps-1710581f.js";import"./objectWithoutPropertiesLoose-b6174fb2.js";import"./extendSxProp-4d42c058.js";const W={title:"Components/Tabs/Tabs",component:c,tags:["autodocs"]};function p(n){return{id:`simple-tab-${n}`,"aria-controls":`simple-tabpanel-${n}`}}const o={render:n=>{const[r,T]=P.useState(0),h=(m,t)=>{T(t)};function l(m){const{value:t,index:a,...x}=m;return e("div",{role:"tabpanel",hidden:t!==a,id:`simple-tabpanel-${a}`,"aria-labelledby":`simple-tab-${a}`,...x,children:t===a&&e(f,{sx:{p:3},children:i(g,{children:["Hello from panel ",a]})})})}return i(v,{children:[i(c,{value:r,onChange:h,...n,children:[e(s,{label:"Item One",...p(0)}),e(s,{label:"Item Two",...p(1)}),e(s,{label:"Item Three",...p(2),disabled:!0})]}),e(l,{value:r,index:0}),e(l,{value:r,index:1}),e(l,{value:r,index:2})]})}};var d,u,b;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: (args: TabsProps) => {
+    interface TabPanelProps {
+      children?: React.ReactNode;
+      index: number;
+      value: number;
+    }
+    const [value, setValue] = useState(0);
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+      setValue(newValue);
+    };
+    function CustomTabPanel(props: TabPanelProps) {
+      const {
+        value,
+        index,
+        ...other
+      } = props;
+      return <div role="tabpanel" hidden={value !== index} id={\`simple-tabpanel-\${index}\`} aria-labelledby={\`simple-tab-\${index}\`} {...other}>
+          {value === index && <Box sx={{
+          p: 3
+        }}>
+              <Typography>Hello from panel {index}</Typography>
+            </Box>}
+        </div>;
+    }
+    return <>
+        <Tabs value={value} onChange={handleChange} {...args}>
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} disabled />
+        </Tabs>
+        <CustomTabPanel value={value} index={0} />
+        <CustomTabPanel value={value} index={1} />
+        <CustomTabPanel value={value} index={2} />
+      </>;
+  }
+}`,...(b=(u=o.parameters)==null?void 0:u.docs)==null?void 0:b.source}}};const X=["_Tabs"];export{o as _Tabs,X as __namedExportsOrder,W as default};
+//# sourceMappingURL=Tabs.stories-85414441.js.map
