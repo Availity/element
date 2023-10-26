3a54ce337a4ac31c32aa525ff661afca794e56e8
Greg Martin <greg.martin@availity.com>
2023-10-26 12:12:47 -0400

    docs(mui-backdrop): handle open via the storybook params

--- packages/backdrop/src/lib/Backdrop.stories.tsx
+++ packages/backdrop/src/lib/Backdrop.stories.tsx
@@ -1,7 +1,6 @@
 // Each exported component in the package should have its own stories file
-import { useState } from 'react';
+
 import type { Meta, StoryObj } from '@storybook/react';
-import { Button } from '@availity/mui-button';
 import { CircularProgress } from '@mui/material';
 import { Backdrop, BackdropProps } from './Backdrop';
 
@@ -14,22 +13,9 @@ const meta: Meta<typeof Backdrop> = {
 export default meta;
 
 export const _Backdrop: StoryObj<typeof Backdrop> = {
-  render: (args: BackdropProps) => {
-    const [open, setOpen] = useState(false);
-    const handleClose = () => {
-      setOpen(false);
-    };
-    const handleOpen = () => {
-      setOpen(true);
-    };
-    return (
-      <div>
-        <Button onClick={handleOpen}>Show backdrop</Button>
-        <Backdrop {...args} open={open} onClick={handleClose} />
-      </div>
-    );
-  },
+  render: (args: BackdropProps) => <Backdrop {...args} />,
   args: {
     children: <CircularProgress />,
+    open: false,
   },
 };
