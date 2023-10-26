22371ab65d4c6b70b7a02407949ffad5ebaa267a
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-10-26 16:20:19 +0000

    Deploy preview for PR 130 🛫

---
+++ pr-preview/pr-130/assets/PaginationItem.stories-1ac77d9a.js
@@ -0,0 +1,49 @@
+import{j as t,a as g}from"./jsx-runtime-390e5fc8.js";import{P as i}from"./PaginationItem-a02d4ec5.js";import{G as e}from"./Grid-d51fec4b.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-f35eb24e.js";import"./faCircleArrowRight-5066acd3.js";import"./faMagnifyingGlass-482fd726.js";import"./SvgIcon-ab7aa383.js";import"./createTheme-e6b56ddb.js";import"./clsx-8416b751.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./useThemeProps-db7202c2.js";import"./styled-188025da.js";import"./generateUtilityClasses-f776b170.js";import"./useTheme-3a36d7a0.js";import"./LastPage-bfb2a0a5.js";import"./createSvgIcon-e4b87ef5.js";import"./ButtonBase-dcf5e891.js";import"./emotion-react.browser.esm-69a15469.js";import"./TransitionGroupContext-c71596b3.js";import"./useForkRef-153a0a89.js";import"./useIsFocusVisible-5e7b1ff1.js";import"./useEnhancedEffect-460150e6.js";import"./extendSxProp-4d42c058.js";const F={title:"Components/Pagination/PaginationItem",component:i,tags:["autodocs"],args:{type:"next"}},r={render:x=>t(i,{...x})},n={render:()=>g(e,{container:!0,spacing:1,children:[t(e,{item:!0,xs:3,children:"Text"}),t(e,{item:!0,xs:9,children:"Outlined"}),t(e,{item:!0,xs:3,children:t(i,{variant:"text",type:"page",page:"1",selected:!0})}),t(e,{item:!0,xs:9,children:t(i,{variant:"outlined",type:"page",page:"1",selected:!0})}),t(e,{item:!0,xs:3,children:t(i,{variant:"text",type:"page",page:"1"})}),t(e,{item:!0,xs:9,children:t(i,{variant:"outlined",type:"page",page:"1"})}),t(e,{item:!0,xs:3,children:t(i,{variant:"text",type:"first"})}),t(e,{item:!0,xs:9,children:t(i,{variant:"outlined",type:"first"})}),t(e,{item:!0,xs:3,children:t(i,{variant:"text",type:"previous"})}),t(e,{item:!0,xs:9,children:t(i,{variant:"outlined",type:"previous"})}),t(e,{item:!0,xs:3,children:t(i,{variant:"text",type:"next"})}),t(e,{item:!0,xs:9,children:t(i,{variant:"outlined",type:"next"})}),t(e,{item:!0,xs:3,children:t(i,{variant:"text",type:"last"})}),t(e,{item:!0,xs:9,children:t(i,{variant:"outlined",type:"last"})})]})};var a,o,p;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: (args: PaginationItemProps) => <PaginationItem {...args} />
+}`,...(p=(o=r.parameters)==null?void 0:o.docs)==null?void 0:p.source}}};var m,s,d;n.parameters={...n.parameters,docs:{...(m=n.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => <Grid container spacing={1}>
+      <Grid item xs={3}>
+        Text
+      </Grid>
+      <Grid item xs={9}>
+        Outlined
+      </Grid>
+      <Grid item xs={3}>
+        <PaginationItem variant="text" type="page" page="1" selected />
+      </Grid>
+      <Grid item xs={9}>
+        <PaginationItem variant="outlined" type="page" page="1" selected />
+      </Grid>
+      <Grid item xs={3}>
+        <PaginationItem variant="text" type="page" page="1" />
+      </Grid>
+      <Grid item xs={9}>
+        <PaginationItem variant="outlined" type="page" page="1" />
+      </Grid>
+      <Grid item xs={3}>
+        <PaginationItem variant="text" type="first" />
+      </Grid>
+      <Grid item xs={9}>
+        <PaginationItem variant="outlined" type="first" />
+      </Grid>
+      <Grid item xs={3}>
+        <PaginationItem variant="text" type="previous" />
+      </Grid>
+      <Grid item xs={9}>
+        <PaginationItem variant="outlined" type="previous" />
+      </Grid>
+      <Grid item xs={3}>
+        <PaginationItem variant="text" type="next" />
+      </Grid>
+      <Grid item xs={9}>
+        <PaginationItem variant="outlined" type="next" />
+      </Grid>
+      <Grid item xs={3}>
+        <PaginationItem variant="text" type="last" />
+      </Grid>
+      <Grid item xs={9}>
+        <PaginationItem variant="outlined" type="last" />
+      </Grid>
+    </Grid>
+}`,...(d=(s=n.parameters)==null?void 0:s.docs)==null?void 0:d.source}}};const H=["_PaginationItem","_Types"];export{r as _PaginationItem,n as _Types,H as __namedExportsOrder,F as default};
+//# sourceMappingURL=PaginationItem.stories-1ac77d9a.js.map
---
+++ pr-preview/pr-130/assets/TabPanel.stories-ee7e893e.js
@@ -0,0 +1,15 @@
+import{a as p,j as r}from"./jsx-runtime-390e5fc8.js";import{T as m,a as s,b as l}from"./TabPanel-19999f5f.js";import{D as n}from"./index-717ad5e2.js";import{T as b}from"./Tab-5d38a7aa.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./extends-90dea224.js";import"./clsx-8416b751.js";import"./createTheme-e6b56ddb.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./styled-188025da.js";import"./useThemeProps-db7202c2.js";import"./generateUtilityClasses-f776b170.js";import"./Divider-e6df2ce9.js";import"./dividerClasses-d4e8510c.js";import"./react-is.production.min-a192e302.js";import"./useTheme-3a36d7a0.js";import"./ownerWindow-698471fc.js";import"./ownerDocument-613eb639.js";import"./useEnhancedEffect-460150e6.js";import"./KeyboardArrowRight-5ae0643c.js";import"./createSvgIcon-e4b87ef5.js";import"./SvgIcon-ab7aa383.js";import"./ButtonBase-dcf5e891.js";import"./emotion-react.browser.esm-69a15469.js";import"./TransitionGroupContext-c71596b3.js";import"./useForkRef-153a0a89.js";import"./useIsFocusVisible-5e7b1ff1.js";import"./useSlotProps-1710581f.js";import"./objectWithoutPropertiesLoose-b6174fb2.js";const K={title:"Components/Tabs/TabPanel",component:m,tags:["autodocs"]},a={render:i=>p(m,{value:"1",children:[r(s,{"aria-label":"lab API tabs example",children:r(b,{label:"Item One",value:"1"})}),r(n,{}),r(l,{...i,value:"1"})]}),args:{children:"Item One"}};var t,e,o;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: (args: TabPanelProps) => {
+    return <TabContext value="1">
+        <TabList aria-label="lab API tabs example">
+          <Tab label="Item One" value="1" />
+        </TabList>
+        <Divider />
+        <TabPanel {...args} value="1" />
+      </TabContext>;
+  },
+  args: {
+    children: 'Item One'
+  }
+}`,...(o=(e=a.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const M=["_TabPanel"];export{a as _TabPanel,M as __namedExportsOrder,K as default};
+//# sourceMappingURL=TabPanel.stories-ee7e893e.js.map
