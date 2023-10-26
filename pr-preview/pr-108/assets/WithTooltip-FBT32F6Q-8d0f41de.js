5f1859ac90e9c88b77113334c8b3bca97fd63c35
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-10-26 18:01:29 +0000

    Deploy preview for PR 108 🛫

--- pr-preview/pr-108/assets/WithTooltip-FBT32F6Q-8d0f41de.js
+++
@@ -1,2 +0,0 @@
-import{W as s,W as f,a as u}from"./index-95ede410.js";import"./iframe-71bb9f46.js";import"../sb-preview/runtime.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./chunk-4NMOSTKD-052c8761.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-8a077077.js";import"./index-f8236e9a.js";import"./index-356e4a49.js";export{s as WithToolTipState,f as WithTooltip,u as WithTooltipPure};
-//# sourceMappingURL=WithTooltip-FBT32F6Q-8d0f41de.js.map
---
+++ pr-preview/pr-108/assets/introduction-3ea178f1.js
@@ -0,0 +1,69 @@
+import{j as n,a,F as e}from"./jsx-runtime-390e5fc8.js";import{M as s,g as m}from"./index-187a7956.js";import"./chunk-PCJTTTQV-0ce7d78b.js";import{u as r}from"./index-da4379a2.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-b8e91a7c.js";import"../sb-preview/runtime.js";import"./chunk-4NMOSTKD-052c8761.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-8a077077.js";import"./index-f8236e9a.js";import"./index-356e4a49.js";import"./react-18-b9b6ed02.js";const l=`# @availity/mui-layout
+
+> Availity MUI layout components to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-layout.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-layout)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-layout.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-layout)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-layout?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-layout/package.json)
+
+## Documentation
+
+This package extends the MUI layout components:
+
+- [MUI Box Docs](https://mui.com/components/Box/)
+- [MUI Container Docs](https://mui.com/components/Container/)
+- [MUI Grid v2 Docs](https://mui.com/components/Grid2/)
+- [MUI Stack Docs](https://mui.com/components/Stack/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-layout-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://zeroheight.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+_This package has a few peer dependencies. Add \`@mui/material\` & \`@emotion/react\` to your project if not already installed._
+
+\`\`\`bash
+npm install @availity/mui-layout
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-layout
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Grid } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Grid } from '@availity/mui-layout';
+\`\`\`
+`;function o(t){return a(e,{children:[n(s,{title:"Components/Layout/Introduction"}),`
+`,n(m,{children:l})]})}function j(t={}){const{wrapper:i}=Object.assign({},r(),t.components);return i?n(i,Object.assign({},t,{children:n(o,t)})):o()}export{j as default};
+//# sourceMappingURL=introduction-3ea178f1.js.map
