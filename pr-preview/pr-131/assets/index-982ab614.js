365db6a703950b6c96aa84bdaaa68f5ce0b9d51e
LauRoxx <LauRoxx@users.noreply.github.com>
2023-11-06 16:50:05 +0000

    Deploy preview for PR 131 🛫

---
+++ pr-preview/pr-131/assets/index-982ab614.js
@@ -0,0 +1,2 @@
+import{j as o}from"./jsx-runtime-390e5fc8.js";import{C as i}from"./Chip-3f4a5391.js";var e=r=>o(i,{...r,color:"default",size:"medium"});export{e as C};
+//# sourceMappingURL=index-982ab614.js.map
