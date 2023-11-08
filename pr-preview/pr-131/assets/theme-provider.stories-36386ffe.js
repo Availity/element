8009ba8183493b0bfaedcf574040e48aec9b32ec
LauRoxx <LauRoxx@users.noreply.github.com>
2023-11-08 21:32:56 +0000

    Remove preview for PR 131 🛬

--- pr-preview/pr-131/assets/theme-provider.stories-36386ffe.js
+++
@@ -1,9 +0,0 @@
-import{a as s,F as m,j as r}from"./jsx-runtime-390e5fc8.js";import{T as d}from"./index-721ea205.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-ac58eaa3.js";import"./index-fd483b67.js";import"./createTheme-e6b56ddb.js";import"./useThemeProps-db7202c2.js";import"./GlobalStyles-8a837dd0.js";import"./emotion-react.browser.esm-69a15469.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";const e={render:()=>s(m,{children:[r(d,{children:r("div",{children:"This component is inside the ThemeProvider"})}),r("div",{children:"This component is outside the ThemeProvider"})]})},x={title:"Design System/ThemeProvider"};var i,o,t;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
-  render: () => <>
-    <ThemeProvider>
-      <div>This component is inside the ThemeProvider</div>
-    </ThemeProvider>
-    <div>This component is outside the ThemeProvider</div>
-  </>
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,x as default};
-//# sourceMappingURL=theme-provider.stories-36386ffe.js.map
