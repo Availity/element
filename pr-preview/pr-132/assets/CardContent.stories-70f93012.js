8c15a9cd7bac5fa69022d7ab163b348c586b0c85
jordan-a-young <jordan-a-young@users.noreply.github.com>
2023-11-08 18:29:09 +0000

    Remove preview for PR 132 🛬

--- pr-preview/pr-132/assets/CardContent.stories-70f93012.js
+++
@@ -1,9 +0,0 @@
-import{j as t}from"./jsx-runtime-390e5fc8.js";import{C as e}from"./CardContent-96e671a6.js";import{C as d}from"./Card-36839347.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./createTheme-e6b56ddb.js";import"./clsx-8416b751.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./styled-78cd751f.js";import"./useThemeProps-285a8f6c.js";import"./generateUtilityClasses-2915a045.js";import"./Paper-adaa179d.js";const j={title:"Components/Card/CardContent",component:e,tags:["autodocs"]},r={render:s=>t(d,{children:t(e,{...s})}),args:{children:"This text is a child of CardContent"}};var o,n,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: (args: CardContentProps) => <Card>
-      <CardContent {...args} />
-    </Card>,
-  args: {
-    children: 'This text is a child of CardContent'
-  }
-}`,...(a=(n=r.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const T=["_CardContent"];export{r as _CardContent,T as __namedExportsOrder,j as default};
-//# sourceMappingURL=CardContent.stories-70f93012.js.map
