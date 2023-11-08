fac182e5a4ecf4451617b68ebd3787136d7e6ce6
LauRoxx <LauRoxx@users.noreply.github.com>
2023-11-08 21:24:49 +0000

    Deploy preview for PR 136 🛫

---
+++ pr-preview/pr-136/assets/DocsRenderer-EYKKDMVH-1babf196.js
@@ -0,0 +1,2 @@
+import{D as l,d as u}from"./chunk-PCJTTTQV-cc5e9f35.js";import"./iframe-d2d520e6.js";import"../sb-preview/runtime.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./react-18-b9b6ed02.js";import"./index-8a077077.js";import"./index-fed68bef.js";import"./chunk-4NMOSTKD-052c8761.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-f8236e9a.js";import"./index-356e4a49.js";export{l as DocsRenderer,u as defaultComponents};
+//# sourceMappingURL=DocsRenderer-EYKKDMVH-1babf196.js.map
---
+++ pr-preview/pr-136/assets/StatusChip.stories-fa0fd35e.js
@@ -0,0 +1,21 @@
+import{j as e,a as d,F as u}from"./jsx-runtime-390e5fc8.js";import{C as f}from"./Chip-a33b6866.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./createTheme-e6b56ddb.js";import"./clsx-8416b751.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./createSvgIcon-c7fbf131.js";import"./SvgIcon-f3e07af8.js";import"./useThemeProps-285a8f6c.js";import"./styled-78cd751f.js";import"./generateUtilityClasses-2915a045.js";import"./ButtonBase-da7ceb67.js";import"./emotion-react.browser.esm-69a15469.js";import"./TransitionGroupContext-c71596b3.js";import"./useForkRef-153a0a89.js";import"./useIsFocusVisible-5e7b1ff1.js";import"./useEnhancedEffect-460150e6.js";const r=({color:a="default",...c})=>e(f,{color:a,...c,size:"small"});try{r.displayName="StatusChip",r.__docgenInfo={description:"",displayName:"StatusChip",props:{color:{defaultValue:{value:"default"},description:"The color of the component.",name:"color",required:!1,type:{name:'"primary" | "secondary" | "error" | "info" | "success" | "warning" | "default"'}},className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},style:{defaultValue:null,description:"",name:"style",required:!1,type:{name:"CSSProperties"}},classes:{defaultValue:null,description:"Override or extend the styles applied to the component.",name:"classes",required:!1,type:{name:"Partial<ChipClasses> & Partial<ClassNameMap<never>>"}},sx:{defaultValue:null,description:"The system prop that allows defining system overrides as well as additional CSS styles.",name:"sx",required:!1,type:{name:"SxProps<Theme>"}},ref:{defaultValue:null,description:"",name:"ref",required:!1,type:{name:"Ref<HTMLDivElement>"}},label:{defaultValue:null,description:"The content of the component.",name:"label",required:!1,type:{name:"ReactNode"}},tabIndex:{defaultValue:null,description:"@ignore",name:"tabIndex",required:!1,type:{name:"number"}},component:{defaultValue:null,description:"",name:"component",required:!1,type:{name:"ElementType<any>"}}}}}catch{}const E={title:"Components/Chip/StatusChip",component:r,tags:["autodocs"],args:{label:"Chip",color:"default"}},t={render:a=>e(r,{...a}),args:{label:"Chip",color:"primary"}},o={render:()=>d(u,{children:["Item 1 ",e(r,{color:"success",label:"Approved"}),e("br",{}),"Item 2 ",e(r,{color:"info",label:"Info"}),e("br",{}),"Item 3 ",e(r,{color:"warning",label:"Pending"}),e("br",{}),"Item 4 ",e(r,{color:"error",label:"Declined"}),e("br",{}),"Item 5 ",e(r,{color:"secondary",label:"Neutral"}),e("br",{})]})};var l,s,n;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: (args: StatusChipProps) => <StatusChip {...args} />,
+  args: {
+    label: 'Chip',
+    color: 'primary'
+  }
+}`,...(n=(s=t.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};var i,p,m;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: () => <>
+      Item 1 <StatusChip color="success" label="Approved" />
+      <br />
+      Item 2 <StatusChip color="info" label="Info" />
+      <br />
+      Item 3 <StatusChip color="warning" label="Pending" />
+      <br />
+      Item 4 <StatusChip color="error" label="Declined" />
+      <br />
+      Item 5 <StatusChip color="secondary" label="Neutral" />
+      <br />
+    </>
+}`,...(m=(p=o.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const A=["_Chip","_Status"];export{t as _Chip,o as _Status,A as __namedExportsOrder,E as default};
+//# sourceMappingURL=StatusChip.stories-fa0fd35e.js.map
