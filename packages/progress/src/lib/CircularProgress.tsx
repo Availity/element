e1582378175587c13d6b42245469d3c548146881
Greg Martin <greg.martin@availity.com>
2023-11-08 17:01:18 -0500

    refactor(mui-progress): add error/success states and loadingCaption

--- packages/progress/src/lib/CircularProgress.tsx
+++ packages/progress/src/lib/CircularProgress.tsx
@@ -1,12 +1,65 @@
+import { useEffect, useState } from 'react';
 import {
   CircularProgress as MuiCircularProgress,
   CircularProgressProps as MuiCircularProgressProps,
+  Stack,
 } from '@mui/material';
+import { SuccessCircleIcon, WarningCircleIcon } from '@availity/mui-icon';
+import { Typography } from '@availity/mui-typography';
 
 export interface CircularProgressProps extends MuiCircularProgressProps {
   color?: 'primary' | 'inherit';
+  error?: boolean;
+  success?: boolean;
+  loadingCaption?: boolean;
 }
 
-export const CircularProgress = ({ ...props }: CircularProgressProps): JSX.Element => {
-  return <MuiCircularProgress {...props} />;
+export type StatusIconProps = {
+  status: string;
+};
+
+const StatusIcon = ({ status }: StatusIconProps) => {
+  switch (status) {
+    case 'error':
+      return <WarningCircleIcon color="error" fontSize="large" titleAccess="loading error" />;
+    case 'success':
+      return <SuccessCircleIcon color="success" fontSize="large" titleAccess="loading successful" />;
+    default:
+      return null;
+  }
+};
+
+const getCaptionText = (status: string) =>
+  ({
+    loading: 'Loading',
+    error: 'Loading error',
+    success: 'Loading successful',
+  }[status] || '');
+
+export const CircularProgress = ({
+  loadingCaption = true,
+  error = false,
+  success = false,
+  ...props
+}: CircularProgressProps): JSX.Element => {
+  const [status, setStatus] = useState('loading');
+
+  useEffect(() => {
+    if (error) {
+      setStatus('error');
+    } else if (success) {
+      setStatus('success');
+    } else {
+      setStatus('loading');
+    }
+  }, [error, success]);
+
+  return (
+    <Stack width="fit-content" alignItems="center">
+      {status === 'loading' ? <MuiCircularProgress {...props} /> : <StatusIcon status={status} />}
+      <Typography marginTop="8px" variant="caption">
+        {loadingCaption || error || success ? getCaptionText(status) : null}
+      </Typography>
+    </Stack>
+  );
 };
