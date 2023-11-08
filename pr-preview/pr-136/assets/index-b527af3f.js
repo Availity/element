a3d463fcac25b010747d4a78cc8beb8e86c0b61f
LauRoxx <LauRoxx@users.noreply.github.com>
2023-11-08 21:38:56 +0000

    Remove preview for PR 136 🛬

--- pr-preview/pr-136/assets/index-b527af3f.js
+++
@@ -1,2 +0,0 @@
-import{j as o}from"./jsx-runtime-390e5fc8.js";import{C as i}from"./Chip-a33b6866.js";var e=r=>o(i,{...r,color:"default",size:"medium"});export{e as C};
-//# sourceMappingURL=index-b527af3f.js.map
