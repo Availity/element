f9b6f9f01a640a3ab28df08475653dc76979e22c
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-11-15 18:26:13 +0000

    Remove preview for PR 140 🛬

--- pr-preview/pr-140/assets/createSvgIcon-c7fbf131.js
+++
@@ -1,2 +0,0 @@
-import{_ as i}from"./createTheme-e6b56ddb.js";import{r as o}from"./index-570b25c1.js";import{j as c}from"./jsx-runtime-390e5fc8.js";import{S as t}from"./SvgIcon-f3e07af8.js";function d(e,m){function r(n,a){return c(t,i({"data-testid":`${m}Icon`,ref:a},n,{children:e}))}return r.muiName=t.muiName,o.memo(o.forwardRef(r))}export{d as c};
-//# sourceMappingURL=createSvgIcon-c7fbf131.js.map
