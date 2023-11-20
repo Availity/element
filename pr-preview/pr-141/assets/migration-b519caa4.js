29f1c89e4dc49b47eba84c87ccdd71379ee75887
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-11-20 19:34:01 +0000

    Deploy preview for PR 141 🛫

---
+++ pr-preview/pr-141/assets/migration-b519caa4.js
@@ -0,0 +1,80 @@
+import{a as i,j as o,F as d}from"./jsx-runtime-a3bcee63.js";import"./chunk-PCJTTTQV-a5d2888b.js";import{M as c}from"./index-dc2e3a57.js";import"./index-5496e90f.js";import{M as a}from"./index-fe51f95b.js";import"./index-0cd865b2.js";import"./index-85bef318.js";import"./index-534b1a75.js";import{B as m}from"./index-128172a6.js";import"./index-cb507f8c.js";import{C as h}from"./index-07d1cb73.js";import"./index-96688515.js";import"./index-e6397f93.js";import"./index-570b25c1.js";import{T as r}from"./index-bd90de69.js";import{u as l}from"./index-da4379a2.js";import"./iframe-545458d8.js";import"../sb-preview/runtime.js";import"./react-18-b9b6ed02.js";import"./index-8a077077.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./_baseFor-53bad3ef.js";import"./index-e9d7ebdf.js";import"./_initCloneObject-31384e14.js";import"./index-356e4a49.js";import"./Button-d84dd9a0.js";import"./createTheme-bcd1d157.js";import"./clsx-7dc4e18d.js";import"./styled-7846e708.js";import"./useThemeProps-018254e6.js";import"./generateUtilityClasses-70c60a0e.js";import"./ButtonBase-d80b4855.js";import"./emotion-react.browser.esm-474fda6d.js";import"./TransitionGroupContext-c71596b3.js";import"./useForkRef-153a0a89.js";import"./useIsFocusVisible-5e7b1ff1.js";import"./useEventCallback-e1b9edb9.js";import"./useEnhancedEffect-460150e6.js";import"./Tooltip-c716d13a.js";import"./useTheme-76c02901.js";import"./objectWithoutPropertiesLoose-b6174fb2.js";import"./ownerDocument-613eb639.js";import"./Grow-e87a736b.js";import"./utils-1ac07bae.js";import"./useSlotProps-f4be827f.js";import"./useControlled-9b1271e0.js";import"./useId-6f4bfce0.js";import"./IconButton-de653fbd.js";import"./LoadingButton-7f26c21d.js";import"./extends-90dea224.js";import"./CircularProgress-83597315.js";import"./faCircleArrowRight-e7789e56.js";import"./faMagnifyingGlass-482fd726.js";import"./SvgIcon-951d7748.js";import"./TextField-785bc636.js";import"./FormLabel-16418a49.js";import"./utils-3d35716f.js";import"./isMuiElement-6907f060.js";import"./Select-be273af1.js";import"./react-is.production.min-a192e302.js";import"./Menu-932c384c.js";import"./Popover-277c5654.js";import"./Modal-8fa896b2.js";import"./Backdrop-49825a13.js";import"./ownerWindow-03d1c82d.js";import"./createChainedFunction-0bab83cf.js";import"./Paper-722ac472.js";import"./debounce-517eeb3c.js";import"./createSvgIcon-60c2b2ae.js";import"./OutlinedInput-bd5ed4eb.js";import"./GlobalStyles-7ba993a7.js";import"./FormHelperText-ae2a8f0e.js";import"./index-9681c8ff.js";import"./Box-0b96d669.js";import"./extendSxProp-9115426f.js";import"./FormControlLabel-625d44f3.js";import"./Typography-f61e077a.js";import"./Stack-8a417692.js";import"./styled-11d1647d.js";import"./Divider-fcaf2f3a.js";import"./dividerClasses-b19abef8.js";import"./index-fd483b67.js";import"./Avatar-4358e0ce.js";import"./Badge-41479d7d.js";import"./usePreviousProps-9cd3673d.js";import"./Checkbox-837bf21d.js";import"./index-5d036f5e.js";import"./index-7c0e1f2b.js";import"./isPlainObject-272efb33.js";import"./ToggleButtonGroup-303ca8bb.js";import"./ToggleButton-7fcf6cce.js";import"./Container-a1530f69.js";import"./TablePagination-e7d9bda9.js";import"./TableCell-fb4a8185.js";import"./KeyboardArrowRight-f02ae11e.js";import"./PaginationItem-822000d2.js";import"./MenuItem-21f22cdd.js";import"./TableRow-c3ef4dbb.js";import"./TableContainer-417e126a.js";import"./Pagination-fdc7dc59.js";import"./TableSortLabel-7b4a57ce.js";import"./Link-a674eae6.js";import"./Chip-31690be4.js";import"./index-6845c7ce.js";import"./LocalizationProvider-4189851f.js";function t(n){const e=Object.assign({h2:"h2",p:"p",pre:"pre",code:"code",a:"a",h3:"h3",ul:"ul",li:"li",table:"table",thead:"thead",tr:"tr",th:"th",tbody:"tbody",td:"td",blockquote:"blockquote",strong:"strong",h4:"h4"},l(),n.components);return o(d,{children:[i(c,{title:"BS4 Migration/Getting Started"}),`
+`,i(e.h2,{id:"installation",children:"Installation"}),`
+`,i(e.p,{children:"To install the library in your project, you can use either NPM or Yarn:"}),`
+`,i(e.pre,{children:i(e.code,{className:"language-sh",children:`npm install @availity/element
+`})}),`
+`,i(e.p,{children:"OR"}),`
+`,i(e.pre,{children:i(e.code,{className:"language-sh",children:`yarn add @availity/element
+`})}),`
+`,i(e.h2,{id:"usage",children:"Usage"}),`
+`,o(e.p,{children:["The ",i(e.a,{href:"https://availity.github.io/element/?path=/docs/element--docs",target:"_blank",rel:"nofollow noopener noreferrer",children:"@availity/element"})," package contains all of the components and hooks for the design system. We recommend using this package when you want to use multiple components."]}),`
+`,i(e.p,{children:"To use the components in your project, you will need to import them first:"}),`
+`,i(e.pre,{children:i(e.code,{className:"language-tsx",children:`import { Button, ThemeProvider } from '@availity/element';
+`})}),`
+`,o(e.p,{children:["Make sure all of the components you use are inside the ",i(e.code,{children:"ThemeProvider"})," component. We recommend placing it in your root ",i(e.code,{children:"index.{js|tsx}"})," file."]}),`
+`,i(e.pre,{children:i(e.code,{className:"language-jsx",children:`import { ThemeProvider } from '@availity/element';
+
+const App = () => {
+  return <ThemeProvider>{/* Application code. Most likely a router. */}</ThemeProvider>;
+};
+`})}),`
+`,i(e.h2,{id:"pattern-differences",children:"Pattern Differences"}),`
+`,i(e.h3,{id:"composition-and-custom-components",children:"Composition and Custom Components"}),`
+`,o(e.p,{children:["There are a few gotchas when creating components. This is a brief overview taken from the ",i(e.a,{href:"https://mui.com/material-ui/guides/composition",target:"_blank",rel:"nofollow noopener noreferrer",children:"Mui Composition Docs"}),". Visit the Mui docs for more details and code examples."]}),`
+`,o(e.ul,{children:[`
+`,o(e.li,{children:["Refs (most common issue so far)",`
+`,o(e.ul,{children:[`
+`,o(e.li,{children:["When using a custom component for ",i(e.code,{children:"children"})," or ",i(e.code,{children:"component"})," make sure it can take a ref. This means for function components to wrap it in a ",i(e.code,{children:"forwardRef"}),"."]}),`
+`]}),`
+`]}),`
+`,o(e.li,{children:["Wrapping Components that have ",i(e.code,{children:"muiName"})," property",`
+`,o(e.ul,{children:[`
+`,o(e.li,{children:["If you're wrapping a component that has the ",i(e.code,{children:"muiName"})," property, be sure to also assign it to the wrapped component."]}),`
+`]}),`
+`]}),`
+`,o(e.li,{children:["Using the ",i(e.code,{children:"component"})," property to replace the root element",`
+`,o(e.ul,{children:[`
+`,i(e.li,{children:"avoid inline functions and pass a static component instead"}),`
+`,i(e.li,{children:"be aware of prop name collisions when prop forwarding"}),`
+`,i(e.li,{children:"with Typescript: the type of the props should be used with type arguments"}),`
+`]}),`
+`]}),`
+`]}),`
+`,i(e.h3,{id:"common-props",children:"Common Props"}),`
+`,o(e.ul,{children:[`
+`,o(e.li,{children:[i(e.code,{children:"tag"}),": While Reactstrap uses ",i(e.code,{children:"tag"})," to change the root element, Mui uses ",i(e.code,{children:"component"}),". See ",i(e.a,{href:"#composition-and-custom-components",children:"Composition"})," notes above."]}),`
+`,o(e.li,{children:[i(e.code,{children:"pill"}),"/",i(e.code,{children:"outline"}),"/etc: Different styling variations are now contained within the ",i(e.code,{children:"variant"})," prop."]}),`
+`]}),`
+`,i(e.h3,{id:"component-equivalents",children:"Component Equivalents"}),`
+`,i(e.p,{children:"Not all classes/components from Bootstrap/Reactstrap have a similar name or 1:1 equivalent in MUI. Listed below are some notable changes"}),`
+`,o(e.table,{children:[i(e.thead,{children:o(e.tr,{children:[i(e.th,{children:"Bootstrap"}),i(e.th,{children:"MUI"}),i(e.th,{children:"Description"}),i(e.th,{children:"Example Use"}),i(e.th,{align:"center",children:"Sample"})]})}),o(e.tbody,{children:[o(e.tr,{children:[o(e.td,{children:[i(e.code,{children:"Badge"}),i("sup",{children:"1"})]}),i(e.td,{children:i(e.code,{children:"Badge"})}),i(e.td,{children:"Small corner overlay of base element"}),i(e.td,{children:"Counters"}),i(e.td,{align:"center",children:i(r,{children:i(m,{color:"primary",badgeContent:"5",variant:"dot",children:i(a,{fontSize:"large"})})})})]}),o(e.tr,{children:[o(e.td,{children:[i(e.code,{children:"Badge"}),i("sup",{children:"2"})]}),i(e.td,{children:i(e.code,{children:"Chip"})}),i(e.td,{children:"Inline colored label/descriptor"}),i(e.td,{children:"Status"}),i(e.td,{align:"center",children:i(r,{children:i(h,{color:"warning",label:"Pending",size:"small"})})})]}),o(e.tr,{children:[o(e.td,{children:[i(e.code,{children:"Column"}),"/",i(e.code,{children:"Row"})]}),i(e.td,{children:i(e.code,{children:"Grid"})}),i(e.td,{children:"Horizontal and Vertical layout"}),i(e.td,{}),i(e.td,{align:"center"})]})]})]}),`
+`,i(e.h2,{id:"migration-strategy",children:"Migration Strategy"}),`
+`,i(e.h3,{id:"drop-in-components-and-the-migration-theme",children:"Drop-in Components and the Migration Theme"}),`
+`,i(e.p,{children:"To help break up the work needed for migration, we have created a bootstrap migration theme. The migration theme should allow you to slowly start replacing components without disrupting your design until you are ready to upgrade to the new theme."}),`
+`,o(e.p,{children:["The migration theme is accessible by passing ",i(e.code,{children:"theme='legacyBS'"})," to the ",i(e.code,{children:"ThemeProvider"})]}),`
+`,o(e.blockquote,{children:[`
+`,o(e.p,{children:[i(e.strong,{children:"NOTE"}),`
+Be careful not to deviate from the provided Mui migration styles. Otherwise it may cause issues once you have finished migration and switch to the new Mui theme.`]}),`
+`]}),`
+`,i(e.h4,{id:"recommended-early-component-swaps",children:"Recommended Early Component Swaps"}),`
+`,o(e.ul,{children:[`
+`,o(e.li,{children:["layout (",i(e.code,{children:"Box"}),", ",i(e.code,{children:"Grid"}),", ",i(e.code,{children:"Container"}),")"]}),`
+`,i(e.li,{children:"button?"}),`
+`,i(e.li,{children:"table"}),`
+`,i(e.li,{children:"menu"}),`
+`,i(e.li,{children:"list (excluding list-group)"}),`
+`]}),`
+`,i(e.h3,{id:"form",children:"Form"}),`
+`,o(e.p,{children:["Where you're coming from will determine your strategy for form migration. We have moved away from ",i(e.code,{children:"formik"})," (used by ",i(e.code,{children:"@availity/form"}),") in favor of ",i(e.code,{children:"react-hook-forms"}),". We have also provided the base components to utilize with your own logic/libraries."]}),`
+`,i(e.p,{children:"See the Form migration docs for more information."}),`
+`,i(e.h3,{id:"components-without-a-drop-in-option",children:"Components Without a Drop-in Option"}),`
+`,i(e.p,{children:"There are many bootstrap elements and other design guidelines that will not be making it into the new Mui theme. These components we recommend saving for last so the changes will be introduced as you are switching to the new theme."}),`
+`,i(e.h4,{id:"required-ux-changes",children:"Required UX changes"}),`
+`,o(e.ul,{children:[`
+`,o(e.li,{children:["list-group",`
+`,o(e.ul,{children:[`
+`,i(e.li,{children:"does not exist in Mui. UX to provide new design guidelines for how to change this component."}),`
+`]}),`
+`]}),`
+`]})]})}function hi(n={}){const{wrapper:e}=Object.assign({},l(),n.components);return e?i(e,Object.assign({},n,{children:i(t,n)})):t(n)}export{hi as default};
+//# sourceMappingURL=migration-b519caa4.js.map
