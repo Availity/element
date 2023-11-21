37cac37f2670d669b7b6c38cbc52fd1453e3e4e4
L. Roxanne young <Roxanne.Young@availity.com>
2023-11-21 16:52:34 -0500

    feat(mui-autocomplete): use new select slots and styling

--- packages/autocomplete/src/lib/Autocomplete.tsx
+++ packages/autocomplete/src/lib/Autocomplete.tsx
@@ -1,11 +1,14 @@
+import { forwardRef } from 'react';
 import {
   Autocomplete as MuiAutocomplete,
   AutocompleteProps as MuiAutocompleteProps,
   AutocompleteRenderInputParams,
+  IconButton as MuiIconButton,
+  IconButtonProps as MuiIconButtonProps,
   ChipTypeMap,
 } from '@mui/material';
 import { TextField, TextFieldProps } from '@availity/mui-textfield';
-import { SelectPropOverrides } from '@availity/mui-form-utils';
+import { SelectDivider, SelectExpandIcon } from '@availity/mui-form-utils';
 
 export interface AutocompleteProps<
   T,
@@ -13,9 +16,19 @@ export interface AutocompleteProps<
   DisableClearable extends boolean | undefined,
   FreeSolo extends boolean | undefined,
   ChipComponent extends React.ElementType = ChipTypeMap['defaultComponent']
-> extends Omit<MuiAutocompleteProps<T, Multiple, DisableClearable, FreeSolo, ChipComponent>, 'renderInput'> {
+> extends Omit<
+    MuiAutocompleteProps<T, Multiple, DisableClearable, FreeSolo, ChipComponent>,
+    'renderInput' | 'slotProps'
+  > {
   FieldProps?: TextFieldProps;
 }
+
+const PopupIndicatorWrapper = forwardRef<HTMLButtonElement, MuiIconButtonProps>((props, ref) => (
+  <>
+    <SelectDivider orientation="vertical" className="MuiSelect-avEndAdornmentDivider" />
+    <MuiIconButton {...props} ref={ref} />
+  </>
+));
 
 export const Autocomplete = <
   T,
@@ -27,10 +40,23 @@ export const Autocomplete = <
   FieldProps,
   ...props
 }: AutocompleteProps<T, Multiple, DisableClearable, FreeSolo, ChipComponent>): JSX.Element => {
+  const resolvedProps = (params: AutocompleteRenderInputParams) => ({
+    InputProps: {
+      ...FieldProps?.InputProps,
+      ...params?.InputProps,
+    },
+    inputProps: {
+      ...FieldProps?.inputProps,
+      ...params?.inputProps,
+    },
+  });
   return (
     <MuiAutocomplete
-      renderInput={(params: AutocompleteRenderInputParams) => <TextField {...FieldProps} {...params} />}
-      popupIcon={<SelectPropOverrides.IconComponent />}
+      renderInput={(params: AutocompleteRenderInputParams) => (
+        <TextField {...FieldProps} {...params} {...resolvedProps(params)} />
+      )}
+      popupIcon={<SelectExpandIcon className="MuiSelect-avExpandIcon" />}
+      slotProps={{ popupIndicator: { component: PopupIndicatorWrapper } }}
       {...props}
     />
   );
