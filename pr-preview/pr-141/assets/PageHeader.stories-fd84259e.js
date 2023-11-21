fa6ec9cd494b0452812b0f6b9856f27006e14a46
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-11-21 18:16:18 +0000

    Deploy preview for PR 141 🛫

---
+++ pr-preview/pr-141/assets/PageHeader.stories-fd84259e.js
@@ -0,0 +1,46 @@
+import{j as o,a as e}from"./jsx-runtime-a3bcee63.js";import{D as w}from"./index-507c6617.js";import{T as u}from"./index-96688515.js";import{B as L}from"./index-b0be4767.js";import{L as S}from"./index-cb507f8c.js";import{B as j}from"./index-5496e90f.js";import{F as q}from"./index-0a7ab4a6.js";import{G as t}from"./Grid-af1a3ff2.js";import{C}from"./Container-a1530f69.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./Divider-fcaf2f3a.js";import"./createTheme-bcd1d157.js";import"./clsx-7dc4e18d.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./styled-7846e708.js";import"./useThemeProps-018254e6.js";import"./dividerClasses-b19abef8.js";import"./generateUtilityClasses-70c60a0e.js";import"./Typography-f61e077a.js";import"./extendSxProp-9115426f.js";import"./index-cb4dbded.js";import"./faCircleArrowRight-e7789e56.js";import"./faMagnifyingGlass-482fd726.js";import"./SvgIcon-951d7748.js";import"./Breadcrumbs-b153b1d7.js";import"./react-is.production.min-a192e302.js";import"./createSvgIcon-60c2b2ae.js";import"./ButtonBase-d80b4855.js";import"./emotion-react.browser.esm-474fda6d.js";import"./TransitionGroupContext-c71596b3.js";import"./useForkRef-153a0a89.js";import"./useIsFocusVisible-5e7b1ff1.js";import"./useEventCallback-e1b9edb9.js";import"./useEnhancedEffect-460150e6.js";import"./useSlotProps-f4be827f.js";import"./objectWithoutPropertiesLoose-b6174fb2.js";import"./Link-a674eae6.js";import"./Button-d84dd9a0.js";import"./Tooltip-c716d13a.js";import"./useTheme-76c02901.js";import"./ownerDocument-613eb639.js";import"./Grow-e87a736b.js";import"./index-8a077077.js";import"./utils-1ac07bae.js";import"./useControlled-9b1271e0.js";import"./useId-6f4bfce0.js";import"./IconButton-de653fbd.js";import"./LoadingButton-7f26c21d.js";import"./extends-90dea224.js";import"./CircularProgress-83597315.js";import"./index-aa1f890f.js";import"./Popover-277c5654.js";import"./Modal-8fa896b2.js";import"./Backdrop-49825a13.js";import"./ownerWindow-03d1c82d.js";import"./createChainedFunction-0bab83cf.js";import"./Paper-722ac472.js";import"./debounce-517eeb3c.js";import"./index-7c0e1f2b.js";import"./_baseFor-53bad3ef.js";import"./_initCloneObject-31384e14.js";import"./isPlainObject-272efb33.js";import"./ToggleButtonGroup-303ca8bb.js";import"./ToggleButton-7fcf6cce.js";import"./index-9e86b6d5.js";import"./index-a5c9266e.js";import"./index-9681c8ff.js";import"./Box-0b96d669.js";import"./FormControlLabel-625d44f3.js";import"./utils-3d35716f.js";import"./Stack-8a417692.js";import"./styled-11d1647d.js";import"./FormLabel-16418a49.js";import"./isMuiElement-6907f060.js";import"./FormHelperText-ae2a8f0e.js";import"./OutlinedInput-bd5ed4eb.js";import"./GlobalStyles-7ba993a7.js";import"./Select-be273af1.js";import"./Menu-932c384c.js";import"./TextField-785bc636.js";import"./index.esm-d81a0d8c.js";const a=({breadcrumbs:r,buttons:i,feedback:v=!1,headerText:c,helpLink:d})=>o(t,{component:C,container:!0,direction:"column",marginTop:"1rem",marginBottom:"1.25rem",children:[r||d?o(t,{direction:"row",item:!0,container:!0,justifyContent:"space-between",marginBottom:4,children:[r&&e(t,{item:!0,children:e(L,{...r})}),d&&e(t,{item:!0,marginLeft:2,children:o(u,{variant:"body1",children:["Need help? ",e(S,{href:d,target:"_blank",children:"Watch a demo"})]})})]}):null,o(t,{direction:"row",item:!0,container:!0,marginBottom:2,alignItems:"center",justifyContent:"space-between",children:[e(t,{item:!0,children:e(u,{variant:"h1",children:c})}),o(t,{item:!0,container:!0,width:"auto",children:[i&&i.length>0&&(i==null?void 0:i.map(k=>e(t,{item:!0,marginLeft:2,height:"100%",children:e(j,{...k,size:"large",color:"secondary"})}))),v?e(t,{item:!0,marginLeft:2,height:"100%",children:e(q,{appName:c})}):null]})]}),e(w,{})]});try{a.displayName="PageHeader",a.__docgenInfo={description:"",displayName:"PageHeader",props:{breadcrumbs:{defaultValue:null,description:"",name:"breadcrumbs",required:!0,type:{name:"BreadcrumbsProps"}},buttons:{defaultValue:null,description:"",name:"buttons",required:!1,type:{name:"ButtonsProps[]"}},feedback:{defaultValue:{value:"false"},description:"",name:"feedback",required:!1,type:{name:"boolean"}},headerText:{defaultValue:null,description:"",name:"headerText",required:!0,type:{name:"string"}},helpLink:{defaultValue:null,description:"",name:"helpLink",required:!1,type:{name:"string"}}}}}catch{}const mr={title:"Components/PageHeader/PageHeader",component:a,tags:["autodocs"]},n={render:r=>e(a,{...r}),args:{headerText:"This text is a child of PageHeader",breadcrumbs:{active:"This Page"}}},m={render:r=>e(a,{...r}),args:{headerText:"This text is a child of PageHeader",breadcrumbs:{active:"This Page"},helpLink:"https://www.availity.com"}},s={render:r=>e(a,{...r}),args:{headerText:"This text is a child of PageHeader",breadcrumbs:{active:"This Page"},buttons:[{children:"Button 1",key:"button1"}]}},p={render:r=>e(a,{...r}),args:{headerText:"This text is a child of PageHeader",breadcrumbs:{active:"This Page"},buttons:[{children:"Button 1",key:"button1"},{children:"Button 2",key:"button2"}]}};var l,g,h;n.parameters={...n.parameters,docs:{...(l=n.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: (args: PageHeaderProps) => <PageHeader {...args} />,
+  args: {
+    headerText: 'This text is a child of PageHeader',
+    breadcrumbs: {
+      active: 'This Page'
+    }
+  }
+}`,...(h=(g=n.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};var P,f,b;m.parameters={...m.parameters,docs:{...(P=m.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  render: (args: PageHeaderProps) => <PageHeader {...args} />,
+  args: {
+    headerText: 'This text is a child of PageHeader',
+    breadcrumbs: {
+      active: 'This Page'
+    },
+    helpLink: 'https://www.availity.com'
+  }
+}`,...(b=(f=m.parameters)==null?void 0:f.docs)==null?void 0:b.source}}};var H,T,y;s.parameters={...s.parameters,docs:{...(H=s.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  render: (args: PageHeaderProps) => <PageHeader {...args} />,
+  args: {
+    headerText: 'This text is a child of PageHeader',
+    breadcrumbs: {
+      active: 'This Page'
+    },
+    buttons: [{
+      children: 'Button 1',
+      key: 'button1'
+    }]
+  }
+}`,...(y=(T=s.parameters)==null?void 0:T.docs)==null?void 0:y.source}}};var x,B,_;p.parameters={...p.parameters,docs:{...(x=p.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: (args: PageHeaderProps) => <PageHeader {...args} />,
+  args: {
+    headerText: 'This text is a child of PageHeader',
+    breadcrumbs: {
+      active: 'This Page'
+    },
+    buttons: [{
+      children: 'Button 1',
+      key: 'button1'
+    }, {
+      children: 'Button 2',
+      key: 'button2'
+    }]
+  }
+}`,...(_=(B=p.parameters)==null?void 0:B.docs)==null?void 0:_.source}}};const sr=["_PageHeader","_PageHeaderHelp","_PageHeaderSingleButton","_PageHeaderDoubleButtons"];export{n as _PageHeader,p as _PageHeaderDoubleButtons,m as _PageHeaderHelp,s as _PageHeaderSingleButton,sr as __namedExportsOrder,mr as default};
+//# sourceMappingURL=PageHeader.stories-fd84259e.js.map
