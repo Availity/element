466a2099f48925d0172cb9fd3d89599fe5e55d14
LauRoxx <LauRoxx@users.noreply.github.com>
2023-11-22 20:04:17 +0000

    Deploy preview for PR 148 🛫

---
+++ pr-preview/pr-148/assets/introduction-f04f94be.js
@@ -0,0 +1,64 @@
+import{a as n,j as e,F as o}from"./jsx-runtime-a3bcee63.js";import{M as s,l as m}from"./index-6cb335f6.js";import"./chunk-PCJTTTQV-f19fc8c3.js";import{u as r}from"./index-da4379a2.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-8c4c80f3.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-8a077077.js";import"./_baseFor-53bad3ef.js";import"./index-e9d7ebdf.js";import"./_initCloneObject-31384e14.js";import"./index-356e4a49.js";import"./react-18-b9b6ed02.js";const p=`# @availity/mui-tabs
+
+> Availity MUI Tabs component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-tabs.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-tabs)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-tabs.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-tabs)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-tabs?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-tabs/package.json)
+
+## Documentation
+
+This package extends the MUI Tabs component: [MUI Tabs Docs](https://mui.com/components/tabs/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-tabs-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://zeroheight.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+_This package has a few peer dependencies. Add \`@mui/material\` & \`@emotion/react\` to your project if not already installed._
+
+\`\`\`bash
+npm install @availity/mui-tabs
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-tabs
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Tabs } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Tabs } from '@availity/mui-tabs';
+\`\`\`
+`;function i(t){return e(o,{children:[n(s,{title:"Components/Tabs/Introduction"}),`
+`,n(m,{children:p})]})}function I(t={}){const{wrapper:a}=Object.assign({},r(),t.components);return a?n(a,Object.assign({},t,{children:n(i,t)})):i()}export{I as default};
+//# sourceMappingURL=introduction-f04f94be.js.map
---
+++ pr-preview/pr-148/assets/useEnhancedEffect-460150e6.js
@@ -0,0 +1,2 @@
+import{r as e}from"./index-570b25c1.js";const t=typeof window<"u"?e.useLayoutEffect:e.useEffect,c=t;export{c as u};
+//# sourceMappingURL=useEnhancedEffect-460150e6.js.map
