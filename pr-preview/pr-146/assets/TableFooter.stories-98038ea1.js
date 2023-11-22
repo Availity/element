7429788d40f89d9206057ae4d0fd019404146280
LauRoxx <LauRoxx@users.noreply.github.com>
2023-11-22 12:48:31 +0000

    Deploy preview for PR 146 🛫

---
+++ pr-preview/pr-146/assets/TableFooter.stories-98038ea1.js
@@ -0,0 +1,22 @@
+import{a as r}from"./jsx-runtime-a3bcee63.js";import{T as a,a as b,b as c}from"./TablePagination-27ee8569.js";import{a as T}from"./TableRow-873b7b78.js";import{T as g}from"./TableCell-55b1321f.js";import"./TableContainer-a6126a8f.js";import"./TableSortLabel-39abad19.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./TablePagination-682021df.js";import"./createTheme-3fee607a.js";import"./clsx-b831246b.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./TableCell-81c8412e.js";import"./useThemeProps-98dc6282.js";import"./styled-523cb007.js";import"./generateUtilityClasses-b58f947a.js";import"./KeyboardArrowRight-94c6c8ce.js";import"./createSvgIcon-ca656bec.js";import"./SvgIcon-075b1db6.js";import"./useTheme-c07bb8e7.js";import"./PaginationItem-0a0028eb.js";import"./ButtonBase-44753119.js";import"./emotion-react.browser.esm-b00e6d55.js";import"./TransitionGroupContext-c71596b3.js";import"./useForkRef-153a0a89.js";import"./useIsFocusVisible-5e7b1ff1.js";import"./useEventCallback-e1b9edb9.js";import"./useEnhancedEffect-460150e6.js";import"./IconButton-3fd19b93.js";import"./Select-a4487aaa.js";import"./react-is.production.min-a192e302.js";import"./Menu-5950de0d.js";import"./Popover-3b512651.js";import"./Modal-a1db681c.js";import"./Backdrop-4a58ec86.js";import"./utils-1ac07bae.js";import"./index-8a077077.js";import"./objectWithoutPropertiesLoose-b6174fb2.js";import"./ownerDocument-613eb639.js";import"./ownerWindow-03d1c82d.js";import"./useSlotProps-039d2b0a.js";import"./createChainedFunction-0bab83cf.js";import"./Grow-8a00b99f.js";import"./Paper-389d56e7.js";import"./debounce-517eeb3c.js";import"./utils-3d35716f.js";import"./useId-6f4bfce0.js";import"./useControlled-9b1271e0.js";import"./OutlinedInput-0cb1099f.js";import"./GlobalStyles-5f9800bb.js";import"./MenuItem-72d68a40.js";import"./dividerClasses-871b72e6.js";import"./index-8e81bbc6.js";import"./faCircleArrowRight-f69b7367.js";import"./faMagnifyingGlass-482fd726.js";import"./Pagination-5d0fba7b.js";import"./TableRow-edc17b29.js";import"./TableContainer-8cec70ef.js";import"./Box-51bd3ea0.js";import"./extendSxProp-b1abb964.js";import"./TableSortLabel-65a2c334.js";const _r={title:"Components/Table/TableFooter",component:a,tags:["autodocs"],args:{}},o={render:t=>r(b,{children:r(a,{...t,children:r(T,{children:r(g,{children:"Footer"})})})})},e={render:t=>r(b,{children:r(a,{...t,children:r(T,{children:r(c,{count:50,page:0,rowsPerPage:10,rowsPerPageOptions:[5,10,25,{value:-1,label:"all"}],onPageChange:()=>null})})})}),args:{}};var i,p,m;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: (args: TableFooterProps) => <Table>
+      <TableFooter {...args}>
+        <TableRow>
+          <TableCell>Footer</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+}`,...(m=(p=o.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var n,l,s;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: (args: TableFooterProps) => <Table>
+      <TableFooter {...args}>
+        <TableRow>
+          <TablePagination count={50} page={0} rowsPerPage={10} rowsPerPageOptions={[5, 10, 25, {
+          value: -1,
+          label: 'all'
+        }]} onPageChange={() => null} />
+        </TableRow>
+      </TableFooter>
+    </Table>,
+  args: {}
+}`,...(s=(l=e.parameters)==null?void 0:l.docs)==null?void 0:s.source}}};const fr=["_TableFooter","_Pagination"];export{e as _Pagination,o as _TableFooter,fr as __namedExportsOrder,_r as default};
+//# sourceMappingURL=TableFooter.stories-98038ea1.js.map
