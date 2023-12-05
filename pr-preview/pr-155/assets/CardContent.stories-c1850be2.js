b341c4b4a044fa0e689d845057a744a46948530b
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-12-05 21:11:17 +0000

    Deploy preview for PR 155 🛫

---
+++ pr-preview/pr-155/assets/CardContent.stories-c1850be2.js
@@ -0,0 +1,9 @@
+import{a as t}from"./jsx-runtime-a3bcee63.js";import{C as e}from"./CardContent-f5868c8c.js";import{C as d}from"./Card-15dbf581.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./createTheme-3fee607a.js";import"./clsx-b831246b.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./styled-523cb007.js";import"./useThemeProps-98dc6282.js";import"./generateUtilityClasses-b58f947a.js";import"./Card-9bfaf6e1.js";import"./Paper-389d56e7.js";const j={title:"Components/Card/CardContent",component:e,tags:["autodocs"]},r={render:s=>t(d,{children:t(e,{...s})}),args:{children:"This text is a child of CardContent"}};var o,a,n;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: (args: CardContentProps) => <Card>
+      <CardContent {...args} />
+    </Card>,
+  args: {
+    children: 'This text is a child of CardContent'
+  }
+}`,...(n=(a=r.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};const E=["_CardContent"];export{r as _CardContent,E as __namedExportsOrder,j as default};
+//# sourceMappingURL=CardContent.stories-c1850be2.js.map
