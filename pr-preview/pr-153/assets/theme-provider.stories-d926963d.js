fbc26e995b6e79b34a012164daac6d08689fbbd7
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-12-05 14:59:37 +0000

    Deploy preview for PR 153 🛫

--- pr-preview/pr-153/assets/theme-provider.stories-d926963d.js
+++
@@ -1,9 +0,0 @@
-import{j as s,F as m,a as r}from"./jsx-runtime-a3bcee63.js";import{T as d}from"./index-5bece438.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-c45350bb.js";import"./index-26edfdad.js";import"./createTheme-3fee607a.js";import"./LocalizationProvider-c8fffce8.js";import"./useThemeProps-98dc6282.js";import"./GlobalStyles-5f9800bb.js";import"./emotion-react.browser.esm-b00e6d55.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";const e={render:()=>s(m,{children:[r(d,{children:r("div",{children:"This component is inside the ThemeProvider"})}),r("div",{children:"This component is outside the ThemeProvider"})]})},g={title:"Design System/ThemeProvider"};var i,o,t;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
-  render: () => <>
-    <ThemeProvider>
-      <div>This component is inside the ThemeProvider</div>
-    </ThemeProvider>
-    <div>This component is outside the ThemeProvider</div>
-  </>
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const j=["Default"];export{e as Default,j as __namedExportsOrder,g as default};
-//# sourceMappingURL=theme-provider.stories-d926963d.js.map
