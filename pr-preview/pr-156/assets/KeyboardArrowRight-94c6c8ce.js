aca68e7a079c314b143db43b0cba65b7bbdccf73
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-12-06 21:03:17 +0000

    Deploy preview for PR 156 🛫

---
+++ pr-preview/pr-156/assets/KeyboardArrowRight-94c6c8ce.js
@@ -0,0 +1,2 @@
+import"./index-570b25c1.js";import{c as r}from"./createSvgIcon-ca656bec.js";import{a as o}from"./jsx-runtime-a3bcee63.js";const s=r(o("path",{d:"M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z"}),"KeyboardArrowLeft"),d=r(o("path",{d:"M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"}),"KeyboardArrowRight");export{d as K,s as a};
+//# sourceMappingURL=KeyboardArrowRight-94c6c8ce.js.map
