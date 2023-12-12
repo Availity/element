9112b9c7298a48543513b01d53af6526d7f78e4e
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-12-12 19:52:36 +0000

    Deploy preview for PR 158 🛫

---
+++ pr-preview/pr-158/assets/CardMedia.stories-b5cb1679.js
@@ -0,0 +1,13 @@
+import{a}from"./jsx-runtime-a3bcee63.js";import{C as i}from"./CardMedia-27991a86.js";import{C as n}from"./Card-15dbf581.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./createTheme-3fee607a.js";import"./clsx-b831246b.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./useThemeProps-98dc6282.js";import"./styled-523cb007.js";import"./generateUtilityClasses-b58f947a.js";import"./Card-9bfaf6e1.js";import"./Paper-389d56e7.js";const v={title:"Components/Card/CardMedia",component:i,tags:["autodocs"]},r={render:s=>a(n,{children:a(i,{...s})}),args:{image:"https://avatars.githubusercontent.com/u/329985?s=200&v=4",sx:{height:"100px"},"aria-label":"Orange rectangle with interlocking AV logo"}};var t,e,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: (args: CardMediaProps) => <Card>
+      <CardMedia {...args} />
+    </Card>,
+  args: {
+    image: 'https://avatars.githubusercontent.com/u/329985?s=200&v=4',
+    sx: {
+      height: '100px'
+    },
+    'aria-label': 'Orange rectangle with interlocking AV logo'
+  }
+}`,...(o=(e=r.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const _=["_CardMedia"];export{r as _CardMedia,_ as __namedExportsOrder,v as default};
+//# sourceMappingURL=CardMedia.stories-b5cb1679.js.map
