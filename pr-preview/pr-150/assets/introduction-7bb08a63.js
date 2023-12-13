8878ef9ddc45da4f027623d2f7ca6d29c67a3536
jordan-a-young <jordan-a-young@users.noreply.github.com>
2023-12-13 19:09:37 +0000

    Deploy preview for PR 150 🛫

---
+++ pr-preview/pr-150/assets/introduction-7bb08a63.js
@@ -0,0 +1,64 @@
+import{a as i,j as a,F as o}from"./jsx-runtime-a3bcee63.js";import{M as m,l as s}from"./index-649de8bd.js";import"./chunk-PCJTTTQV-6a4772c0.js";import{u as p}from"./index-da4379a2.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-6385c4c0.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-8a077077.js";import"./_baseFor-53bad3ef.js";import"./index-e9d7ebdf.js";import"./_initCloneObject-31384e14.js";import"./index-356e4a49.js";import"./react-18-b9b6ed02.js";const r=`# @availity/mui-chip
+
+> Availity MUI Chip component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-chip.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-chip)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-chip.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-chip)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-chip?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-chip/package.json)
+
+## Documentation
+
+This package extends the MUI Chip component: [MUI Chip Docs](https://mui.com/components/chip/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-chip-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://zeroheight.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+_This package has a few peer dependencies. Add \`@mui/material\` & \`@emotion/react\` to your project if not already installed._
+
+\`\`\`bash
+npm install @availity/mui-chip
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-chip
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Chip } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Chip } from '@availity/mui-chip';
+\`\`\`
+`;function e(n){return a(o,{children:[i(m,{title:"Components/Chip/Introduction"}),`
+`,i(s,{children:r})]})}function I(n={}){const{wrapper:t}=Object.assign({},p(),n.components);return t?i(t,Object.assign({},n,{children:i(e,n)})):e()}export{I as default};
+//# sourceMappingURL=introduction-7bb08a63.js.map
