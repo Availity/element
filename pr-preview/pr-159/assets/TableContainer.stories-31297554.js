712a3c0762276ae4e7182a525032b6a27c9650d2
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-12-13 15:55:22 +0000

    Remove preview for PR 159 🛬

--- pr-preview/pr-159/assets/TableContainer.stories-31297554.js
+++
@@ -1,4 +0,0 @@
-import{a as s}from"./jsx-runtime-a3bcee63.js";import{T as t}from"./TableContainer-a6126a8f.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./TableContainer-8cec70ef.js";import"./createTheme-3fee607a.js";import"./clsx-b831246b.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./useThemeProps-98dc6282.js";import"./styled-523cb007.js";import"./generateUtilityClasses-b58f947a.js";const _={title:"Components/Table/TableContainer",component:t,tags:["autodocs"],args:{}},r={render:n=>s(t,{...n})};var o,e,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: (args: TableContainerProps) => <TableContainer {...args} />
-}`,...(a=(e=r.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};const f=["_TableContainer"];export{r as _TableContainer,f as __namedExportsOrder,_ as default};
-//# sourceMappingURL=TableContainer.stories-31297554.js.map
