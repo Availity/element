3010a33b7ba9eec8873fbc91ea23a4f8d44949cb
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-12-13 14:56:31 +0000

    Deploy preview for PR 159 🛫

---
+++ pr-preview/pr-159/assets/introduction-1c99af95.js
@@ -0,0 +1,64 @@
+import{a as i,j as a,F as o}from"./jsx-runtime-a3bcee63.js";import{M as m,l as s}from"./index-22077a93.js";import"./chunk-PCJTTTQV-8a2edb58.js";import{u as l}from"./index-da4379a2.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-46dd9848.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-8a077077.js";import"./_baseFor-53bad3ef.js";import"./index-e9d7ebdf.js";import"./_initCloneObject-31384e14.js";import"./index-356e4a49.js";import"./react-18-b9b6ed02.js";const r=`# @availity/mui-link
+
+> Availity MUI Link component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-link.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-link)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-link.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-link)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-link?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-link/package.json)
+
+## Documentation
+
+This package extends the MUI Link component: [MUI Link Docs](https://mui.com/components/link/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-link-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://zeroheight.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+_This package has a few peer dependencies. Install \`@mui/material\`, \`@emotion/react\`, & \`@mui/system\` if it is not in your application already._
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/mui-link
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-link
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Link } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Link } from '@availity/mui-link';
+\`\`\`
+`;function e(n){return a(o,{children:[i(m,{title:"Components/Link/Introduction"}),`
+`,i(s,{children:r})]})}function L(n={}){const{wrapper:t}=Object.assign({},l(),n.components);return t?i(t,Object.assign({},n,{children:i(e,n)})):e()}export{L as default};
+//# sourceMappingURL=introduction-1c99af95.js.map
