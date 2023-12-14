860969ea0221a4e5c62c50692ffd42607674504b
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-12-14 19:15:41 +0000

    Deploy preview for PR 163 🛫

---
+++ pr-preview/pr-163/assets/introduction-9af05b9c.js
@@ -0,0 +1,64 @@
+import{a as n,j as e,F as o}from"./jsx-runtime-a3bcee63.js";import{M as p,l as r}from"./index-8868d0b4.js";import"./chunk-PCJTTTQV-cbd41f5c.js";import{u as m}from"./index-da4379a2.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-b40af941.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-8a077077.js";import"./_baseFor-53bad3ef.js";import"./index-e9d7ebdf.js";import"./_initCloneObject-31384e14.js";import"./index-356e4a49.js";import"./react-18-b9b6ed02.js";const s=`# @availity/mui-typography
+
+> Availity MUI Typography component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-typography.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-typography)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-typography.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-typography)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-typography?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-typography/package.json)
+
+## Documentation
+
+This package extends the MUI Typography component: [MUI Typography Docs](https://mui.com/components/typography/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-typography-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://zeroheight.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+_This package has a few peer dependencies. Add \`@mui/material\` & \`@emotion/react\` to your project if not already installed._
+
+\`\`\`bash
+npm install @availity/mui-typography
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-typography
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Typography } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Typography } from '@availity/mui-typography';
+\`\`\`
+`;function i(t){return e(o,{children:[n(p,{title:"Components/Typography/Introduction"}),`
+`,n(r,{children:s})]})}function I(t={}){const{wrapper:a}=Object.assign({},m(),t.components);return a?n(a,Object.assign({},t,{children:n(i,t)})):i()}export{I as default};
+//# sourceMappingURL=introduction-9af05b9c.js.map
