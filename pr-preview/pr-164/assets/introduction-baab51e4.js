cabe4a11630a76071a025e0d400e3a42abf20616
jordan-a-young <jordan-a-young@users.noreply.github.com>
2023-12-15 20:03:46 +0000

    Deploy preview for PR 164 🛫

---
+++ pr-preview/pr-164/assets/introduction-baab51e4.js
@@ -0,0 +1,64 @@
+import{a as t,j as i,F as a}from"./jsx-runtime-a3bcee63.js";import{M as p,l as r}from"./index-a09a46d7.js";import"./chunk-PCJTTTQV-e5e65aff.js";import{u as m}from"./index-da4379a2.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-4be3dff7.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-8a077077.js";import"./_baseFor-53bad3ef.js";import"./index-e9d7ebdf.js";import"./_initCloneObject-31384e14.js";import"./index-356e4a49.js";import"./react-18-b9b6ed02.js";const s=`# @availity/mui-popover
+
+> Availity MUI Popover component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-popover.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-popover)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-popover.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-popover)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-popover?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-popover/package.json)
+
+## Documentation
+
+This package extends the MUI Popover component: [MUI Popover Docs](https://mui.com/components/popover/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-popover-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://zeroheight.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+_This package has a few peer dependencies. Add \`@mui/material\` & \`@emotion/react\` to your project if not already installed._
+
+\`\`\`bash
+npm install @availity/mui-popover
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-popover
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Popover } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Popover } from '@availity/mui-popover';
+\`\`\`
+`;function o(n){return i(a,{children:[t(p,{title:"Components/Popover/Introduction"}),`
+`,t(r,{children:s})]})}function I(n={}){const{wrapper:e}=Object.assign({},m(),n.components);return e?t(e,Object.assign({},n,{children:t(o,n)})):o()}export{I as default};
+//# sourceMappingURL=introduction-baab51e4.js.map
