c85c45a5a79464a51bb7f5568c28d9500b93315e
LauRoxx <LauRoxx@users.noreply.github.com>
2023-12-15 20:18:10 +0000

    Deploy preview for PR 165 🛫

---
+++ pr-preview/pr-165/assets/FormLabel.stories-4425d241.js
@@ -0,0 +1,28 @@
+import{a as r,j as m,F as f}from"./jsx-runtime-a3bcee63.js";import"./FieldHelpIcon-99432cf6.js";import{a as o}from"./FormControlLabel-a7198c4e.js";import{F as L}from"./Select-86845ae4.js";import{I as x}from"./Input-028a0127.js";import{F as b}from"./FormLabel-16418a49.js";import{O as g}from"./OutlinedInput-ce25e1de.js";import{v as I}from"./visuallyHidden-14c3de6e.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-9681c8ff.js";import"./index-a94774e7.js";import"./faCircleArrowRight-f69b7367.js";import"./faMagnifyingGlass-482fd726.js";import"./SvgIcon-951d7748.js";import"./createTheme-bcd1d157.js";import"./clsx-7dc4e18d.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./useThemeProps-018254e6.js";import"./styled-7846e708.js";import"./generateUtilityClasses-70c60a0e.js";import"./index-762a5bc1.js";import"./Button-d84dd9a0.js";import"./ButtonBase-d80b4855.js";import"./emotion-react.browser.esm-474fda6d.js";import"./TransitionGroupContext-c71596b3.js";import"./useForkRef-153a0a89.js";import"./useIsFocusVisible-5e7b1ff1.js";import"./useEventCallback-e1b9edb9.js";import"./useEnhancedEffect-460150e6.js";import"./Tooltip-7ded6962.js";import"./useTheme-76c02901.js";import"./objectWithoutPropertiesLoose-df62547b.js";import"./ownerDocument-613eb639.js";import"./Grow-e87a736b.js";import"./index-8a077077.js";import"./utils-1ac07bae.js";import"./useSlotProps-ddd7eed7.js";import"./isHostComponent-73d6e646.js";import"./useControlled-9b1271e0.js";import"./useId-6f4bfce0.js";import"./IconButton-de653fbd.js";import"./LoadingButton-7f26c21d.js";import"./extends-90dea224.js";import"./CircularProgress-83597315.js";import"./Box-0b96d669.js";import"./extendSxProp-9115426f.js";import"./FormControlLabel-625d44f3.js";import"./utils-3d35716f.js";import"./Typography-f61e077a.js";import"./Stack-8a417692.js";import"./styled-11d1647d.js";import"./FormHelperText-ae2a8f0e.js";import"./Divider-fcaf2f3a.js";import"./dividerClasses-b19abef8.js";import"./Select-93ca49f3.js";import"./react-is.production.min-a192e302.js";import"./Menu-1707273c.js";import"./Popover-3bb772b9.js";import"./Modal-0e6ffa9c.js";import"./Backdrop-49825a13.js";import"./ownerWindow-03d1c82d.js";import"./createChainedFunction-0bab83cf.js";import"./Paper-722ac472.js";import"./debounce-517eeb3c.js";import"./List-99d50e2c.js";import"./createSvgIcon-60c2b2ae.js";import"./isMuiElement-6907f060.js";import"./GlobalStyles-7ba993a7.js";const Vr={title:"Components/FormUtils/FormLabel",component:o,tags:["autodocs"],parameters:{docs:{description:{component:"Label component for use with individual inputs, form controls,  and form groups. For labels to use with `checkbox`/`radio` see `FormControlLabel`."}}}},e={render:h=>r(o,{...h}),args:{children:"This text is a child of FormLabel",required:!0}},t={render:()=>m(f,{children:[r(o,{children:"Default"}),r(o,{helpTopicId:"1234",children:"With Field Help Icon"}),r(o,{required:!0,children:"Required"}),r(o,{error:!0,children:"Error"}),m(b,{disabled:!0,size:"small",children:[r(o,{htmlFor:"disabled",children:"Disabled"}),r(g,{id:"disabled",sx:I})]})]})},i={render:()=>m(b,{required:!0,size:"small",children:[r(o,{htmlFor:"field-1",helpTopicId:"1234",children:"Example"}),r(x,{id:"field-1",defaultValue:"default value"}),r(L,{children:"Helper Text"})]})};var l,p,a;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: (args: FormLabelProps) => <FormLabel {...args} />,
+  args: {
+    children: 'This text is a child of FormLabel',
+    required: true
+  }
+}`,...(a=(p=e.parameters)==null?void 0:p.docs)==null?void 0:a.source}}};var s,d,n;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: () => <>
+      <FormLabel>Default</FormLabel>
+      <FormLabel helpTopicId="1234">With Field Help Icon</FormLabel>
+      <FormLabel required>Required</FormLabel>
+      <FormLabel error>Error</FormLabel>
+      {/* A disabled label by itself will throw contrast warning unless used correctly with a disabled input. */}
+      <FormControl disabled size="small">
+        <FormLabel htmlFor="disabled">Disabled</FormLabel>
+        <OutlinedInput id="disabled" sx={visuallyHidden} />
+      </FormControl>
+    </>
+}`,...(n=(d=t.parameters)==null?void 0:d.docs)==null?void 0:n.source}}};var c,u,F;i.parameters={...i.parameters,docs:{...(c=i.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: () => <FormControl required size="small">
+      <FormLabel htmlFor="field-1" helpTopicId="1234">
+        Example
+      </FormLabel>
+      <Input id="field-1" defaultValue="default value" />
+      <FormHelperText>Helper Text</FormHelperText>
+    </FormControl>
+}`,...(F=(u=i.parameters)==null?void 0:u.docs)==null?void 0:F.source}}};const Wr=["_FormLabel","_States","_Controls"];export{i as _Controls,e as _FormLabel,t as _States,Wr as __namedExportsOrder,Vr as default};
+//# sourceMappingURL=FormLabel.stories-4425d241.js.map
