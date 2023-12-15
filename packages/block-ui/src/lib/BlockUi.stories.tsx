23d24f53900a713d43a069711180dd1c386c620a
Jordan Young <jordan.young@availity.com>
2023-12-15 09:53:18 -0500

    feat(mui-block-ui): add more text and button to story

--- packages/block-ui/src/lib/BlockUi.stories.tsx
+++ packages/block-ui/src/lib/BlockUi.stories.tsx
@@ -1,9 +1,10 @@
 // Each exported component in the package should have its own stories file
 import type { Meta, StoryObj } from '@storybook/react';
 import { Paper } from '@availity/mui-paper';
+import { Button } from '@availity/mui-button';
+import { Typography } from '@availity/mui-typography';
 
 import { BlockUi, BlockUiProps } from './BlockUi';
-import { Typography } from '@availity/mui-typography';
 
 const meta: Meta<typeof BlockUi> = {
   title: 'Components/BlockUi/BlockUi',
@@ -20,9 +21,19 @@ export const _BlockUi: StoryObj<typeof BlockUi> = {
         <Paper sx={{ padding: '1.5rem', minHeight: '250px' }}>
           <Typography variant="h1">Header</Typography>
           <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at sapien efficitur turpis semper consequat.
+            Cras vitae est quis ex vestibulum ultricies. Phasellus turpis massa, commodo eu nisi fringilla, cursus
+            convallis ex. Fusce at hendrerit quam. Praesent vel leo eget ex feugiat eleifend vitae ut nunc. Sed ut
+            lobortis enim. Aliquam non sagittis elit.
           </Typography>
+          <Typography paragraph>
+            Donec elementum urna in nisi ultricies, id venenatis nunc dictum. Phasellus fringilla nibh tellus, eget
+            fermentum magna mollis suscipit. In ac leo vel ipsum luctus molestie id vel sem. Nullam tortor nisl,
+            convallis vel purus vel, efficitur placerat lorem. Donec elementum gravida massa eget volutpat. Donec enim
+            sapien, vestibulum ut erat ac, tincidunt ullamcorper justo. Quisque accumsan facilisis tincidunt. Duis
+            molestie lorem a eros convallis sodales. Aenean malesuada leo eros, sed rutrum enim commodo quis
+          </Typography>
+          <Button>Click</Button>
         </Paper>
       </BlockUi>
     );
