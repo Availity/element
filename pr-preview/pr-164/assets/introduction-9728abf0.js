f245f6e1511476747271bb8c42623ba0fbabab06
jordan-a-young <jordan-a-young@users.noreply.github.com>
2023-12-19 19:07:39 +0000

    Deploy preview for PR 164 🛫

---
+++ pr-preview/pr-164/assets/introduction-9728abf0.js
@@ -0,0 +1,64 @@
+import{a as t,j as e,F as o}from"./jsx-runtime-a3bcee63.js";import{M as r,l as m}from"./index-d4fa0ca4.js";import"./chunk-PCJTTTQV-ed914fd1.js";import{u as s}from"./index-da4379a2.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-12787c64.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-8a077077.js";import"./_baseFor-53bad3ef.js";import"./index-e9d7ebdf.js";import"./_initCloneObject-31384e14.js";import"./index-356e4a49.js";import"./react-18-b9b6ed02.js";const p=`# @availity/mui-backdrop
+
+> Availity MUI Backdrop component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-backdrop.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-backdrop)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-backdrop.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-backdrop)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-backdrop?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-backdrop/package.json)
+
+## Documentation
+
+This package extends the MUI Backdrop component: [MUI Backdrop Docs](https://mui.com/components/backdrop/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-backdrop-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://zeroheight.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+_This package has a few peer dependencies. Add \`@mui/material\` & \`@emotion/react\` to your project if not already installed._
+
+\`\`\`bash
+npm install @availity/mui-backdrop
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-backdrop
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Backdrop } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Backdrop } from '@availity/mui-backdrop';
+\`\`\`
+`;function i(n){return e(o,{children:[t(r,{title:"Components/Backdrop/Introduction"}),`
+`,t(m,{children:p})]})}function x(n={}){const{wrapper:a}=Object.assign({},s(),n.components);return a?t(a,Object.assign({},n,{children:t(i,n)})):i()}export{x as default};
+//# sourceMappingURL=introduction-9728abf0.js.map
