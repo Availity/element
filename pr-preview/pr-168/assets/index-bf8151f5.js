2627596f2e6fa4912cc31129a1bd8b7f179f3828
LauRoxx <LauRoxx@users.noreply.github.com>
2023-12-19 14:02:52 +0000

    Remove preview for PR 168 🛬

--- pr-preview/pr-168/assets/index-bf8151f5.js
+++
@@ -1,2 +0,0 @@
-import{r as p}from"./index-570b25c1.js";import{a as s}from"./jsx-runtime-a3bcee63.js";import{M as t}from"./Typography-f453bb5c.js";var e=p.forwardRef(({children:r,...o},a)=>s(t,{...o,ref:a,children:r}));e.__docgenInfo={description:"",methods:[],displayName:"Typography"};export{e as T};
-//# sourceMappingURL=index-bf8151f5.js.map
