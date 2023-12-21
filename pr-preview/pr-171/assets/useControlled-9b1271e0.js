33b16c4e3757d6cb0df95a221ed13ccd0b115f91
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-12-21 19:22:34 +0000

    Deploy preview for PR 171 🛫

---
+++ pr-preview/pr-171/assets/useControlled-9b1271e0.js
@@ -0,0 +1,2 @@
+import{r as e}from"./index-570b25c1.js";function i({controlled:t,default:s,name:c,state:f="value"}){const{current:a}=e.useRef(t!==void 0),[u,n]=e.useState(s),o=a?t:u,l=e.useCallback(r=>{a||n(r)},[]);return[o,l]}export{i as u};
+//# sourceMappingURL=useControlled-9b1271e0.js.map
