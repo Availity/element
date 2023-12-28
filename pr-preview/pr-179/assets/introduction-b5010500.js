782b974f0a7501a7414d56f69948de475058da8d
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2023-12-28 20:24:48 +0000

    Deploy preview for PR 179 🛫

---
+++ pr-preview/pr-179/assets/introduction-b5010500.js
@@ -0,0 +1,65 @@
+import{a as t,j as a,F as o}from"./jsx-runtime-a3bcee63.js";import{M as m,l as p}from"./index-d3d85803.js";import"./chunk-PCJTTTQV-40e56c37.js";import{u as r}from"./index-da4379a2.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-c8ce2b79.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-8a077077.js";import"./_baseFor-53bad3ef.js";import"./index-e9d7ebdf.js";import"./_initCloneObject-31384e14.js";import"./index-356e4a49.js";import"./react-18-b9b6ed02.js";const s=`# @availity/mui-paper
+
+> Availity MUI Paper component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-paper.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-paper)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-paper.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-paper)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-paper?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-paper/package.json)
+
+- ⚠️ _needs component theme added to @availity/theme_
+- ⚠️ _needs UI Team Verification (must conform to [Availity's Figma Design Standards](https://www.figma.com/file/bf6l79hehcKdRjb5Kzq1De/Element--MUI-%F0%9F%9A%A7?node-id=4662%3A14&t=QMqHeT6daeqXbjBR-1) - private link)_
+
+## Documentation
+
+This package extends the MUI Paper component: [MUI Paper Docs](https://mui.com/components/paper/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-paper-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://zeroheight.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/mui-paper
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-paper
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Paper } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Paper } from '@availity/mui-paper';
+\`\`\`
+`;function i(n){return a(o,{children:[t(m,{title:"Components/Paper/Introduction"}),`
+`,t(p,{children:s})]})}function k(n={}){const{wrapper:e}=Object.assign({},r(),n.components);return e?t(e,Object.assign({},n,{children:t(i,n)})):i()}export{k as default};
+//# sourceMappingURL=introduction-b5010500.js.map
