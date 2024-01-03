2d3090ab67b701a9e23783f3ce9cab65e3758c54
jordan-a-young <jordan-a-young@users.noreply.github.com>
2024-01-03 19:57:39 +0000

    Deploy preview for PR 180 🛫

---
+++ pr-preview/pr-180/assets/index-02876ca7.js
@@ -0,0 +1,2 @@
+import{a as o}from"./jsx-runtime-a3bcee63.js";import{C as a}from"./Chip-e09b065a.js";var e=r=>o(a,{...r,color:"default",size:"medium"});export{e as C};
+//# sourceMappingURL=index-02876ca7.js.map
