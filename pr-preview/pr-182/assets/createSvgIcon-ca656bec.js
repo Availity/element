d5e1d90fb8173167ecbbf1ce2ebe5814c5071686
LauRoxx <LauRoxx@users.noreply.github.com>
2024-01-08 14:16:34 +0000

    Deploy preview for PR 182 🛫

---
+++ pr-preview/pr-182/assets/createSvgIcon-ca656bec.js
@@ -0,0 +1,2 @@
+import{_ as i}from"./createTheme-3fee607a.js";import{r as o}from"./index-570b25c1.js";import{a as c}from"./jsx-runtime-a3bcee63.js";import{S as t}from"./SvgIcon-075b1db6.js";function d(e,m){function r(a,n){return c(t,i({"data-testid":`${m}Icon`,ref:n},a,{children:e}))}return r.muiName=t.muiName,o.memo(o.forwardRef(r))}export{d as c};
+//# sourceMappingURL=createSvgIcon-ca656bec.js.map
