bcdd73bdcc1c56837ac76219d1d8183a644937ba
LauRoxx <LauRoxx@users.noreply.github.com>
2024-01-09 15:09:05 +0000

    Remove preview for PR 182 🛬

--- pr-preview/pr-182/assets/index-bcabbcd5.js
+++
@@ -1,2 +0,0 @@
-import{r as o}from"./index-570b25c1.js";import{a as e}from"./jsx-runtime-a3bcee63.js";import{M as p}from"./Paper-389d56e7.js";var s=o.forwardRef((r,a)=>e(p,{...r,ref:a}));s.__docgenInfo={description:"",methods:[],displayName:"Paper"};export{s as P};
-//# sourceMappingURL=index-bcabbcd5.js.map
