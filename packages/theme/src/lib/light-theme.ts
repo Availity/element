43eae9a4ab1b70d6a3a295bf8e54a3423ef6638a
Jordan Young <jordan.young@availity.com>
2024-01-11 09:21:50 -0500

    fix: update datepicker styles

--- packages/theme/src/lib/light-theme.ts
+++ packages/theme/src/lib/light-theme.ts
@@ -701,15 +701,14 @@ export const lightTheme = {
     },
     MuiDayPicker: {
       styleOverrides: {
-        monthContainer: {
-          padding: '12px 8px 0px',
+        header: {
+          paddingBottom: '12px',
         },
       },
     },
     MuiCalendarPicker: {
       styleOverrides: {
         root: {
-          height: '328px',
           width: '310px',
           '.MuiIconButton-root': {
             border: '0',
