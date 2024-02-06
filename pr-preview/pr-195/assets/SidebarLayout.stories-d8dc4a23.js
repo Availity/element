03361333e227cb8cb6f1584b627abb1b23bec95e
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2024-02-06 20:34:32 +0000

    Deploy preview for PR 195 🛫

---
+++ pr-preview/pr-195/assets/SidebarLayout.stories-d8dc4a23.js
@@ -0,0 +1,39 @@
+import"./index-570b25c1.js";import{M as d,U as s,e as c}from"./index-2fdde1e3.js";import"./index-5efdcb36.js";import"./index-53288845.js";import{a as r,j as m,F as l}from"./jsx-runtime-a3bcee63.js";import"./index-1180c295.js";import"./index-a5050c18.js";import"./index-31fb134e.js";import"./index-fd48023f.js";import"./index-edff813b.js";import"./index-0e9deb8d.js";import"./index-77d4584f.js";import{C as e,G as t}from"./index-6e8561aa.js";import"./index-bf8151f5.js";import"./index-bcabbcd5.js";import"./index-4810b236.js";import{T as h}from"./index-1a1bce77.js";import{H as u,S as x,a as g,T as b,D as S}from"./DataGridSection-5a0c255b.js";import{u as a}from"./index-da4379a2.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-925055a7.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-8a077077.js";import"./_baseFor-53bad3ef.js";import"./index-e9d7ebdf.js";import"./_initCloneObject-31384e14.js";import"./index-356e4a49.js";import"./Button-af0ca45f.js";import"./createTheme-3fee607a.js";import"./clsx-b831246b.js";import"./styled-523cb007.js";import"./useThemeProps-98dc6282.js";import"./generateUtilityClasses-b58f947a.js";import"./ButtonBase-44753119.js";import"./emotion-react.browser.esm-b00e6d55.js";import"./TransitionGroupContext-c71596b3.js";import"./useForkRef-153a0a89.js";import"./useIsFocusVisible-5e7b1ff1.js";import"./useEventCallback-e1b9edb9.js";import"./useEnhancedEffect-460150e6.js";import"./Tooltip-91ad21bd.js";import"./useTheme-c07bb8e7.js";import"./objectWithoutPropertiesLoose-df62547b.js";import"./ownerDocument-613eb639.js";import"./Grow-8a00b99f.js";import"./utils-1ac07bae.js";import"./useSlotProps-95ccb7bd.js";import"./isHostComponent-73d6e646.js";import"./useControlled-9b1271e0.js";import"./useId-6f4bfce0.js";import"./IconButton-3fd19b93.js";import"./LoadingButton-1ec35b70.js";import"./extends-90dea224.js";import"./CircularProgress-96ade162.js";import"./faCircleArrowRight-f69b7367.js";import"./faMagnifyingGlass-482fd726.js";import"./SvgIcon-075b1db6.js";import"./Autocomplete-ca582a28.js";import"./Select-473a43cb.js";import"./react-is.production.min-a192e302.js";import"./Menu-113dab7f.js";import"./Popover-252d82a4.js";import"./Modal-1f45fd5c.js";import"./Backdrop-4a58ec86.js";import"./ownerWindow-03d1c82d.js";import"./createChainedFunction-0bab83cf.js";import"./Paper-389d56e7.js";import"./debounce-517eeb3c.js";import"./List-446c66c9.js";import"./utils-3d35716f.js";import"./createSvgIcon-ca656bec.js";import"./OutlinedInput-71306fdf.js";import"./GlobalStyles-5f9800bb.js";import"./Close-41aa9caa.js";import"./usePreviousProps-9cd3673d.js";import"./Chip-e09b065a.js";import"./index-676fb896.js";import"./Avatar-a58ed318.js";import"./Link-b7684510.js";import"./Typography-f453bb5c.js";import"./extendSxProp-b1abb964.js";import"./Badge-5ea17acd.js";import"./Checkbox-c8372156.js";import"./SwitchBase-1f7cd2d8.js";import"./TablePagination-fd4f0c22.js";import"./TableCell-81c8412e.js";import"./KeyboardArrowRight-94c6c8ce.js";import"./PaginationItem-0a0028eb.js";import"./MenuItem-70f75cad.js";import"./dividerClasses-871b72e6.js";import"./TableRow-edc17b29.js";import"./TableContainer-8cec70ef.js";import"./Pagination-5d0fba7b.js";import"./Box-51bd3ea0.js";import"./TableSortLabel-65a2c334.js";import"./TextField-471fdd1c.js";import"./FormControl-6b396c3c.js";import"./isMuiElement-6907f060.js";import"./FormLabel-3e0f5024.js";import"./FormHelperText-e428b621.js";import"./index.esm-d81a0d8c.js";import"./Grid-62fb460c.js";import"./Container-3c3a93d6.js";import"./styled-5c6c15e1.js";import"./index-9681c8ff.js";import"./FormControlLabel-1ceb9da1.js";import"./Stack-3fcb8940.js";import"./Divider-2d957b7a.js";import"./Grid2-2ceca868.js";import"./index-7c0e1f2b.js";import"./isPlainObject-272efb33.js";import"./ToggleButtonGroup-2e6e00f5.js";import"./ToggleButton-54342354.js";import"./index-3831495e.js";import"./dayjs.min-bf3dc41b.js";import"./LocalizationProvider-c8fffce8.js";import"./index-8dcd6760.js";import"./Breadcrumbs-3266d315.js";import"./InputAdornment-e680d39b.js";import"./index-0ab557ce.js";import"./ListItem-4f8e5ad1.js";import"./index-02876ca7.js";import"./visuallyHidden-14c3de6e.js";import"./index-1fc0ca9a.js";function n(o){const i=Object.assign({h1:"h1",p:"p",em:"em"},a(),o.components);return m(l,{children:[r(d,{title:"Layouts/Sidebar/MDX"}),`
+`,r(i.h1,{id:"sidebar-layout",children:"Sidebar Layout"}),`
+`,r(i.p,{children:r(i.em,{children:"Work In Progress"})}),`
+`,r(s,{children:r(h,{children:r(e,{maxWidth:!1,sx:{bgcolor:"background.canvas",minHeight:"75%",maxHeight:"100%",padding:"1rem"},children:m(e,{fixed:!0,children:[r(u,{}),m(t,{container:!0,spacing:2,children:[r(t,{xs:12,sm:"auto",children:r(x,{})}),m(t,{xs:!0,children:[r("div",{role:"tabpanel",children:m(t,{container:!0,spacing:2,children:[r(t,{xs:12,children:r(g,{})}),r(t,{xs:12,children:r(b,{})}),r(t,{xs:12,children:r(S,{})})]})}),r("div",{role:"tabpanel",hidden:!0,children:r(i.p,{children:"future section"})}),r("div",{role:"tabpanel",hidden:!0,children:r(i.p,{children:"future section"})})]})]})]})})})}),`
+`,r(c,{code:`<ThemeProvider>
+  <Container maxWidth={false} sx={{bgcolor:'background.canvas', padding:'1rem'}}>
+    <Container fixed>
+      <HeaderSection/>
+      <Grid container spacing={2}>
+        <Grid xs={12} sm='auto'>
+          <SideNavSection/>
+        </Grid>
+        <Grid xs>
+          <div role="tabpanel">
+              <Grid container spacing={2}>
+                <Grid xs={12}>
+                  <SearchSection/>
+                </Grid>
+                <Grid xs={12}>
+                  <TablesSection/>
+                </Grid>
+                <Grid xs={12}>
+                  <DataGridSection />
+                </Grid>
+              </Grid>
+          </div>
+          <div role="tabpanel" hidden>
+            future section
+          </div>
+          <div role="tabpanel" hidden>
+            future section
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
+  </Container>
+</ThemeProvider>
+`})]})}function G(o={}){const{wrapper:i}=Object.assign({},a(),o.components);return i?r(i,{...o,children:r(n,{...o})}):n(o)}const f=()=>{throw new Error("Docs-only story")};f.parameters={docsOnly:!0};const p={title:"Layouts/Sidebar/MDX",tags:["stories-mdx"],includeStories:["__page"]};p.parameters=p.parameters||{};p.parameters.docs={...p.parameters.docs||{},page:G};const Ni=["__page"];export{Ni as __namedExportsOrder,f as __page,p as default};
+//# sourceMappingURL=SidebarLayout.stories-d8dc4a23.js.map
