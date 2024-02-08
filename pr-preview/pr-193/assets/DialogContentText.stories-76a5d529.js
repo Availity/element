648ecdf4860e4da8d834d1df1542b2cbc870503e
jordan-a-young <jordan-a-young@users.noreply.github.com>
2024-02-08 21:02:25 +0000

    Remove preview for PR 193 🛬

--- pr-preview/pr-193/assets/DialogContentText.stories-76a5d529.js
+++
@@ -1,7 +0,0 @@
-import{a as s}from"./jsx-runtime-a3bcee63.js";import{D as n}from"./DialogContentText-3cb417da.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./createTheme-3fee607a.js";import"./clsx-b831246b.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./styled-523cb007.js";import"./useThemeProps-98dc6282.js";import"./generateUtilityClasses-b58f947a.js";import"./Typography-f453bb5c.js";import"./extendSxProp-b1abb964.js";const _={title:"Components/Dialog/DialogContentText",component:n,tags:["autodocs"]},o={render:a=>s(n,{...a}),args:{children:"Modal Content Body"}};var t,r,e;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: (args: DialogContentTextProps) => <DialogContentText {...args} />,
-  args: {
-    children: 'Modal Content Body'
-  }
-}`,...(e=(r=o.parameters)==null?void 0:r.docs)==null?void 0:e.source}}};const f=["_DialogContentText"];export{o as _DialogContentText,f as __namedExportsOrder,_ as default};
-//# sourceMappingURL=DialogContentText.stories-76a5d529.js.map
--- pr-preview/pr-193/assets/DialogContext-1d24d37f.js
+++
@@ -1,2 +0,0 @@
-import{r as t}from"./index-570b25c1.js";const o=t.createContext({}),r=o;export{r as D};
-//# sourceMappingURL=DialogContext-1d24d37f.js.map
