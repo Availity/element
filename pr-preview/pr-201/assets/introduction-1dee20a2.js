6ef7628b4ecc06662ee84eedf8ce7ee7dd4def36
jordan-a-young <jordan-a-young@users.noreply.github.com>
2024-02-08 21:57:26 +0000

    Deploy preview for PR 201 🛫

---
+++ pr-preview/pr-201/assets/introduction-1dee20a2.js
@@ -0,0 +1,64 @@
+import{a as e,j as i,F as a}from"./jsx-runtime-a3bcee63.js";import{M as m,l as s}from"./index-6a90fa48.js";import"./chunk-PCJTTTQV-04b8fba7.js";import{u as p}from"./index-da4379a2.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./iframe-929cbae9.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./index-8a077077.js";import"./_baseFor-53bad3ef.js";import"./index-e9d7ebdf.js";import"./_initCloneObject-31384e14.js";import"./index-356e4a49.js";import"./react-18-b9b6ed02.js";const l=`# @availity/mui-autocomplete
+
+> Availity MUI Autocomplete component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-autocomplete.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-autocomplete)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-autocomplete.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-autocomplete)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-autocomplete?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-autocomplete/package.json)
+
+## Documentation
+
+This package extends the MUI Autocomplete component: [MUI Autocomplete Docs](https://mui.com/components/autocomplete/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-autocomplete-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://zeroheight.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+_This package has a few peer dependencies. Add \`@mui/material\` & \`@emotion/react\` to your project if not already installed._
+
+\`\`\`bash
+npm install @availity/mui-autocomplete
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-autocomplete
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Autocomplete } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Autocomplete } from '@availity/mui-autocomplete';
+\`\`\`
+`;function o(t){return i(a,{children:[e(m,{title:"Components/Autocomplete/Introduction"}),`
+`,e(s,{children:l})]})}function I(t={}){const{wrapper:n}=Object.assign({},p(),t.components);return n?e(n,Object.assign({},t,{children:e(o,t)})):o()}export{I as default};
+//# sourceMappingURL=introduction-1dee20a2.js.map
