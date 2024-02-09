094382446fb148d396f287e2dc118e3fad6887a9
L. Roxanne young <Roxanne.Young@availity.com>
2024-02-09 12:20:20 -0500

    feat(mui-icon): add fallback icon

--- packages/icon/src/lib/FaSvgIcon.tsx
+++ packages/icon/src/lib/FaSvgIcon.tsx
@@ -17,8 +17,20 @@ export interface FaSvgIconProps extends SvgIconProps {
   fontSize?: 'xsmall' | 'small' | 'medium' | 'large' | 'inherit';
 }
 
+const NotDefinedIcon: IconDefinition = {
+  prefix: 'fas',
+  iconName: 'notdef',
+  icon: [
+    384,
+    512,
+    [],
+    'e1fe',
+    'M64 390.3L153.5 256 64 121.7V390.3zM102.5 448H281.5L192 313.7 102.5 448zm128-192L320 390.3V121.7L230.5 256zM281.5 64H102.5L192 198.3 281.5 64zM0 48C0 21.5 21.5 0 48 0H336c26.5 0 48 21.5 48 48V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V48z',
+  ],
+};
+
 export const FaSvgIcon = React.forwardRef<SVGSVGElement, FaSvgIconProps>((props, ref) => {
-  const { icon, ...svgProps } = props;
+  const { icon = NotDefinedIcon, ...svgProps } = props;
 
   const {
     icon: [width, height, , , svgPathData],
