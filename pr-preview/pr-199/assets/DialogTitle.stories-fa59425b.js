1a2b13786b4dc27633c3dd7ad6519ab5c6ad1554
LauRoxx <LauRoxx@users.noreply.github.com>
2024-02-16 15:13:44 +0000

    Remove preview for PR 199 🛬

--- pr-preview/pr-199/assets/DialogTitle.stories-fa59425b.js
+++
@@ -1,7 +0,0 @@
-import{a as s}from"./jsx-runtime-a3bcee63.js";import{D as e}from"./DialogTitle-61b661ba.js";import"./index-570b25c1.js";import"./_commonjsHelpers-042e6b4d.js";import"./createTheme-3fee607a.js";import"./clsx-b831246b.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-16f43d9e.js";import"./styled-523cb007.js";import"./useThemeProps-98dc6282.js";import"./dialogTitleClasses-b181f96d.js";import"./generateUtilityClasses-b58f947a.js";import"./DialogContext-1d24d37f.js";import"./Typography-f453bb5c.js";import"./extendSxProp-b1abb964.js";const M={title:"Components/Dialog/DialogTitle",component:e,tags:["autodocs"]},o={render:a=>s(e,{...a}),args:{children:"Modal Title"}};var r,t,i;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  render: (args: DialogTitleProps) => <DialogTitle {...args} />,
-  args: {
-    children: 'Modal Title'
-  }
-}`,...(i=(t=o.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};const j=["_DialogTitle"];export{o as _DialogTitle,j as __namedExportsOrder,M as default};
-//# sourceMappingURL=DialogTitle.stories-fa59425b.js.map
--- pr-preview/pr-199/assets/useTheme-c07bb8e7.js
+++
@@ -1,2 +0,0 @@
-import"./index-570b25c1.js";import{d as m}from"./useThemeProps-98dc6282.js";import{u as t,T as o}from"./createTheme-3fee607a.js";function a(){const e=t(m);return e[o]||e}export{a as u};
-//# sourceMappingURL=useTheme-c07bb8e7.js.map
