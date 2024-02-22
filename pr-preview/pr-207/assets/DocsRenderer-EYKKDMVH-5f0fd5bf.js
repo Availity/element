2b2cd497dc1bdeb1cdbf51d62fdc46b2bb395938
LauRoxx <LauRoxx@users.noreply.github.com>
2024-02-22 13:44:56 +0000

    Remove preview for PR 207 🛬

--- pr-preview/pr-207/assets/DocsRenderer-EYKKDMVH-5f0fd5bf.js
+++
@@ -1,2 +0,0 @@
-import{D as s,d as t}from"./iframe-bb6fa7a8.js";import"../sb-preview/runtime.js";export{s as DocsRenderer,t as defaultComponents};
-//# sourceMappingURL=DocsRenderer-EYKKDMVH-5f0fd5bf.js.map
--- pr-preview/pr-207/assets/syntaxhighlighter-QTQ2UBB4-081db829.js
+++
@@ -1,2 +0,0 @@
-import{S as r,c as e,s as i}from"./iframe-bb6fa7a8.js";import"../sb-preview/runtime.js";export{r as SyntaxHighlighter,e as createCopyToClipboardFunction,i as default};
-//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-081db829.js.map
