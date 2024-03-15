9dd289ceaf08913b11df0fecfa8f907c21348ee4
LauRoxx <LauRoxx@users.noreply.github.com>
2024-03-15 19:39:56 +0000

    Remove preview for PR 211 🛬

--- pr-preview/pr-211/assets/DocsRenderer-EYKKDMVH-6c540727.js
+++
@@ -1,2 +0,0 @@
-import{D as s,d as t}from"./iframe-996e95a8.js";import"../sb-preview/runtime.js";export{s as DocsRenderer,t as defaultComponents};
-//# sourceMappingURL=DocsRenderer-EYKKDMVH-6c540727.js.map
--- pr-preview/pr-211/assets/WithTooltip-FBT32F6Q-3119ec26.js
+++
@@ -1,2 +0,0 @@
-import{W as a,W as p,a as W}from"./iframe-996e95a8.js";import"../sb-preview/runtime.js";export{a as WithToolTipState,p as WithTooltip,W as WithTooltipPure};
-//# sourceMappingURL=WithTooltip-FBT32F6Q-3119ec26.js.map
