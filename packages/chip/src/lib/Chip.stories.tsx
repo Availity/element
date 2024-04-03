f650ff0b1ae98e64290c1e31922956b31bbc3b56
Greg Martin <greg.martin@availity.com>
2024-04-03 13:19:14 -0400

    perf(mui-chip): use path imports for material deps

--- packages/chip/src/lib/Chip.stories.tsx
+++ packages/chip/src/lib/Chip.stories.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Avatar } from '@availity/mui-avatar';
-import { Collapse } from '@mui/material';
+import Collapse from '@mui/material/Collapse';
 import { Chip, ChipProps } from './Chip';
 
 const meta: Meta<typeof Chip> = {
