11f9c9e0e573e4cf81b8e70064066580ef41056f
Greg Martin <greg.martin@availity.com>
2024-04-03 13:53:06 -0400

    perf(mui-page-header): use path imports for material deps

--- packages/page-header/src/lib/PageHeader.tsx
+++ packages/page-header/src/lib/PageHeader.tsx
@@ -1,4 +1,5 @@
-import { Container, Grid } from '@mui/material';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import { Divider } from '@availity/mui-divider';
 import { Typography } from '@availity/mui-typography';
 import { Breadcrumbs, BreadcrumbsProps } from '@availity/mui-breadcrumbs';
