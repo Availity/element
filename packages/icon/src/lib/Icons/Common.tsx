dd0da1faeb3d94e082880ac48f4ffbfd1ddf0260
Greg Martin <greg.martin@availity.com>
2024-04-03 13:44:11 -0400

    perf(mui-icon): use path imports for material deps

--- packages/icon/src/lib/Icons/Common.tsx
+++ packages/icon/src/lib/Icons/Common.tsx
@@ -9,50 +9,35 @@ import { faEllipsis } from '@fortawesome/free-solid-svg-icons/faEllipsis';
 import { faEllipsisV } from '@fortawesome/free-solid-svg-icons/faEllipsisV';
 import { faPrint } from '@fortawesome/free-solid-svg-icons/faPrint';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass';
-import { SvgIconProps } from '@mui/material';
+import { SvgIconProps } from '@mui/material/SvgIcon';
 import { FaSvgIcon } from '../FaSvgIcon';
 
-export const HomeIcon = ({ ...props }: SvgIconProps): JSX.Element => (
-  <FaSvgIcon icon={faHome} {...props}/>
-);
+export const HomeIcon = ({ ...props }: SvgIconProps): JSX.Element => <FaSvgIcon icon={faHome} {...props} />;
 
-export const MailIcon = ({ ...props }: SvgIconProps): JSX.Element => (
-  <FaSvgIcon icon={faEnvelope} {...props}/>
-);
+export const MailIcon = ({ ...props }: SvgIconProps): JSX.Element => <FaSvgIcon icon={faEnvelope} {...props} />;
 
-export const UserIcon = ({ ...props }: SvgIconProps): JSX.Element => (
-  <FaSvgIcon icon={faUser} {...props}/>
-);
+export const UserIcon = ({ ...props }: SvgIconProps): JSX.Element => <FaSvgIcon icon={faUser} {...props} />;
 
-export const UsersIcon = ({ ...props }: SvgIconProps): JSX.Element => (
-  <FaSvgIcon icon={faUsers} {...props}/>
-);
+export const UsersIcon = ({ ...props }: SvgIconProps): JSX.Element => <FaSvgIcon icon={faUsers} {...props} />;
 
 export const OpenInNewIcon = ({ ...props }: SvgIconProps): JSX.Element => (
-  <FaSvgIcon icon={faExternalLink} aria-hidden={false} titleAccess='(opens in new window)' {...props}/>
+  <FaSvgIcon icon={faExternalLink} aria-hidden={false} titleAccess="(opens in new window)" {...props} />
 );
 
-export const SettingsIcon = ({ ...props }: SvgIconProps): JSX.Element => (
-  <FaSvgIcon icon={faCog} {...props}/>
-);
+export const SettingsIcon = ({ ...props }: SvgIconProps): JSX.Element => <FaSvgIcon icon={faCog} {...props} />;
 
 export const MoreHorizontalIcon = ({ ...props }: SvgIconProps): JSX.Element => (
-  <FaSvgIcon icon={faEllipsis} {...props}/>
+  <FaSvgIcon icon={faEllipsis} {...props} />
 );
 
 export const MoreVerticalIcon = ({ ...props }: SvgIconProps): JSX.Element => (
-  <FaSvgIcon icon={faEllipsisV} {...props}/>
+  <FaSvgIcon icon={faEllipsisV} {...props} />
 );
 
-export const MenuIcon = ({ ...props }: SvgIconProps): JSX.Element => (
-  <FaSvgIcon icon={faBars} {...props}/>
-);
+export const MenuIcon = ({ ...props }: SvgIconProps): JSX.Element => <FaSvgIcon icon={faBars} {...props} />;
 
-export const PrintIcon = ({ ...props }: SvgIconProps): JSX.Element => (
-  <FaSvgIcon icon={faPrint} {...props}/>
-);
+export const PrintIcon = ({ ...props }: SvgIconProps): JSX.Element => <FaSvgIcon icon={faPrint} {...props} />;
 
 export const SearchIcon = ({ ...props }: SvgIconProps): JSX.Element => (
-  <FaSvgIcon icon={faMagnifyingGlass} {...props}/>
+  <FaSvgIcon icon={faMagnifyingGlass} {...props} />
 );
-
