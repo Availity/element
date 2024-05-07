376189a8137243c417748625024c857e2f2576a1
Greg Martin <greg.martin@availity.com>
2024-05-07 16:06:56 -0400

    refactor(mui-spaces): remove modal stuff temporarily

--- packages/spaces/src/lib/modals/modal-types.tsx
+++
@@ -1,8 +0,0 @@
-import { Space } from '../spaces-types';
-
-export type DisclaimerModalProps = {
-  parentPayerSpaces: Space[];
-  name: string;
-  state: { selectedOption: Space };
-  setState: (state: object) => void;
-};
