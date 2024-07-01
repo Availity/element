f26ba183b83a6857c1759444ff89203e8b9d8be5
Jordan Young <jordan.young@availity.com>
2024-07-01 10:56:32 -0400

    feat: add providers

--- packages/mock/src/lib/routes.ts
+++ packages/mock/src/lib/routes.ts
@@ -3,6 +3,7 @@ export const AXI_USER_PERMISSIONS = '/api/internal/v1/axi-user-permissions';
 export const LOG = '/api/v1/log-messages';
 export const REGIONS = '/api/sdk/platform/v1/regions';
 export const ORGANIZATIONS = '/api/sdk/platform/v1/organizations';
+export const PROVIDERS = '/api/internal/v1/providers';
 export const USERS = '/api/sdk/platform/v1/users/me';
 
 // Misc
