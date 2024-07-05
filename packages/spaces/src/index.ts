4210726cf475c197f4c6d1d3815d9d14bc474906
Greg Martin <greg.martin@availity.com>
2024-07-05 15:10:58 -0400

    refactor(mui-spaces): temporarily remove SpacesLink reference

--- packages/spaces/src/index.ts
+++ packages/spaces/src/index.ts
@@ -1,9 +1,8 @@
 export * from './lib/Spaces';
-export * from './lib/SpacesLink/SpacesLink';
+// export * from './lib/SpacesLink/SpacesLink';
 export * from './lib/SpacesAgreement';
 export * from './lib/SpacesDisclaimer';
 export * from './lib/SpacesGhostText';
-export * from './lib/SpacesIcon';
 export * from './lib/SpacesImage';
 
 export type { Space } from './lib/spaces-types';
