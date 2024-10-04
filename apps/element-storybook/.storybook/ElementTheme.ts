74a38b7cac710c3f36552ff5c86d8e776446fbb6
Roxanne Young <roxanne.young@availity.net>
2024-10-04 13:46:04 -0400

    docs: add Element logo

--- apps/element-storybook/.storybook/ElementTheme.ts
+++ apps/element-storybook/.storybook/ElementTheme.ts
@@ -1,11 +1,13 @@
 import { create } from '@storybook/theming';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import ElementLogo from '../../../static/availity-Element-Logo.png'
 
 // https://storybook.js.org/docs/7/configure/user-interface/theming#create-a-theme-quickstart
 export default create({
   base: 'light',
   brandTitle: 'Availity Element',
   brandUrl: 'https://zeroheight.com/2e36e50c7/p/15d54a-element-design-system',
-  brandImage: 'https://cdn.zeroheight.com/styleguide_logos/80401-default/4af3be7723f7a3f5bc054865_availity-Element-Logo.png?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9jZG4uemVyb2hlaWdodC5jb20vc3R5bGVndWlkZV9sb2dvcy84MDQwMS1kZWZhdWx0LzRhZjNiZTc3MjNmN2EzZjViYzA1NDg2NV9hdmFpbGl0eS1FbGVtZW50LUxvZ28ucG5nIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNzI4MDQ5NzM1fX19XX0_&Signature=MeoHVzwwWEhY3EqpIO4H90PBIv1dw947xlzITIt2zUlZhSsq4aPBOY86feHjjcwMauzzs0WMMnqELUnVzNeqxxS0fJFX~IMTWSew4OwVUX~Yt4YBoHpYe3L4-BUFkTxlFL39P0JoN2X8OrBvsF3eoRFQK5iEcSOrRgAp9Gel~2~~mc0PiwlYLkQKSf1VSLxHHB9L9t4QiA0qd9zgwP8QGKgzPyAVFD8sWJ4b~btThgcjhGt-jNNPinPP9V9YbafkWKhVTfhBCwrOd0QNWzEA2dDJjR-g46hkgRM8RvhmKKN~7sQwOm1IOvSXqryxGLz37xoNg7jk449zglWY57ylSw__&Key-Pair-Id=KDUNFXWNWK45P',
+  brandImage: ElementLogo,
   brandTarget: '_self',
 
   //
