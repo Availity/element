031f86e7821716cdf93c375f5118dbe02431ad44
Greg Martin <greg.martin@availity.com>
2024-10-25 10:24:52 -0400

    fix(mui-accordion): cleanup Accordion styling to account for new h3 layer

--- packages/accordion/src/lib/Accordion.tsx
+++ packages/accordion/src/lib/Accordion.tsx
@@ -10,14 +10,7 @@ export type AccordionProps = {
    * @default false
    */
   disableNestedStyling?: boolean;
-} & Omit<
-  MuiAccordionProps,
-  'component'
-  | 'elevation'
-  | 'TransitionComponent'
-  | 'TransitionProps'
-  | 'variant'
->;
+} & Omit<MuiAccordionProps, 'component' | 'elevation' | 'TransitionComponent' | 'TransitionProps' | 'variant'>;
 
 const FilledAccordion = styled(MuiAccordion, {
   name: 'MuiAccordion',
@@ -25,9 +18,9 @@ const FilledAccordion = styled(MuiAccordion, {
   overridesResolver: (props, styles) => styles.avFilled,
 })(({ theme }) => ({
   borderColor: theme.palette.grey[100],
-  '> .MuiAccordionSummary-root': {
+  '> .MuiAccordion-heading .MuiAccordionSummary-root': {
     backgroundColor: theme.palette.grey[100],
-  }
+  },
 }));
 
 const OutlinedAccordion = styled(MuiAccordion, {
@@ -36,21 +29,23 @@ const OutlinedAccordion = styled(MuiAccordion, {
   overridesResolver: (props, styles) => styles.avOutlined,
 })(({ theme }) => ({
   borderColor: theme.palette.divider,
-  '> .MuiAccordionSummary-root:not(.Mui-focusVisible, :hover, :active)': {
+  '> .MuiAccordion-heading .MuiAccordionSummary-root:not(.Mui-focusVisible, :hover, :active)': {
     backgroundColor: theme.palette.background.paper,
   },
-  '> .MuiCollapse-root > .MuiCollapse-wrapper > .MuiCollapse-wrapperInner > .MuiAccordion-region > .MuiAccordionDetails-root:first-of-type': {
-    borderTop: `1px solid ${theme.palette.divider}`,
-  }
+  '> .MuiCollapse-root > .MuiCollapse-wrapper > .MuiCollapse-wrapperInner > .MuiAccordion-region > .MuiAccordionDetails-root:first-of-type':
+    {
+      borderTop: `1px solid ${theme.palette.divider}`,
+    },
 }));
 
 export const Accordion = forwardRef<HTMLDivElement, AccordionProps>((allProps, ref) => {
-  const { variant = "filled", disableNestedStyling = false, className, ...props } = allProps;
+  const { variant = 'filled', disableNestedStyling = false, className, ...props } = allProps;
 
-  const classnames = `${variant === "filled" ? "MuiAccordion-avFilled" : "MuiAccordion-avOutlined"}${disableNestedStyling ? " Av-disableNested" : ""} ${className || ''}`
+  const classnames = `${variant === 'filled' ? 'MuiAccordion-avFilled' : 'MuiAccordion-avOutlined'}${disableNestedStyling ? ' Av-disableNested' : ''} ${className || ''}`;
 
-  return variant === "filled" ?
-    <FilledAccordion className={classnames} {...props} ref={ref}/>
-    :
-    <OutlinedAccordion className={classnames} {...props} ref={ref}/>
+  return variant === 'filled' ? (
+    <FilledAccordion className={classnames} {...props} ref={ref} />
+  ) : (
+    <OutlinedAccordion className={classnames} {...props} ref={ref} />
+  );
 });
