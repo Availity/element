3bae15fe1f766af4d25e6df049d26385ef86ec28
Greg Martin <greg.martin@availity.com>
2024-10-29 14:17:00 -0400

    docs(mui-alert): cleanup AlertTitle props in storybook

--- packages/alert/src/lib/AlertTitle.stories.tsx
+++ packages/alert/src/lib/AlertTitle.stories.tsx
@@ -3,27 +3,55 @@ import type { StoryObj } from '@storybook/react';
 import Collapse from '@mui/material/Collapse';
 import { Alert } from './Alert';
 import { AlertTitle } from './AlertTitle';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { SystemPropsList } from '../../../../data/MuiSystemProperties';
+
+// The AlertTitle type pulled in all the Typography props as well
+const excludedProps = [
+  ...SystemPropsList,
+  'align',
+  'className',
+  'component',
+  'gutterBottom',
+  'noWrap',
+  'paragraph',
+  'style',
+  'variant',
+  'variantMapping',
+];
 
 export default {
   title: 'Components/Alert/AlertTitle',
   component: AlertTitle,
+  parameters: {
+    docs: {
+      controls: {
+        exclude: excludedProps,
+      },
+    },
+    canvas: {
+      controls: {
+        exclude: excludedProps,
+      },
+    },
+  },
   tags: ['autodocs'],
   args: {
     children: 'This text is inside the AlertTitle',
-  }
+  },
 };
 
 export const _AlertTitle: StoryObj<typeof AlertTitle> = {
-  render: (args ) => (
+  render: (args) => (
     <Alert severity="info">
-      <AlertTitle {...args}/>
+      <AlertTitle {...args} />
       This text is inside the Alert
     </Alert>
-  )
+  ),
 };
 
 export const _InDismissableAlert: StoryObj<typeof AlertTitle> = {
-  render: (args ) =>  {
+  render: (args) => {
     const [visible, setVisible] = useState(true);
 
     const onClose = () => {
@@ -34,10 +62,10 @@ export const _InDismissableAlert: StoryObj<typeof AlertTitle> = {
     return (
       <Collapse in={visible}>
         <Alert severity="info" onClose={onClose}>
-          <AlertTitle {...args}/>
+          <AlertTitle {...args} />
           This text is inside the Alert
         </Alert>
       </Collapse>
     );
-  }
+  },
 };
