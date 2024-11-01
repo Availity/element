f070ef8c3473d3b851651dd878f2bfd183f6d3bd
Greg Martin <greg.martin@availity.com>
2024-11-01 17:38:20 -0400

    test(mui-page-header): add logo test

--- packages/page-header/src/lib/PageHeader.test.tsx
+++ packages/page-header/src/lib/PageHeader.test.tsx
@@ -1,7 +1,29 @@
 import { render, waitForElementToBeRemoved } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Spaces } from '@availity/mui-spaces';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { server } from '@availity/mock/src/lib/server';
 import { PageHeader } from './PageHeader';
 
+const mockImage = jest.fn();
+
+jest.mock('react-image', () => ({
+  ...(jest.requireActual('react-image') as any),
+  Img: (props: any) => mockImage(props),
+}));
+
 describe('PageHeader', () => {
+  beforeAll(() => {
+    // Start the interception.
+    server.listen();
+  });
+
+  afterEach(() => {
+    // Remove any handlers you may have added
+    // in individual tests (runtime handlers).
+    server.resetHandlers();
+    jest.restoreAllMocks();
+  });
   test('should render simple page header successfully', () => {
     const { getByText } = render(<PageHeader breadcrumbs={{ active: 'This page' }} headerText="This is the header" />);
     expect(getByText('This is the header')).toBeTruthy();
@@ -32,5 +54,21 @@ describe('PageHeader', () => {
     expect(getByText('This is the header')).toBeTruthy();
     expect(getByText('This page')).toBeTruthy();
     expect(getByText('Button 1')).toBeTruthy();
+  });
+
+  test('should render page header with a logo successfully', async () => {
+    const queryClient = new QueryClient();
+    const { getByText, getByRole } = render(
+      <QueryClientProvider client={queryClient}>
+        <Spaces spaceIds={['11', '22', '33']}>
+          <PageHeader breadcrumbs={{ active: 'This page' }} headerText="This is the header" logo={{ spaceId: '11' }} />
+        </Spaces>
+      </QueryClientProvider>
+    );
+    expect(getByText('This is the header')).toBeTruthy();
+    expect(getByText('This page')).toBeTruthy();
+    await waitForElementToBeRemoved(getByRole('progressbar'));
+    // Ensure the correct src was selected.
+    expect(mockImage.mock.calls[0][0].src).toEqual('/spaces/test_logo.png');
   });
 });
