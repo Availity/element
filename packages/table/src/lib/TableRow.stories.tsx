72fb7f061a0fd1c8e128040c9666497e3eae5a62
Roxanne Young <Roxanne.Young@availity.com>
2024-11-21 09:57:05 -0500

    docs: add expandable table row examples

--- packages/table/src/lib/TableRow.stories.tsx
+++ packages/table/src/lib/TableRow.stories.tsx
@@ -3,6 +3,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { TableRow, TableRowProps } from './TableRow';
 import { TableBody, TableCell } from '..';
+import { useState } from 'react';
+import { IconButton } from '@availity/mui-button';
+import { CollapseIcon, ExpandIcon } from '@availity/mui-icon';
+import { Box, Grid } from '@availity/mui-layout';
+import { Collapse } from '@availity/mui-transitions';
+import { Typography } from '@availity/mui-typography';
 
 const meta: Meta<typeof TableRow> = {
   title: 'Components/Table/TableRow',
@@ -25,4 +31,88 @@ export const _TableRow: StoryObj<typeof TableRow> = {
       </TableRow>
     </TableBody>
   ),
+};
+
+export const _CollapsibleRow: StoryObj<typeof TableRow> = {
+  render: (args: TableRowProps) => {
+    const [open, setOpen] = useState(false);
+
+    return (
+    <TableBody>
+      <TableRow {...args}>
+        <TableCell padding="checkbox">
+          <IconButton
+            title="expand row"
+            size="medium"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <ExpandIcon /> : <CollapseIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell>Cell 1</TableCell>
+        <TableCell>Cell 2</TableCell>
+        <TableCell>Cell 3</TableCell>
+      </TableRow>
+      <TableRow {...args}>
+        <TableCell style={{ padding: 0, paddingLeft: "32px" }} colSpan={12} >
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ padding: 2 }}>
+              <Grid container spacing={2} wrap="wrap">
+                <Grid xs>
+                  <Typography variant="body2" sx={{fontWeight: "bold"}}>
+                    Extra Data 1
+                  </Typography>
+                  <Typography variant="body2">
+                    Lorem Ipsum
+                  </Typography>
+                </Grid>
+                <Grid xs>
+                  <Typography variant="body2" sx={{fontWeight: "bold"}}>
+                    Extra Data 2
+                  </Typography>
+                  <Typography variant="body2">
+                    Lorem Ipsum
+                  </Typography>
+                </Grid>
+                <Grid xs>
+                  <Typography variant="body2" sx={{fontWeight: "bold"}}>
+                    Extra Data 3
+                  </Typography>
+                  <Typography variant="body2">
+                    Lorem Ipsum
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid container spacing={2} wrap="wrap">
+                <Grid xs>
+                  <Typography variant="body2" sx={{fontWeight: "bold"}}>
+                    Extra Data 4
+                  </Typography>
+                  <Typography variant="body2">
+                    Lorem Ipsum
+                  </Typography>
+                </Grid>
+                <Grid xs>
+                  <Typography variant="body2" sx={{fontWeight: "bold"}}>
+                    Extra Data 5
+                  </Typography>
+                  <Typography variant="body2">
+                    Lorem Ipsum
+                  </Typography>
+                </Grid>
+                <Grid xs>
+                  <Typography variant="body2" sx={{fontWeight: "bold"}}>
+                    Extra Data 6
+                  </Typography>
+                  <Typography variant="body2">
+                    Lorem Ipsum
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </TableBody>
+  )},
 };
