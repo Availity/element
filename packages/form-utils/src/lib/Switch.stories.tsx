8fd01176cebcaae12f9576fc0989c9ce26cfcaa8
Greg Martin <greg.martin@availity.com>
2024-12-19 15:38:27 -0500

    docs(mui-form-utils): cleanup switch stories

--- packages/form-utils/src/lib/Switch.stories.tsx
+++ packages/form-utils/src/lib/Switch.stories.tsx
@@ -1,7 +1,8 @@
 // Each exported component in the package should have its own stories file
 
+import { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { FormControlLabel, Switch, SwitchProps } from '..';
+import { FormControlLabel, Switch, SwitchProps, FormGroup, FormLabel } from '..';
 import { Typography } from '@availity/mui-typography';
 
 const meta: Meta<typeof Switch> = {
@@ -30,39 +31,100 @@ const meta: Meta<typeof Switch> = {
 export default meta;
 
 export const _Switch: StoryObj<typeof Switch> = {
-  render: (args: SwitchProps) => <Switch inputProps={{ 'aria-label': 'example' }} {...args} />,
-};
-
-export const _WithLabel: StoryObj<typeof Switch> = {
-  render: () => <FormControlLabel control={<Switch sx={{ marginRight: '6px' }} />} label="Form Control Label" />,
+  render: (args: SwitchProps) => {
+    const [checked, setChecked] = useState(false);
+    return (
+      <FormGroup>
+        <FormLabel htmlFor="switch1">Label</FormLabel>
+        <FormControlLabel
+          control={<Switch {...args} onChange={(event) => setChecked(event.target.checked)} />}
+          label={checked ? 'On' : 'Off'}
+        />
+      </FormGroup>
+    );
+  },
+  args: {
+    id: 'switch1',
+    sx: { marginRight: '6px' },
+    inputProps: {
+      'aria-label': 'example',
+    },
+  },
 };
 
 export const _WithCheckedIcon: StoryObj<typeof Switch> = {
-  render: () => (
-    <>
-      <FormControlLabel
-        control={<Switch showCheckedIcon size="small" sx={{ marginRight: '6px' }} defaultChecked />}
-        label="Form Control Label"
-      />
-      <Typography variant="caption" children="The checked icon only applies to small Switches" />
-    </>
-  ),
+  render: () => {
+    const [checked, setChecked] = useState(true);
+    return (
+      <FormGroup>
+        <FormLabel htmlFor="switch2">Label</FormLabel>
+        <FormControlLabel
+          control={
+            <Switch
+              id="switch2"
+              showCheckedIcon
+              size="small"
+              sx={{ marginRight: '6px' }}
+              onChange={(event) => setChecked(event.target.checked)}
+              defaultChecked
+            />
+          }
+          label={checked ? 'On' : 'Off'}
+        />
+        <Typography variant="caption" children="The checked icon only applies to small Switches" />
+      </FormGroup>
+    );
+  },
 };
 
 export const _Sizes: StoryObj<typeof Switch> = {
-  render: () => (
-    <>
-      <FormControlLabel control={<Switch size="small" sx={{ marginRight: '6px' }} />} label="small" />
-      <FormControlLabel control={<Switch size="medium" sx={{ marginRight: '6px' }} />} label="medium" />
-    </>
-  ),
+  render: () => {
+    const [smallChecked, setSmallChecked] = useState(false);
+    const [defaultChecked, setDefaultChecked] = useState(false);
+    return (
+      <>
+        <FormGroup>
+          <FormLabel htmlFor="smallSwitch">Small Switch</FormLabel>
+          <FormControlLabel
+            control={
+              <Switch
+                id="smallSwitch"
+                size="small"
+                sx={{ marginRight: '6px' }}
+                onChange={(event) => setSmallChecked(event.target.checked)}
+              />
+            }
+            label={smallChecked ? 'On' : 'Off'}
+          />
+        </FormGroup>
+        <FormGroup>
+          <FormLabel htmlFor="defaultSwitch">Default Switch</FormLabel>
+          <FormControlLabel
+            control={
+              <Switch
+                id="defaultSwitch"
+                size="medium"
+                sx={{ marginRight: '6px' }}
+                onChange={(event) => setDefaultChecked(event.target.checked)}
+              />
+            }
+            label={defaultChecked ? 'On' : 'Off'}
+          />
+        </FormGroup>
+      </>
+    );
+  },
 };
 
 export const _States: StoryObj<typeof Switch> = {
   render: () => (
     <>
-      <FormControlLabel control={<Switch sx={{ marginRight: '6px' }} />} label="unchecked" />
-      <FormControlLabel control={<Switch defaultChecked={true} sx={{ marginRight: '6px' }} />} label="checked" />
+      <FormControlLabel control={<Switch sx={{ marginRight: '6px' }} />} label="unchecked" checked={false} />
+      <FormControlLabel
+        control={<Switch defaultChecked={true} sx={{ marginRight: '6px' }} />}
+        label="checked"
+        checked
+      />
       <FormControlLabel control={<Switch sx={{ marginRight: '6px' }} />} label="disabled unchecked" disabled />
       <FormControlLabel
         control={<Switch defaultChecked={true} sx={{ marginRight: '6px' }} />}
