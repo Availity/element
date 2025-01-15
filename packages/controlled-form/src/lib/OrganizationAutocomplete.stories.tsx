75adf503bca1397652de427a32e9b0e13d62e92f
Greg Martin <greg.martin@availity.com>
2025-01-15 16:50:40 -0500

    refactor(mui-controlled-form): change register to Controller in OrganizationAutocomplete

---
+++ packages/controlled-form/src/lib/OrganizationAutocomplete.stories.tsx
@@ -0,0 +1,76 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ControlledOrganizationAutocomplete } from './OrganizationAutocomplete';
+import { ControlledForm } from './ControlledForm';
+import { Button } from '@availity/mui-button';
+import { useFormContext } from 'react-hook-form';
+import { Paper } from '@availity/mui-paper';
+import { Typography } from '@availity/mui-typography';
+import { Grid } from '@availity/mui-layout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { missingRHFprops } from '../../docs/propDefinitions';
+
+const meta: Meta<typeof ControlledOrganizationAutocomplete> = {
+  title: 'Components/ControlledForm/Autocomplete/ControlledOrganizationAutocomplete',
+  component: ControlledOrganizationAutocomplete,
+  tags: ['autodocs'],
+  argTypes: missingRHFprops,
+};
+
+export default meta;
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export const _ControlledOrganizationAutoComplete: StoryObj<typeof ControlledOrganizationAutocomplete> = {
+  render: (args) => {
+    const SubmittedValues = () => {
+      const {
+        getValues,
+        formState: { isSubmitSuccessful },
+      } = useFormContext();
+
+      return isSubmitSuccessful ? (
+        <Paper sx={{ padding: '1.5rem', marginTop: '1.5rem' }}>
+          <Typography variant="h2">Submitted Values</Typography>
+          <pre>{JSON.stringify(getValues(), null, 2)}</pre>
+        </Paper>
+      ) : null;
+    };
+
+    const Actions = () => {
+      const {
+        reset,
+        formState: { isSubmitSuccessful },
+      } = useFormContext();
+      return (
+        <Grid container direction="row" justifyContent="space-between">
+          <Button disabled={!isSubmitSuccessful} children="Reset" color="secondary" onClick={() => reset()} />
+          <Button type="submit" disabled={isSubmitSuccessful} children="Submit" />
+        </Grid>
+      );
+    };
+    return (
+      <QueryClientProvider client={client}>
+        <ControlledForm values={{}} onSubmit={(data) => data}>
+          <ControlledOrganizationAutocomplete {...args} />
+          <Actions />
+          <SubmittedValues />
+        </ControlledForm>
+      </QueryClientProvider>
+    );
+  },
+  args: {
+    name: 'controlledOrganizationAutocomplete',
+    FieldProps: {
+      label: 'Organization Select',
+      helperText: 'Select an Organization from the list',
+      placeholder: 'Select...',
+      fullWidth: false,
+    },
+  },
+};
--- packages/controlled-form/src/lib/OrganizationAutocomplete.tsx
+++ packages/controlled-form/src/lib/OrganizationAutocomplete.tsx
@@ -1,17 +1,62 @@
 import { OrganizationAutocomplete, OrgAutocompleteProps } from '@availity/mui-autocomplete';
-import { useFormContext, RegisterOptions, FieldValues } from 'react-hook-form';
+import { useFormContext, Controller, RegisterOptions, FieldValues, ControllerProps } from 'react-hook-form';
 
-type ControlledOrgAutocompleteProps = OrgAutocompleteProps & {
-  name: string;
-  registerOptions?: RegisterOptions<FieldValues, string>;
-};
+type ControlledOrgAutocompleteProps = Omit<OrgAutocompleteProps, 'name'> &
+  Omit<RegisterOptions<FieldValues, string>, 'disabled' | 'valueAsNumber' | 'valueAsDate' | 'setValueAs'> &
+  Pick<ControllerProps, 'defaultValue' | 'shouldUnregister' | 'name'>;
 
 export const ControlledOrganizationAutocomplete = ({
   name,
-  registerOptions,
+  defaultValue,
+  deps,
+  max,
+  maxLength,
+  min,
+  minLength,
+  onBlur,
+  onChange,
+  pattern,
+  required,
+  shouldUnregister,
+  validate,
+  value,
   ...rest
 }: ControlledOrgAutocompleteProps) => {
-  const { register } = useFormContext();
+  const { control } = useFormContext();
 
-  return <OrganizationAutocomplete {...rest} {...register(name, registerOptions)} />;
+  return (
+    <Controller
+      name={name}
+      control={control}
+      defaultValue={defaultValue}
+      rules={{
+        deps,
+        max,
+        maxLength,
+        min,
+        minLength,
+        onBlur,
+        onChange,
+        pattern,
+        required,
+        shouldUnregister,
+        validate,
+        value,
+      }}
+      shouldUnregister={shouldUnregister}
+      render={({ field: { onChange, value, onBlur } }) => (
+        <OrganizationAutocomplete
+          {...rest}
+          onChange={(event, value, reason) => {
+            if (reason === 'clear') {
+              onChange(null);
+            }
+            onChange(value);
+          }}
+          onBlur={onBlur}
+          value={value || null}
+        />
+      )}
+    />
+  );
 };
