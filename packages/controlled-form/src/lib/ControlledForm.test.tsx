16c16b98743f8bae504a02709936cc252f4a82e1
Greg Martin <greg.martin@availity.com>
2025-01-21 16:25:26 -0500

    feat(mui-controlled-form): add yup resolver support

--- packages/controlled-form/src/lib/ControlledForm.test.tsx
+++ packages/controlled-form/src/lib/ControlledForm.test.tsx
@@ -1,13 +1,67 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import * as yup from 'yup';
 import { ControlledForm } from './ControlledForm';
+import { ControlledTextField } from './TextField';
+import { useFormContext } from 'react-hook-form';
+import { Paper } from '@availity/mui-paper';
+import { Typography } from '@availity/mui-typography';
+import { Grid } from '@availity/mui-layout';
+import { Button } from '@availity/mui-button';
+
+const SubmittedValues = () => {
+  const {
+    getValues,
+    formState: { isSubmitSuccessful },
+  } = useFormContext();
+
+  return isSubmitSuccessful ? (
+    <Paper sx={{ padding: '1.5rem', marginTop: '1.5rem' }}>
+      <Typography variant="h2">Submitted Values</Typography>
+      <pre data-testid="result">{JSON.stringify(getValues(), null, 2)}</pre>
+    </Paper>
+  ) : null;
+};
+
+const Actions = () => {
+  const {
+    formState: { isSubmitSuccessful },
+  } = useFormContext();
+  return (
+    <Grid container direction="row" justifyContent="space-between">
+      <Button type="submit" disabled={isSubmitSuccessful} children="Submit" />
+    </Grid>
+  );
+};
+
+const onSubmit = jest.fn();
 
 describe('ControlledForm', () => {
-  test('should render successfully', () => {
-    const { getByText } = render(
-      <ControlledForm onSubmit={(data) => data} values={{}}>
-        Test
+  test('should render successfully', async () => {
+    const screen = render(
+      <ControlledForm
+        values={{ controlledTextField: undefined }}
+        onSubmit={onSubmit}
+        schema={yup.object({ controlledTextField: yup.string().max(10) })}
+      >
+        <ControlledTextField
+          name="controlledTextField"
+          placeholder="Name"
+          inputProps={{
+            'data-testid': 'testTextField',
+          }}
+        />
+        <Actions />
+        <SubmittedValues />
       </ControlledForm>
     );
-    expect(getByText('Test')).toBeTruthy();
+
+    const input = screen.getByTestId('testTextField');
+
+    fireEvent.change(input, { target: { value: 'This is too much text' } });
+
+    fireEvent.click(screen.getByText('Submit'));
+
+    await waitFor(() => expect(onSubmit).toHaveBeenCalledTimes(0));
+    await waitFor(() => expect(screen.findByText('controlledTextField must be at most 10 characters')).toBeDefined());
   });
 });
--- packages/controlled-form/src/lib/ControlledForm.tsx
+++ packages/controlled-form/src/lib/ControlledForm.tsx
@@ -1,13 +1,25 @@
 import { FormHTMLAttributes } from 'react';
-import { useForm, SubmitHandler, FormProvider } from 'react-hook-form';
+import { useForm, SubmitHandler, FormProvider, Resolver } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 export type ControlledFormProps = {
   onSubmit: SubmitHandler<any>;
   values: Record<string, any>;
+  schema?: yup.ObjectSchema<any>;
 } & FormHTMLAttributes<HTMLFormElement>;
 
-export const ControlledForm = ({ onSubmit, values, ...rest }: ControlledFormProps) => {
-  const methods = useForm({ values });
+type UseFormOptions = {
+  values: Record<string, any>;
+  resolver?: Resolver;
+};
+
+export const ControlledForm = ({ onSubmit, values, schema, ...rest }: ControlledFormProps) => {
+  const useFormOptions: UseFormOptions = { values };
+  if (schema) {
+    useFormOptions.resolver = yupResolver(schema);
+  }
+  const methods = useForm(useFormOptions);
 
   return (
     <FormProvider {...methods}>
