39ab1a4c63fd1c5e18101f4c8fbb1e31d8d38b09
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-02-13 22:03:01 +0000

    Deploy preview for PR 561 🛫

---
+++ pr-preview/pr-561/assets/ToggleButton.stories-D3JinAnO.js
@@ -0,0 +1,25 @@
+import{j as o}from"./jsx-runtime-CcL-W3nW.js";import{m as T}from"./index-C_7pZhMU.js";import{T as a}from"./ToggleButton-yXo7Isdq.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./faUser-CAcveCor.js";import"./SvgIcon-DSdWmKAH.js";import"./generateUtilityClass-CVWqFxsA.js";import"./identifier-Dtqimryx.js";import"./memoTheme-D-rVhYRp.js";import"./styled-CF1hcdwO.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./ToggleButton-FLUHnJ1i.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./ButtonBase-DbVRbsLs.js";import"./useTimeout-B8wmcRZV.js";import"./useEnhancedEffect-D2f2tbda.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";import"./TransitionGroupContext-D-IBNIfW.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./useForkRef-BVrIj09m.js";import"./isFocusVisible-B8k4qzLc.js";const A={title:"Components/ToggleButton/ToggleButton",component:a,tags:["autodocs"],argTypes:{size:{options:["small","medium","large"]}}},r={render:e=>o.jsx(a,{...e}),args:{value:"test",children:"This text is a child of ToggleButton"}},t={render:e=>o.jsx(a,{...e}),args:{value:"1",children:o.jsx(T,{}),"aria-label":"Home"}},s={render:e=>o.jsx(a,{...e}),args:{value:"1",children:o.jsxs(o.Fragment,{children:[o.jsx(T,{fontSize:"small",sx:{paddingRight:".5rem"}}),"Home"]})}};var n,m,g;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: (args: ToggleButtonProps) => <ToggleButton {...args} />,
+  args: {
+    value: 'test',
+    children: 'This text is a child of ToggleButton'
+  }
+}`,...(g=(m=r.parameters)==null?void 0:m.docs)==null?void 0:g.source}}};var i,l,p;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: (args: ToggleButtonProps) => <ToggleButton {...args} />,
+  args: {
+    value: '1',
+    children: <HomeIcon />,
+    'aria-label': 'Home'
+  }
+}`,...(p=(l=t.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var c,u,d;s.parameters={...s.parameters,docs:{...(c=s.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: (args: ToggleButtonProps) => <ToggleButton {...args} />,
+  args: {
+    value: '1',
+    children: <>
+        <HomeIcon fontSize="small" sx={{
+        paddingRight: '.5rem'
+      }} />
+        Home
+      </>
+  }
+}`,...(d=(u=s.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};const D=["_ToggleButton","_ToggleButtonIcon","_ToggleButtonIconLabel"];export{r as _ToggleButton,t as _ToggleButtonIcon,s as _ToggleButtonIconLabel,D as __namedExportsOrder,A as default};
