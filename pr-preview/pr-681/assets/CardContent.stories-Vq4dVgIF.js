e17a3c0bc63d9dfe7f13e179427c9eb89b74daf4
LauRoxx <LauRoxx@users.noreply.github.com>
2025-02-21 19:54:35 +0000

    Remove preview for PR 681 🛬

--- pr-preview/pr-681/assets/CardContent.stories-Vq4dVgIF.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./jsx-runtime-CcL-W3nW.js";import{C as a}from"./CardContent-DxJHwFcl.js";import{C as i}from"./Card-CVw4CHCy.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./generateUtilityClass-Bqc1qv72.js";import"./createTheme-CKCRSfxM.js";import"./generateUtilityClasses-DyVAdLaw.js";import"./DefaultPropsProvider-CIAoFptl.js";import"./styled-Dbr85gFc.js";import"./Card-WzviUhG6.js";import"./Paper-CvfFJp8a.js";import"./useTheme-ChS1Y5Lw.js";import"./useTheme-BUEqHFQV.js";import"./memoTheme-DvanvhnP.js";const O={title:"Components/Card/CardContent",component:a,tags:["autodocs"]},r={render:s=>t.jsx(i,{children:t.jsx(a,{...s})}),args:{children:"This text is a child of CardContent"}};var o,n,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: (args: CardContentProps) => <Card>
-      <CardContent {...args} />
-    </Card>,
-  args: {
-    children: 'This text is a child of CardContent'
-  }
-}`,...(e=(n=r.parameters)==null?void 0:n.docs)==null?void 0:e.source}}};const P=["_CardContent"];export{r as _CardContent,P as __namedExportsOrder,O as default};
