25278751b70ac0a54c768b4448e4cae56f4a5892
LauRoxx <LauRoxx@users.noreply.github.com>
2025-02-25 02:41:16 +0000

    Deploy preview for PR 561 🛫

---
+++ pr-preview/pr-561/assets/FormLabel.stories-BWEJ-fp6.js
@@ -0,0 +1,27 @@
+import{j as r}from"./jsx-runtime-CcL-W3nW.js";import"./FieldHelpIcon-DCwf6RI6.js";import{F as o}from"./FormLabel-DZW0f75R.js";import{F as h}from"./Switch-YubXNe9S.js";import{I as x}from"./Input-BS67x0-8.js";import"./index-BtPDELIe.js";import{F}from"./FormControl-BPSU2g7t.js";import{O as f}from"./OutlinedInput-CQlxW3ax.js";import{v as L}from"./visuallyHidden-Dan1xhjv.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DMWJB0MK.js";import"./index-0eQTE8wr.js";import"./faCircleArrowRight-fEvkKBkH.js";import"./faUser-DXPB7Q64.js";import"./faSortDown-nq-jkck6.js";import"./SvgIcon-DSdWmKAH.js";import"./generateUtilityClass-CVWqFxsA.js";import"./identifier-Dtqimryx.js";import"./memoTheme-D-rVhYRp.js";import"./styled-CF1hcdwO.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./index-DLsH_p52.js";import"./Tooltip-CiiaKkqp.js";import"./index-nos-0K8U.js";import"./useTheme-DUkCvbx_.js";import"./useTheme-C2CrmYKc.js";import"./useSlot-DnkSxE3X.js";import"./resolveComponentProps-Dg20wxiM.js";import"./useForkRef-BVrIj09m.js";import"./useTimeout-B8wmcRZV.js";import"./useEnhancedEffect-D2f2tbda.js";import"./useControlled-WdbyInBW.js";import"./useId-DszduLV4.js";import"./getReactElementRef-AnOffBoZ.js";import"./Portal-CBAx_nZm.js";import"./utils-D5ZkK9XY.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";import"./TransitionGroupContext-D-IBNIfW.js";import"./index-BEx9gAkE.js";import"./ownerDocument-DW-IO8s5.js";import"./useSlotProps-BAIpVL6Y.js";import"./isFocusVisible-B8k4qzLc.js";import"./IconButton-Caxu-qgA.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./ButtonBase-DbVRbsLs.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./CircularProgress-D8w9ZPs6.js";import"./Button-BAEbeAyH.js";import"./LoadingButton-9DxABi24.js";import"./FormControlLabel-GqYWwHWT.js";import"./formControlState-Dq1zat_P.js";import"./useFormControl-68oiojFJ.js";import"./Typography-CvSvtD2a.js";import"./index-f0Aa_BgU.js";import"./extendSxProp-B5AgUjQT.js";import"./Box-DWnb8VZB.js";import"./FormLabel-98YPf6ct.js";import"./RadioGroup-BogPRRDq.js";import"./SwitchBase-Z9YgLJlx.js";import"./createSvgIcon-TketyoFF.js";import"./createChainedFunction-BO_9K8Jh.js";import"./Select-hLZ2S4VH.js";import"./Menu-Byu17A6t.js";import"./Popover-ChEuM7rO.js";import"./isHostComponent-DVu5iVWx.js";import"./ownerWindow-HkKU3E4x.js";import"./debounce-Be36O1Ab.js";import"./Paper-DXcIEVia.js";import"./Modal-B5s0Thvt.js";import"./Backdrop-B4gbyDGC.js";import"./Fade-Dl4gmST4.js";import"./List-ChBzRQcW.js";import"./utils-DoM3o7-Q.js";import"./Stack-BcsM4Vc0.js";import"./styled-vBjI3IQc.js";import"./useThemeProps-BIK-1DKm.js";import"./Divider-jUONNV4j.js";import"./dividerClasses-BmOcEkDu.js";import"./faCheck-B05F4ORy.js";import"./Switch-BqVUWLaW.js";import"./isMuiElement-DAzOCCHH.js";const Qr={title:"Form Components/FormUtils/FormLabel",component:o,tags:["autodocs"],parameters:{docs:{description:{component:"Label component for use with individual inputs, form controls,  and form groups. For labels to use with `checkbox`/`radio` see `FormControlLabel`."}}}},e={render:b=>r.jsx(o,{...b}),args:{children:"This text is a child of FormLabel",required:!0}},t={render:()=>r.jsxs(r.Fragment,{children:[r.jsx(o,{children:"Default"}),r.jsx(o,{helpTopicId:"1234",children:"With Field Help Icon"}),r.jsx(o,{required:!0,children:"Required"}),r.jsx(o,{error:!0,children:"Error"}),r.jsxs(F,{disabled:!0,size:"small",children:[r.jsx(o,{htmlFor:"disabled",children:"Disabled"}),r.jsx(f,{id:"disabled",sx:L})]})]})},i={render:()=>r.jsxs(F,{required:!0,size:"small",children:[r.jsx(o,{htmlFor:"field-1",helpTopicId:"1234",children:"Example"}),r.jsx(x,{id:"field-1",defaultValue:"default value"}),r.jsx(h,{children:"Helper Text"})]})};var m,p,l;e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: (args: FormLabelProps) => <FormLabel {...args} />,
+  args: {
+    children: 'This text is a child of FormLabel',
+    required: true
+  }
+}`,...(l=(p=e.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var s,a,n;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: () => <>
+      <FormLabel>Default</FormLabel>
+      <FormLabel helpTopicId="1234">With Field Help Icon</FormLabel>
+      <FormLabel required>Required</FormLabel>
+      <FormLabel error>Error</FormLabel>
+      {/* A disabled label by itself will throw contrast warning unless used correctly with a disabled input. */}
+      <FormControl disabled size="small">
+        <FormLabel htmlFor="disabled">Disabled</FormLabel>
+        <OutlinedInput id="disabled" sx={visuallyHidden} />
+      </FormControl>
+    </>
+}`,...(n=(a=t.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};var d,c,u;i.parameters={...i.parameters,docs:{...(d=i.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: () => <FormControl required size="small">
+      <FormLabel htmlFor="field-1" helpTopicId="1234">
+        Example
+      </FormLabel>
+      <Input id="field-1" defaultValue="default value" />
+      <FormHelperText>Helper Text</FormHelperText>
+    </FormControl>
+}`,...(u=(c=i.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};const Xr=["_FormLabel","_States","_Controls"];export{i as _Controls,e as _FormLabel,t as _States,Xr as __namedExportsOrder,Qr as default};
---
+++ pr-preview/pr-561/assets/introduction-CEx9k9rU.js
@@ -0,0 +1,63 @@
+import{j as n}from"./jsx-runtime-CcL-W3nW.js";import{useMDXComponents as e}from"./index-CfSBfIZi.js";import{M as o,c as r}from"./index-zoIqCIji.js";import"./DocsRenderer-CFRXHY34-zQMXdQp7.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-51afW8Au.js";import"./index-BEx9gAkE.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./client-BqYpGkAY.js";const s=`# @availity/mui-backdrop
+
+> Availity MUI Backdrop component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-backdrop.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-backdrop)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-backdrop.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-backdrop)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-backdrop?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-backdrop/package.json)
+
+## Documentation
+
+This package extends the MUI Backdrop component: [MUI Backdrop Docs](https://mui.com/components/backdrop/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-backdrop-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://design.availity.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+_This package has a few peer dependencies. Add \`@mui/material\` & \`@emotion/react\` to your project if not already installed._
+
+\`\`\`bash
+npm install @availity/mui-backdrop
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-backdrop
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Backdrop } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Backdrop } from '@availity/mui-backdrop';
+\`\`\`
+`;function i(t){return n.jsxs(n.Fragment,{children:[n.jsx(o,{title:"Components/Backdrop/Introduction"}),`
+`,n.jsx(r,{children:s})]})}function v(t={}){const{wrapper:a}={...e(),...t.components};return a?n.jsx(a,{...t,children:n.jsx(i,{...t})}):i()}export{v as default};
