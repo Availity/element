d4eecec68268141d5a8216faff4909a41cbf67fc
jordan-a-young <jordan-a-young@users.noreply.github.com>
2025-02-28 20:11:43 +0000

    Deploy preview for PR 685 🛫

---
+++ pr-preview/pr-685/assets/ListItemIcon.stories-BsDU8LHu.js
@@ -0,0 +1,44 @@
+import{j as t}from"./jsx-runtime-CcL-W3nW.js";import{g as o}from"./index-4AU0IlPL.js";import{G as s}from"./index-Cyp_3NIz.js";import{T as a}from"./index-BtPDELIe.js";import{d as n,L as m,a as p,c as d,b as c}from"./ListSubheader-CPOqqkHA.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./faCircleArrowRight-BeBmJtPK.js";import"./faUser-DXPB7Q64.js";import"./faSortDown-nq-jkck6.js";import"./SvgIcon-DSdWmKAH.js";import"./generateUtilityClass-CVWqFxsA.js";import"./identifier-Dtqimryx.js";import"./memoTheme-D-rVhYRp.js";import"./styled-CF1hcdwO.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./Box-DWnb8VZB.js";import"./useTheme-C2CrmYKc.js";import"./extendSxProp-B5AgUjQT.js";import"./Grid2-y9MPRr2u.js";import"./useTheme-DUkCvbx_.js";import"./styled-vBjI3IQc.js";import"./isMuiElement-DAzOCCHH.js";import"./useThemeProps-BIK-1DKm.js";import"./Stack-BcsM4Vc0.js";import"./Container-Cqq8qXXg.js";import"./Typography-CvSvtD2a.js";import"./index-f0Aa_BgU.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./List-ChBzRQcW.js";import"./ListItem-BaRQg_q0.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-BVrIj09m.js";import"./ListItemText-BAfvFchG.js";import"./useEnhancedEffect-D2f2tbda.js";import"./ButtonBase-DbVRbsLs.js";import"./useTimeout-B8wmcRZV.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";import"./TransitionGroupContext-D-IBNIfW.js";import"./isFocusVisible-B8k4qzLc.js";import"./listItemTextClasses-QA0fdXLT.js";import"./useSlot-DnkSxE3X.js";import"./resolveComponentProps-Dg20wxiM.js";import"./useThemeProps-NV-uCBd-.js";import"./ListSubheader-DDrjAA_E.js";const ct={title:"Components/List/ListItemIcon",component:n,tags:["autodocs"],argTypes:{children:{control:!1}}},i={render:e=>t.jsx(n,{...e,children:t.jsx(o,{})})},r={render:()=>t.jsxs(s,{container:!0,spacing:8,children:[t.jsxs(s,{size:{xs:12,sm:6},component:"nav","aria-labelledby":"favorites-default-spacing-header",children:[t.jsx(a,{variant:"h6",component:"div",id:"favorites-default-spacing-header",children:"Default Spacing"}),t.jsx(m,{children:[0,1,2,3].map(e=>t.jsx(p,{disablePadding:!0,children:t.jsxs(d,{children:[t.jsx(n,{children:t.jsx(o,{})}),t.jsx(c,{primary:"List item",secondary:"Secondary"})]})},e))})]}),t.jsxs(s,{size:{xs:12,sm:6},component:"nav","aria-labelledby":"favorites-dense-spacing-header",children:[t.jsx(a,{variant:"h6",component:"div",id:"favorites-dense-spacing-header",children:"Dense Spacing"}),t.jsx(m,{dense:!0,children:[4,5,6,7].map(e=>t.jsx(p,{disablePadding:!0,children:t.jsxs(d,{children:[t.jsx(n,{children:t.jsx(o,{})}),t.jsx(c,{primary:"List item",secondary:"Secondary"})]})},e))})]})]})};var l,I,L;i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: (args: ListItemIconProps) => <ListItemIcon {...args}>
+      <HeartEmptyIcon />
+    </ListItemIcon>
+}`,...(L=(I=i.parameters)==null?void 0:I.docs)==null?void 0:L.source}}};var y,h,g;r.parameters={...r.parameters,docs:{...(y=r.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: () => <Grid container spacing={8}>
+      <Grid size={{
+      xs: 12,
+      sm: 6
+    }} component="nav" aria-labelledby="favorites-default-spacing-header">
+        <Typography variant="h6" component="div" id="favorites-default-spacing-header">
+          Default Spacing
+        </Typography>
+        <List>
+          {[0, 1, 2, 3].map(value => <ListItem key={value} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <HeartEmptyIcon />
+                </ListItemIcon>
+                <ListItemText primary="List item" secondary="Secondary" />
+              </ListItemButton>
+            </ListItem>)}
+        </List>
+      </Grid>
+      <Grid size={{
+      xs: 12,
+      sm: 6
+    }} component="nav" aria-labelledby="favorites-dense-spacing-header">
+        <Typography variant="h6" component="div" id="favorites-dense-spacing-header">
+          Dense Spacing
+        </Typography>
+        <List dense>
+          {[4, 5, 6, 7].map(value => <ListItem key={value} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <HeartEmptyIcon />
+                </ListItemIcon>
+                <ListItemText primary="List item" secondary="Secondary" />
+              </ListItemButton>
+            </ListItem>)}
+        </List>
+      </Grid>
+    </Grid>
+}`,...(g=(h=r.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};const lt=["_ListItemIcon","_FavoritesList"];export{r as _FavoritesList,i as _ListItemIcon,lt as __namedExportsOrder,ct as default};
