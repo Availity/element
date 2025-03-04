d0399fd58123fe99048b2dced84ebd6fe5f434e4
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-03-04 22:04:24 +0000

    Deploy preview for PR 695 🛫

---
+++ pr-preview/pr-695/assets/Spaces.stories-CejzS4mF.js
@@ -0,0 +1,38 @@
+import{j as p}from"./jsx-runtime-CcL-W3nW.js";import{P as e}from"./index-BrWjJmRs.js";import{T as i}from"./index-BtPDELIe.js";import{S}from"./index-Cyp_3NIz.js";import{S as d,u as y,a as l}from"./Spaces-BSds1Z2T.js";import{i as u}from"./suspense-BqeUBMhm.js";import{Q as x}from"./queryClient-CjDmLjqo.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Paper-DXcIEVia.js";import"./generateUtilityClass-CVWqFxsA.js";import"./identifier-Dtqimryx.js";import"./useTheme-DUkCvbx_.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./useTheme-C2CrmYKc.js";import"./styled-CF1hcdwO.js";import"./memoTheme-D-rVhYRp.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./Typography-CvSvtD2a.js";import"./index-f0Aa_BgU.js";import"./extendSxProp-B5AgUjQT.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./Box-DWnb8VZB.js";import"./Grid2-y9MPRr2u.js";import"./styled-vBjI3IQc.js";import"./isMuiElement-DAzOCCHH.js";import"./useThemeProps-BIK-1DKm.js";import"./Stack-BcsM4Vc0.js";import"./Container-Cqq8qXXg.js";import"./index-C5soOJ6z.js";import"./index-uZUcOW5W.js";import"./___vite-browser-external_commonjs-proxy-DRaEfepn.js";import"./index-BXb5JWGK.js";import"./_toKey-MoF_TmUD.js";import"./Img-C-YSH53L.js";import"./index-DLsH_p52.js";import"./Tooltip-CiiaKkqp.js";import"./index-nos-0K8U.js";import"./useSlot-DnkSxE3X.js";import"./resolveComponentProps-Dg20wxiM.js";import"./useForkRef-BVrIj09m.js";import"./useTimeout-B8wmcRZV.js";import"./useEnhancedEffect-D2f2tbda.js";import"./useControlled-WdbyInBW.js";import"./useId-DszduLV4.js";import"./getReactElementRef-AnOffBoZ.js";import"./Portal-CBAx_nZm.js";import"./utils-D5ZkK9XY.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";import"./TransitionGroupContext-D-IBNIfW.js";import"./index-BEx9gAkE.js";import"./ownerDocument-DW-IO8s5.js";import"./useSlotProps-BAIpVL6Y.js";import"./isFocusVisible-B8k4qzLc.js";import"./IconButton-Caxu-qgA.js";import"./ButtonBase-DbVRbsLs.js";import"./CircularProgress-D8w9ZPs6.js";import"./Button-BAEbeAyH.js";import"./LoadingButton-9DxABi24.js";import"./index-4AU0IlPL.js";import"./faCircleArrowRight-BeBmJtPK.js";import"./faUser-DXPB7Q64.js";import"./faSortDown-nq-jkck6.js";import"./SvgIcon-DSdWmKAH.js";import"./index-CfUZyVvj.js";import"./Alert-ui2dSBj_.js";import"./createSvgIcon-TketyoFF.js";import"./Close-CpXO-CFZ.js";import"./AlertTitle-PObx1HD8.js";import"./Dialog-bGx9ZkTF.js";import"./DialogContext-DL4ikWki.js";import"./Fade-Dl4gmST4.js";import"./Modal-B5s0Thvt.js";import"./ownerWindow-HkKU3E4x.js";import"./createChainedFunction-BO_9K8Jh.js";import"./Backdrop-B4gbyDGC.js";import"./DialogTitle-BWg8K7UO.js";import"./dialogTitleClasses-BqOfRSGi.js";import"./DialogActions-FhQa7Naj.js";import"./DialogContent-DwpRh2db.js";import"./DialogContentText-Cpbos6Hc.js";import"./index-Dk74W0Oi.js";import"./index-DMWJB0MK.js";import"./dayjs.min-DnLU8EWa.js";import"./index-CgN_Fp17.js";import"./LinearProgress-aFncvf6g.js";const Zp={title:"Components/Spaces/Spaces",component:d,tags:["autodocs"]},g=new x,a=({spaceId:r})=>{const o=l(r),t=o==null?void 0:o.find(h=>h.configurationId===r);return p.jsx("div",{children:p.jsx("span",{id:`space-for-${r}`,children:t?`Space ${t==null?void 0:t.configurationId} is in provider`:`Space ${r} is not in provider`})})},f=({children:r})=>{const{loading:o}=y();return o?p.jsx("span",{children:"loading..."}):p.jsx("div",{children:r})},s={render:r=>p.jsx(u,{client:g,children:p.jsx(d,{...r,children:p.jsx(f,{children:p.jsxs(S,{spacing:2,children:[p.jsxs(e,{children:[p.jsx(i,{children:"Space 1 was passed in the props."}),p.jsx(a,{spaceId:"1"})]}),p.jsxs(e,{children:[p.jsx(i,{children:"Space 2 was fetched from the api via the spaceId passed in the props."}),p.jsx(a,{spaceId:"2"})]}),p.jsxs(e,{children:[p.jsx(i,{children:"Space 3 was not returned."}),p.jsx(a,{spaceId:"3"})]}),p.jsxs(e,{children:[p.jsx(i,{children:"Space 11 was fetched from the api via the payerId passed in the props."}),p.jsx(a,{spaceId:"11"})]})]})})})}),args:{spaces:[{id:"1",configurationId:"1",type:"space",name:"Space 1"}],spaceIds:["2"],payerIds:["a"]}};var m,n,c;s.parameters={...s.parameters,docs:{...(m=s.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: (args: SpacesProps) => {
+    return <QueryClientProvider client={queryClient}>
+        <Spaces {...args}>
+          <SpaceContainer>
+            <Stack spacing={2}>
+              <Paper>
+                <Typography>Space 1 was passed in the props.</Typography>
+                <SpaceComponent spaceId="1" />
+              </Paper>
+              <Paper>
+                <Typography>Space 2 was fetched from the api via the spaceId passed in the props.</Typography>
+                <SpaceComponent spaceId="2" />
+              </Paper>
+              <Paper>
+                <Typography>Space 3 was not returned.</Typography>
+                <SpaceComponent spaceId="3" />
+              </Paper>
+              <Paper>
+                <Typography>Space 11 was fetched from the api via the payerId passed in the props.</Typography>
+                <SpaceComponent spaceId="11" />
+              </Paper>
+            </Stack>
+          </SpaceContainer>
+        </Spaces>
+      </QueryClientProvider>;
+  },
+  args: {
+    spaces: [{
+      id: '1',
+      configurationId: '1',
+      type: 'space',
+      name: 'Space 1'
+    }],
+    spaceIds: ['2'],
+    payerIds: ['a']
+  }
+}`,...(c=(n=s.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const pr=["_Spaces"];export{s as _Spaces,pr as __namedExportsOrder,Zp as default};
