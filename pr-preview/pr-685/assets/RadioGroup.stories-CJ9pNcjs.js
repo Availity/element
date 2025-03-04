91363b9704526daf54312fdfb35d35b69a741ab7
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-03-04 15:58:38 +0000

    Deploy preview for PR 685 🛫

---
+++ pr-preview/pr-685/assets/RadioGroup.stories-CJ9pNcjs.js
@@ -0,0 +1,46 @@
+import{j as o}from"./jsx-runtime-CcL-W3nW.js";import{a as l,R as r,F as T}from"./Switch-CR3eS5U8.js";import"./FieldHelpIcon-D_brM4fg.js";import{F as p,a as e}from"./FormLabel-DZdwVmmu.js";import"./Input-BS67x0-8.js";import"./index-BtPDELIe.js";import{F as d}from"./FormControl-BPSU2g7t.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./RadioGroup-BogPRRDq.js";import"./generateUtilityClass-CVWqFxsA.js";import"./identifier-Dtqimryx.js";import"./SwitchBase-Z9YgLJlx.js";import"./styled-CF1hcdwO.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./useFormControl-68oiojFJ.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./useControlled-WdbyInBW.js";import"./ButtonBase-DbVRbsLs.js";import"./useTimeout-B8wmcRZV.js";import"./useEnhancedEffect-D2f2tbda.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";import"./TransitionGroupContext-D-IBNIfW.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./useForkRef-BVrIj09m.js";import"./isFocusVisible-B8k4qzLc.js";import"./createSvgIcon-TketyoFF.js";import"./SvgIcon-DSdWmKAH.js";import"./memoTheme-D-rVhYRp.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useId-DszduLV4.js";import"./formControlState-Dq1zat_P.js";import"./index-4AU0IlPL.js";import"./faCircleArrowRight-BeBmJtPK.js";import"./faUser-DXPB7Q64.js";import"./faSortDown-nq-jkck6.js";import"./Select-hLZ2S4VH.js";import"./Menu-Byu17A6t.js";import"./index-nos-0K8U.js";import"./useSlotProps-BAIpVL6Y.js";import"./resolveComponentProps-Dg20wxiM.js";import"./Popover-ChEuM7rO.js";import"./isHostComponent-DVu5iVWx.js";import"./useSlot-DnkSxE3X.js";import"./Portal-CBAx_nZm.js";import"./useTheme-DUkCvbx_.js";import"./useTheme-C2CrmYKc.js";import"./utils-D5ZkK9XY.js";import"./index-BEx9gAkE.js";import"./getReactElementRef-AnOffBoZ.js";import"./ownerDocument-DW-IO8s5.js";import"./ownerWindow-HkKU3E4x.js";import"./debounce-Be36O1Ab.js";import"./Paper-DXcIEVia.js";import"./Modal-B5s0Thvt.js";import"./Backdrop-B4gbyDGC.js";import"./Fade-Dl4gmST4.js";import"./List-ChBzRQcW.js";import"./utils-DoM3o7-Q.js";import"./OutlinedInput-CQlxW3ax.js";import"./index-f0Aa_BgU.js";import"./extendSxProp-B5AgUjQT.js";import"./Stack-BcsM4Vc0.js";import"./styled-vBjI3IQc.js";import"./useThemeProps-BIK-1DKm.js";import"./Divider-jUONNV4j.js";import"./dividerClasses-BmOcEkDu.js";import"./faCheck-B05F4ORy.js";import"./Switch-BqVUWLaW.js";import"./index-DMWJB0MK.js";import"./index-DLsH_p52.js";import"./Tooltip-CiiaKkqp.js";import"./IconButton-Caxu-qgA.js";import"./CircularProgress-D8w9ZPs6.js";import"./Button-BAEbeAyH.js";import"./LoadingButton-9DxABi24.js";import"./FormControlLabel-GqYWwHWT.js";import"./Typography-CvSvtD2a.js";import"./Box-DWnb8VZB.js";import"./FormLabel-98YPf6ct.js";import"./isMuiElement-DAzOCCHH.js";const Yo={title:"Form Components/FormUtils/RadioGroup",component:l,tags:["autodocs"]},i={render:a=>o.jsxs(d,{children:[o.jsx(p,{component:"legend",id:"radio-buttons-group-label",children:"Radio Group"}),o.jsxs(l,{"aria-labelledby":"radio-buttons-group-label",defaultValue:"one",name:"radio-buttons-group",...a,children:[o.jsx(e,{value:"one",control:o.jsx(r,{}),label:"Radio One"}),o.jsx(e,{value:"two",control:o.jsx(r,{}),label:"Radio Two",helpTopicId:"1234"}),o.jsx(e,{value:"three",control:o.jsx(r,{}),label:"Radio Three"})]})]}),args:{}},n={render:a=>o.jsxs(o.Fragment,{children:[o.jsxs(d,{margin:"normal",required:!0,children:[o.jsx(p,{id:"required-radio-buttons-group-label",children:"Required"}),o.jsxs(l,{"aria-labelledby":"required-radio-buttons-group-label",defaultValue:"one",name:"required-radio-buttons-group",...a,children:[o.jsx(e,{value:"one",control:o.jsx(r,{}),label:"Radio One"}),o.jsx(e,{value:"two",control:o.jsx(r,{}),label:"Radio Two",helpTopicId:"1234"}),o.jsx(e,{value:"three",control:o.jsx(r,{}),label:"Radio Three"})]})]}),o.jsxs(d,{margin:"normal",error:!0,children:[o.jsx(p,{id:"error-radio-buttons-group-label",children:"Error"}),o.jsxs(l,{"aria-labelledby":"error-radio-buttons-group-label","aria-describedby":"error-radio-buttons-group-helpertext",name:"error-radio-buttons-group",...a,children:[o.jsx(e,{value:"one",control:o.jsx(r,{}),label:"Radio One"}),o.jsx(e,{value:"two",control:o.jsx(r,{}),label:"Radio Two",helpTopicId:"1234"}),o.jsx(e,{value:"three",control:o.jsx(r,{}),label:"Radio Three"})]}),o.jsx(T,{id:"error-radio-buttons-group-helpertext",children:"Error Message"})]})]}),args:{}},t={render:a=>o.jsxs(d,{children:[o.jsx(p,{className:"legend",id:"radio-buttons-group-legend-label",children:"Radio Group"}),o.jsxs(l,{"aria-labelledby":"radio-buttons-group-legend-label",defaultValue:"one",name:"radio-buttons-group",...a,children:[o.jsx(e,{value:"one",control:o.jsx(r,{}),label:"Radio One"}),o.jsx(e,{value:"two",control:o.jsx(r,{}),label:"Radio Two",helpTopicId:"1234"}),o.jsx(e,{value:"three",control:o.jsx(r,{}),label:"Radio Three"})]})]}),args:{}};var m,s,u;i.parameters={...i.parameters,docs:{...(m=i.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: (args: RadioGroupProps) => <FormControl>
+      <FormLabel component="legend" id="radio-buttons-group-label">
+        Radio Group
+      </FormLabel>
+      <RadioGroup aria-labelledby="radio-buttons-group-label" defaultValue="one" name="radio-buttons-group" {...args}>
+        <FormControlLabel value="one" control={<Radio />} label="Radio One" />
+        <FormControlLabel value="two" control={<Radio />} label="Radio Two" helpTopicId="1234" />
+        <FormControlLabel value="three" control={<Radio />} label="Radio Three" />
+      </RadioGroup>
+    </FormControl>,
+  args: {}
+}`,...(u=(s=i.parameters)==null?void 0:s.docs)==null?void 0:u.source}}};var b,c,g;n.parameters={...n.parameters,docs:{...(b=n.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: (args: RadioGroupProps) => <>
+      <FormControl margin="normal" required>
+        <FormLabel id="required-radio-buttons-group-label">Required</FormLabel>
+        <RadioGroup aria-labelledby="required-radio-buttons-group-label" defaultValue="one" name="required-radio-buttons-group" {...args}>
+          <FormControlLabel value="one" control={<Radio />} label="Radio One" />
+          <FormControlLabel value="two" control={<Radio />} label="Radio Two" helpTopicId="1234" />
+          <FormControlLabel value="three" control={<Radio />} label="Radio Three" />
+        </RadioGroup>
+      </FormControl>
+      <FormControl margin="normal" error>
+        <FormLabel id="error-radio-buttons-group-label">Error</FormLabel>
+        <RadioGroup aria-labelledby="error-radio-buttons-group-label" aria-describedby="error-radio-buttons-group-helpertext" name="error-radio-buttons-group" {...args}>
+          <FormControlLabel value="one" control={<Radio />} label="Radio One" />
+          <FormControlLabel value="two" control={<Radio />} label="Radio Two" helpTopicId="1234" />
+          <FormControlLabel value="three" control={<Radio />} label="Radio Three" />
+        </RadioGroup>
+        <FormHelperText id="error-radio-buttons-group-helpertext">Error Message</FormHelperText>
+      </FormControl>
+    </>,
+  args: {}
+}`,...(g=(c=n.parameters)==null?void 0:c.docs)==null?void 0:g.source}}};var R,x,h,F,j;t.parameters={...t.parameters,docs:{...(R=t.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  render: (args: RadioGroupProps) => <FormControl>
+      <FormLabel className="legend" id="radio-buttons-group-legend-label">
+        Radio Group
+      </FormLabel>
+      <RadioGroup aria-labelledby="radio-buttons-group-legend-label" defaultValue="one" name="radio-buttons-group" {...args}>
+        <FormControlLabel value="one" control={<Radio />} label="Radio One" />
+        <FormControlLabel value="two" control={<Radio />} label="Radio Two" helpTopicId="1234" />
+        <FormControlLabel value="three" control={<Radio />} label="Radio Three" />
+      </RadioGroup>
+    </FormControl>,
+  args: {}
+}`,...(h=(x=t.parameters)==null?void 0:x.docs)==null?void 0:h.source},description:{story:'Legacy styling for the group label can be achieved with the `"legend"` class on the `FormLabel`.',...(j=(F=t.parameters)==null?void 0:F.docs)==null?void 0:j.description}}};const Zo=["_RadioGroup","_States","_Legacy"];export{t as _Legacy,i as _RadioGroup,n as _States,Zo as __namedExportsOrder,Yo as default};
---
+++ pr-preview/pr-685/assets/TextField.stories-U2dabl2t.js
@@ -0,0 +1,39 @@
+import{j as t}from"./jsx-runtime-CcL-W3nW.js";import{h as n}from"./TextField-CW6KxaMh.js";import{B as s}from"./index-DLsH_p52.js";import{P as c}from"./index-BrWjJmRs.js";import{T as h}from"./index-BtPDELIe.js";import{G as x}from"./index-Cyp_3NIz.js";import{T as f,a as g}from"./Types-DFZlQxsW.js";import"./AsyncAutocomplete-CKL0S_JK.js";import"./Checkbox-CXPubYjX.js";import{u as T,F as S}from"./index.esm-D1k4U55D.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-f4L0U9g7.js";import"./index-Cy29Pico.js";import"./index-DS2qViMa.js";import"./index-DMWJB0MK.js";import"./index-4AU0IlPL.js";import"./faCircleArrowRight-BeBmJtPK.js";import"./faUser-DXPB7Q64.js";import"./faSortDown-nq-jkck6.js";import"./SvgIcon-DSdWmKAH.js";import"./generateUtilityClass-CVWqFxsA.js";import"./identifier-Dtqimryx.js";import"./memoTheme-D-rVhYRp.js";import"./styled-CF1hcdwO.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./faCheck-B05F4ORy.js";import"./Select-hLZ2S4VH.js";import"./formControlState-Dq1zat_P.js";import"./useFormControl-68oiojFJ.js";import"./Menu-Byu17A6t.js";import"./index-nos-0K8U.js";import"./useSlotProps-BAIpVL6Y.js";import"./resolveComponentProps-Dg20wxiM.js";import"./useForkRef-BVrIj09m.js";import"./Popover-ChEuM7rO.js";import"./isHostComponent-DVu5iVWx.js";import"./useSlot-DnkSxE3X.js";import"./Portal-CBAx_nZm.js";import"./useTheme-DUkCvbx_.js";import"./useTheme-C2CrmYKc.js";import"./utils-D5ZkK9XY.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";import"./TransitionGroupContext-D-IBNIfW.js";import"./index-BEx9gAkE.js";import"./useTimeout-B8wmcRZV.js";import"./useEnhancedEffect-D2f2tbda.js";import"./getReactElementRef-AnOffBoZ.js";import"./ownerDocument-DW-IO8s5.js";import"./ownerWindow-HkKU3E4x.js";import"./debounce-Be36O1Ab.js";import"./Paper-DXcIEVia.js";import"./Modal-B5s0Thvt.js";import"./createChainedFunction-BO_9K8Jh.js";import"./Backdrop-B4gbyDGC.js";import"./Fade-Dl4gmST4.js";import"./List-ChBzRQcW.js";import"./utils-DoM3o7-Q.js";import"./useControlled-WdbyInBW.js";import"./useId-DszduLV4.js";import"./createSvgIcon-TketyoFF.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./OutlinedInput-CQlxW3ax.js";import"./index-f0Aa_BgU.js";import"./extendSxProp-B5AgUjQT.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./FormLabel-98YPf6ct.js";import"./FormControlLabel-GqYWwHWT.js";import"./Typography-CvSvtD2a.js";import"./Switch-BqVUWLaW.js";import"./SwitchBase-Z9YgLJlx.js";import"./ButtonBase-DbVRbsLs.js";import"./isFocusVisible-B8k4qzLc.js";import"./RadioGroup-BogPRRDq.js";import"./Box-DWnb8VZB.js";import"./Divider-jUONNV4j.js";import"./dividerClasses-BmOcEkDu.js";import"./Stack-BcsM4Vc0.js";import"./styled-vBjI3IQc.js";import"./useThemeProps-BIK-1DKm.js";import"./TextField-BxbLM7nG.js";import"./FormControl-BPSU2g7t.js";import"./isMuiElement-DAzOCCHH.js";import"./index-C5soOJ6z.js";import"./index-uZUcOW5W.js";import"./___vite-browser-external_commonjs-proxy-DRaEfepn.js";import"./index-BXb5JWGK.js";import"./_toKey-MoF_TmUD.js";import"./Autocomplete-DYfkOmMd.js";import"./Close-CpXO-CFZ.js";import"./usePreviousProps-BYCwus4J.js";import"./Tooltip-CiiaKkqp.js";import"./Chip-ALB5H4lg.js";import"./IconButton-Caxu-qgA.js";import"./CircularProgress-D8w9ZPs6.js";import"./ListSubheader-DDrjAA_E.js";import"./useInfiniteQuery-BlwNwEaC.js";import"./useBaseQuery-CJ5trF24.js";import"./suspense-BqeUBMhm.js";import"./index-BsiHxf1b.js";import"./DatePicker--IEYhjMf.js";import"./useThemeProps-NV-uCBd-.js";import"./index-Dk74W0Oi.js";import"./enUS-BqLCxLyo.js";import"./visuallyHidden-Dan1xhjv.js";import"./InputAdornment-DYZl9z8K.js";import"./Button-BAEbeAyH.js";import"./DialogActions-FhQa7Naj.js";import"./ListItem-BaRQg_q0.js";import"./Dialog-bGx9ZkTF.js";import"./DialogContext-DL4ikWki.js";import"./DialogContent-DwpRh2db.js";import"./dialogTitleClasses-BqOfRSGi.js";import"./LoadingButton-9DxABi24.js";import"./Grid2-y9MPRr2u.js";import"./Container-Cqq8qXXg.js";import"./index-B-bCkWqS.js";import"./Checkbox-imGzR8Us.js";const Lt={title:"Form Components/Controlled Form/ControlledTextField",component:n,tags:["autodocs"],argTypes:{...g,...f,helperText:{type:"string",table:{category:"Input Props"}}}},o={render:i=>{var e,m,p;const r=T({values:{[i.name]:""}});return t.jsx(S,{...r,children:t.jsxs("form",{onSubmit:r.handleSubmit(u=>u),children:[t.jsx(n,{...i}),t.jsxs(x,{container:!0,direction:"row",justifyContent:"space-between",marginTop:1,children:[t.jsx(s,{disabled:!((e=r==null?void 0:r.formState)!=null&&e.isSubmitSuccessful),children:"Reset",color:"secondary",onClick:()=>r.reset()}),t.jsx(s,{type:"submit",disabled:(m=r==null?void 0:r.formState)==null?void 0:m.isSubmitSuccessful,children:"Submit"})]}),(p=r==null?void 0:r.formState)!=null&&p.isSubmitSuccessful?t.jsxs(c,{sx:{padding:"1.5rem",marginTop:"1.5rem"},children:[t.jsx(h,{variant:"h2",children:"Submitted Values"}),t.jsx("pre",{"data-testid":"result",children:JSON.stringify(r.getValues(),null,2)})]}):null]})})},args:{name:"controlledTextField",helperText:"This is some helper text",placeholder:"Name",required:!0,rules:{required:"This field is required.",maxLength:{value:10,message:"Too long"}},label:"TextField Label"}};var l,a,d;o.parameters={...o.parameters,docs:{...(l=o.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: (args: ControlledTextFieldProps) => {
+    const methods = useForm({
+      values: {
+        [args.name]: ''
+      }
+    });
+    return <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(data => data)}>
+          <ControlledTextField {...args} />
+          <Grid container direction="row" justifyContent="space-between" marginTop={1}>
+            <Button disabled={!methods?.formState?.isSubmitSuccessful} children="Reset" color="secondary" onClick={() => methods.reset()} />
+            <Button type="submit" disabled={methods?.formState?.isSubmitSuccessful} children="Submit" />
+          </Grid>
+          {methods?.formState?.isSubmitSuccessful ? <Paper sx={{
+          padding: '1.5rem',
+          marginTop: '1.5rem'
+        }}>
+              <Typography variant="h2">Submitted Values</Typography>
+              <pre data-testid="result">{JSON.stringify(methods.getValues(), null, 2)}</pre>
+            </Paper> : null}
+        </form>
+      </FormProvider>;
+  },
+  args: {
+    name: 'controlledTextField',
+    helperText: 'This is some helper text',
+    placeholder: 'Name',
+    required: true,
+    rules: {
+      required: 'This field is required.',
+      maxLength: {
+        value: 10,
+        message: 'Too long'
+      }
+    },
+    label: 'TextField Label'
+  }
+}`,...(d=(a=o.parameters)==null?void 0:a.docs)==null?void 0:d.source}}};const Nt=["_ControlledTextField"];export{o as _ControlledTextField,Nt as __namedExportsOrder,Lt as default};
