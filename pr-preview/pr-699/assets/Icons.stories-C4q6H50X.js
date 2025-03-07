052ae9255d63765740a093f6f46d6bbaf17f0282
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-03-07 15:44:13 +0000

    Deploy preview for PR 699 🛫

---
+++ pr-preview/pr-699/assets/Icons.stories-C4q6H50X.js
@@ -0,0 +1,62 @@
+import{j as e}from"./jsx-runtime-CcL-W3nW.js";import{G as n}from"./index-Cyp_3NIz.js";import{H as i}from"./Common-Q4byVbFa.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Box-DWnb8VZB.js";import"./identifier-Dtqimryx.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./generateUtilityClass-CVWqFxsA.js";import"./useTheme-C2CrmYKc.js";import"./extendSxProp-B5AgUjQT.js";import"./Grid2-y9MPRr2u.js";import"./useTheme-DUkCvbx_.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./styled-vBjI3IQc.js";import"./styled-CF1hcdwO.js";import"./isMuiElement-DAzOCCHH.js";import"./useThemeProps-BIK-1DKm.js";import"./Stack-BcsM4Vc0.js";import"./Container-Cqq8qXXg.js";import"./faUser-DXPB7Q64.js";import"./SvgIcon-DSdWmKAH.js";import"./memoTheme-D-rVhYRp.js";const F={title:"Components/Icon/Named Icons",component:i,tags:["autodocs"],argTypes:{color:{control:"text"},fontSize:{control:"text"}}},r={render:p=>e.jsx(i,{...p})},s={render:()=>e.jsxs(n,{container:!0,spacing:1,children:[e.jsx(n,{size:{xs:2},children:"Inherit (default)"}),e.jsx(n,{size:{xs:10},children:e.jsx(i,{fontSize:"inherit"})}),e.jsx(n,{size:{xs:2},children:"XXSmall"}),e.jsx(n,{size:{xs:10},children:e.jsx(i,{fontSize:"xxsmall"})}),e.jsx(n,{size:{xs:2},children:"XSmall"}),e.jsx(n,{size:{xs:10},children:e.jsx(i,{fontSize:"xsmall"})}),e.jsx(n,{size:{xs:2},children:"Small"}),e.jsx(n,{size:{xs:10},children:e.jsx(i,{fontSize:"small"})}),e.jsx(n,{size:{xs:2},children:"Medium"}),e.jsx(n,{size:{xs:10},children:e.jsx(i,{fontSize:"medium"})}),e.jsx(n,{size:{xs:2},children:"Large"}),e.jsx(n,{size:{xs:10},children:e.jsx(i,{fontSize:"large"})}),e.jsx(n,{size:{xs:2},children:"XLarge"}),e.jsx(n,{size:{xs:10},children:e.jsx(i,{fontSize:"xlarge"})})]})};var o,t,d,m,a;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: (args: Omit<FaSvgIconProps, 'icon'>) => <HomeIcon {...args} />
+}`,...(d=(t=r.parameters)==null?void 0:t.docs)==null?void 0:d.source},description:{story:"Any named icon, like the `<HomeIcon />` below, extend mui's `SvgIcon` component and accept the same parameters.\n\nIcon is `aria-hidden` by default. If not accompanied with any on screen text then must pass `aria-hidden={false}`.",...(a=(m=r.parameters)==null?void 0:m.docs)==null?void 0:a.description}}};var c,x,l;s.parameters={...s.parameters,docs:{...(c=s.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: () => <Grid container spacing={1}>
+      <Grid size={{
+      xs: 2
+    }}>Inherit (default)</Grid>
+      <Grid size={{
+      xs: 10
+    }}>
+        <HomeIcon fontSize="inherit" />
+      </Grid>
+      <Grid size={{
+      xs: 2
+    }}>XXSmall</Grid>
+      <Grid size={{
+      xs: 10
+    }}>
+        <HomeIcon fontSize="xxsmall" />
+      </Grid>
+      <Grid size={{
+      xs: 2
+    }}>XSmall</Grid>
+      <Grid size={{
+      xs: 10
+    }}>
+        <HomeIcon fontSize="xsmall" />
+      </Grid>
+      <Grid size={{
+      xs: 2
+    }}>Small</Grid>
+      <Grid size={{
+      xs: 10
+    }}>
+        <HomeIcon fontSize="small" />
+      </Grid>
+      <Grid size={{
+      xs: 2
+    }}>Medium</Grid>
+      <Grid size={{
+      xs: 10
+    }}>
+        <HomeIcon fontSize="medium" />
+      </Grid>
+      <Grid size={{
+      xs: 2
+    }}>Large</Grid>
+      <Grid size={{
+      xs: 10
+    }}>
+        <HomeIcon fontSize="large" />
+      </Grid>
+      <Grid size={{
+      xs: 2
+    }}>XLarge</Grid>
+      <Grid size={{
+      xs: 10
+    }}>
+        <HomeIcon fontSize="xlarge" />
+      </Grid>
+    </Grid>
+}`,...(l=(x=s.parameters)==null?void 0:x.docs)==null?void 0:l.source}}};const N=["_HomeIcon","_Sizes"];export{r as _HomeIcon,s as _Sizes,N as __namedExportsOrder,F as default};
---
+++ pr-preview/pr-699/assets/useEnhancedEffect-D2f2tbda.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-DcWiA9TO.js";const f=typeof window<"u"?e.useLayoutEffect:e.useEffect;export{f as u};
