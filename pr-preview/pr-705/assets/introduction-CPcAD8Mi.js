76e9d1c175bbf141e933141c5a74b025c2e9ffad
jordan-a-young <jordan-a-young@users.noreply.github.com>
2025-03-12 19:55:50 +0000

    Deploy preview for PR 705 🛫

---
+++ pr-preview/pr-705/assets/introduction-CPcAD8Mi.js
@@ -0,0 +1,156 @@
+import{j as e}from"./jsx-runtime-CcL-W3nW.js";import{useMDXComponents as o}from"./index-CfSBfIZi.js";import{M as p,c as i}from"./index-CTSQPo6g.js";import"./DocsRenderer-CFRXHY34-CsbKXa9v.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Dyt9Hnsg.js";import"./index-BEx9gAkE.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./client-BqYpGkAY.js";const m=`# @availity/mui-popover
+
+> Availity MUI Popover component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-popover.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-popover)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-popover.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-popover)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-popover?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-popover/package.json)
+
+## Documentation
+
+This package extends the MUI Popover component: [MUI Popover Docs](https://mui.com/components/popover/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-popover-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://design.availity.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+_This package has a few peer dependencies. Add \`@mui/material\` & \`@emotion/react\` to your project if not already installed._
+
+\`\`\`bash
+npm install @availity/mui-popover
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-popover
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Popover } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Popover } from '@availity/mui-popover';
+\`\`\`
+`,r=`# Changelog
+
+This file was generated using [@jscutlery/semver](https://github.com/jscutlery/semver).
+
+## [1.0.1](https://github.com/Availity/element/compare/@availity/mui-popover@1.0.0...@availity/mui-popover@1.0.1) (2025-03-07)
+
+### Dependency Updates
+
+* \`mui-typography\` updated to version \`1.0.0\`
+## [1.0.0](https://github.com/Availity/element/compare/@availity/mui-popover@1.0.0-alpha.0...@availity/mui-popover@1.0.0) (2025-02-25)
+
+### Dependency Updates
+
+* \`mui-typography\` updated to version \`0.2.0\`
+
+### ⚠ BREAKING CHANGES
+
+* upgrade to @mui/material v6
+
+### Miscellaneous Chores
+
+* element v1 release ([a6e3567](https://github.com/Availity/element/commit/a6e35671185b9f13d25c7a39c4488ecb8774633e))
+
+## [1.0.0-alpha.0](https://github.com/Availity/element/compare/@availity/mui-popover@0.2.0...@availity/mui-popover@1.0.0-alpha.0) (2025-02-24)
+
+
+### ⚠ BREAKING CHANGES
+
+* upgraded to @mui/material v6
+* **element:** upgraded to @mui/material v6
+
+### Features
+
+* **element:** upgrade to @mui/material v6 ([cb958bb](https://github.com/Availity/element/commit/cb958bba99a4f1ee6dab323f0ff54b69e6fd3493))
+* upgrade @mui/material ([571453a](https://github.com/Availity/element/commit/571453a34b21c344594ab4c03bc497d19aba942b))
+* upgrade to MUI v6 ([7febd6f](https://github.com/Availity/element/commit/7febd6fd4fd58e87e1c97a832cea3b4595a35d58))
+
+## [0.2.0](https://github.com/Availity/element/compare/@availity/mui-popover@0.1.6...@availity/mui-popover@0.2.0) (2025-02-04)
+
+### Dependency Updates
+
+* \`mui-typography\` updated to version \`0.1.6\`
+
+### Features
+
+* configure package.json to allow esm to be used ([5a5393d](https://github.com/Availity/element/commit/5a5393de761f52608e714dd94a05106937dd95db))
+
+## [0.1.6](https://github.com/Availity/element/compare/@availity/mui-popover@0.1.5...@availity/mui-popover@0.1.6) (2024-10-31)
+
+### Dependency Updates
+
+* \`mui-typography\` updated to version \`0.1.5\`
+## [0.1.5](https://github.com/Availity/element/compare/@availity/mui-popover@0.1.4...@availity/mui-popover@0.1.5) (2024-06-14)
+
+### Dependency Updates
+
+* \`mui-typography\` updated to version \`0.1.4\`
+## [0.1.4](https://github.com/Availity/element/compare/@availity/mui-popover@0.1.3...@availity/mui-popover@0.1.4) (2024-04-19)
+
+### Dependency Updates
+
+* \`mui-typography\` updated to version \`0.1.3\`
+## [0.1.3](https://github.com/Availity/element/compare/@availity/mui-popover@0.1.2...@availity/mui-popover@0.1.3) (2024-04-04)
+
+### Dependency Updates
+
+* \`mui-typography\` updated to version \`0.1.7\`
+
+### Performance Improvements
+
+* **mui-popover:** use path imports for material deps ([8859fc7](https://github.com/Availity/element/commit/8859fc75b30e99886f48ddb3b10676975ab8f4a9))
+
+## [0.1.2](https://github.com/Availity/element/compare/@availity/mui-popover@0.1.1...@availity/mui-popover@0.1.2) (2024-02-20)
+
+### Dependency Updates
+
+* \`mui-typography\` updated to version \`0.1.6\`
+## [0.1.1](https://github.com/Availity/element/compare/@availity/mui-popover@0.1.0...@availity/mui-popover@0.1.1) (2023-11-16)
+
+### Dependency Updates
+
+- \`mui-typography\` updated to version \`0.1.5\`
+
+## 0.1.0 (2023-11-09)
+
+### Dependency Updates
+
+- \`mui-typography\` updated to version \`0.1.4\`
+
+### Features
+
+- **mui-popover:** initial commit ([f36b454](https://github.com/Availity/element/commit/f36b45416551d24c2c840b1fd65489399b388835))
+`;function a(t){return e.jsxs(e.Fragment,{children:[e.jsx(p,{title:"Components/Popover/Introduction"}),`
+`,e.jsx(i,{children:m}),`
+`,e.jsx(i,{children:r})]})}function A(t={}){const{wrapper:n}={...o(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(a,{...t})}):a()}export{A as default};
