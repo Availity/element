255bc7d5ab6e0f329273c3722a1cc906e8c03498
LauRoxx <LauRoxx@users.noreply.github.com>
2025-03-13 13:42:54 +0000

    Deploy preview for PR 706 🛫

---
+++ pr-preview/pr-706/assets/SpacesImage.stories-zTd31JE_.js
@@ -0,0 +1,58 @@
+import{j as r}from"./jsx-runtime-CcL-W3nW.js";import{C as _,a as T}from"./index-BpAoOrMG.js";import{b as o,S as s}from"./Spaces-DTNgR7Xt.js";import{i as a}from"./suspense-BqeUBMhm.js";import{Q as f}from"./queryClient-CjDmLjqo.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Card-BsRsTGD-.js";import"./generateUtilityClass-CVWqFxsA.js";import"./identifier-Dtqimryx.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./styled-CF1hcdwO.js";import"./Paper-DXcIEVia.js";import"./useTheme-DUkCvbx_.js";import"./useTheme-C2CrmYKc.js";import"./memoTheme-D-rVhYRp.js";import"./CardHeader-7LVbLnwQ.js";import"./useSlot-DnkSxE3X.js";import"./resolveComponentProps-Dg20wxiM.js";import"./useForkRef-BVrIj09m.js";import"./Typography-CvSvtD2a.js";import"./index-f0Aa_BgU.js";import"./extendSxProp-B5AgUjQT.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./Chip-ALB5H4lg.js";import"./createSvgIcon-TketyoFF.js";import"./SvgIcon-DSdWmKAH.js";import"./ButtonBase-DbVRbsLs.js";import"./useTimeout-B8wmcRZV.js";import"./useEnhancedEffect-D2f2tbda.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";import"./TransitionGroupContext-D-IBNIfW.js";import"./isFocusVisible-B8k4qzLc.js";import"./index-D1JqcBhz.js";import"./index-uZUcOW5W.js";import"./___vite-browser-external_commonjs-proxy-DRaEfepn.js";import"./_toKey-MoF_TmUD.js";import"./index-CuxoZ4Bj.js";import"./index-DLsH_p52.js";import"./Tooltip-CiiaKkqp.js";import"./index-nos-0K8U.js";import"./useControlled-WdbyInBW.js";import"./useId-DszduLV4.js";import"./getReactElementRef-AnOffBoZ.js";import"./Portal-CBAx_nZm.js";import"./utils-D5ZkK9XY.js";import"./index-BEx9gAkE.js";import"./ownerDocument-DW-IO8s5.js";import"./useSlotProps-BAIpVL6Y.js";import"./IconButton-Caxu-qgA.js";import"./CircularProgress-D8w9ZPs6.js";import"./Button-BAEbeAyH.js";import"./LoadingButton-9DxABi24.js";import"./index-EBJ2OYCQ.js";import"./faCircleArrowRight-BeBmJtPK.js";import"./faUser-DXPB7Q64.js";import"./faSortDown-nq-jkck6.js";import"./index-CONQltkH.js";import"./Alert-ui2dSBj_.js";import"./Close-CpXO-CFZ.js";import"./AlertTitle-PObx1HD8.js";import"./Dialog-bGx9ZkTF.js";import"./DialogContext-DL4ikWki.js";import"./Fade-Dl4gmST4.js";import"./Modal-B5s0Thvt.js";import"./ownerWindow-HkKU3E4x.js";import"./createChainedFunction-BO_9K8Jh.js";import"./Backdrop-B4gbyDGC.js";import"./DialogTitle-BWg8K7UO.js";import"./dialogTitleClasses-BqOfRSGi.js";import"./DialogActions-FhQa7Naj.js";import"./DialogContent-DwpRh2db.js";import"./DialogContentText-Cpbos6Hc.js";import"./index-DMWJB0MK.js";import"./dayjs.min-DnLU8EWa.js";import"./Img-DYob_IF5.js";import"./index-Dk74W0Oi.js";import"./index-BqJWUzrs.js";import"./index-BtPDELIe.js";import"./Stack-BcsM4Vc0.js";import"./styled-vBjI3IQc.js";import"./useThemeProps-BIK-1DKm.js";import"./LinearProgress-aFncvf6g.js";import"./Box-DWnb8VZB.js";import"./index-Cyp_3NIz.js";import"./Grid2-y9MPRr2u.js";import"./isMuiElement-DAzOCCHH.js";import"./Container-Cqq8qXXg.js";const de={title:"Components/Spaces/SpacesImage",component:o,tags:["autodocs"],argTypes:{spaceId:{table:{readonly:!0}}}},p=new f,i={render:e=>r.jsx(a,{client:p,children:r.jsx(s,{spaceIds:["11","22","33"],children:r.jsx(o,{...e})})}),args:{spaceId:"33"}},t={render:e=>r.jsx(a,{client:p,children:r.jsx(s,{spaceIds:["11","22","33"],children:r.jsx(o,{...e})})}),args:{spaceId:"11",imageType:"images.logo"}},m={render:e=>r.jsx(a,{client:p,children:r.jsx(s,{spaceIds:["11","22","33"],children:r.jsx(o,{...e})})}),args:{spaceId:"22",imageType:"images.tile"}},c={render:e=>r.jsx(a,{client:p,children:r.jsx(s,{spaceIds:["11","22","33"],children:r.jsx(o,{...e})})}),args:{spaceId:"22",imageType:"images.billboard"}},n={render:e=>r.jsx(a,{client:p,children:r.jsx(s,{spaceIds:["11","22","33"],children:r.jsx(_,{children:r.jsx(T,{...e})})})}),args:{title:"Card header",subheader:"Subheader",statusChipProps:{position:"right",label:"Approved",color:"success"},logo:r.jsx(o,{spaceId:"11",imageType:"images.logo"})}};var d,l,g;i.parameters={...i.parameters,docs:{...(d=i.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: (args: SpacesImageProps) => <QueryClientProvider client={queryClient}>
+      <Spaces spaceIds={['11', '22', '33']}>
+        <SpacesImage {...args} />
+      </Spaces>
+    </QueryClientProvider>,
+  args: {
+    spaceId: '33'
+  }
+}`,...(g=(l=i.parameters)==null?void 0:l.docs)==null?void 0:g.source}}};var u,S,I;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: (args: SpacesImageProps) => <QueryClientProvider client={queryClient}>
+      <Spaces spaceIds={['11', '22', '33']}>
+        <SpacesImage {...args} />
+      </Spaces>
+    </QueryClientProvider>,
+  args: {
+    spaceId: '11',
+    imageType: 'images.logo'
+  }
+}`,...(I=(S=t.parameters)==null?void 0:S.docs)==null?void 0:I.source}}};var C,y,h;m.parameters={...m.parameters,docs:{...(C=m.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: (args: SpacesImageProps) => <QueryClientProvider client={queryClient}>
+      <Spaces spaceIds={['11', '22', '33']}>
+        <SpacesImage {...args} />
+      </Spaces>
+    </QueryClientProvider>,
+  args: {
+    spaceId: '22',
+    imageType: 'images.tile'
+  }
+}`,...(h=(y=m.parameters)==null?void 0:y.docs)==null?void 0:h.source}}};var x,j,P;c.parameters={...c.parameters,docs:{...(x=c.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: (args: SpacesImageProps) => <QueryClientProvider client={queryClient}>
+      <Spaces spaceIds={['11', '22', '33']}>
+        <SpacesImage {...args} />
+      </Spaces>
+    </QueryClientProvider>,
+  args: {
+    spaceId: '22',
+    imageType: 'images.billboard'
+  }
+}`,...(P=(j=c.parameters)==null?void 0:j.docs)==null?void 0:P.source}}};var b,v,Q;n.parameters={...n.parameters,docs:{...(b=n.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: (args: CardHeaderProps) => <QueryClientProvider client={queryClient}>
+      <Spaces spaceIds={['11', '22', '33']}>
+        <Card>
+          <CardHeader {...args} />
+        </Card>
+      </Spaces>
+    </QueryClientProvider>,
+  args: {
+    title: 'Card header',
+    subheader: 'Subheader',
+    statusChipProps: {
+      position: 'right',
+      label: 'Approved',
+      color: 'success'
+    },
+    logo: <SpacesImage spaceId="11" imageType="images.logo" />
+  }
+}`,...(Q=(v=n.parameters)==null?void 0:v.docs)==null?void 0:Q.source}}};const le=["_SpacesImage","_SpacesLogo","_SpacesTile","_SpacesBillboard","_CardHeaderWithLogo"];export{n as _CardHeaderWithLogo,c as _SpacesBillboard,i as _SpacesImage,t as _SpacesLogo,m as _SpacesTile,le as __namedExportsOrder,de as default};
