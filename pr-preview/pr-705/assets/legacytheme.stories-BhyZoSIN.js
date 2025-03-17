4cb83aa5d65760b214f1693d1ab78d914b9c22cb
jordan-a-young <jordan-a-young@users.noreply.github.com>
2025-03-17 14:15:11 +0000

    Remove preview for PR 705 🛬

--- pr-preview/pr-705/assets/legacytheme.stories-BhyZoSIN.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./jsx-runtime-CcL-W3nW.js";import{l as n}from"./legacy-theme-CvWFs_ol.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DXGSQb0h.js";const d={title:"Design System/Theme/Legacy",parameters:{docs:{description:{component:`Raw theme object (with component overrides removed for brevity).
-
-Useful for knowing what values are directly accessible through System Properties.`}}}},{components:h,...a}=n,e={render:()=>t.jsx("pre",{children:JSON.stringify(a,null,2)})};var r,o,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  render: () => <pre>{JSON.stringify(legacyThemeWOOverrides, null, 2)}</pre>
-}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const g=["RawTheme"];export{e as RawTheme,g as __namedExportsOrder,d as default};
