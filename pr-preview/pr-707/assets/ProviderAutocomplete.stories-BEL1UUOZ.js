b8e5468867c7980c95de33f7e18726975127a4b5
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-03-21 14:18:01 +0000

    Deploy preview for PR 707 🛫

---
+++ pr-preview/pr-707/assets/ProviderAutocomplete.stories-BEL1UUOZ.js
@@ -0,0 +1,37 @@
+import{j as t}from"./jsx-runtime-CcL-W3nW.js";import{e as a}from"./TextField-Cphqymkg.js";import{B as p}from"./index-DLsH_p52.js";import{P as c}from"./index-BrWjJmRs.js";import{T as f}from"./index-BtPDELIe.js";import{G as S}from"./index-Cyp_3NIz.js";import{P as h,a as P}from"./Types-DO5E-3ha.js";import"./AsyncAutocomplete-Btuv64PF.js";import"./Checkbox-CXPubYjX.js";import{u as b,F as g}from"./index.esm-D1k4U55D.js";import{i as v}from"./suspense-BqeUBMhm.js";import{Q as C}from"./queryClient-CjDmLjqo.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-DQhzKoXp.js";import"./index-Bvghr9Hi.js";import"./index-H2z8eHTn.js";import"./index-DMWJB0MK.js";import"./index-EBJ2OYCQ.js";import"./faCircleArrowRight-BeBmJtPK.js";import"./faUser-DXPB7Q64.js";import"./faSortDown-nq-jkck6.js";import"./SvgIcon-DSdWmKAH.js";import"./generateUtilityClass-CVWqFxsA.js";import"./identifier-Dtqimryx.js";import"./memoTheme-D-rVhYRp.js";import"./styled-CF1hcdwO.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./faCheck-B05F4ORy.js";import"./Select-hLZ2S4VH.js";import"./formControlState-Dq1zat_P.js";import"./useFormControl-68oiojFJ.js";import"./Menu-Byu17A6t.js";import"./index-nos-0K8U.js";import"./useSlotProps-BAIpVL6Y.js";import"./resolveComponentProps-Dg20wxiM.js";import"./useForkRef-BVrIj09m.js";import"./Popover-ChEuM7rO.js";import"./isHostComponent-DVu5iVWx.js";import"./useSlot-DnkSxE3X.js";import"./Portal-CBAx_nZm.js";import"./useTheme-DUkCvbx_.js";import"./useTheme-C2CrmYKc.js";import"./utils-D5ZkK9XY.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";import"./TransitionGroupContext-D-IBNIfW.js";import"./index-BEx9gAkE.js";import"./useTimeout-B8wmcRZV.js";import"./useEnhancedEffect-D2f2tbda.js";import"./getReactElementRef-AnOffBoZ.js";import"./ownerDocument-DW-IO8s5.js";import"./ownerWindow-HkKU3E4x.js";import"./debounce-Be36O1Ab.js";import"./Paper-DXcIEVia.js";import"./Modal-B5s0Thvt.js";import"./createChainedFunction-BO_9K8Jh.js";import"./Backdrop-B4gbyDGC.js";import"./Fade-Dl4gmST4.js";import"./List-ChBzRQcW.js";import"./utils-DoM3o7-Q.js";import"./useControlled-WdbyInBW.js";import"./useId-DszduLV4.js";import"./createSvgIcon-TketyoFF.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./OutlinedInput-CQlxW3ax.js";import"./index-f0Aa_BgU.js";import"./extendSxProp-B5AgUjQT.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./FormLabel-98YPf6ct.js";import"./FormControlLabel-GqYWwHWT.js";import"./Typography-CvSvtD2a.js";import"./Switch-BqVUWLaW.js";import"./SwitchBase-Z9YgLJlx.js";import"./ButtonBase-DbVRbsLs.js";import"./isFocusVisible-B8k4qzLc.js";import"./RadioGroup-BogPRRDq.js";import"./Box-DWnb8VZB.js";import"./Divider-jUONNV4j.js";import"./dividerClasses-BmOcEkDu.js";import"./Stack-BcsM4Vc0.js";import"./styled-vBjI3IQc.js";import"./useThemeProps-BIK-1DKm.js";import"./TextField-BxbLM7nG.js";import"./FormControl-BPSU2g7t.js";import"./isMuiElement-DAzOCCHH.js";import"./Grid2-y9MPRr2u.js";import"./index-D1JqcBhz.js";import"./index-uZUcOW5W.js";import"./___vite-browser-external_commonjs-proxy-DRaEfepn.js";import"./_toKey-MoF_TmUD.js";import"./Autocomplete-DYfkOmMd.js";import"./Close-CpXO-CFZ.js";import"./usePreviousProps-BYCwus4J.js";import"./Tooltip-CiiaKkqp.js";import"./Chip-ALB5H4lg.js";import"./IconButton-Caxu-qgA.js";import"./CircularProgress-D8w9ZPs6.js";import"./ListSubheader-DDrjAA_E.js";import"./useInfiniteQuery-BlwNwEaC.js";import"./useBaseQuery-CJ5trF24.js";import"./index-BbNhb2zs.js";import"./DatePicker--IEYhjMf.js";import"./useThemeProps-NV-uCBd-.js";import"./index-Dk74W0Oi.js";import"./enUS-BqLCxLyo.js";import"./visuallyHidden-Dan1xhjv.js";import"./InputAdornment-DYZl9z8K.js";import"./Button-BAEbeAyH.js";import"./DialogActions-FhQa7Naj.js";import"./ListItem-BaRQg_q0.js";import"./Dialog-bGx9ZkTF.js";import"./DialogContext-DL4ikWki.js";import"./DialogContent-DwpRh2db.js";import"./dialogTitleClasses-BqOfRSGi.js";import"./LoadingButton-9DxABi24.js";import"./Container-Cqq8qXXg.js";import"./index-B-bCkWqS.js";import"./Checkbox-imGzR8Us.js";const Vt={title:"Form Components/Controlled Form/Autocomplete/ControlledProviderAutocomplete",component:a,tags:["autodocs"],argTypes:{...P,...h}},y=new C({defaultOptions:{queries:{refetchOnWindowFocus:!1}}}),o={render:n=>{var i,e,m;const r=b();return t.jsx(v,{client:y,children:t.jsx(g,{...r,children:t.jsxs("form",{onSubmit:r.handleSubmit(u=>u),children:[t.jsx(a,{...n}),t.jsxs(S,{container:!0,direction:"row",justifyContent:"space-between",marginTop:1,children:[t.jsx(p,{disabled:!((i=r==null?void 0:r.formState)!=null&&i.isSubmitSuccessful),children:"Reset",color:"secondary",onClick:()=>r.reset()}),t.jsx(p,{type:"submit",disabled:(e=r==null?void 0:r.formState)==null?void 0:e.isSubmitSuccessful,children:"Submit"})]}),(m=r==null?void 0:r.formState)!=null&&m.isSubmitSuccessful?t.jsxs(c,{sx:{padding:"1.5rem",marginTop:"1.5rem"},children:[t.jsx(f,{variant:"h2",children:"Submitted Values"}),t.jsx("pre",{"data-testid":"result",children:JSON.stringify(r.getValues(),null,2)})]}):null]})})})},args:{name:"controlledProviderAutocomplete",FieldProps:{label:"Provider Select",helperText:"Select a Provider from the list",placeholder:"Select...",fullWidth:!1},limit:10,customerId:"1234",rules:{required:"This is required."}}};var s,l,d;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: args => {
+    const methods = useForm();
+    return <QueryClientProvider client={client}>
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(data => data)}>
+            <ControlledProviderAutocomplete {...args} />
+            <Grid container direction="row" justifyContent="space-between" marginTop={1}>
+              <Button disabled={!methods?.formState?.isSubmitSuccessful} children="Reset" color="secondary" onClick={() => methods.reset()} />
+              <Button type="submit" disabled={methods?.formState?.isSubmitSuccessful} children="Submit" />
+            </Grid>
+            {methods?.formState?.isSubmitSuccessful ? <Paper sx={{
+            padding: '1.5rem',
+            marginTop: '1.5rem'
+          }}>
+                <Typography variant="h2">Submitted Values</Typography>
+                <pre data-testid="result">{JSON.stringify(methods.getValues(), null, 2)}</pre>
+              </Paper> : null}
+          </form>
+        </FormProvider>
+      </QueryClientProvider>;
+  },
+  args: {
+    name: 'controlledProviderAutocomplete',
+    FieldProps: {
+      label: 'Provider Select',
+      helperText: 'Select a Provider from the list',
+      placeholder: 'Select...',
+      fullWidth: false
+    },
+    limit: 10,
+    customerId: '1234',
+    rules: {
+      required: 'This is required.'
+    }
+  }
+}`,...(d=(l=o.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};const _t=["_ControlledProviderAutoComplete"];export{o as _ControlledProviderAutoComplete,_t as __namedExportsOrder,Vt as default};
