7cc3239ead2267fddf273d525e32e4a66ffe7d6e
LauRoxx <LauRoxx@users.noreply.github.com>
2025-03-24 17:11:49 +0000

    Deploy preview for PR 708 🛫

---
+++ pr-preview/pr-708/assets/Utils.stories-eGfUwxgR.js
@@ -0,0 +1,13 @@
+import{j as r}from"./jsx-runtime-CcL-W3nW.js";import{P as h}from"./index-BrWjJmRs.js";import{T as e}from"./index-BtPDELIe.js";import{v as o}from"./visuallyHidden-Dan1xhjv.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Paper-DXcIEVia.js";import"./generateUtilityClass-CVWqFxsA.js";import"./identifier-Dtqimryx.js";import"./useTheme-DUkCvbx_.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./useTheme-C2CrmYKc.js";import"./styled-CF1hcdwO.js";import"./memoTheme-D-rVhYRp.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./Typography-CvSvtD2a.js";import"./index-f0Aa_BgU.js";import"./extendSxProp-B5AgUjQT.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";const M={title:"Utils/Utils",tags:["autodocs"]},i={render:()=>r.jsxs(h,{sx:{padding:"12px 16px"},children:[r.jsx(e,{variant:"h3",children:"Raw"}),r.jsx("pre",{children:JSON.stringify(o,null,2)}),r.jsx(e,{variant:"h3",children:"Example"}),r.jsx(e,{children:"This text is visible"}),r.jsx(e,{sx:o,children:"This text is visually hidden"})]})};i.storyName="visuallyHidden";const s={render:()=>r.jsx("div",{children:"More utilities to be added"})};var t,a,p,d,n;i.parameters={...i.parameters,docs:{...(t=i.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: () => <Paper sx={{
+    padding: '12px 16px'
+  }}>
+      <Typography variant="h3">Raw</Typography>
+      <pre>{JSON.stringify(visuallyHidden, null, 2)}</pre>
+      <Typography variant="h3">Example</Typography>
+      <Typography>This text is visible</Typography>
+      <Typography sx={visuallyHidden}>This text is visually hidden</Typography>
+    </Paper>
+}`,...(p=(a=i.parameters)==null?void 0:a.docs)==null?void 0:p.source},description:{story:"css properties to visually hide content while leaving it accessible to assistive technologies.",...(n=(d=i.parameters)==null?void 0:d.docs)==null?void 0:n.description}}};var l,m,c;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: () => <div>More utilities to be added</div>
+}`,...(c=(m=s.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const U=["_visuallyhidden","_ComingSoon"];export{s as _ComingSoon,U as __namedExportsOrder,i as _visuallyhidden,M as default};
