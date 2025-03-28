8d948624415a6fda8118763b03899d736b7e8fe9
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-03-28 18:56:52 +0000

    Deploy preview for PR 713 🛫

---
+++ pr-preview/pr-713/assets/useThemeProps-NV-uCBd-.js
@@ -0,0 +1,1 @@
+import{d as m}from"./DefaultPropsProvider-_cNB_dWy.js";import{T as o}from"./identifier-Dtqimryx.js";import{u as s}from"./useThemeProps-BIK-1DKm.js";function a({props:e,name:r}){return s({props:e,name:r,defaultTheme:m,themeId:o})}export{a as u};
