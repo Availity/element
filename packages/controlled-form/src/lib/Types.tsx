36f7772ae2f21a597879cbc52904080df015a642
Greg Martin <greg.martin@availity.com>
2025-03-28 16:15:29 -0400

    refactor(mui-controlled-form): implement default selection in ProviderAutocomplete

--- packages/controlled-form/src/lib/Types.tsx
+++ packages/controlled-form/src/lib/Types.tsx
@@ -242,6 +242,8 @@ export const RadioGroupPropsCategorized: RadioGroupPropsObject = {
 
 export const ProviderAutocompletePropsCategorized: ProviderAutocompletePropsObject = {
   classes: { table: { category: 'Input Props' } },
+  defaultToFirstOption: { table: { category: 'Input Props' } },
+  defaultToOnlyOption: { table: { category: 'Input Props' } },
   id: { table: { category: 'Input Props' } },
   onKeyDown: { table: { category: 'Input Props' } },
   sx: { table: { category: 'Input Props' } },
