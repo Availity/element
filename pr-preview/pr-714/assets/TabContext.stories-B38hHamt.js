98c7e41d1e94e4b173ee1719c26d5ea0962b53c9
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-03-28 20:23:47 +0000

    Deploy preview for PR 714 🛫

---
+++ pr-preview/pr-714/assets/TabContext.stories-B38hHamt.js
@@ -0,0 +1,18 @@
+import{j as e}from"./jsx-runtime-CcL-W3nW.js";import{T as l,a as s,b as t}from"./TabPanel-DXVr1xqK.js";import{D as n}from"./index-AUbJaabt.js";import{T as r}from"./Tab-B14qCAFG.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./TabList-3n6T6Cz4.js";import"./Tabs-Q_4h7ycJ.js";import"./generateUtilityClass-CVWqFxsA.js";import"./identifier-Dtqimryx.js";import"./memoTheme-D-rVhYRp.js";import"./styled-CF1hcdwO.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./ButtonBase-DbVRbsLs.js";import"./useTimeout-B8wmcRZV.js";import"./useEnhancedEffect-D2f2tbda.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";import"./TransitionGroupContext-D-IBNIfW.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./useForkRef-BVrIj09m.js";import"./isFocusVisible-B8k4qzLc.js";import"./index-nos-0K8U.js";import"./useTheme-DUkCvbx_.js";import"./useTheme-C2CrmYKc.js";import"./debounce-Be36O1Ab.js";import"./ownerWindow-HkKU3E4x.js";import"./ownerDocument-DW-IO8s5.js";import"./KeyboardArrowRight-PXT6z23P.js";import"./createSvgIcon-TketyoFF.js";import"./SvgIcon-DSdWmKAH.js";import"./useSlotProps-BAIpVL6Y.js";import"./resolveComponentProps-Dg20wxiM.js";import"./useThemeProps-NV-uCBd-.js";import"./useThemeProps-BIK-1DKm.js";import"./Divider-jUONNV4j.js";import"./dividerClasses-BmOcEkDu.js";const V={title:"Components/Tabs/TabContext",component:l,tags:["autodocs"]},a={render:p=>e.jsxs(l,{...p,children:[e.jsxs(s,{onChange:()=>{},"aria-label":"lab API tabs example",children:[e.jsx(r,{label:"Item One",value:"1"}),e.jsx(r,{label:"Item Two",value:"2"}),e.jsx(r,{label:"Item Three",value:"3"})]}),e.jsx(n,{}),e.jsx(t,{value:"1",children:"Item One"}),e.jsx(t,{value:"2",children:"Item Two"}),e.jsx(t,{value:"3",children:"Item Three"})]}),args:{value:"1"}};var o,m,i;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: (args: TabContextProps) => {
+    return <TabContext {...args}>
+        <TabList onChange={() => {}} aria-label="lab API tabs example">
+          <Tab label="Item One" value="1" />
+          <Tab label="Item Two" value="2" />
+          <Tab label="Item Three" value="3" />
+        </TabList>
+        <Divider />
+        <TabPanel value="1">Item One</TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+      </TabContext>;
+  },
+  args: {
+    value: '1'
+  }
+}`,...(i=(m=a.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};const W=["_TabContext"];export{a as _TabContext,W as __namedExportsOrder,V as default};
