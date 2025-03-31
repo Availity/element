fe98cfa34f8a78fb64b0eab864f5476ac2b1d638
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-03-31 19:40:51 +0000

    Deploy preview for PR 719 🛫

---
+++ pr-preview/pr-719/assets/TableSortLabel.stories-CLYvzgAy.js
@@ -0,0 +1,46 @@
+import{j as e}from"./jsx-runtime-CcL-W3nW.js";import{T as i}from"./TableSortLabel-CuFJtD9P.js";import{G as r}from"./index-Cyp_3NIz.js";import{r as b}from"./index-DcWiA9TO.js";import"./index-CTQn_wDL.js";import"./faCircleArrowRight-6L8v8FIg.js";import"./faUser-DXPB7Q64.js";import"./faSortDown-nq-jkck6.js";import"./SvgIcon-DSdWmKAH.js";import"./generateUtilityClass-CVWqFxsA.js";import"./identifier-Dtqimryx.js";import"./memoTheme-D-rVhYRp.js";import"./styled-CF1hcdwO.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./Box-DWnb8VZB.js";import"./useTheme-C2CrmYKc.js";import"./extendSxProp-B5AgUjQT.js";import"./visuallyHidden-Dan1xhjv.js";import"./TableSortLabel-IF83hpqd.js";import"./createSvgIcon-TketyoFF.js";import"./useSlot-DnkSxE3X.js";import"./resolveComponentProps-Dg20wxiM.js";import"./useForkRef-BVrIj09m.js";import"./ButtonBase-DbVRbsLs.js";import"./useTimeout-B8wmcRZV.js";import"./useEnhancedEffect-D2f2tbda.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";import"./TransitionGroupContext-D-IBNIfW.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./isFocusVisible-B8k4qzLc.js";import"./Grid2-y9MPRr2u.js";import"./useTheme-DUkCvbx_.js";import"./styled-vBjI3IQc.js";import"./isMuiElement-DAzOCCHH.js";import"./useThemeProps-BIK-1DKm.js";import"./Stack-BcsM4Vc0.js";import"./Container-Cqq8qXXg.js";import"./_commonjsHelpers-BosuxZz1.js";const ee={title:"Components/Table/TableSortLabel",component:i,tags:["autodocs"],args:{children:"TableSortLabel content"}},t={render:p=>{const[o,x]=b.useState();return e.jsx(i,{active:!!o,direction:o,onClick:()=>x(o==="asc"?"desc":"asc"),...p})}},n={render:()=>e.jsxs(r,{container:!0,spacing:1,children:[e.jsx(r,{size:{xs:3},children:"Default"}),e.jsx(r,{size:{xs:9},children:e.jsx(i,{children:"Column Header"})}),e.jsx(r,{size:{xs:3},children:"Active Undefined"}),e.jsx(r,{size:{xs:9},children:e.jsx(i,{active:!0,children:"Column Header"})}),e.jsx(r,{size:{xs:3},children:"Active Ascending"}),e.jsx(r,{size:{xs:9},children:e.jsx(i,{active:!0,direction:"asc",children:"Column Header"})}),e.jsx(r,{size:{xs:3},children:"Active Descending"}),e.jsx(r,{size:{xs:9},children:e.jsx(i,{active:!0,direction:"desc",children:"Column Header"})})]})};var s,a,d;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: (args: TableSortLabelProps) => {
+    type Order = 'asc' | 'desc' | undefined;
+    const [order, setOrder] = useState<Order>();
+    return <TableSortLabel active={!!order} direction={order} onClick={() => setOrder(order === 'asc' ? 'desc' : 'asc')} {...args} />;
+  }
+}`,...(d=(a=t.parameters)==null?void 0:a.docs)==null?void 0:d.source}}};var c,m,l;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: () => <Grid container spacing={1}>
+      <Grid size={{
+      xs: 3
+    }}>Default</Grid>
+      <Grid size={{
+      xs: 9
+    }}>
+        <TableSortLabel>Column Header</TableSortLabel>
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>Active Undefined</Grid>
+      <Grid size={{
+      xs: 9
+    }}>
+        <TableSortLabel active>Column Header</TableSortLabel>
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>Active Ascending</Grid>
+      <Grid size={{
+      xs: 9
+    }}>
+        <TableSortLabel active direction="asc">
+          Column Header
+        </TableSortLabel>
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>Active Descending</Grid>
+      <Grid size={{
+      xs: 9
+    }}>
+        <TableSortLabel active direction="desc">
+          Column Header
+        </TableSortLabel>
+      </Grid>
+    </Grid>
+}`,...(l=(m=n.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};const re=["_TableSortLabel","_Variants"];export{t as _TableSortLabel,n as _Variants,re as __namedExportsOrder,ee as default};
