b49e41b6775a971acad554d3fff0bb281f36cfac
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-04-01 18:53:07 +0000

    Deploy preview for PR 722 🛫

---
+++ pr-preview/pr-722/assets/introduction-CvEYYje-.js
@@ -0,0 +1,133 @@
+import{j as e}from"./jsx-runtime-CcL-W3nW.js";import{useMDXComponents as m}from"./index-CfSBfIZi.js";import{M as p,c as i}from"./index-O46QuFKr.js";import"./DocsRenderer-CFRXHY34-ChBkcBCN.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Cw4QivJ_.js";import"./index-BEx9gAkE.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./client-BqYpGkAY.js";const o=`# @availity/mui-paper
+
+> Availity MUI Paper component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-paper.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-paper)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-paper.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-paper)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-paper?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-paper/package.json)
+
+## Documentation
+
+This package extends the MUI Paper component: [MUI Paper Docs](https://mui.com/components/paper/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-paper-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://design.availity.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/mui-paper
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-paper
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Paper } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Paper } from '@availity/mui-paper';
+\`\`\`
+`,r=`# Changelog
+
+This file was generated using [@jscutlery/semver](https://github.com/jscutlery/semver).
+
+## [1.0.1](https://github.com/Availity/element/compare/@availity/mui-paper@1.0.0...@availity/mui-paper@1.0.1) (2025-03-07)
+
+## [1.0.0](https://github.com/Availity/element/compare/@availity/mui-paper@1.0.0-alpha.0...@availity/mui-paper@1.0.0) (2025-02-25)
+
+
+### ⚠ BREAKING CHANGES
+
+* upgrade to @mui/material v6
+
+### Miscellaneous Chores
+
+* element v1 release ([a6e3567](https://github.com/Availity/element/commit/a6e35671185b9f13d25c7a39c4488ecb8774633e))
+
+## [1.0.0-alpha.0](https://github.com/Availity/element/compare/@availity/mui-paper@0.2.0...@availity/mui-paper@1.0.0-alpha.0) (2025-02-24)
+
+
+### ⚠ BREAKING CHANGES
+
+* upgraded to @mui/material v6
+* **element:** upgraded to @mui/material v6
+
+### Features
+
+* **element:** upgrade to @mui/material v6 ([cb958bb](https://github.com/Availity/element/commit/cb958bba99a4f1ee6dab323f0ff54b69e6fd3493))
+* upgrade @mui/material ([571453a](https://github.com/Availity/element/commit/571453a34b21c344594ab4c03bc497d19aba942b))
+* upgrade to MUI v6 ([7febd6f](https://github.com/Availity/element/commit/7febd6fd4fd58e87e1c97a832cea3b4595a35d58))
+
+## [0.2.0](https://github.com/Availity/element/compare/@availity/mui-paper@0.1.9...@availity/mui-paper@0.2.0) (2025-02-04)
+
+
+### Features
+
+* configure package.json to allow esm to be used ([5a5393d](https://github.com/Availity/element/commit/5a5393de761f52608e714dd94a05106937dd95db))
+
+## [0.1.9](https://github.com/Availity/element/compare/@availity/mui-paper@0.1.8...@availity/mui-paper@0.1.9) (2024-04-19)
+
+## [0.1.8](https://github.com/Availity/element/compare/@availity/mui-paper@0.1.7...@availity/mui-paper@0.1.8) (2024-04-04)
+
+
+### Performance Improvements
+
+* **mui-paper:** use path imports for material deps ([fa4bcf2](https://github.com/Availity/element/commit/fa4bcf27a1f0946593f71634fe2ba0408c5729cb))
+
+## [0.1.7](https://github.com/Availity/element/compare/@availity/mui-paper@0.1.6...@availity/mui-paper@0.1.7) (2024-02-20)
+
+## [0.1.6](https://github.com/Availity/element/compare/@availity/mui-paper@0.1.5...@availity/mui-paper@0.1.6) (2023-11-16)
+
+### Bug Fixes
+
+- **mui-paper:** add back sx prop ([71495e1](https://github.com/Availity/element/commit/71495e1855ddfcc5f7c5c7f4bb2c98fbe37d5252))
+
+## [0.1.5](https://github.com/Availity/element/compare/@availity/mui-paper@0.1.4...@availity/mui-paper@0.1.5) (2023-11-08)
+
+## [0.1.4](https://github.com/Availity/element/compare/@availity/mui-paper@0.1.3...@availity/mui-paper@0.1.4) (2023-11-07)
+
+## [0.1.3](https://github.com/Availity/element/compare/@availity/mui-paper@0.1.2...@availity/mui-paper@0.1.3) (2023-10-06)
+
+## [0.1.2](https://github.com/Availity/element/compare/@availity/mui-paper@0.1.1...@availity/mui-paper@0.1.2) (2023-08-23)
+
+## [0.1.1](https://github.com/Availity/element/compare/@availity/mui-paper@0.1.0...@availity/mui-paper@0.1.1) (2023-05-23)
+
+## 0.1.0 (2023-05-22)
+
+### Features
+
+- **mui-paper:** add paper component ([4bf7776](https://github.com/Availity/element/commit/4bf7776a0f19e3566983e176901233a6292fa745))
+- **mui-paper:** refactor how props are spread ([ecddf69](https://github.com/Availity/element/commit/ecddf6908fdb69418705cb854aa84c419de3ee55))
+`;function n(t){return e.jsxs(e.Fragment,{children:[e.jsx(p,{title:"Components/Paper/Introduction"}),`
+`,e.jsx(i,{children:o}),`
+`,e.jsx(i,{children:r})]})}function A(t={}){const{wrapper:a}={...m(),...t.components};return a?e.jsx(a,{...t,children:e.jsx(n,{...t})}):n()}export{A as default};
