1c0bd70774ddcf3765f2ac41eb59c710e7520d01
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-04-02 21:07:33 +0000

    Deploy preview for PR 723 🛫

---
+++ pr-preview/pr-723/assets/Button.stories-8mCzQPix.js
@@ -0,0 +1,128 @@
+import{j as r}from"./jsx-runtime-CcL-W3nW.js";import{G as n,B as A}from"./index-Cyp_3NIz.js";import{A as d,c as l}from"./index-BTr7iyfv.js";import{B as o}from"./Button-DVND9WLW.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Box-DWnb8VZB.js";import"./identifier-Dtqimryx.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./generateUtilityClass-CVWqFxsA.js";import"./useTheme-C2CrmYKc.js";import"./extendSxProp-B5AgUjQT.js";import"./Grid2-y9MPRr2u.js";import"./useTheme-DUkCvbx_.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./styled-vBjI3IQc.js";import"./styled-CF1hcdwO.js";import"./isMuiElement-DAzOCCHH.js";import"./useThemeProps-BIK-1DKm.js";import"./Stack-BcsM4Vc0.js";import"./Container-Cqq8qXXg.js";import"./faCircleArrowRight-6L8v8FIg.js";import"./faUser-DXPB7Q64.js";import"./faSortDown-nq-jkck6.js";import"./SvgIcon-DSdWmKAH.js";import"./memoTheme-D-rVhYRp.js";import"./Button-DDPbJ3HZ.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./useId-DszduLV4.js";import"./ButtonBase-DbVRbsLs.js";import"./useTimeout-B8wmcRZV.js";import"./useEnhancedEffect-D2f2tbda.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";import"./TransitionGroupContext-D-IBNIfW.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./useForkRef-BVrIj09m.js";import"./isFocusVisible-B8k4qzLc.js";import"./CircularProgress-D8w9ZPs6.js";const pr={title:"Components/Button/Button",component:o,tags:["autodocs"]},i={render:({...t})=>r.jsx(o,{...t,children:"Button"}),args:{color:"primary",size:"large"}},e={render:()=>r.jsxs(n,{container:!0,spacing:1,children:[r.jsx(n,{size:{xs:12},children:r.jsx(o,{color:"primary",children:"Primary"})}),r.jsx(n,{size:{xs:12},children:r.jsx(o,{color:"secondary",children:"Secondary"})}),r.jsx(n,{size:{xs:12},children:r.jsx(o,{color:"tertiary",children:"Tertiary"})})]})},s={render:()=>r.jsxs(n,{container:!0,spacing:1,children:[r.jsx(n,{size:{xs:12},children:r.jsx(o,{color:"tertiary",size:"small",children:"Small Button"})}),r.jsx(n,{size:{xs:12},children:r.jsx(o,{color:"secondary",size:"medium",children:"Medium Button"})}),r.jsx(n,{size:{xs:12},children:r.jsx(o,{color:"primary",size:"large",children:"Large Button"})})]})},c={render:()=>r.jsxs(n,{container:!0,spacing:1,children:[r.jsx(n,{size:{xs:3},children:r.jsx(o,{color:"primary",size:"large",startIcon:r.jsx(d,{}),children:"Large"})}),r.jsx(n,{size:{xs:3},children:r.jsx(o,{color:"primary",size:"medium",startIcon:r.jsx(d,{}),children:"Medium"})}),r.jsx(n,{size:{xs:3},children:r.jsx(o,{color:"primary",size:"small",startIcon:r.jsx(d,{}),children:"Small"})}),r.jsx(n,{size:{xs:3}}),r.jsx(n,{size:{xs:3},children:r.jsx(o,{color:"primary",size:"large",endIcon:r.jsx(l,{}),children:"Large"})}),r.jsx(n,{size:{xs:3},children:r.jsx(o,{color:"primary",size:"medium",endIcon:r.jsx(l,{}),children:"Medium"})}),r.jsx(n,{size:{xs:3},children:r.jsx(o,{color:"primary",size:"small",endIcon:r.jsx(l,{}),children:"Small"})}),r.jsx(n,{size:{xs:3}})]})},a={render:()=>{const t={mr:1,mb:1};return r.jsxs(A,{sx:{display:"flex",flexWrap:"wrap",mr:-1,mb:-1},children:[r.jsx(o,{color:"tertiary",sx:t,children:"Button 1"}),r.jsx(o,{color:"tertiary",sx:t,children:"Button 2"}),r.jsx(o,{color:"tertiary",sx:t,children:"Button 3"}),r.jsx(o,{color:"tertiary",sx:t,children:"Button 4"})]})}};var m,u,p;i.parameters={...i.parameters,docs:{...(m=i.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: ({
+    ...args
+  }: ButtonProps) => <Button {...args}>Button</Button>,
+  args: {
+    color: 'primary',
+    size: 'large'
+  }
+}`,...(p=(u=i.parameters)==null?void 0:u.docs)==null?void 0:p.source}}};var x,B,y,z,j;e.parameters={...e.parameters,docs:{...(x=e.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: () => <Grid container spacing={1}>
+      <Grid size={{
+      xs: 12
+    }}>
+        <Button color="primary">Primary</Button>
+      </Grid>
+      <Grid size={{
+      xs: 12
+    }}>
+        <Button color="secondary">Secondary</Button>
+      </Grid>
+      <Grid size={{
+      xs: 12
+    }}>
+        <Button color="tertiary">Tertiary</Button>
+      </Grid>
+    </Grid>
+}`,...(y=(B=e.parameters)==null?void 0:B.docs)==null?void 0:y.source},description:{story:"Availity approved button colors and variations",...(j=(z=e.parameters)==null?void 0:z.docs)==null?void 0:j.description}}};var g,h,G;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: () => <Grid container spacing={1}>
+      <Grid size={{
+      xs: 12
+    }}>
+        <Button color="tertiary" size="small">
+          Small Button
+        </Button>
+      </Grid>
+      <Grid size={{
+      xs: 12
+    }}>
+        <Button color="secondary" size="medium">
+          Medium Button
+        </Button>
+      </Grid>
+      <Grid size={{
+      xs: 12
+    }}>
+        <Button color="primary" size="large">
+          Large Button
+        </Button>
+      </Grid>
+    </Grid>
+}`,...(G=(h=s.parameters)==null?void 0:h.docs)==null?void 0:G.source}}};var I,S,f;c.parameters={...c.parameters,docs:{...(I=c.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: () => <Grid container spacing={1}>
+      <Grid size={{
+      xs: 3
+    }}>
+        <Button color="primary" size="large" startIcon={<ArrowCircleLeftIcon />}>
+          Large
+        </Button>
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>
+        <Button color="primary" size="medium" startIcon={<ArrowCircleLeftIcon />}>
+          Medium
+        </Button>
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>
+        <Button color="primary" size="small" startIcon={<ArrowCircleLeftIcon />}>
+          Small
+        </Button>
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }} />
+      <Grid size={{
+      xs: 3
+    }}>
+        <Button color="primary" size="large" endIcon={<ArrowCircleRightIcon />}>
+          Large
+        </Button>
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>
+        <Button color="primary" size="medium" endIcon={<ArrowCircleRightIcon />}>
+          Medium
+        </Button>
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>
+        <Button color="primary" size="small" endIcon={<ArrowCircleRightIcon />}>
+          Small
+        </Button>
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }} />
+    </Grid>
+}`,...(f=(S=c.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};var _,C,w;a.parameters={...a.parameters,docs:{...(_=a.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  render: () => {
+    const groupedButtonStyles = {
+      mr: 1,
+      mb: 1
+    };
+    return <Box sx={{
+      display: 'flex',
+      flexWrap: 'wrap',
+      mr: -1,
+      mb: -1
+    }}>
+        <Button color="tertiary" sx={groupedButtonStyles}>
+          Button 1
+        </Button>
+        <Button color="tertiary" sx={groupedButtonStyles}>
+          Button 2
+        </Button>
+        <Button color="tertiary" sx={groupedButtonStyles}>
+          Button 3
+        </Button>
+        <Button color="tertiary" sx={groupedButtonStyles}>
+          Button 4
+        </Button>
+      </Box>;
+  }
+}`,...(w=(C=a.parameters)==null?void 0:C.docs)==null?void 0:w.source}}};const xr=["_Button","_Colors","_ButtonSizes","_ButtonWithIcon","_DisconnectedButtonGroup"];export{i as _Button,s as _ButtonSizes,c as _ButtonWithIcon,e as _Colors,a as _DisconnectedButtonGroup,xr as __namedExportsOrder,pr as default};
