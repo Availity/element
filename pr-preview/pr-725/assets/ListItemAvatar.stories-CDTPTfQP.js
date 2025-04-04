72bb6b41d8d724f6dc286d603becab29a18f38cf
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-04-04 14:23:09 +0000

    Deploy preview for PR 725 🛫

---
+++ pr-preview/pr-725/assets/ListItemAvatar.stories-CDTPTfQP.js
@@ -0,0 +1,54 @@
+import{j as t}from"./jsx-runtime-CcL-W3nW.js";import{I as m}from"./index-DLsH_p52.js";import{A as n}from"./index-G0n5v8r3.js";import{U as o,f as p}from"./index-CTQn_wDL.js";import{G as a}from"./index-Cyp_3NIz.js";import{T as d}from"./index-BtPDELIe.js";import{f as s,L as c,a as l,c as u,b as h}from"./ListSubheader-CPOqqkHA.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Tooltip-CiiaKkqp.js";import"./generateUtilityClass-CVWqFxsA.js";import"./identifier-Dtqimryx.js";import"./index-nos-0K8U.js";import"./useTheme-DUkCvbx_.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./useTheme-C2CrmYKc.js";import"./styled-CF1hcdwO.js";import"./memoTheme-D-rVhYRp.js";import"./useSlot-DnkSxE3X.js";import"./resolveComponentProps-Dg20wxiM.js";import"./useForkRef-BVrIj09m.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./useTimeout-B8wmcRZV.js";import"./useEnhancedEffect-D2f2tbda.js";import"./useControlled-WdbyInBW.js";import"./useId-DszduLV4.js";import"./getReactElementRef-AnOffBoZ.js";import"./Portal-CBAx_nZm.js";import"./utils-D5ZkK9XY.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";import"./TransitionGroupContext-D-IBNIfW.js";import"./index-BEx9gAkE.js";import"./ownerDocument-DW-IO8s5.js";import"./useSlotProps-BAIpVL6Y.js";import"./isFocusVisible-B8k4qzLc.js";import"./IconButton-Caxu-qgA.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./ButtonBase-DbVRbsLs.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./CircularProgress-D8w9ZPs6.js";import"./Button-BAEbeAyH.js";import"./LoadingButton-9DxABi24.js";import"./index-D2yV3Vr7.js";import"./index-B-wyh3_N.js";import"./___vite-browser-external_commonjs-proxy-DRaEfepn.js";import"./_toKey-MoF_TmUD.js";import"./Avatar-BRBuwdDZ.js";import"./createSvgIcon-TketyoFF.js";import"./SvgIcon-DSdWmKAH.js";import"./faCircleArrowRight-6L8v8FIg.js";import"./faUser-DXPB7Q64.js";import"./faSortDown-nq-jkck6.js";import"./Box-DWnb8VZB.js";import"./extendSxProp-B5AgUjQT.js";import"./Grid2-y9MPRr2u.js";import"./styled-vBjI3IQc.js";import"./isMuiElement-DAzOCCHH.js";import"./useThemeProps-BIK-1DKm.js";import"./Stack-BcsM4Vc0.js";import"./Container-Cqq8qXXg.js";import"./Typography-CvSvtD2a.js";import"./index-f0Aa_BgU.js";import"./List-ChBzRQcW.js";import"./ListItem-BaRQg_q0.js";import"./isHostComponent-DVu5iVWx.js";import"./ListItemText-BAfvFchG.js";import"./listItemTextClasses-QA0fdXLT.js";import"./useThemeProps-NV-uCBd-.js";import"./ListSubheader-DDrjAA_E.js";const Et={title:"Components/List/ListItemAvatar",component:s,tags:["autodocs"],argTypes:{children:{control:!1}}},i={render:r=>t.jsx(s,{...r,children:t.jsx(n,{size:"m",children:t.jsx(o,{})})})},e={render:()=>t.jsxs(a,{container:!0,spacing:8,children:[t.jsxs(a,{size:{xs:12,sm:6},children:[t.jsx(d,{variant:"h6",component:"div",id:"users-default-spacing-header",children:"Default Spacing"}),t.jsx(c,{"aria-labelledby":"users-default-spacing-header",children:[0,1,2,3].map(r=>t.jsx(l,{disablePadding:!0,secondaryAction:t.jsx(m,{title:"message",children:t.jsx(p,{})}),children:t.jsxs(u,{children:[t.jsx(s,{children:t.jsx(n,{size:"m",children:t.jsx(o,{})})}),t.jsx(h,{primary:"List item",secondary:"Secondary"})]})},r))})]}),t.jsxs(a,{size:{xs:12,sm:6},children:[t.jsx(d,{variant:"h6",component:"div",id:"users-dense-spacing-header",children:"Dense Spacing"}),t.jsx(c,{dense:!0,"aria-labelledby":"users-dense-spacing-header",children:[4,5,6,7].map(r=>t.jsx(l,{disablePadding:!0,secondaryAction:t.jsx(m,{title:"message",children:t.jsx(p,{})}),children:t.jsxs(u,{children:[t.jsx(s,{children:t.jsx(n,{size:"m",children:t.jsx(o,{})})}),t.jsx(h,{primary:"List item",secondary:"Secondary"})]})},r))})]})]})};var x,I,L;i.parameters={...i.parameters,docs:{...(x=i.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: (args: ListItemAvatarProps) => <ListItemAvatar {...args}>
+      <Avatar size="m">
+        <UserIcon />
+      </Avatar>
+    </ListItemAvatar>
+}`,...(L=(I=i.parameters)==null?void 0:I.docs)==null?void 0:L.source}}};var g,y,v;e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: () => <Grid container spacing={8}>
+      <Grid size={{
+      xs: 12,
+      sm: 6
+    }}>
+        <Typography variant="h6" component="div" id="users-default-spacing-header">
+          Default Spacing
+        </Typography>
+        <List aria-labelledby="users-default-spacing-header">
+          {[0, 1, 2, 3].map(value => <ListItem key={value} disablePadding secondaryAction={<IconButton title="message">
+                  <MailIcon />
+                </IconButton>}>
+              <ListItemButton>
+                <ListItemAvatar>
+                  <Avatar size="m">
+                    <UserIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="List item" secondary="Secondary" />
+              </ListItemButton>
+            </ListItem>)}
+        </List>
+      </Grid>
+      <Grid size={{
+      xs: 12,
+      sm: 6
+    }}>
+        <Typography variant="h6" component="div" id="users-dense-spacing-header">
+          Dense Spacing
+        </Typography>
+        <List dense aria-labelledby="users-dense-spacing-header">
+          {[4, 5, 6, 7].map(value => <ListItem key={value} disablePadding secondaryAction={<IconButton title="message">
+                  <MailIcon />
+                </IconButton>}>
+              <ListItemButton>
+                <ListItemAvatar>
+                  <Avatar size="m">
+                    <UserIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary="List item" secondary="Secondary" />
+              </ListItemButton>
+            </ListItem>)}
+        </List>
+      </Grid>
+    </Grid>
+}`,...(v=(y=e.parameters)==null?void 0:y.docs)==null?void 0:v.source}}};const Ct=["_ListItemAvatar","_UserList"];export{i as _ListItemAvatar,e as _UserList,Ct as __namedExportsOrder,Et as default};
---
+++ pr-preview/pr-725/assets/Pagination.stories-xy2F74R-.js
@@ -0,0 +1,41 @@
+import{j as t}from"./jsx-runtime-CcL-W3nW.js";import{G as i}from"./index-Cyp_3NIz.js";import{P as r}from"./Pagination-Dy03B0rv.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Box-DWnb8VZB.js";import"./identifier-Dtqimryx.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./generateUtilityClass-CVWqFxsA.js";import"./useTheme-C2CrmYKc.js";import"./extendSxProp-B5AgUjQT.js";import"./Grid2-y9MPRr2u.js";import"./useTheme-DUkCvbx_.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./styled-vBjI3IQc.js";import"./styled-CF1hcdwO.js";import"./isMuiElement-DAzOCCHH.js";import"./useThemeProps-BIK-1DKm.js";import"./Stack-BcsM4Vc0.js";import"./Container-Cqq8qXXg.js";import"./PaginationItem-D3jXkDen.js";import"./index-CTQn_wDL.js";import"./faCircleArrowRight-6L8v8FIg.js";import"./faUser-DXPB7Q64.js";import"./faSortDown-nq-jkck6.js";import"./SvgIcon-DSdWmKAH.js";import"./memoTheme-D-rVhYRp.js";import"./PaginationItem-BsVNDaT9.js";import"./index-nos-0K8U.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./createSvgIcon-TketyoFF.js";import"./useSlot-DnkSxE3X.js";import"./resolveComponentProps-Dg20wxiM.js";import"./useForkRef-BVrIj09m.js";import"./ButtonBase-DbVRbsLs.js";import"./useTimeout-B8wmcRZV.js";import"./useEnhancedEffect-D2f2tbda.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";import"./TransitionGroupContext-D-IBNIfW.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./isFocusVisible-B8k4qzLc.js";import"./Pagination-BtNphvVn.js";import"./useControlled-WdbyInBW.js";const et={title:"Components/Pagination/Pagination",component:r,tags:["autodocs"],args:{count:10}},n={render:g=>t.jsx(r,{...g})},o={render:()=>t.jsxs(i,{container:!0,spacing:1,children:[t.jsx(i,{size:{xs:2},children:"Text"}),t.jsx(i,{size:{xs:10},children:t.jsx(r,{variant:"text",count:10,"aria-label":"text pagination"})}),t.jsx(i,{size:{xs:2},children:"Outlined"}),t.jsx(i,{size:{xs:10},children:t.jsx(r,{variant:"outlined",count:10,"aria-label":"outlined pagination"})})]})},e={render:()=>t.jsxs(i,{container:!0,spacing:1,children:[t.jsx(i,{size:{xs:2},children:"Text"}),t.jsx(i,{size:{xs:10},children:t.jsx(r,{variant:"text","aria-label":"text pagination",count:10,boundaryCount:0,hideNextButton:!0,hidePrevButton:!0,showFirstButton:!0,showLastButton:!0})}),t.jsx(i,{size:{xs:2},children:"Outlined"}),t.jsx(i,{size:{xs:10},children:t.jsx(r,{variant:"outlined","aria-label":"outlined pagination",count:10,boundaryCount:0,hideNextButton:!0,hidePrevButton:!0,showFirstButton:!0,showLastButton:!0})})]})};var a,s,d;n.parameters={...n.parameters,docs:{...(a=n.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: (args: PaginationProps) => <Pagination {...args} />
+}`,...(d=(s=n.parameters)==null?void 0:s.docs)==null?void 0:d.source}}};var p,u,m;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => <Grid container spacing={1}>
+      <Grid size={{
+      xs: 2
+    }}>Text</Grid>
+      <Grid size={{
+      xs: 10
+    }}>
+        <Pagination variant="text" count={10} aria-label="text pagination" />
+      </Grid>
+      <Grid size={{
+      xs: 2
+    }}>Outlined</Grid>
+      <Grid size={{
+      xs: 10
+    }}>
+        <Pagination variant="outlined" count={10} aria-label="outlined pagination" />
+      </Grid>
+    </Grid>
+}`,...(m=(u=o.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};var c,x,l;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: () => <Grid container spacing={1}>
+      <Grid size={{
+      xs: 2
+    }}>Text</Grid>
+      <Grid size={{
+      xs: 10
+    }}>
+        <Pagination variant="text" aria-label="text pagination" count={10} boundaryCount={0} hideNextButton hidePrevButton showFirstButton showLastButton />
+      </Grid>
+      <Grid size={{
+      xs: 2
+    }}>Outlined</Grid>
+      <Grid size={{
+      xs: 10
+    }}>
+        <Pagination variant="outlined" aria-label="outlined pagination" count={10} boundaryCount={0} hideNextButton hidePrevButton showFirstButton showLastButton />
+      </Grid>
+    </Grid>
+}`,...(l=(x=e.parameters)==null?void 0:x.docs)==null?void 0:l.source}}};const at=["_Pagination","_Variants","_FirstLast"];export{e as _FirstLast,n as _Pagination,o as _Variants,at as __namedExportsOrder,et as default};
