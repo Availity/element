cfaf9639eb7b19814e7e56db4102fdab913cb1a8
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-04-04 18:37:47 +0000

    Deploy preview for PR 726 🛫

---
+++ pr-preview/pr-726/assets/PaginationItem.stories-D-EYcT_B.js
@@ -0,0 +1,72 @@
+import{j as t}from"./jsx-runtime-CcL-W3nW.js";import{G as i}from"./index-Cyp_3NIz.js";import{P as e}from"./PaginationItem-BVvIxSxX.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Box-DWnb8VZB.js";import"./identifier-Dtqimryx.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./generateUtilityClass-CVWqFxsA.js";import"./useTheme-C2CrmYKc.js";import"./extendSxProp-B5AgUjQT.js";import"./Grid2-y9MPRr2u.js";import"./useTheme-DUkCvbx_.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./styled-vBjI3IQc.js";import"./styled-CF1hcdwO.js";import"./isMuiElement-DAzOCCHH.js";import"./useThemeProps-BIK-1DKm.js";import"./Stack-BcsM4Vc0.js";import"./Container-Cqq8qXXg.js";import"./index-CTQn_wDL.js";import"./faCircleArrowRight-6L8v8FIg.js";import"./faUser-DXPB7Q64.js";import"./faSortDown-nq-jkck6.js";import"./SvgIcon-DSdWmKAH.js";import"./memoTheme-D-rVhYRp.js";import"./PaginationItem-BsVNDaT9.js";import"./index-nos-0K8U.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./createSvgIcon-TketyoFF.js";import"./useSlot-DnkSxE3X.js";import"./resolveComponentProps-Dg20wxiM.js";import"./useForkRef-BVrIj09m.js";import"./ButtonBase-DbVRbsLs.js";import"./useTimeout-B8wmcRZV.js";import"./useEnhancedEffect-D2f2tbda.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";import"./TransitionGroupContext-D-IBNIfW.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./isFocusVisible-B8k4qzLc.js";const Z={title:"Components/Pagination/PaginationItem",component:e,tags:["autodocs"],args:{type:"next"}},n={render:m=>t.jsx(e,{...m})},r={render:()=>t.jsxs(i,{container:!0,spacing:1,children:[t.jsx(i,{size:{xs:3},children:"Text"}),t.jsx(i,{size:{xs:9},children:"Outlined"}),t.jsx(i,{size:{xs:3},children:t.jsx(e,{variant:"text",type:"page",page:"1",selected:!0})}),t.jsx(i,{size:{xs:9},children:t.jsx(e,{variant:"outlined",type:"page",page:"1",selected:!0})}),t.jsx(i,{size:{xs:3},children:t.jsx(e,{variant:"text",type:"page",page:"1"})}),t.jsx(i,{size:{xs:9},children:t.jsx(e,{variant:"outlined",type:"page",page:"1"})}),t.jsx(i,{size:{xs:3},children:t.jsx(e,{variant:"text",type:"first"})}),t.jsx(i,{size:{xs:9},children:t.jsx(e,{variant:"outlined",type:"first"})}),t.jsx(i,{size:{xs:3},children:t.jsx(e,{variant:"text",type:"previous"})}),t.jsx(i,{size:{xs:9},children:t.jsx(e,{variant:"outlined",type:"previous"})}),t.jsx(i,{size:{xs:3},children:t.jsx(e,{variant:"text",type:"next"})}),t.jsx(i,{size:{xs:9},children:t.jsx(e,{variant:"outlined",type:"next"})}),t.jsx(i,{size:{xs:3},children:t.jsx(e,{variant:"text",type:"last"})}),t.jsx(i,{size:{xs:9},children:t.jsx(e,{variant:"outlined",type:"last"})})]})};var s,a,o;n.parameters={...n.parameters,docs:{...(s=n.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: (args: PaginationItemProps) => <PaginationItem {...args} />
+}`,...(o=(a=n.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};var p,x,d;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => <Grid container spacing={1}>
+      <Grid size={{
+      xs: 3
+    }}>Text</Grid>
+      <Grid size={{
+      xs: 9
+    }}>Outlined</Grid>
+      <Grid size={{
+      xs: 3
+    }}>
+        <PaginationItem variant="text" type="page" page="1" selected />
+      </Grid>
+      <Grid size={{
+      xs: 9
+    }}>
+        <PaginationItem variant="outlined" type="page" page="1" selected />
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>
+        <PaginationItem variant="text" type="page" page="1" />
+      </Grid>
+      <Grid size={{
+      xs: 9
+    }}>
+        <PaginationItem variant="outlined" type="page" page="1" />
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>
+        <PaginationItem variant="text" type="first" />
+      </Grid>
+      <Grid size={{
+      xs: 9
+    }}>
+        <PaginationItem variant="outlined" type="first" />
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>
+        <PaginationItem variant="text" type="previous" />
+      </Grid>
+      <Grid size={{
+      xs: 9
+    }}>
+        <PaginationItem variant="outlined" type="previous" />
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>
+        <PaginationItem variant="text" type="next" />
+      </Grid>
+      <Grid size={{
+      xs: 9
+    }}>
+        <PaginationItem variant="outlined" type="next" />
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>
+        <PaginationItem variant="text" type="last" />
+      </Grid>
+      <Grid size={{
+      xs: 9
+    }}>
+        <PaginationItem variant="outlined" type="last" />
+      </Grid>
+    </Grid>
+}`,...(d=(x=r.parameters)==null?void 0:x.docs)==null?void 0:d.source}}};const $=["_PaginationItem","_Types"];export{n as _PaginationItem,r as _Types,$ as __namedExportsOrder,Z as default};
