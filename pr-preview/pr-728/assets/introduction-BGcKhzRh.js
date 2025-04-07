715db24346a4598d5ee83b85981978a346846ba2
ekemp88 <ekemp88@users.noreply.github.com>
2025-04-07 20:20:26 +0000

    Deploy preview for PR 728 🛫

---
+++ pr-preview/pr-728/assets/introduction-BGcKhzRh.js
@@ -0,0 +1,188 @@
+import{j as e}from"./jsx-runtime-CcL-W3nW.js";import{useMDXComponents as o}from"./index-CfSBfIZi.js";import{M as m,c as n}from"./index-D32vX9cE.js";import"./DocsRenderer-CFRXHY34-DR3wBG9X.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-uCCTJwGU.js";import"./index-BEx9gAkE.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./client-BqYpGkAY.js";const u=`# @availity/mui-authorize
+
+> Availity MUI Authorize component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-authorize.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-authorize)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-authorize.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-authorize)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-authorize?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-authorize/package.json)
+
+## Documentation
+
+This package extends the MUI Authorize component: [MUI Authorize Docs](https://mui.com/components/authorize/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-authorize-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://design.availity.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+_This package has a few peer dependencies. Add \`@mui/material\` & \`@emotion/react\` to your project if not already installed._
+
+\`\`\`bash
+npm install @availity/mui-authorize
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-authorize
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Authorize } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Authorize } from '@availity/mui-authorize';
+\`\`\`
+`,r=`# Changelog
+
+This file was generated using [@jscutlery/semver](https://github.com/jscutlery/semver).
+
+## [1.0.2](https://github.com/Availity/element/compare/@availity/mui-authorize@1.0.1...@availity/mui-authorize@1.0.2) (2025-03-13)
+
+### Dependency Updates
+
+* \`@availity/mock\` updated to version \`1.0.1\`
+## [1.0.1](https://github.com/Availity/element/compare/@availity/mui-authorize@1.0.0...@availity/mui-authorize@1.0.1) (2025-03-07)
+
+### Dependency Updates
+
+* \`mui-block-ui\` updated to version \`1.0.0\`
+## [1.0.0](https://github.com/Availity/element/compare/@availity/mui-authorize@1.0.0-alpha.0...@availity/mui-authorize@1.0.0) (2025-02-25)
+
+### Dependency Updates
+
+* \`mui-block-ui\` updated to version \`0.2.0\`
+* \`@availity/mock\` updated to version \`0.2.0\`
+
+### ⚠ BREAKING CHANGES
+
+* upgrade to @mui/material v6
+
+### Miscellaneous Chores
+
+* element v1 release ([a6e3567](https://github.com/Availity/element/commit/a6e35671185b9f13d25c7a39c4488ecb8774633e))
+
+## [1.0.0-alpha.0](https://github.com/Availity/element/compare/@availity/mui-authorize@0.2.0...@availity/mui-authorize@1.0.0-alpha.0) (2025-02-24)
+
+
+### ⚠ BREAKING CHANGES
+
+* upgraded to @mui/material v6
+* **element:** upgraded to @mui/material v6
+
+### Features
+
+* **element:** upgrade to @mui/material v6 ([cb958bb](https://github.com/Availity/element/commit/cb958bba99a4f1ee6dab323f0ff54b69e6fd3493))
+* upgrade @mui/material ([571453a](https://github.com/Availity/element/commit/571453a34b21c344594ab4c03bc497d19aba942b))
+* upgrade to MUI v6 ([7febd6f](https://github.com/Availity/element/commit/7febd6fd4fd58e87e1c97a832cea3b4595a35d58))
+
+## [0.2.0](https://github.com/Availity/element/compare/@availity/mui-authorize@0.1.12...@availity/mui-authorize@0.2.0) (2025-02-04)
+
+### Dependency Updates
+
+* \`mui-block-ui\` updated to version \`0.1.12\`
+
+### Features
+
+* configure package.json to allow esm to be used ([5a5393d](https://github.com/Availity/element/commit/5a5393de761f52608e714dd94a05106937dd95db))
+
+## [0.1.12](https://github.com/Availity/element/compare/@availity/mui-authorize@0.1.11...@availity/mui-authorize@0.1.12) (2025-01-27)
+
+### Dependency Updates
+
+* \`mui-block-ui\` updated to version \`0.1.11\`
+## [0.1.11](https://github.com/Availity/element/compare/@availity/mui-authorize@0.1.10...@availity/mui-authorize@0.1.11) (2025-01-23)
+
+### Dependency Updates
+
+* \`mui-block-ui\` updated to version \`0.1.10\`
+## [0.1.10](https://github.com/Availity/element/compare/@availity/mui-authorize@0.1.9...@availity/mui-authorize@0.1.10) (2025-01-22)
+
+### Dependency Updates
+
+* \`mui-block-ui\` updated to version \`0.1.9\`
+## [0.1.9](https://github.com/Availity/element/compare/@availity/mui-authorize@0.1.8...@availity/mui-authorize@0.1.9) (2025-01-06)
+
+### Dependency Updates
+
+* \`mui-block-ui\` updated to version \`0.1.8\`
+## [0.1.8](https://github.com/Availity/element/compare/@availity/mui-authorize@0.1.7...@availity/mui-authorize@0.1.8) (2025-01-03)
+
+### Dependency Updates
+
+* \`mui-block-ui\` updated to version \`0.1.7\`
+## [0.1.7](https://github.com/Availity/element/compare/@availity/mui-authorize@0.1.6...@availity/mui-authorize@0.1.7) (2024-12-23)
+
+
+### Bug Fixes
+
+* **mui-authorize:** add note about where to find useauthorize hook ([e541f82](https://github.com/Availity/element/commit/e541f82f358cf6257b9c10bae2ad52282500a81c))
+
+## [0.1.6](https://github.com/Availity/element/compare/@availity/mui-authorize@0.1.5...@availity/mui-authorize@0.1.6) (2024-12-09)
+
+### Dependency Updates
+
+* \`mui-block-ui\` updated to version \`0.1.5\`
+## [0.1.5](https://github.com/Availity/element/compare/@availity/mui-authorize@0.1.4...@availity/mui-authorize@0.1.5) (2024-10-31)
+
+### Dependency Updates
+
+* \`mui-block-ui\` updated to version \`0.1.4\`
+## [0.1.4](https://github.com/Availity/element/compare/@availity/mui-authorize@0.1.3...@availity/mui-authorize@0.1.4) (2024-08-08)
+
+### Dependency Updates
+
+* \`mui-block-ui\` updated to version \`0.1.3\`
+## [0.1.3](https://github.com/Availity/element/compare/@availity/mui-authorize@0.1.2...@availity/mui-authorize@0.1.3) (2024-08-05)
+
+### Dependency Updates
+
+* \`mui-block-ui\` updated to version \`0.1.2\`
+## [0.1.2](https://github.com/Availity/element/compare/@availity/mui-authorize@0.1.1...@availity/mui-authorize@0.1.2) (2024-07-22)
+
+### Dependency Updates
+
+* \`mui-block-ui\` updated to version \`0.1.1\`
+## [0.1.1](https://github.com/Availity/element/compare/@availity/mui-authorize@0.1.0...@availity/mui-authorize@0.1.1) (2024-06-14)
+
+### Dependency Updates
+
+* \`mui-block-ui\` updated to version \`0.1.0\`
+## 0.1.0 (2024-05-10)
+
+### Dependency Updates
+
+* \`mui-block-ui\` updated to version \`0.1.0\`
+
+### Features
+
+* **mui-authorize:** add authorize package ([ff64501](https://github.com/Availity/element/commit/ff6450141021d74657e53e85213ce00b638a6465))
+`;function a(t){return e.jsxs(e.Fragment,{children:[e.jsx(m,{title:"Components/Authorize/Introduction"}),`
+`,e.jsx(n,{children:u}),`
+`,e.jsx(n,{children:r})]})}function A(t={}){const{wrapper:i}={...o(),...t.components};return i?e.jsx(i,{...t,children:e.jsx(a,{...t})}):a()}export{A as default};
