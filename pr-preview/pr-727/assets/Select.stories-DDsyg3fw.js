2af850c75ecdc2e45f160fe418a6d9e049e5050c
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-04-08 17:03:55 +0000

    Deploy preview for PR 727 🛫

---
+++ pr-preview/pr-727/assets/Select.stories-DDsyg3fw.js
@@ -0,0 +1,101 @@
+import{j as e}from"./jsx-runtime-CcL-W3nW.js";import{r as g}from"./index-DcWiA9TO.js";import{C as V}from"./index-b-MJ-ye7.js";import{T as $}from"./index-BtPDELIe.js";import"./FieldHelpIcon-BH8d8Jml.js";import{F as v}from"./FormLabel-DsOIeMF3.js";import{S as h}from"./Switch-Bc70wffX.js";import"./Input-BS67x0-8.js";import{F as x}from"./FormControl-BPSU2g7t.js";import{v as b}from"./visuallyHidden-Dan1xhjv.js";import{M as o}from"./MenuItem-DHI5k2qe.js";import{B as q}from"./Box-DWnb8VZB.js";import"./_commonjsHelpers-BosuxZz1.js";import"./Chip-ALB5H4lg.js";import"./generateUtilityClass-CVWqFxsA.js";import"./identifier-Dtqimryx.js";import"./createSvgIcon-TketyoFF.js";import"./SvgIcon-DSdWmKAH.js";import"./memoTheme-D-rVhYRp.js";import"./styled-CF1hcdwO.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./useForkRef-BVrIj09m.js";import"./ButtonBase-DbVRbsLs.js";import"./useTimeout-B8wmcRZV.js";import"./useEnhancedEffect-D2f2tbda.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";import"./TransitionGroupContext-D-IBNIfW.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./isFocusVisible-B8k4qzLc.js";import"./Typography-CvSvtD2a.js";import"./index-f0Aa_BgU.js";import"./extendSxProp-B5AgUjQT.js";import"./useTheme-C2CrmYKc.js";import"./index-DMWJB0MK.js";import"./index-CTQn_wDL.js";import"./faCircleArrowRight-6L8v8FIg.js";import"./faUser-DXPB7Q64.js";import"./faSortDown-nq-jkck6.js";import"./index-DLsH_p52.js";import"./Tooltip-CiiaKkqp.js";import"./index-nos-0K8U.js";import"./useTheme-DUkCvbx_.js";import"./useSlot-DnkSxE3X.js";import"./resolveComponentProps-Dg20wxiM.js";import"./useControlled-WdbyInBW.js";import"./useId-DszduLV4.js";import"./getReactElementRef-AnOffBoZ.js";import"./Portal-CBAx_nZm.js";import"./utils-D5ZkK9XY.js";import"./index-BEx9gAkE.js";import"./ownerDocument-DW-IO8s5.js";import"./useSlotProps-BAIpVL6Y.js";import"./IconButton-Caxu-qgA.js";import"./CircularProgress-D8w9ZPs6.js";import"./Button-BAEbeAyH.js";import"./LoadingButton-9DxABi24.js";import"./FormControlLabel-GqYWwHWT.js";import"./formControlState-Dq1zat_P.js";import"./useFormControl-68oiojFJ.js";import"./FormLabel-98YPf6ct.js";import"./RadioGroup-BogPRRDq.js";import"./SwitchBase-Z9YgLJlx.js";import"./createChainedFunction-BO_9K8Jh.js";import"./Select-hLZ2S4VH.js";import"./Menu-Byu17A6t.js";import"./Popover-ChEuM7rO.js";import"./isHostComponent-DVu5iVWx.js";import"./ownerWindow-HkKU3E4x.js";import"./debounce-Be36O1Ab.js";import"./Paper-DXcIEVia.js";import"./Modal-B5s0Thvt.js";import"./Backdrop-B4gbyDGC.js";import"./Fade-Dl4gmST4.js";import"./List-ChBzRQcW.js";import"./utils-DoM3o7-Q.js";import"./OutlinedInput-CQlxW3ax.js";import"./Stack-BcsM4Vc0.js";import"./styled-vBjI3IQc.js";import"./useThemeProps-BIK-1DKm.js";import"./Divider-jUONNV4j.js";import"./dividerClasses-BmOcEkDu.js";import"./faCheck-B05F4ORy.js";import"./Switch-BqVUWLaW.js";import"./isMuiElement-DAzOCCHH.js";import"./listItemTextClasses-QA0fdXLT.js";const pt={title:"Form Components/FormUtils/Select",component:h,tags:["autodocs"],args:{size:"small"},parameters:{docs:{controls:{exclude:["placeholder"]}}}},u={render:({required:r,size:a,...t})=>{const[i,s]=g.useState(""),m=p=>{s(p.target.value)};return e.jsxs(x,{required:r,size:a,children:[e.jsx(v,{id:`${t.id}-label`,sx:b,children:t.label}),e.jsxs(h,{value:i,onChange:m,labelId:`${t.id}-label`,...t,children:[e.jsx(o,{value:10,children:"10"}),e.jsx(o,{value:20,children:"20"}),e.jsx(o,{value:30,children:"30"})]})]})},args:{label:"count",id:"count",size:"small"}},d={render:({required:r,size:a,...t})=>{const[i,s]=g.useState(""),m=p=>{s(p.target.value)};return e.jsxs(x,{required:r,size:a,children:[e.jsx(v,{id:`${t.id}-label`,sx:b,children:t.label}),e.jsxs(h,{value:i,onChange:m,labelId:`${t.id}-label`,...t,children:[e.jsx(o,{value:10,children:"10"}),e.jsx(o,{value:20,children:"20"}),e.jsx(o,{value:30,children:"30"})]})]})},args:{label:"count",id:"count",size:"small",displayEmpty:!0,renderValue:r=>r||e.jsx($,{color:"text.placeholder",children:"Placeholder"})}},c={render:({required:r,size:a,...t})=>{const[i,s]=g.useState([]),m=n=>{const{target:{value:l}}=n;s(typeof l=="string"?l.split(","):l)},p=["one","two","three","four"];return e.jsxs(x,{required:r,size:a,children:[e.jsx(v,{id:`${t.id}-label`,sx:b,children:t.label}),e.jsx(h,{...t,labelId:`${t.id}-label`,value:i,onChange:m,renderValue:n=>e.jsx(q,{sx:{display:"flex",flexWrap:"wrap",gap:.5},children:n.map(l=>e.jsx(V,{label:l},l))}),children:p.map(n=>e.jsx(o,{value:n,children:n},n))})]})},args:{label:"counts",id:"multiple-chip",size:"small",multiple:!0}};var C,S,f;u.parameters={...u.parameters,docs:{...(C=u.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: ({
+    required,
+    size,
+    ...args
+  }: SelectProps) => {
+    const [count, setCount] = useState('');
+    const handleChange = (event: SelectChangeEvent) => {
+      setCount(event.target.value as string);
+    };
+    return <FormControl required={required} size={size}>
+        <FormLabel id={\`\${args.id}-label\`} sx={visuallyHidden}>
+          {args.label}
+        </FormLabel>
+        <Select value={count} onChange={handleChange} labelId={\`\${args.id}-label\`} {...args}>
+          <MenuItem value={10}>10</MenuItem>
+          <MenuItem value={20}>20</MenuItem>
+          <MenuItem value={30}>30</MenuItem>
+        </Select>
+      </FormControl>;
+  },
+  args: {
+    label: 'count',
+    id: 'count',
+    size: 'small'
+  }
+}`,...(f=(S=u.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};var y,j,I;d.parameters={...d.parameters,docs:{...(y=d.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: ({
+    required,
+    size,
+    ...args
+  }: SelectProps) => {
+    const [count, setCount] = useState('');
+    const handleChange = (event: SelectChangeEvent) => {
+      setCount(event.target.value as string);
+    };
+    return <FormControl required={required} size={size}>
+        <FormLabel id={\`\${args.id}-label\`} sx={visuallyHidden}>
+          {args.label}
+        </FormLabel>
+        <Select value={count} onChange={handleChange} labelId={\`\${args.id}-label\`} {...args}>
+          <MenuItem value={10}>10</MenuItem>
+          <MenuItem value={20}>20</MenuItem>
+          <MenuItem value={30}>30</MenuItem>
+        </Select>
+      </FormControl>;
+  },
+  args: {
+    label: 'count',
+    id: 'count',
+    size: 'small',
+    displayEmpty: true,
+    renderValue: (selected: string) => {
+      if (!selected) {
+        return <Typography color="text.placeholder">Placeholder</Typography>;
+      }
+      return selected;
+    }
+  }
+}`,...(I=(j=d.parameters)==null?void 0:j.docs)==null?void 0:I.source}}};var M,F,z;c.parameters={...c.parameters,docs:{...(M=c.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  render: ({
+    required,
+    size,
+    ...args
+  }: SelectProps) => {
+    const [multiValue, setMultiValue] = useState<string[]>([]);
+    const handleChange = (event: SelectChangeEvent<typeof multiValue>) => {
+      const {
+        target: {
+          value
+        }
+      } = event;
+      setMultiValue(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value);
+    };
+    const options = ['one', 'two', 'three', 'four'];
+    return <FormControl required={required} size={size}>
+        <FormLabel id={\`\${args.id}-label\`} sx={visuallyHidden}>
+          {args.label}
+        </FormLabel>
+        <Select {...args} labelId={\`\${args.id}-label\`} value={multiValue} onChange={handleChange} renderValue={(selected: typeof multiValue) => <Box sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: 0.5
+      }}>
+              {selected.map(value => <Chip key={value} label={value} />)}
+            </Box>}>
+          {options.map(option => <MenuItem key={option} value={option}>
+              {option}
+            </MenuItem>)}
+        </Select>
+      </FormControl>;
+  },
+  args: {
+    label: 'counts',
+    id: 'multiple-chip',
+    size: 'small',
+    multiple: true
+  }
+}`,...(z=(F=c.parameters)==null?void 0:F.docs)==null?void 0:z.source}}};const ut=["_Select","_SelectPlaceholder","_MultiSelect"];export{c as _MultiSelect,u as _Select,d as _SelectPlaceholder,ut as __namedExportsOrder,pt as default};
