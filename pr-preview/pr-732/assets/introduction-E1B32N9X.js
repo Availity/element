72d553940d097532b1e95014fd400e45a4ea2d0d
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-04-10 21:19:23 +0000

    Deploy preview for PR 732 🛫

---
+++ pr-preview/pr-732/assets/introduction-E1B32N9X.js
@@ -0,0 +1,187 @@
+import{j as t}from"./jsx-runtime-CcL-W3nW.js";import{useMDXComponents as m}from"./index-CfSBfIZi.js";import{M as s,c as a}from"./index-BtPB_RQ9.js";import"./DocsRenderer-CFRXHY34-D4FXtY6v.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-D2N0D8L7.js";import"./index-BEx9gAkE.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./client-BqYpGkAY.js";const o=`# @availity/mui-tabs
+
+> Availity MUI Tabs component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-tabs.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-tabs)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-tabs.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-tabs)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-tabs?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-tabs/package.json)
+
+## Documentation
+
+This package extends the MUI Tabs component: [MUI Tabs Docs](https://mui.com/components/tabs/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-tabs-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://design.availity.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+_This package has a few peer dependencies. Add \`@mui/material\` & \`@emotion/react\` to your project if not already installed._
+
+\`\`\`bash
+npm install @availity/mui-tabs
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-tabs
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { Tabs } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { Tabs } from '@availity/mui-tabs';
+\`\`\`
+`,l=`# Changelog
+
+This file was generated using [@jscutlery/semver](https://github.com/jscutlery/semver).
+
+## [1.2.0](https://github.com/Availity/element/compare/@availity/mui-tabs@1.1.0...@availity/mui-tabs@1.2.0) (2025-03-24)
+
+
+### Features
+
+* **mui-tabs:** add icon and examples ([af6172d](https://github.com/Availity/element/commit/af6172d25882c03b543fff108424a6875027a5a0))
+
+## [1.1.0](https://github.com/Availity/element/compare/@availity/mui-tabs@1.0.1...@availity/mui-tabs@1.1.0) (2025-03-13)
+
+
+### Features
+
+* **mui-tabs:** add secondary tabs ([8cfca7c](https://github.com/Availity/element/commit/8cfca7ce2d8b1dd6b8ccd7bbed2ddb506b159f38))
+
+## [1.0.1](https://github.com/Availity/element/compare/@availity/mui-tabs@1.0.0...@availity/mui-tabs@1.0.1) (2025-03-07)
+
+### Dependency Updates
+
+* \`mui-divider\` updated to version \`1.0.0\`
+## [1.0.0](https://github.com/Availity/element/compare/@availity/mui-tabs@1.0.0-alpha.0...@availity/mui-tabs@1.0.0) (2025-02-25)
+
+### Dependency Updates
+
+* \`mui-divider\` updated to version \`0.2.0\`
+
+### ⚠ BREAKING CHANGES
+
+* upgrade to @mui/material v6
+
+### Miscellaneous Chores
+
+* element v1 release ([a6e3567](https://github.com/Availity/element/commit/a6e35671185b9f13d25c7a39c4488ecb8774633e))
+
+## [1.0.0-alpha.0](https://github.com/Availity/element/compare/@availity/mui-tabs@0.2.0...@availity/mui-tabs@1.0.0-alpha.0) (2025-02-24)
+
+
+### ⚠ BREAKING CHANGES
+
+* upgraded to @mui/material v6
+* **element:** upgraded to @mui/material v6
+
+### Features
+
+* **element:** upgrade to @mui/material v6 ([cb958bb](https://github.com/Availity/element/commit/cb958bba99a4f1ee6dab323f0ff54b69e6fd3493))
+* upgrade @mui/material ([571453a](https://github.com/Availity/element/commit/571453a34b21c344594ab4c03bc497d19aba942b))
+* upgrade to MUI v6 ([7febd6f](https://github.com/Availity/element/commit/7febd6fd4fd58e87e1c97a832cea3b4595a35d58))
+
+## [0.2.0](https://github.com/Availity/element/compare/@availity/mui-tabs@0.1.10...@availity/mui-tabs@0.2.0) (2025-02-04)
+
+### Dependency Updates
+
+* \`mui-divider\` updated to version \`0.1.10\`
+
+### Features
+
+* configure package.json to allow esm to be used ([5a5393d](https://github.com/Availity/element/commit/5a5393de761f52608e714dd94a05106937dd95db))
+
+## [0.1.10](https://github.com/Availity/element/compare/@availity/mui-tabs@0.1.9...@availity/mui-tabs@0.1.10) (2024-10-31)
+
+### Dependency Updates
+
+* \`mui-divider\` updated to version \`0.1.9\`
+## [0.1.9](https://github.com/Availity/element/compare/@availity/mui-tabs@0.1.8...@availity/mui-tabs@0.1.9) (2024-04-19)
+
+### Dependency Updates
+
+* \`mui-divider\` updated to version \`0.1.8\`
+## [0.1.8](https://github.com/Availity/element/compare/@availity/mui-tabs@0.1.7...@availity/mui-tabs@0.1.8) (2024-04-04)
+
+### Dependency Updates
+
+* \`mui-divider\` updated to version \`0.3.6\`
+
+### Performance Improvements
+
+* **mui-tabs:** use path imports for material deps ([708de9f](https://github.com/Availity/element/commit/708de9f03b089bb3e4a4d4a5c66ab63a3304e399))
+
+## [0.1.7](https://github.com/Availity/element/compare/@availity/mui-tabs@0.1.6...@availity/mui-tabs@0.1.7) (2024-02-20)
+
+### Dependency Updates
+
+* \`mui-divider\` updated to version \`0.3.5\`
+## [0.1.6](https://github.com/Availity/element/compare/@availity/mui-tabs@0.1.5...@availity/mui-tabs@0.1.6) (2023-11-08)
+
+### Dependency Updates
+
+- \`mui-divider\` updated to version \`0.3.4\`
+
+## [0.1.5](https://github.com/Availity/element/compare/@availity/mui-tabs@0.1.4...@availity/mui-tabs@0.1.5) (2023-11-07)
+
+## [0.1.4](https://github.com/Availity/element/compare/@availity/mui-tabs@0.1.3...@availity/mui-tabs@0.1.4) (2023-10-06)
+
+## [0.1.3](https://github.com/Availity/element/compare/@availity/mui-tabs@0.1.2...@availity/mui-tabs@0.1.3) (2023-08-23)
+
+## [0.1.2](https://github.com/Availity/element/compare/@availity/mui-tabs@0.1.1...@availity/mui-tabs@0.1.2) (2023-08-22)
+
+### Bug Fixes
+
+- **mui-tabs:** export subcomponents ([f06759f](https://github.com/Availity/element/commit/f06759fa83a32aa8f7f549fa0011ab4fe183e77a))
+
+## [0.1.1](https://github.com/Availity/element/compare/@availity/mui-tabs@0.1.0...@availity/mui-tabs@0.1.1) (2023-08-16)
+
+### Bug Fixes
+
+- **mui-tabs:** fix invalid ARIA attributes in Tab stories ([483b1ad](https://github.com/Availity/element/commit/483b1ad0fd30025cbce96e03dec0e496467f75ef))
+
+## 0.1.0 (2023-07-14)
+
+### Features
+
+- **mui-tabs:** tab component initial commit ([8b20c7e](https://github.com/Availity/element/commit/8b20c7e6be1759a256f110be91f2113c011a8727))
+- **mui-tabs:** tabcontext component initial commit ([9b52724](https://github.com/Availity/element/commit/9b52724abcc70aff355377fff3b81177df271441))
+- **mui-tabs:** tablist component initial commit ([9c445a3](https://github.com/Availity/element/commit/9c445a3d963b5d52852e0a216d058c4e6d6e76b4))
+- **mui-tabs:** tabpanel component initial commit ([d3af6d7](https://github.com/Availity/element/commit/d3af6d7ab1487b39f958e8f4ecbfe73d19cf86d9))
+- **mui-tabs:** tabs component initial commit ([d4e7818](https://github.com/Availity/element/commit/d4e7818f4123d215e4cd0f5df475b5fb0eadc69a))
+
+### Bug Fixes
+
+- **mui-tabs:** disableRipple is not allowed on DOM elements ([ad0ada9](https://github.com/Availity/element/commit/ad0ada98922f339ec79cf64f081b54c34107c0b1))
+`;function n(i){return t.jsxs(t.Fragment,{children:[t.jsx(s,{title:"Components/Tabs/Introduction"}),`
+`,t.jsx(a,{children:o}),`
+`,t.jsx(a,{children:l})]})}function A(i={}){const{wrapper:e}={...m(),...i.components};return e?t.jsx(e,{...i,children:t.jsx(n,{...i})}):n()}export{A as default};
