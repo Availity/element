d15a32b8547fcd7e4719ef8deadcd13ce4256050
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-04-10 14:55:53 +0000

    Deploy preview for PR 731 🛫

---
+++ pr-preview/pr-731/assets/CardActions.stories-NN2mFrum.js
@@ -0,0 +1,22 @@
+import{j as r}from"./jsx-runtime-BTJTZTIL.js";import{C as s}from"./CardActions-CmYBJcI-.js";import{C as l}from"./Card-DuhMFH9L.js";import{B as m,I as C}from"./index-DnNfvsmq.js";import{d as u}from"./index-Bp9oCidm.js";import"./index-ChsGqxH_.js";import"./index-BgYLq7fD.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./generateUtilityClass-BeVSWKCg.js";import"./identifier-CudAVvBZ.js";import"./generateUtilityClasses-whvyra8-.js";import"./DefaultPropsProvider-CWkNlXkE.js";import"./styled-CKuRQFSa.js";import"./Card-cxokKFMq.js";import"./Paper-4P5FqKie.js";import"./useTheme-Cx1wRXKO.js";import"./useTheme-Ib01SJ3a.js";import"./memoTheme-CpoNRPWX.js";import"./Tooltip-CIyLwlBA.js";import"./index-DyJdRKzS.js";import"./useSlot-1VRyIerd.js";import"./resolveComponentProps-CCNxqvxI.js";import"./useForkRef-Dz4aGlyk.js";import"./useTimeout-iC_34LsC.js";import"./useEnhancedEffect-j2zQLnYm.js";import"./useControlled-DxC1uCGX.js";import"./useId-DIK8ekB4.js";import"./getReactElementRef-Df0xbma2.js";import"./Portal-C2J4q3Ji.js";import"./utils-tFrEP9Vf.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./TransitionGroupContext-BKlA24Pm.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./ownerDocument-DW-IO8s5.js";import"./useSlotProps-BmqT0tgx.js";import"./isFocusVisible-B8k4qzLc.js";import"./IconButton-D_kG6RhR.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./ButtonBase-D97P5WV0.js";import"./emotion-react.browser.esm-v7Jm8XFw.js";import"./CircularProgress-fcRSDrpN.js";import"./Button-yLAd6_L_.js";import"./LoadingButton-Cl76w144.js";import"./faCircleArrowRight-B14F5Piw.js";import"./faUser-D77uLSLS.js";import"./faSortDown-BIc1sKK4.js";import"./SvgIcon-CiBNlF9H.js";const er={title:"Components/Card/CardActions",component:s,tags:["autodocs"]},o={render:i=>r.jsx(l,{children:r.jsxs(s,{...i,children:[r.jsx(m,{color:"secondary",children:"Cancel"}),r.jsx(m,{color:"primary",children:"Submit"})]})}),args:{sx:{justifyContent:"flex-end"}}},t={render:i=>r.jsx(l,{children:r.jsx(s,{...i,children:r.jsx(C,{color:"primary",title:"search",size:"small",children:r.jsx(u,{fontSize:"xsmall"})})})}),args:{}};var n,a,p;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: (args: CardActionsProps) => <Card>
+      <CardActions {...args}>
+        <Button color="secondary">Cancel</Button>
+        <Button color="primary">Submit</Button>
+      </CardActions>
+    </Card>,
+  args: {
+    sx: {
+      justifyContent: 'flex-end'
+    }
+  }
+}`,...(p=(a=o.parameters)==null?void 0:a.docs)==null?void 0:p.source}}};var e,c,d;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: (args: CardActionsProps) => <Card>
+      <CardActions {...args}>
+        <IconButton color="primary" title="search" size="small">
+          <SearchIcon fontSize="xsmall" />
+        </IconButton>
+      </CardActions>
+    </Card>,
+  args: {}
+}`,...(d=(c=t.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};const cr=["_CardActions","_CardActionsIconButtons"];export{o as _CardActions,t as _CardActionsIconButtons,cr as __namedExportsOrder,er as default};
