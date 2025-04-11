16d06f711279700453886d04f454c02d06aaacc8
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-04-11 15:35:00 +0000

    Deploy preview for PR 732 🛫

---
+++ pr-preview/pr-732/assets/Alert.stories-HtxeQP04.js
@@ -0,0 +1,47 @@
+import{j as e}from"./jsx-runtime-CcL-W3nW.js";import{e as u}from"./index-DcWiA9TO.js";import{A as s}from"./Alert-Dc5IftoZ.js";import{C as d}from"./Collapse-quvbLMTx.js";import{L as P}from"./Link-DYJ9uraP.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-dekIPCUU.js";import"./Tooltip-CiiaKkqp.js";import"./generateUtilityClass-CVWqFxsA.js";import"./identifier-Dtqimryx.js";import"./index-nos-0K8U.js";import"./useTheme-DUkCvbx_.js";import"./DefaultPropsProvider-_cNB_dWy.js";import"./useTheme-C2CrmYKc.js";import"./styled-CF1hcdwO.js";import"./memoTheme-D-rVhYRp.js";import"./useSlot-DnkSxE3X.js";import"./resolveComponentProps-Dg20wxiM.js";import"./useForkRef-BVrIj09m.js";import"./generateUtilityClasses-BGOCnVj7.js";import"./useTimeout-B8wmcRZV.js";import"./useEnhancedEffect-D2f2tbda.js";import"./useControlled-WdbyInBW.js";import"./useId-DszduLV4.js";import"./getReactElementRef-AnOffBoZ.js";import"./Portal-CBAx_nZm.js";import"./utils-D5ZkK9XY.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";import"./TransitionGroupContext-D-IBNIfW.js";import"./index-BEx9gAkE.js";import"./ownerDocument-DW-IO8s5.js";import"./useSlotProps-BAIpVL6Y.js";import"./isFocusVisible-B8k4qzLc.js";import"./IconButton-Caxu-qgA.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./ButtonBase-DbVRbsLs.js";import"./emotion-react.browser.esm-CU7ej9d2.js";import"./CircularProgress-D8w9ZPs6.js";import"./Button-DDPbJ3HZ.js";import"./LoadingButton-BRK58AHE.js";import"./index-BTr7iyfv.js";import"./faCircleArrowRight-6L8v8FIg.js";import"./faUser-DXPB7Q64.js";import"./faSortDown-nq-jkck6.js";import"./SvgIcon-DSdWmKAH.js";import"./Alert-ui2dSBj_.js";import"./createSvgIcon-TketyoFF.js";import"./Close-CpXO-CFZ.js";import"./Paper-DXcIEVia.js";import"./Typography-CvSvtD2a.js";import"./index-f0Aa_BgU.js";import"./extendSxProp-B5AgUjQT.js";const Pe={title:"Components/Alert/Alert",component:s,tags:["autodocs"],args:{severity:"info",children:"This text is inside the alert"}},a={render:t=>e.jsx(s,{severity:"info",...t})},n={render:t=>{const[i,r]=u.useState(!0),o=()=>{r(!1),setTimeout(()=>r(!0),1e3)};return e.jsx(d,{in:i,children:e.jsx(s,{onClose:o,...t})})}},m={render:t=>{const[i,r]=u.useState(!0),o=()=>{r(!1),setTimeout(()=>r(!0),1e3)},p=e.jsx(P,{component:"button",onClick:o,children:"Action"});return e.jsx(d,{in:i,children:e.jsx(s,{action:p,...t})})}},c={render:t=>{const[i,r]=u.useState(!0),o=()=>{r(!1),setTimeout(()=>r(!0),1e3)},p=e.jsx(P,{component:"button",onClick:o,children:"Action"});return e.jsx(d,{in:i,children:e.jsx(s,{onClose:o,action:p,...t})})}},R=["info","success","warning","error"],l={render:()=>e.jsx(e.Fragment,{children:R.map(t=>e.jsxs(e.Fragment,{children:[e.jsxs(s,{severity:t,children:["This is a ",t," alert — check it out!"]},t),e.jsx("br",{})]}))})};var b,A,C;a.parameters={...a.parameters,docs:{...(b=a.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: (args: Omit<AlertProps, 'ref'>) => <Alert severity="info" {...args} />
+}`,...(C=(A=a.parameters)==null?void 0:A.docs)==null?void 0:C.source}}};var f,v,g,h,x;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: (args: Omit<AlertProps, 'ref'>) => {
+    const [visible, setVisible] = React.useState(true);
+    const onClose = () => {
+      setVisible(false);
+      setTimeout(() => setVisible(true), 1000);
+    };
+    return <Collapse in={visible}>
+        <Alert onClose={onClose} {...args} />
+      </Collapse>;
+  }
+}`,...(g=(v=n.parameters)==null?void 0:v.docs)==null?void 0:g.source},description:{story:"The `Collapse` component is a recommended way to handle visual dismissing of an alert.\nCheck out the Material UI docs for more information.",...(x=(h=n.parameters)==null?void 0:h.docs)==null?void 0:x.description}}};var j,k,S;m.parameters={...m.parameters,docs:{...(j=m.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: (args: Omit<AlertProps, 'ref'>) => {
+    const [visible, setVisible] = React.useState(true);
+    const onClose = () => {
+      setVisible(false);
+      setTimeout(() => setVisible(true), 1000);
+    };
+    const action = <Link component="button" onClick={onClose}>Action</Link>;
+    return <Collapse in={visible}>
+        <Alert action={action} {...args} />
+      </Collapse>;
+  }
+}`,...(S=(k=m.parameters)==null?void 0:k.docs)==null?void 0:S.source}}};var _,V,T;c.parameters={...c.parameters,docs:{...(_=c.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  render: (args: Omit<AlertProps, 'ref'>) => {
+    const [visible, setVisible] = React.useState(true);
+    const onClose = () => {
+      setVisible(false);
+      setTimeout(() => setVisible(true), 1000);
+    };
+    const action = <Link component="button" onClick={onClose}>Action</Link>;
+    return <Collapse in={visible}>
+        <Alert onClose={onClose} action={action} {...args} />
+      </Collapse>;
+  }
+}`,...(T=(V=c.parameters)==null?void 0:V.docs)==null?void 0:T.source}}};var y,L,O;l.parameters={...l.parameters,docs:{...(y=l.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: () => <>
+    {severities.map(severity => <>
+        <Alert severity={severity} key={severity}>
+          This is a {severity} alert — check it out!
+        </Alert>
+        <br />
+      </>)}
+  </>
+}`,...(O=(L=l.parameters)==null?void 0:L.docs)==null?void 0:O.source}}};const Re=["_Alert","_DismissableAlert","_Action","_ActionAndClose","_Severities"];export{m as _Action,c as _ActionAndClose,a as _Alert,n as _DismissableAlert,l as _Severities,Re as __namedExportsOrder,Pe as default};
---
+++ pr-preview/pr-732/assets/introduction-YMv42y9l.js
@@ -0,0 +1,182 @@
+import{j as e}from"./jsx-runtime-CcL-W3nW.js";import{useMDXComponents as a}from"./index-CfSBfIZi.js";import{M as m,c as n}from"./index-CzYI0Y3_.js";import"./DocsRenderer-CFRXHY34-B9NE_b2X.js";import"./index-DcWiA9TO.js";import"./_commonjsHelpers-BosuxZz1.js";import"./iframe-Cp7f9itr.js";import"./index-BEx9gAkE.js";import"./index-BedKbVpA.js";import"./index-DrFu-skq.js";import"./client-BqYpGkAY.js";const d=`# @availity/mui-codemod
+
+> Availity MUI Codemods to be used when upgrading major versions of @availity/element.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-codemod.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-codemod)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-codemod.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-codemod)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-codemod?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-codemod/package.json)
+
+## Documentation
+
+This package extends the MUI v6 Codemods: [MUI Codemod Docs](https://mui.com/material-ui/migration/upgrade-to-v6/)
+
+## Usage
+
+### Migrate Grid props
+
+\`npx @availity/mui-codemod@latest v1.0.0/grid-v2-props <path/to/folder>\`
+
+### Migrate sx props
+
+\`npx @availity/mui-codemod@latest v1.0.0/sx-props <path/to/folder>\`
+
+### Migrate system props
+
+\`npx @availity/mui-codemod@latest v1.0.0/system-props <path/to/folder>\`
+`,p=`# Changelog
+
+This file was generated using [@jscutlery/semver](https://github.com/jscutlery/semver).
+
+## [1.0.23](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.22...@availity/mui-codemod@1.0.23) (2025-04-10)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.22\`
+## [1.0.22](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.21...@availity/mui-codemod@1.0.22) (2025-04-09)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.21\`
+## [1.0.21](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.20...@availity/mui-codemod@1.0.21) (2025-04-08)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.20\`
+## [1.0.20](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.19...@availity/mui-codemod@1.0.20) (2025-04-08)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.19\`
+## [1.0.19](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.18...@availity/mui-codemod@1.0.19) (2025-04-04)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.18\`
+## [1.0.18](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.17...@availity/mui-codemod@1.0.18) (2025-04-04)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.17\`
+## [1.0.17](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.16...@availity/mui-codemod@1.0.17) (2025-04-04)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.16\`
+## [1.0.16](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.15...@availity/mui-codemod@1.0.16) (2025-03-31)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.15\`
+## [1.0.15](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.14...@availity/mui-codemod@1.0.15) (2025-03-31)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.14\`
+## [1.0.14](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.13...@availity/mui-codemod@1.0.14) (2025-03-27)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.13\`
+## [1.0.13](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.12...@availity/mui-codemod@1.0.13) (2025-03-27)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.12\`
+## [1.0.12](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.11...@availity/mui-codemod@1.0.12) (2025-03-25)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.11\`
+## [1.0.11](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.10...@availity/mui-codemod@1.0.11) (2025-03-24)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.10\`
+## [1.0.10](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.9...@availity/mui-codemod@1.0.10) (2025-03-21)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.9\`
+## [1.0.9](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.8...@availity/mui-codemod@1.0.9) (2025-03-13)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.8\`
+## [1.0.8](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.7...@availity/mui-codemod@1.0.8) (2025-03-13)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.7\`
+## [1.0.7](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.6...@availity/mui-codemod@1.0.7) (2025-03-12)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.6\`
+## [1.0.6](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.5...@availity/mui-codemod@1.0.6) (2025-03-07)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.5\`
+* \`mui-layout\` updated to version \`1.0.5\`
+* \`mui-typography\` updated to version \`1.0.5\`
+## [1.0.5](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.4...@availity/mui-codemod@1.0.5) (2025-03-06)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.4\`
+## [1.0.3-beta.0](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.2...@availity/mui-codemod@1.0.3-beta.0) (2025-03-06)
+
+## [1.0.3-alpha.0](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.2...@availity/mui-codemod@1.0.3-alpha.0) (2025-03-06)
+
+## [1.0.4](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.3...@availity/mui-codemod@1.0.4) (2025-03-06)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.3\`
+## [1.0.3](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.2...@availity/mui-codemod@1.0.3) (2025-03-04)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.2\`
+## [1.0.2](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.1...@availity/mui-codemod@1.0.2) (2025-03-03)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.1\`
+## [1.0.1](https://github.com/Availity/element/compare/@availity/mui-codemod@1.0.0...@availity/mui-codemod@1.0.1) (2025-02-26)
+
+### Dependency Updates
+
+* \`element\` updated to version \`1.0.0\`
+## [1.0.0](https://github.com/Availity/element/compare/@availity/mui-codemod@0.1.0...@availity/mui-codemod@1.0.0) (2025-02-25)
+
+### Dependency Updates
+
+* \`element\` updated to version \`0.1.0\`
+* \`mui-layout\` updated to version \`0.1.0\`
+* \`mui-typography\` updated to version \`0.1.0\`
+
+### ⚠ BREAKING CHANGES
+
+* upgrade to @mui/material v6
+
+### Miscellaneous Chores
+
+* element v1 release ([a6e3567](https://github.com/Availity/element/commit/a6e35671185b9f13d25c7a39c4488ecb8774633e))
+
+## [0.1.0-alpha.1](https://github.com/Availity/element/compare/@availity/mui-codemod@0.1.0-alpha.0...@availity/mui-codemod@0.1.0-alpha.1) (2025-02-25)
+
+
+### Bug Fixes
+
+* **mui-codemod:** fix src references ([249d425](https://github.com/Availity/element/commit/249d425be22d33c30afa406bf3728f409875dcfd))
+
+## 0.1.0-alpha.0 (2025-02-24)
+
+
+### Features
+
+* **mui-codemod:** add MUI v6 codemods for element users ([3be1a1b](https://github.com/Availity/element/commit/3be1a1ba2dab07968d26361bf4965b96fae682b5))
+`;function o(t){return e.jsxs(e.Fragment,{children:[e.jsx(m,{title:"Utils/Codemod/Introduction"}),`
+`,e.jsx(n,{children:d}),`
+`,e.jsx(n,{children:p})]})}function D(t={}){const{wrapper:i}={...a(),...t.components};return i?e.jsx(i,{...t,children:e.jsx(o,{...t})}):o()}export{D as default};
