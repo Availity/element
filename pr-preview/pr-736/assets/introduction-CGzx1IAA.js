34cb4214190b71f685704c4dce613847fa9c7cbb
LauRoxx <LauRoxx@users.noreply.github.com>
2025-04-14 13:41:37 +0000

    Deploy preview for PR 736 🛫

---
+++ pr-preview/pr-736/assets/introduction-CGzx1IAA.js
@@ -0,0 +1,263 @@
+import{j as e}from"./jsx-runtime-BTJTZTIL.js";import{useMDXComponents as o}from"./index-XODwqvNS.js";import{M as s,c as n}from"./index-rRFSlsbd.js";import"./DocsRenderer-CFRXHY34-CM1mBSWp.js";import"./index-ChsGqxH_.js";import"./index-BgYLq7fD.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./iframe-Ccs_Qzcz.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./client-DQNj1uEJ.js";const r=`# @availity/mui-progress
+
+> Availity MUI Progress component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-progress.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-progress)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-progress.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-progress)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-progress?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-progress/package.json)
+
+## Documentation
+
+This package extends the MUI Progress component: [MUI Progress Docs](https://mui.com/components/progress/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-progress-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://design.availity.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+_This package has a few peer dependencies. Add \`@mui/material\` & \`@emotion/react\` to your project if not already installed._
+
+\`\`\`bash
+npm install @availity/mui-progress
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-progress
+\`\`\`
+
+### Usage
+
+The two progress components we have are \`CircularProgress\` and \`LinearProgress\`.
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { CircularProgress, LinearProgress } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { CircularProgress, LinearProgress } from '@availity/mui-progress';
+\`\`\`
+`,m=`# Changelog
+
+This file was generated using [@jscutlery/semver](https://github.com/jscutlery/semver).
+
+## [1.0.2](https://github.com/Availity/element/compare/@availity/mui-progress@1.0.1...@availity/mui-progress@1.0.2) (2025-04-11)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`1.0.1\`
+* \`mui-typography\` updated to version \`1.0.1\`
+* \`mui-layout\` updated to version \`1.0.1\`
+## [1.0.1](https://github.com/Availity/element/compare/@availity/mui-progress@1.0.0...@availity/mui-progress@1.0.1) (2025-03-07)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`1.0.0\`
+* \`mui-typography\` updated to version \`1.0.0\`
+* \`mui-layout\` updated to version \`1.0.0\`
+## [1.0.0](https://github.com/Availity/element/compare/@availity/mui-progress@1.0.0-alpha.0...@availity/mui-progress@1.0.0) (2025-02-25)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.5.1\`
+* \`mui-typography\` updated to version \`0.5.1\`
+* \`mui-layout\` updated to version \`0.5.1\`
+
+### ⚠ BREAKING CHANGES
+
+* upgrade to @mui/material v6
+
+### Miscellaneous Chores
+
+* element v1 release ([a6e3567](https://github.com/Availity/element/commit/a6e35671185b9f13d25c7a39c4488ecb8774633e))
+
+## [1.0.0-alpha.0](https://github.com/Availity/element/compare/@availity/mui-progress@0.5.1...@availity/mui-progress@1.0.0-alpha.0) (2025-02-24)
+
+
+### ⚠ BREAKING CHANGES
+
+* upgraded to @mui/material v6
+* **element:** upgraded to @mui/material v6
+
+### Features
+
+* **element:** upgrade to @mui/material v6 ([cb958bb](https://github.com/Availity/element/commit/cb958bba99a4f1ee6dab323f0ff54b69e6fd3493))
+* upgrade @mui/material ([571453a](https://github.com/Availity/element/commit/571453a34b21c344594ab4c03bc497d19aba942b))
+* upgrade to MUI v6 ([7febd6f](https://github.com/Availity/element/commit/7febd6fd4fd58e87e1c97a832cea3b4595a35d58))
+
+## [0.5.1](https://github.com/Availity/element/compare/@availity/mui-progress@0.5.0...@availity/mui-progress@0.5.1) (2025-02-19)
+
+
+### Bug Fixes
+
+* **mui-progress:** allow hiding of percentge ([168da9b](https://github.com/Availity/element/commit/168da9bfa7af44089cea27317e0d6ad9a057db3d))
+
+## [0.5.0](https://github.com/Availity/element/compare/@availity/mui-progress@0.4.5...@availity/mui-progress@0.5.0) (2025-02-04)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.4.5\`
+* \`mui-typography\` updated to version \`0.4.5\`
+* \`mui-layout\` updated to version \`0.4.5\`
+
+### Features
+
+* configure package.json to allow esm to be used ([5a5393d](https://github.com/Availity/element/commit/5a5393de761f52608e714dd94a05106937dd95db))
+
+## [0.4.5](https://github.com/Availity/element/compare/@availity/mui-progress@0.4.4...@availity/mui-progress@0.4.5) (2025-01-27)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.4.4\`
+## [0.4.4](https://github.com/Availity/element/compare/@availity/mui-progress@0.4.3...@availity/mui-progress@0.4.4) (2025-01-22)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.4.3\`
+## [0.4.3](https://github.com/Availity/element/compare/@availity/mui-progress@0.4.2...@availity/mui-progress@0.4.3) (2025-01-06)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.4.2\`
+## [0.4.2](https://github.com/Availity/element/compare/@availity/mui-progress@0.4.1...@availity/mui-progress@0.4.2) (2025-01-03)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.4.1\`
+## [0.4.1](https://github.com/Availity/element/compare/@availity/mui-progress@0.4.0...@availity/mui-progress@0.4.1) (2024-12-09)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.4.0\`
+## [0.4.0](https://github.com/Availity/element/compare/@availity/mui-progress@0.3.1...@availity/mui-progress@0.4.0) (2024-10-31)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.3.1\`
+* \`mui-typography\` updated to version \`0.3.1\`
+* \`mui-layout\` updated to version \`0.3.1\`
+
+### Features
+
+* add FileSelector ([7ad1f7b](https://github.com/Availity/element/commit/7ad1f7bb364bbeb2048d2ff4c9b0a2b1a1e33777))
+
+## [0.3.1](https://github.com/Availity/element/compare/@availity/mui-progress@0.3.0...@availity/mui-progress@0.3.1) (2024-09-09)
+
+
+### Bug Fixes
+
+* resolve accessibility issues in stories ([7e22419](https://github.com/Availity/element/commit/7e2241913f8ad10f467493b605fc0234e6eab5e2))
+
+## [0.3.0](https://github.com/Availity/element/compare/@availity/mui-progress@0.2.2...@availity/mui-progress@0.3.0) (2024-09-03)
+
+
+### Features
+
+* **mui-progress:** add LinearProgress component ([1f82d8f](https://github.com/Availity/element/commit/1f82d8f0691e1dc5d6b5f86993dfc2a0e2e1aeb8))
+
+## [0.2.2](https://github.com/Availity/element/compare/@availity/mui-progress@0.2.1...@availity/mui-progress@0.2.2) (2024-08-08)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.2.1\`
+## [0.2.1](https://github.com/Availity/element/compare/@availity/mui-progress@0.2.0...@availity/mui-progress@0.2.1) (2024-08-05)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.2.0\`
+## [0.2.0](https://github.com/Availity/element/compare/@availity/mui-progress@0.1.9...@availity/mui-progress@0.2.0) (2024-08-01)
+
+
+### Features
+
+* **mui-autocomplete:** add watchParams and inputValue ([1479d65](https://github.com/Availity/element/commit/1479d65830e2878207283639948423cf04fc5bd3))
+
+## [0.1.9](https://github.com/Availity/element/compare/@availity/mui-progress@0.1.8...@availity/mui-progress@0.1.9) (2024-07-22)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.1.8\`
+## [0.1.8](https://github.com/Availity/element/compare/@availity/mui-progress@0.1.7...@availity/mui-progress@0.1.8) (2024-06-14)
+
+### Dependency Updates
+
+* \`mui-typography\` updated to version \`0.1.7\`
+## [0.1.7](https://github.com/Availity/element/compare/@availity/mui-progress@0.1.6...@availity/mui-progress@0.1.7) (2024-04-19)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.1.6\`
+* \`mui-typography\` updated to version \`0.1.6\`
+## [0.1.6](https://github.com/Availity/element/compare/@availity/mui-progress@0.1.5...@availity/mui-progress@0.1.6) (2024-04-04)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.8.1\`
+* \`mui-typography\` updated to version \`0.1.7\`
+
+### Performance Improvements
+
+* **mui-progress:** use path imports for material deps ([e6078af](https://github.com/Availity/element/commit/e6078af0d82a003d704f4f30c23d31d7932d90cd))
+
+## [0.1.5](https://github.com/Availity/element/compare/@availity/mui-progress@0.1.4...@availity/mui-progress@0.1.5) (2024-03-15)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.8.0\`
+## [0.1.4](https://github.com/Availity/element/compare/@availity/mui-progress@0.1.3...@availity/mui-progress@0.1.4) (2024-02-22)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.7.5\`
+## [0.1.3](https://github.com/Availity/element/compare/@availity/mui-progress@0.1.2...@availity/mui-progress@0.1.3) (2024-02-20)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.7.4\`
+* \`mui-typography\` updated to version \`0.1.6\`
+## [0.1.2](https://github.com/Availity/element/compare/@availity/mui-progress@0.1.1...@availity/mui-progress@0.1.2) (2023-11-16)
+
+### Dependency Updates
+
+- \`mui-icon\` updated to version \`0.7.3\`
+- \`mui-typography\` updated to version \`0.1.5\`
+
+## [0.1.1](https://github.com/Availity/element/compare/@availity/mui-progress@0.1.0...@availity/mui-progress@0.1.1) (2023-11-15)
+
+## 0.1.0 (2023-11-14)
+
+### Dependency Updates
+
+- \`mui-icon\` updated to version \`0.7.2\`
+- \`mui-typography\` updated to version \`0.1.4\`
+
+### Features
+
+- **mui-progress:** initial commit ([0012911](https://github.com/Availity/element/commit/00129117d2107d4bdf97a3767119d30ae2edb418))
+`;function a(t){return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"Components/Progress/Introduction"}),`
+`,e.jsx(n,{children:r}),`
+`,e.jsx(n,{children:m})]})}function U(t={}){const{wrapper:i}={...o(),...t.components};return i?e.jsx(i,{...t,children:e.jsx(a,{...t})}):a()}export{U as default};
