e9b6cfe078f4960db1657de63c8c52d005b60778
LauRoxx <LauRoxx@users.noreply.github.com>
2025-04-14 17:24:49 +0000

    Deploy preview for PR 737 🛫

---
+++ pr-preview/pr-737/assets/Avatar.stories-BF6yEt6n.js
@@ -0,0 +1,69 @@
+import{j as r}from"./jsx-runtime-BTJTZTIL.js";import{e as w,r as E}from"./index-BgYLq7fD.js";import{d as L}from"./index-Dl5t6TV_.js";import{U as M,o as C}from"./index-DcwHbAK-.js";import{A as H}from"./Avatar-BkwXUoc0.js";import{G as s}from"./Grid2-JMPt1-k0.js";import"./index-ChsGqxH_.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./index-DLKRbc4C.js";import"./___vite-browser-external_commonjs-proxy-DDYoOVPM.js";import"./index-DuRQpI0H.js";import"./_toKey-DFcMYS53.js";import"./faCircleArrowRight-B14F5Piw.js";import"./faUser-D77uLSLS.js";import"./faSortDown-BIc1sKK4.js";import"./SvgIcon-CiBNlF9H.js";import"./generateUtilityClass-BeVSWKCg.js";import"./identifier-CudAVvBZ.js";import"./memoTheme-CpoNRPWX.js";import"./styled-CKuRQFSa.js";import"./DefaultPropsProvider-CWkNlXkE.js";import"./generateUtilityClasses-whvyra8-.js";import"./createSvgIcon-BWWwXn7v.js";import"./useSlot-1VRyIerd.js";import"./resolveComponentProps-CCNxqvxI.js";import"./useForkRef-Dz4aGlyk.js";import"./useTheme-Cx1wRXKO.js";import"./useTheme-Ib01SJ3a.js";import"./styled-Dbr83ACF.js";import"./extendSxProp-BY7TjwKM.js";import"./isMuiElement-DiOAsJE6.js";import"./useThemeProps-Do5o_sjG.js";const c={xs:{width:18,height:18,fontSize:"caption.fontSize"},s:{width:24,height:24,fontSize:"caption.fontSize"},m:{width:40,height:40,fontSize:"h5.fontSize",fontWeight:"fontWeightBold"},l:{width:80,height:80,fontSize:"h3.fontSize",fontWeight:"fontWeightBold"},xl:{width:140,height:140,fontSize:"h1.fontSize",fontWeight:"fontWeightBold"}},U=(e,a)=>e.split(" ").length>1&&a!=="xs"?`${e.split(" ")[0][0]}${e.split(" ")[1][0]}`:e.charAt(0),W=(e,a)=>typeof e=="string"?U(e,a).toUpperCase():e,t=({children:e,size:a="xl",src:m,sx:V,...b})=>{const[q,p]=w.useState();return E.useEffect(()=>{(async()=>{if(m)p(m);else{const I=(await L.getApplication("AVATAR")).data.settings[0].avatar;p(I)}})()},[m]),r.jsx(H,{...b,variant:"circular",src:e?void 0:q,sx:{...V,...c[a]||c.xl},children:e?W(e,a):r.jsx(M,{})})};try{t.displayName="Avatar",t.__docgenInfo={description:"",displayName:"Avatar",props:{size:{defaultValue:{value:"xl"},description:"The size of the component.",name:"size",required:!1,type:{name:'"m" | "s" | "xs" | "l" | "xl"'}},children:{defaultValue:null,description:"Used to render icon or text elements inside the Avatar if `src` is not set.\nThis can be an element, or just a string.",name:"children",required:!1,type:{name:"ReactNode"}},className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}},style:{defaultValue:null,description:"",name:"style",required:!1,type:{name:"CSSProperties"}},classes:{defaultValue:null,description:"Override or extend the styles applied to the component.",name:"classes",required:!1,type:{name:"Partial<AvatarClasses> & Partial<ClassNameMap<never>>"}},sx:{defaultValue:null,description:"The system prop that allows defining system overrides as well as additional CSS styles.",name:"sx",required:!1,type:{name:"SxProps<Theme>"}},ref:{defaultValue:null,description:"",name:"ref",required:!1,type:{name:"Ref<HTMLDivElement>"}},component:{defaultValue:null,description:"",name:"component",required:!1,type:{name:"ElementType<any, keyof IntrinsicElements>"}},slots:{defaultValue:{value:"{}"},description:"The components used for each slot inside.",name:"slots",required:!1,type:{name:"Partial<AvatarSlots>"}},slotProps:{defaultValue:{value:"{}"},description:"The props used for each slot inside.",name:"slotProps",required:!1,type:{name:"{ img?: SlotProps<ElementType<ImgHTMLAttributes<HTMLImageElement>, keyof IntrinsicElements>, {}, AvatarOwnProps>; }"}},src:{defaultValue:null,description:"The `src` attribute for the `img` element.",name:"src",required:!1,type:{name:"string"}},alt:{defaultValue:null,description:"Used in combination with `src` or `srcSet` to\nprovide an alt attribute for the rendered `img` element.",name:"alt",required:!1,type:{name:"string"}},sizes:{defaultValue:null,description:"The `sizes` attribute for the `img` element.",name:"sizes",required:!1,type:{name:"string"}},srcSet:{defaultValue:null,description:"The `srcSet` attribute for the `img` element.\nUse this attribute for responsive image display.",name:"srcSet",required:!1,type:{name:"string"}},imgProps:{defaultValue:null,description:"[Attributes](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#attributes) applied to the `img` element if the component is used to display an image.\nIt can be used to listen for the loading error event.\n@deprecated Use `slotProps.img` instead. This prop will be removed in v7. See [Migrating from deprecated APIs](/material-ui/migration/migrating-from-deprecated-apis/) for more details.",name:"imgProps",required:!1,type:{name:"ImgHTMLAttributes<HTMLImageElement> & { sx?: SxProps<Theme>; }"}}}}}catch{}const Ae={title:"Components/Avatar/Avatar",component:t,argTypes:{size:{options:["xs","s","m","l","xl"]}},tags:["autodocs"]},i={render:e=>r.jsx(t,{...e})},n={render:e=>r.jsx(t,{...e}),args:{children:"John Doe"}},o={render:e=>r.jsx(t,{...e}),args:{src:"https://avatars.githubusercontent.com/u/329985?s=100",alt:"Orange circle with interlocking AV logo"}},l={render:e=>r.jsx(t,{...e,children:r.jsx(C,{})})},d={render:e=>r.jsxs(s,{container:!0,spacing:1,children:[r.jsx(s,{size:{xs:2},children:"XSmall"}),r.jsx(s,{size:{xs:10},children:r.jsx(t,{...e,size:"xs"})}),r.jsx(s,{size:{xs:2},children:"Small"}),r.jsx(s,{size:{xs:10},children:r.jsx(t,{...e,size:"s"})}),r.jsx(s,{size:{xs:2},children:"Medium"}),r.jsx(s,{size:{xs:10},children:r.jsx(t,{...e,size:"m"})}),r.jsx(s,{size:{xs:2},children:"Large"}),r.jsx(s,{size:{xs:10},children:r.jsx(t,{...e,size:"l"})}),r.jsx(s,{size:{xs:2},children:"XLarge"}),r.jsx(s,{size:{xs:10},children:r.jsx(t,{...e,size:"xl"})})]}),args:{children:"John Doe"},parameters:{controls:{include:"children"}}};var u,g,h;i.parameters={...i.parameters,docs:{...(u=i.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: (args: AvatarProps) => <Avatar {...args} />
+}`,...(h=(g=i.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};var f,x,v;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: (args: AvatarProps) => <Avatar {...args} />,
+  args: {
+    children: 'John Doe'
+  }
+}`,...(v=(x=n.parameters)==null?void 0:x.docs)==null?void 0:v.source}}};var z,A,S;o.parameters={...o.parameters,docs:{...(z=o.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  render: (args: AvatarProps) => <Avatar {...args} />,
+  args: {
+    src: 'https://avatars.githubusercontent.com/u/329985?s=100',
+    alt: 'Orange circle with interlocking AV logo'
+  }
+}`,...(S=(A=o.parameters)==null?void 0:A.docs)==null?void 0:S.source}}};var y,j,G;l.parameters={...l.parameters,docs:{...(y=l.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: (args: AvatarProps) => <Avatar {...args}>
+      <HomeIcon />
+    </Avatar>
+}`,...(G=(j=l.parameters)==null?void 0:j.docs)==null?void 0:G.source}}};var T,_,P;d.parameters={...d.parameters,docs:{...(T=d.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: (args: AvatarProps) => <Grid container spacing={1}>
+      <Grid size={{
+      xs: 2
+    }}>XSmall</Grid>
+      <Grid size={{
+      xs: 10
+    }}>
+        <Avatar {...args} size="xs" />
+      </Grid>
+      <Grid size={{
+      xs: 2
+    }}>Small</Grid>
+      <Grid size={{
+      xs: 10
+    }}>
+        <Avatar {...args} size="s" />
+      </Grid>
+      <Grid size={{
+      xs: 2
+    }}>Medium</Grid>
+      <Grid size={{
+      xs: 10
+    }}>
+        <Avatar {...args} size="m" />
+      </Grid>
+      <Grid size={{
+      xs: 2
+    }}>Large</Grid>
+      <Grid size={{
+      xs: 10
+    }}>
+        <Avatar {...args} size="l" />
+      </Grid>
+      <Grid size={{
+      xs: 2
+    }}>XLarge</Grid>
+      <Grid size={{
+      xs: 10
+    }}>
+        <Avatar {...args} size="xl" />
+      </Grid>
+    </Grid>,
+  args: {
+    children: 'John Doe'
+  },
+  parameters: {
+    controls: {
+      include: 'children'
+    }
+  }
+}`,...(P=(_=d.parameters)==null?void 0:_.docs)==null?void 0:P.source}}};const Se=["_Avatar","_TextAvatar","_CustomSrcAvatar","_IconAvatar","_Sizes"];export{i as _Avatar,o as _CustomSrcAvatar,l as _IconAvatar,d as _Sizes,n as _TextAvatar,Se as __namedExportsOrder,Ae as default};
---
+++ pr-preview/pr-737/assets/SearchBy.stories-BUfAjJMo.js
@@ -0,0 +1,28 @@
+import{j as r}from"./jsx-runtime-BTJTZTIL.js";import{b as p}from"./Switch-D-GvDFdl.js";import"./FieldHelpIcon-CsBnd3N_.js";import"./FormLabel--QKszGQN.js";import"./Input-COUqH-9J.js";import"./index-DSF7aQ5o.js";import{c as h}from"./index-DcwHbAK-.js";import{r as d}from"./index-BgYLq7fD.js";import{T as o}from"./TextField-DBCIsg8u.js";import{M as e}from"./MenuItem-Cszf_9D3.js";import{I as u}from"./InputAdornment-LPaTK-Af.js";import"./index-ChsGqxH_.js";import"./RadioGroup-CZeO-sCM.js";import"./generateUtilityClass-BeVSWKCg.js";import"./identifier-CudAVvBZ.js";import"./SwitchBase-CYtmyT40.js";import"./styled-CKuRQFSa.js";import"./DefaultPropsProvider-CWkNlXkE.js";import"./useFormControl-t_-EwZHM.js";import"./generateUtilityClasses-whvyra8-.js";import"./useControlled-DxC1uCGX.js";import"./ButtonBase-D97P5WV0.js";import"./useTimeout-iC_34LsC.js";import"./useEnhancedEffect-j2zQLnYm.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./TransitionGroupContext-BKlA24Pm.js";import"./emotion-react.browser.esm-v7Jm8XFw.js";import"./useForkRef-Dz4aGlyk.js";import"./isFocusVisible-B8k4qzLc.js";import"./createSvgIcon-BWWwXn7v.js";import"./SvgIcon-CiBNlF9H.js";import"./memoTheme-CpoNRPWX.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useId-DIK8ekB4.js";import"./formControlState-Dq1zat_P.js";import"./Select-IoH-6RCp.js";import"./Menu-GaZ6ImQN.js";import"./index-DyJdRKzS.js";import"./useSlotProps-BmqT0tgx.js";import"./resolveComponentProps-CCNxqvxI.js";import"./Popover-CQBNnNRW.js";import"./isHostComponent-DVu5iVWx.js";import"./useSlot-1VRyIerd.js";import"./Portal-C2J4q3Ji.js";import"./useTheme-Cx1wRXKO.js";import"./useTheme-Ib01SJ3a.js";import"./utils-tFrEP9Vf.js";import"./index-Bxu_udwM.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./index-DdXvKv-Q.js";import"./getReactElementRef-Df0xbma2.js";import"./ownerDocument-DW-IO8s5.js";import"./ownerWindow-HkKU3E4x.js";import"./debounce-Be36O1Ab.js";import"./Paper-4P5FqKie.js";import"./Modal-CezAubrN.js";import"./Backdrop-TXxSV-j9.js";import"./Fade-BCUt9uX4.js";import"./List-eXGzl-Dz.js";import"./utils-DoM3o7-Q.js";import"./OutlinedInput-B93mfhOp.js";import"./index-DfraP8LW.js";import"./extendSxProp-BY7TjwKM.js";import"./Stack-qbqOHiMo.js";import"./styled-Dbr83ACF.js";import"./useThemeProps-Do5o_sjG.js";import"./Divider-DJW73rZ3.js";import"./dividerClasses-0g4QmK6Y.js";import"./faCheck-BUdkY9Qv.js";import"./Switch-CyD42d4W.js";import"./index-CQspzllj.js";import"./Box-Dv31mJKI.js";import"./Grid2-JMPt1-k0.js";import"./isMuiElement-DiOAsJE6.js";import"./Container-DVbvSSVb.js";import"./index-DJKl12U0.js";import"./index-DnNfvsmq.js";import"./Tooltip-CIyLwlBA.js";import"./IconButton-D_kG6RhR.js";import"./CircularProgress-fcRSDrpN.js";import"./Button-yLAd6_L_.js";import"./LoadingButton-Cl76w144.js";import"./FormControlLabel-BzV1MzKU.js";import"./Typography-COSof5sR.js";import"./FormLabel-BDxB3zbH.js";import"./faCircleArrowRight-B14F5Piw.js";import"./faUser-D77uLSLS.js";import"./faSortDown-BIc1sKK4.js";import"./FormControl-BpN01e6f.js";import"./listItemTextClasses-DiJrtlJf.js";const Zr={title:"Form Components/FormUtils/SearchByFormGroup",component:p,tags:["autodocs"]},t={render:()=>{const[s,n]=d.useState(""),c=l=>{n(l.target.value)};return r.jsxs(p,{searchById:"searchby",children:[r.jsxs(o,{id:"searchby",value:s,fullWidth:!1,select:!0,slotProps:{select:{onChange:c,labelId:"searchby-label"}},children:[r.jsx(e,{value:"Parameter 1",children:"Parameter 1"}),r.jsx(e,{value:"Parameter 2",children:"Parameter 2"}),r.jsx(e,{value:"Parameter 3",children:"Parameter 3"})]}),r.jsx(o,{id:"search",placeholder:"search",slotProps:{input:{startAdornment:r.jsx(u,{position:"start",children:r.jsx(h,{"aria-hidden":!1,"aria-label":"search"})})}}})]})}};var m,i,a;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => {
+    const [searchBy, setSearchBy] = useState('');
+    const handleChange = (event: SelectChangeEvent) => {
+      setSearchBy(event.target.value as string);
+    };
+    return <SearchByFormGroup searchById='searchby'>
+      {/* It is recommended to use Autocomplete instead of a select TextField */}
+      <TextField id="searchby" value={searchBy} fullWidth={false} select slotProps={{
+        select: {
+          onChange: handleChange,
+          labelId: "searchby-label"
+        }
+      }}>
+        <MenuItem value="Parameter 1">Parameter 1</MenuItem>
+        <MenuItem value="Parameter 2">Parameter 2</MenuItem>
+        <MenuItem value="Parameter 3">Parameter 3</MenuItem>
+      </TextField>
+      <TextField id="search" placeholder="search" slotProps={{
+        input: {
+          startAdornment: <InputAdornment position="start">
+                <SearchIcon aria-hidden={false} aria-label="search" />
+              </InputAdornment>
+        }
+      }} />
+      </SearchByFormGroup>;
+  }
+}`,...(a=(i=t.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};const $r=["_SearchBy"];export{t as _SearchBy,$r as __namedExportsOrder,Zr as default};
