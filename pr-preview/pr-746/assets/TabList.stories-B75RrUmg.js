df3c74512b01377e5214a94e10d1e3c9509fd237
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-04-24 19:35:57 +0000

    Deploy preview for PR 746 🛫

---
+++ pr-preview/pr-746/assets/TabList.stories-B75RrUmg.js
@@ -0,0 +1,34 @@
+import{j as e}from"./jsx-runtime-BTJTZTIL.js";import{r as T}from"./index-BgYLq7fD.js";import{L as g,U as j}from"./index-BAgvEqVW.js";import{a as i,T as x,b as l}from"./TabPanel-MTiq0kG8.js";import{T as t}from"./Tab-DzfqzfzO.js";import"./index-ChsGqxH_.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./faCircleArrowRight-B14F5Piw.js";import"./faUser-D77uLSLS.js";import"./faSortDown-BIc1sKK4.js";import"./SvgIcon-CiBNlF9H.js";import"./generateUtilityClass-BeVSWKCg.js";import"./identifier-CudAVvBZ.js";import"./memoTheme-CpoNRPWX.js";import"./styled-CKuRQFSa.js";import"./DefaultPropsProvider-CWkNlXkE.js";import"./generateUtilityClasses-whvyra8-.js";import"./TabList-DfEf7-ih.js";import"./Tabs-a-K29MN_.js";import"./ButtonBase-D97P5WV0.js";import"./useTimeout-iC_34LsC.js";import"./useEnhancedEffect-j2zQLnYm.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./TransitionGroupContext-BKlA24Pm.js";import"./emotion-react.browser.esm-v7Jm8XFw.js";import"./useForkRef-Dz4aGlyk.js";import"./isFocusVisible-B8k4qzLc.js";import"./index-DyJdRKzS.js";import"./useTheme-Cx1wRXKO.js";import"./useTheme-Ib01SJ3a.js";import"./debounce-Be36O1Ab.js";import"./ownerWindow-HkKU3E4x.js";import"./ownerDocument-DW-IO8s5.js";import"./KeyboardArrowRight-Dovugu6t.js";import"./createSvgIcon-BWWwXn7v.js";import"./useSlotProps-BmqT0tgx.js";import"./resolveComponentProps-CCNxqvxI.js";import"./useThemeProps-BGrMtKaW.js";import"./useThemeProps-Do5o_sjG.js";const se={title:"Components/Tabs/TabList",component:i,argTypes:{level:{options:["primary","secondary"],control:{type:"radio"}}},tags:["autodocs"]},r={render:n=>{const[a,s]=T.useState("1"),m=(p,P)=>{s(P)};return e.jsxs(x,{value:a,children:[e.jsxs(i,{...n,onChange:m,"aria-label":"lab API tabs example",children:[e.jsx(t,{label:"Item One",value:"1"}),e.jsx(t,{label:"Item Two",value:"2"}),e.jsx(t,{label:"Item Three",value:"3"})]}),e.jsx(l,{value:"1",children:`Hello from Panel ${a}`}),e.jsx(l,{value:"2",children:`Hello from Panel ${a}`}),e.jsx(l,{value:"3",children:`Hello from Panel ${a}`})]})},args:{}},o={render:()=>{const[n,a]=T.useState("1"),s=(m,p)=>{a(p)};return e.jsxs(x,{value:n,children:[e.jsxs(i,{onChange:s,"aria-label":"lab API tabs example",children:[e.jsx(t,{value:"1",icon:e.jsx(g,{}),label:"Links"}),e.jsx(t,{value:"2",icon:e.jsx(j,{}),label:"My Account"})]}),e.jsx(l,{value:"1",children:`Hello from Panel ${n}`}),e.jsx(l,{value:"2",children:`Hello from Panel ${n}`})]})}};var u,c,b;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: (args: TabListProps) => {
+    const [value, setValue] = useState('1');
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+      setValue(newValue);
+    };
+    return <TabContext value={value}>
+        <TabList {...args} onChange={handleChange} aria-label="lab API tabs example">
+          <Tab label="Item One" value="1" />
+          <Tab label="Item Two" value="2" />
+          <Tab label="Item Three" value="3" />
+        </TabList>
+        <TabPanel value="1" children={\`Hello from Panel \${value}\`} />
+        <TabPanel value="2" children={\`Hello from Panel \${value}\`} />
+        <TabPanel value="3" children={\`Hello from Panel \${value}\`} />
+      </TabContext>;
+  },
+  args: {}
+}`,...(b=(c=r.parameters)==null?void 0:c.docs)==null?void 0:b.source}}};var v,d,h;o.parameters={...o.parameters,docs:{...(v=o.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: () => {
+    const [value, setValue] = useState('1');
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+      setValue(newValue);
+    };
+    return <TabContext value={value}>
+        <TabList onChange={handleChange} aria-label="lab API tabs example">
+          <Tab value="1" icon={<LinkIcon />} label="Links" />
+          <Tab value="2" icon={<UserIcon />} label="My Account" />
+        </TabList>
+        <TabPanel value="1" children={\`Hello from Panel \${value}\`} />
+        <TabPanel value="2" children={\`Hello from Panel \${value}\`} />
+      </TabContext>;
+  }
+}`,...(h=(d=o.parameters)==null?void 0:d.docs)==null?void 0:h.source}}};const ie=["_TabList","_WithIcons"];export{r as _TabList,o as _WithIcons,ie as __namedExportsOrder,se as default};
