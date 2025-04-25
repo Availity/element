673d14121b32486cbb884e1d074ed11e33eef896
TheSharpieOne <TheSharpieOne@users.noreply.github.com>
2025-04-25 18:52:08 +0000

    Deploy preview for PR 747 🛫

---
+++ pr-preview/pr-747/assets/TableBody.stories-3J6uZUtb.js
@@ -0,0 +1,19 @@
+import{j as l}from"./jsx-runtime-BTJTZTIL.js";import{a as b,T as r}from"./TableRow-DI5lkLPs.js";import{T as e}from"./TableCell-BTrDLNce.js";import"./index-ChsGqxH_.js";import"./TableRow-CMxK_O_U.js";import"./index-BgYLq7fD.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./generateUtilityClass-BeVSWKCg.js";import"./identifier-CudAVvBZ.js";import"./TableCell-Dihxu9Xc.js";import"./memoTheme-CpoNRPWX.js";import"./styled-CKuRQFSa.js";import"./DefaultPropsProvider-CWkNlXkE.js";import"./generateUtilityClasses-whvyra8-.js";import"./visuallyHidden-Dan1xhjv.js";const g={title:"Components/Table/TableBody",component:b,tags:["autodocs"],args:{}},a={render:C=>l.jsxs(b,{...C,children:[l.jsxs(r,{children:[l.jsx(e,{children:"Cell a1"}),l.jsx(e,{children:"Cell a2"}),l.jsx(e,{children:"Cell a3"})]}),l.jsxs(r,{children:[l.jsx(e,{children:"Cell b1"}),l.jsx(e,{children:"Cell b2"}),l.jsx(e,{children:"Cell b3"})]}),l.jsxs(r,{children:[l.jsx(e,{children:"Cell c1"}),l.jsx(e,{children:"Cell c2"}),l.jsx(e,{children:"Cell c3"})]})]})};var o,s,n;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: (args: TableBodyProps) => <TableBody {...args}>
+      <TableRow>
+        <TableCell>Cell a1</TableCell>
+        <TableCell>Cell a2</TableCell>
+        <TableCell>Cell a3</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell>Cell b1</TableCell>
+        <TableCell>Cell b2</TableCell>
+        <TableCell>Cell b3</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell>Cell c1</TableCell>
+        <TableCell>Cell c2</TableCell>
+        <TableCell>Cell c3</TableCell>
+      </TableRow>
+    </TableBody>
+}`,...(n=(s=a.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const f=["_TableBody"];export{a as _TableBody,f as __namedExportsOrder,g as default};
