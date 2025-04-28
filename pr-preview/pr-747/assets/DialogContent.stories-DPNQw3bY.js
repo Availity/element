2419c9b9fa69895b727b48460c677b26c3aaf758
TheSharpieOne <TheSharpieOne@users.noreply.github.com>
2025-04-28 14:08:52 +0000

    Remove preview for PR 747 🛬

--- pr-preview/pr-747/assets/DialogContent.stories-DPNQw3bY.js
+++
@@ -1,6 +0,0 @@
-import{j as s}from"./jsx-runtime-BTJTZTIL.js";import{D as e}from"./DialogContent-BNzL04OO.js";import"./index-ChsGqxH_.js";import"./DialogContent-tptSoxy4.js";import"./index-BgYLq7fD.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./generateUtilityClass-BeVSWKCg.js";import"./identifier-CudAVvBZ.js";import"./memoTheme-CpoNRPWX.js";import"./styled-CKuRQFSa.js";import"./DefaultPropsProvider-CWkNlXkE.js";import"./generateUtilityClasses-whvyra8-.js";import"./dialogTitleClasses-BXUk1z2I.js";const j={title:"Components/Dialog/DialogContent",component:e,tags:["autodocs"]},o={render:a=>s.jsx(e,{...a}),args:{children:"Modal Content Body"}};var t,r,n;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: (args: DialogContentProps) => <DialogContent {...args} />,
-  args: {
-    children: 'Modal Content Body'
-  }
-}`,...(n=(r=o.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const h=["_DialogContent"];export{o as _DialogContent,h as __namedExportsOrder,j as default};
