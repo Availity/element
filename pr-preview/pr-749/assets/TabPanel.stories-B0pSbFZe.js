24a8dfecaf06396f4b5565db2234426fd7473ee6
jordan-a-young <jordan-a-young@users.noreply.github.com>
2025-04-30 13:33:17 +0000

    Deploy preview for PR 749 🛫

---
+++ pr-preview/pr-749/assets/TabPanel.stories-B0pSbFZe.js
@@ -0,0 +1,14 @@
+import{j as r}from"./jsx-runtime-BTJTZTIL.js";import{T as m,a as p,b as s}from"./TabPanel-wIlx7FBW.js";import{D as l}from"./index-D9T0Evn7.js";import{T as n}from"./Tab-CNnuH62a.js";import"./index-ChsGqxH_.js";import"./TabList-DfEf7-ih.js";import"./index-BgYLq7fD.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./Tabs-a-K29MN_.js";import"./generateUtilityClass-BeVSWKCg.js";import"./identifier-CudAVvBZ.js";import"./memoTheme-CpoNRPWX.js";import"./styled-CKuRQFSa.js";import"./DefaultPropsProvider-CWkNlXkE.js";import"./generateUtilityClasses-whvyra8-.js";import"./ButtonBase-D97P5WV0.js";import"./useTimeout-iC_34LsC.js";import"./useEnhancedEffect-j2zQLnYm.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./TransitionGroupContext-BKlA24Pm.js";import"./emotion-react.browser.esm-v7Jm8XFw.js";import"./useForkRef-Dz4aGlyk.js";import"./isFocusVisible-B8k4qzLc.js";import"./index-DyJdRKzS.js";import"./useTheme-Cx1wRXKO.js";import"./useTheme-Ib01SJ3a.js";import"./debounce-Be36O1Ab.js";import"./ownerWindow-HkKU3E4x.js";import"./ownerDocument-DW-IO8s5.js";import"./KeyboardArrowRight-Dovugu6t.js";import"./createSvgIcon-BWWwXn7v.js";import"./SvgIcon-CiBNlF9H.js";import"./useSlotProps-BmqT0tgx.js";import"./resolveComponentProps-CCNxqvxI.js";import"./useThemeProps-BGrMtKaW.js";import"./useThemeProps-Do5o_sjG.js";import"./Divider-DJW73rZ3.js";import"./dividerClasses-0g4QmK6Y.js";const W={title:"Components/Tabs/TabPanel",component:m,tags:["autodocs"]},t={render:i=>r.jsxs(m,{value:"1",children:[r.jsx(p,{"aria-label":"lab API tabs example",children:r.jsx(n,{label:"Item One",value:"1"})}),r.jsx(l,{}),r.jsx(s,{...i,value:"1"})]}),args:{children:"Item One"}};var a,o,e;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: (args: TabPanelProps) => {
+    return <TabContext value="1">
+        <TabList aria-label="lab API tabs example">
+          <Tab label="Item One" value="1" />
+        </TabList>
+        <Divider />
+        <TabPanel {...args} value="1" />
+      </TabContext>;
+  },
+  args: {
+    children: 'Item One'
+  }
+}`,...(e=(o=t.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const X=["_TabPanel"];export{t as _TabPanel,X as __namedExportsOrder,W as default};
