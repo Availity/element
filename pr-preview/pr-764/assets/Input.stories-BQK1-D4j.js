17e442ad4d27b35f937710ed566fc3aa08cd212d
TheSharpieOne <TheSharpieOne@users.noreply.github.com>
2025-05-20 14:05:25 +0000

    Deploy preview for PR 764 🛫

---
+++ pr-preview/pr-764/assets/Input.stories-BQK1-D4j.js
@@ -0,0 +1,35 @@
+import{j as t}from"./jsx-runtime-BTJTZTIL.js";import{c as l}from"./TextField-Ss4awFDR.js";import{B as e}from"./index-k9OtkSpc.js";import{P as c}from"./index-Bd2OKRRh.js";import{T as f}from"./index-ClMemn8c.js";import{G as S}from"./index-DmY_4rvh.js";import{I as g,a as b}from"./Types-DoqI4C18.js";import"./AsyncAutocomplete-DaHgqzQ6.js";import"./Checkbox-1tbt5O0K.js";import{u as h,F as x}from"./index.esm-Cdw3oWqD.js";import"./index-ChsGqxH_.js";import"./index-wn_G9AKN.js";import"./index-BgYLq7fD.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./index-BREKFP9Y.js";import"./index-YXU10wut.js";import"./index-DJKl12U0.js";import"./index-CmMPo3bx.js";import"./faCircleArrowRight-B14F5Piw.js";import"./faUser-D77uLSLS.js";import"./faSortDown-BIc1sKK4.js";import"./SvgIcon-D3xe1wVB.js";import"./generateUtilityClass-GxznSYtt.js";import"./identifier-CHUoN9pF.js";import"./memoTheme-BLrvrRyo.js";import"./styled-FZfCzBz5.js";import"./DefaultPropsProvider-ArLX9SCD.js";import"./generateUtilityClasses-aotQgNYZ.js";import"./faCheck-BUdkY9Qv.js";import"./Select-D8Pqa1Dd.js";import"./formControlState-Dq1zat_P.js";import"./useFormControl-t_-EwZHM.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./Menu-BUIr0rCt.js";import"./index-DyJdRKzS.js";import"./useSlotProps-DT0tbkWv.js";import"./resolveComponentProps-XMphxBSb.js";import"./useForkRef-Dz4aGlyk.js";import"./Popover-DNs0CrC2.js";import"./isHostComponent-DVu5iVWx.js";import"./useSlot-qvYlW2UV.js";import"./Portal-DEVqEMA6.js";import"./useTheme-BGaXYmwe.js";import"./useTheme-CVByJb8R.js";import"./utils-tFrEP9Vf.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./TransitionGroupContext-BKlA24Pm.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./useTimeout-iC_34LsC.js";import"./useEnhancedEffect-j2zQLnYm.js";import"./getReactElementRef-Df0xbma2.js";import"./ownerDocument-DW-IO8s5.js";import"./ownerWindow-HkKU3E4x.js";import"./debounce-Be36O1Ab.js";import"./Paper-zGtC1N39.js";import"./Modal-BSor9Am9.js";import"./Backdrop-DwIksyfj.js";import"./Fade-rL0rk9XF.js";import"./List-CN2yCDHK.js";import"./OutlinedInput-ClKDCKpR.js";import"./index-Df15702_.js";import"./extendSxProp-BZnzVrgj.js";import"./emotion-react.browser.esm-DW5OmoQI.js";import"./useControlled-DxC1uCGX.js";import"./useId-DIK8ekB4.js";import"./createSvgIcon-DK1T27V3.js";import"./FormControlLabel-NrRuXsuu.js";import"./Typography-geKghQyJ.js";import"./Switch-BMNMooHv.js";import"./SwitchBase-DpeF2ikX.js";import"./ButtonBase-DwAEbHIm.js";import"./isFocusVisible-B8k4qzLc.js";import"./RadioGroup-CGGGzwtq.js";import"./Box-BU2WakPM.js";import"./Divider-CE8hkX1u.js";import"./dividerClasses-Cy3VQ5Zc.js";import"./Stack-2ILcP_1D.js";import"./styled-CsKbBpC_.js";import"./useThemeProps-BNBLMFIr.js";import"./TextField-Dbm3e8rv.js";import"./FormControl-eNKWX08Y.js";import"./isMuiElement-DiOAsJE6.js";import"./Grid2-BbvvmKhh.js";import"./index-Dl5t6TV_.js";import"./index-DLKRbc4C.js";import"./___vite-browser-external_commonjs-proxy-DDYoOVPM.js";import"./index-DuRQpI0H.js";import"./_toKey-DFcMYS53.js";import"./Autocomplete-Dm71EoBJ.js";import"./Close-9bLEWkOn.js";import"./usePreviousProps-DrPGTKoq.js";import"./Tooltip-Xh9QA514.js";import"./Chip-H5O1l7tv.js";import"./IconButton-BFylt634.js";import"./CircularProgress-o1goEU37.js";import"./ListSubheader-BX_8zoPk.js";import"./useInfiniteQuery-W0DjiFdp.js";import"./useBaseQuery-BiPPAl1W.js";import"./suspense-C-kwa4RK.js";import"./index-Dpmba6RM.js";import"./DatePicker-D0YeQkW6.js";import"./useThemeProps-BADT0iz2.js";import"./index-BPy8VV6B.js";import"./index-Chjiymov.js";import"./enUS-Dpj66GLB.js";import"./visuallyHidden-Dan1xhjv.js";import"./InputAdornment-D5eTYx4L.js";import"./Button-DrYdciQ0.js";import"./DialogActions-BLu5LWbN.js";import"./ListItem-ObWngzl3.js";import"./Dialog-D9_v8jKb.js";import"./DialogContext-Dy-mxFGl.js";import"./DialogContent-Bqwt87pB.js";import"./dialogTitleClasses-wn6hUYL7.js";import"./LoadingButton-C6EjcQI6.js";import"./Container-DvcsWH8C.js";import"./index-CC6mOWec.js";import"./Checkbox-DEN02h0-.js";const Gt={title:"Form Components/Controlled Form/ControlledInput",component:l,tags:["autodocs"],argTypes:{...b,...g},parameters:{controls:{exclude:["className","defaultChecked","onError","ref","style","tabIndex"]}}},o={render:u=>{var i,m,p;const r=h();return t.jsx(x,{...r,children:t.jsxs("form",{onSubmit:r.handleSubmit(d=>d),children:[t.jsx(l,{...u}),t.jsxs(S,{container:!0,direction:"row",justifyContent:"space-between",marginTop:1,children:[t.jsx(e,{disabled:!((i=r==null?void 0:r.formState)!=null&&i.isSubmitSuccessful),children:"Reset",color:"secondary",onClick:()=>r.reset()}),t.jsx(e,{type:"submit",disabled:(m=r==null?void 0:r.formState)==null?void 0:m.isSubmitSuccessful,children:"Submit"})]}),(p=r==null?void 0:r.formState)!=null&&p.isSubmitSuccessful?t.jsxs(c,{sx:{padding:"1.5rem",marginTop:"1.5rem"},children:[t.jsx(f,{variant:"h2",children:"Submitted Values"}),t.jsx("pre",{"data-testid":"result",children:JSON.stringify(r.getValues(),null,2)})]}):null]})})},args:{name:"controlledInput",required:!0,rules:{required:"This is required.",maxLength:{value:10,message:"Too long"}},inputProps:{"aria-label":"Input Label"}}};var s,a,n;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: (args: ControlledInputProps) => {
+    const methods = useForm();
+    return <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(data => data)}>
+          <ControlledInput {...args} />
+          <Grid container direction="row" justifyContent="space-between" marginTop={1}>
+            <Button disabled={!methods?.formState?.isSubmitSuccessful} children="Reset" color="secondary" onClick={() => methods.reset()} />
+            <Button type="submit" disabled={methods?.formState?.isSubmitSuccessful} children="Submit" />
+          </Grid>
+          {methods?.formState?.isSubmitSuccessful ? <Paper sx={{
+          padding: '1.5rem',
+          marginTop: '1.5rem'
+        }}>
+              <Typography variant="h2">Submitted Values</Typography>
+              <pre data-testid="result">{JSON.stringify(methods.getValues(), null, 2)}</pre>
+            </Paper> : null}
+        </form>
+      </FormProvider>;
+  },
+  args: {
+    name: 'controlledInput',
+    required: true,
+    rules: {
+      required: 'This is required.',
+      maxLength: {
+        value: 10,
+        message: 'Too long'
+      }
+    },
+    inputProps: {
+      'aria-label': 'Input Label'
+    }
+  }
+}`,...(n=(a=o.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};const Lt=["_ControlledInput"];export{o as _ControlledInput,Lt as __namedExportsOrder,Gt as default};
---
+++ pr-preview/pr-764/assets/TableBody.stories-DP8vffZ1.js
@@ -0,0 +1,19 @@
+import{j as l}from"./jsx-runtime-BTJTZTIL.js";import{a as b,T as r}from"./TableRow-D5lAztty.js";import{T as e}from"./TableCell-BcTZw5PM.js";import"./index-ChsGqxH_.js";import"./TableRow-CYkSGJKZ.js";import"./index-BgYLq7fD.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./generateUtilityClass-GxznSYtt.js";import"./identifier-CHUoN9pF.js";import"./TableCell-DT8dV3s1.js";import"./memoTheme-BLrvrRyo.js";import"./styled-FZfCzBz5.js";import"./DefaultPropsProvider-ArLX9SCD.js";import"./generateUtilityClasses-aotQgNYZ.js";import"./visuallyHidden-Dan1xhjv.js";const g={title:"Components/Table/TableBody",component:b,tags:["autodocs"],args:{}},a={render:C=>l.jsxs(b,{...C,children:[l.jsxs(r,{children:[l.jsx(e,{children:"Cell a1"}),l.jsx(e,{children:"Cell a2"}),l.jsx(e,{children:"Cell a3"})]}),l.jsxs(r,{children:[l.jsx(e,{children:"Cell b1"}),l.jsx(e,{children:"Cell b2"}),l.jsx(e,{children:"Cell b3"})]}),l.jsxs(r,{children:[l.jsx(e,{children:"Cell c1"}),l.jsx(e,{children:"Cell c2"}),l.jsx(e,{children:"Cell c3"})]})]})};var o,s,n;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: (args: TableBodyProps) => <TableBody {...args}>
+      <TableRow>
+        <TableCell>Cell a1</TableCell>
+        <TableCell>Cell a2</TableCell>
+        <TableCell>Cell a3</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell>Cell b1</TableCell>
+        <TableCell>Cell b2</TableCell>
+        <TableCell>Cell b3</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell>Cell c1</TableCell>
+        <TableCell>Cell c2</TableCell>
+        <TableCell>Cell c3</TableCell>
+      </TableRow>
+    </TableBody>
+}`,...(n=(s=a.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const f=["_TableBody"];export{a as _TableBody,f as __namedExportsOrder,g as default};
