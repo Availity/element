3a835c67ff872933f3a16c0361db5ead5560fbc5
Greg Martin <greg.martin@availity.com>
2025-05-21 16:44:40 -0400

    docs(mui-file-selector): add FileSelector examples back

--- packages/file-selector/src/lib/FileSelector2.stories.tsx
+++ packages/file-selector/src/lib/FileSelector2.stories.tsx
@@ -2,39 +2,20 @@
 import { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { FormProvider, useForm } from 'react-hook-form';
-import { QueryClient, QueryClientProvider, useQueryClient } from '@tanstack/react-query';
-import type { default as Upload } from '@availity/upload-core';
 import { Paper } from '@availity/mui-paper';
 import { Alert, AlertProps, AlertTitle } from '@availity/mui-alert';
 import { Grid } from '@availity/mui-layout';
 import { Button } from '@availity/mui-button';
 import Collapse from '@mui/material/Collapse';
 
-import { FileSelector, FileSelectorProps } from './FileSelector';
+import { FileSelector2, FileSelector2Props } from './FileSelector2';
 
-const meta: Meta<typeof FileSelector> = {
-  title: 'Components/File Selector/File Selector',
-  component: FileSelector,
+const meta: Meta<typeof FileSelector2> = {
+  title: 'Components/File Selector/FileSelector2',
+  component: FileSelector2,
   tags: ['autodocs'],
-  decorators: [
-    (Story: () => JSX.Element) => (
-      <QueryClientProvider
-        client={
-          new QueryClient({
-            defaultOptions: {
-              queries: {
-                refetchOnWindowFocus: false,
-              },
-            },
-          })
-        }
-      >
-        <Story />
-      </QueryClientProvider>
-    ),
-  ],
   args: {
-    name: 'file-selector',
+    name: 'file-selector-2',
     allowedFileTypes: ['.txt', '.png', '.pdf'],
     clientId: '123',
     customerId: '456',
@@ -74,39 +55,31 @@ const DismissableAlert = (props: AlertProps) => {
   );
 };
 
-export const _FileSelector: StoryObj<typeof FileSelector> = {
-  render: (props: FileSelectorProps) => {
+export const _FileSelector2: StoryObj<typeof FileSelector2> = {
+  render: (props: FileSelector2Props) => {
     const methods = useForm({
       defaultValues: {
         [props.name]: [] as File[],
       },
     });
 
-    const client = useQueryClient();
-
     const files = methods.watch(props.name);
 
     const handleOnSubmit = (values: Record<string, File[]>) => {
       if (values[props.name].length === 0) return;
-
-      const queries = client.getQueriesData<Upload>(['upload']);
-      const uploads = [];
-      for (const [, data] of queries) {
-        if (data) uploads.push(data);
-      }
     };
 
     return (
       <Paper sx={{ padding: '2rem' }}>
         <FormProvider {...methods}>
           <form onSubmit={methods.handleSubmit(handleOnSubmit)}>
-            <FileSelector {...props}>
+            <FileSelector2 {...props}>
               <DismissableAlert severity="warning">
                 <AlertTitle>Make an Appeal</AlertTitle>
                 This is an example alert. It is not part of the component. `children` you pass to the component will
                 show up here.
               </DismissableAlert>
-            </FileSelector>
+            </FileSelector2>
             {files.length > 0 && (
               <Grid size={{ xs: 12 }} justifyContent="end" display="flex" paddingTop={2.5}>
                 <Button type="submit" sx={{ marginLeft: 'auto', marginRight: 0 }}>
@@ -122,33 +95,25 @@ export const _FileSelector: StoryObj<typeof FileSelector> = {
 };
 
 /** Set `enableDropzone` to `false` for a button only file selector. */
-export const _ButtonOnly: StoryObj<typeof FileSelector> = {
-  render: (props: FileSelectorProps) => {
+export const _FileSelector2ButtonOnly: StoryObj<typeof FileSelector2> = {
+  render: (props: FileSelector2Props) => {
     const methods = useForm({
       defaultValues: {
         [props.name]: [] as File[],
       },
     });
 
-    const client = useQueryClient();
-
     const files = methods.watch(props.name);
 
     const handleOnSubmit = (values: Record<string, File[]>) => {
       if (values[props.name].length === 0) return;
-
-      const queries = client.getQueriesData<Upload>(['upload']);
-      const uploads = [];
-      for (const [, data] of queries) {
-        if (data) uploads.push(data);
-      }
     };
 
     return (
       <Paper sx={{ padding: '2rem' }}>
         <FormProvider {...methods}>
           <form onSubmit={methods.handleSubmit(handleOnSubmit)}>
-            <FileSelector {...props} enableDropArea={false} />
+            <FileSelector2 {...props} enableDropArea={false} />
             {files.length > 0 && (
               <Grid size={{ xs: 12 }} justifyContent="end" display="flex" paddingTop={2.5}>
                 <Button type="submit" sx={{ marginLeft: 'auto', marginRight: 0 }}>
@@ -164,33 +129,25 @@ export const _ButtonOnly: StoryObj<typeof FileSelector> = {
 };
 
 /** Upload password protected files. _For this example, the password for any file is '1234'_ */
-export const _Encrypted: StoryObj<typeof FileSelector> = {
-  render: (props: Omit<FileSelectorProps, 'bucketId'>) => {
+export const _FileSelector2Encrypted: StoryObj<typeof FileSelector2> = {
+  render: (props: Omit<FileSelector2Props, 'bucketId'>) => {
     const methods = useForm({
       defaultValues: {
         [props.name]: [] as File[],
       },
     });
 
-    const client = useQueryClient();
-
     const files = methods.watch(props.name);
 
     const handleOnSubmit = (values: Record<string, File[]>) => {
       if (values[props.name].length === 0) return;
-
-      const queries = client.getQueriesData<Upload>(['upload']);
-      const uploads = [];
-      for (const [, data] of queries) {
-        if (data) uploads.push(data);
-      }
     };
 
     return (
       <Paper sx={{ padding: '2rem' }}>
         <FormProvider {...methods}>
           <form onSubmit={methods.handleSubmit(handleOnSubmit)}>
-            <FileSelector {...props} bucketId="enc" />
+            <FileSelector2 {...props} bucketId="enc" />
             {files.length > 0 && (
               <Grid size={{ xs: 12 }} justifyContent="end" display="flex" paddingTop={2.5}>
                 <Button type="submit" sx={{ marginLeft: 'auto', marginRight: 0 }}>
@@ -211,39 +168,31 @@ export const _Encrypted: StoryObj<typeof FileSelector> = {
   },
 };
 
-export const _FileSelectorCustomTypesMessage: StoryObj<typeof FileSelector> = {
-  render: (props: FileSelectorProps) => {
+export const _FileSelector2CustomTypesMessage: StoryObj<typeof FileSelector2> = {
+  render: (props: FileSelector2Props) => {
     const methods = useForm({
       defaultValues: {
         [props.name]: [] as File[],
       },
     });
 
-    const client = useQueryClient();
-
     const files = methods.watch(props.name);
 
     const handleOnSubmit = (values: Record<string, File[]>) => {
       if (values[props.name].length === 0) return;
-
-      const queries = client.getQueriesData<Upload>(['upload']);
-      const uploads = [];
-      for (const [, data] of queries) {
-        if (data) uploads.push(data);
-      }
     };
 
     return (
       <Paper sx={{ padding: '2rem' }}>
         <FormProvider {...methods}>
           <form onSubmit={methods.handleSubmit(handleOnSubmit)}>
-            <FileSelector {...props}>
+            <FileSelector2 {...props}>
               <DismissableAlert severity="warning">
                 <AlertTitle>Make an Appeal</AlertTitle>
                 This is an example alert. It is not part of the component. `children` you pass to the component will
                 show up here.
               </DismissableAlert>
-            </FileSelector>
+            </FileSelector2>
             {files.length > 0 && (
               <Grid size={{ xs: 12 }} justifyContent="end" display="flex" paddingTop={2.5}>
                 <Button type="submit" sx={{ marginLeft: 'auto', marginRight: 0 }}>
@@ -261,39 +210,31 @@ export const _FileSelectorCustomTypesMessage: StoryObj<typeof FileSelector> = {
   },
 };
 
-export const _FileSelectorCustomSizeMessage: StoryObj<typeof FileSelector> = {
-  render: (props: FileSelectorProps) => {
+export const _FileSelector2CustomSizeMessage: StoryObj<typeof FileSelector2> = {
+  render: (props: FileSelector2Props) => {
     const methods = useForm({
       defaultValues: {
         [props.name]: [] as File[],
       },
     });
 
-    const client = useQueryClient();
-
     const files = methods.watch(props.name);
 
     const handleOnSubmit = (values: Record<string, File[]>) => {
       if (values[props.name].length === 0) return;
-
-      const queries = client.getQueriesData<Upload>(['upload']);
-      const uploads = [];
-      for (const [, data] of queries) {
-        if (data) uploads.push(data);
-      }
     };
 
     return (
       <Paper sx={{ padding: '2rem' }}>
         <FormProvider {...methods}>
           <form onSubmit={methods.handleSubmit(handleOnSubmit)}>
-            <FileSelector {...props}>
+            <FileSelector2 {...props}>
               <DismissableAlert severity="warning">
                 <AlertTitle>Make an Appeal</AlertTitle>
                 This is an example alert. It is not part of the component. `children` you pass to the component will
                 show up here.
               </DismissableAlert>
-            </FileSelector>
+            </FileSelector2>
             {files.length > 0 && (
               <Grid size={{ xs: 12 }} justifyContent="end" display="flex" paddingTop={2.5}>
                 <Button type="submit" sx={{ marginLeft: 'auto', marginRight: 0 }}>
