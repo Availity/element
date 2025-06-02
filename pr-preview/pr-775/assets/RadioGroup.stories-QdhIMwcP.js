6dc0fe256f478d3ca401e2fd9aba9f950b7bad7f
LauRoxx <LauRoxx@users.noreply.github.com>
2025-06-02 17:22:52 +0000

    Deploy preview for PR 775 🛫

---
+++ pr-preview/pr-775/assets/RadioGroup.stories-QdhIMwcP.js
@@ -0,0 +1,64 @@
+import{j as o}from"./jsx-runtime-BTJTZTIL.js";import{d as l,F as t,b as r,R as e,a as p}from"./Switch-C6oQMUTv.js";import"./FieldHelpIcon-Do0u9pLB.js";import"./Input-CaOKvJPL.js";import"./index-ClMemn8c.js";import{F as i}from"./FormControl-eNKWX08Y.js";import"./index-ChsGqxH_.js";import"./index-BgYLq7fD.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./styled-FZfCzBz5.js";import"./DefaultPropsProvider-ArLX9SCD.js";import"./identifier-CHUoN9pF.js";import"./generateUtilityClass-GxznSYtt.js";import"./FormControlLabel-NrRuXsuu.js";import"./memoTheme-BLrvrRyo.js";import"./generateUtilityClasses-aotQgNYZ.js";import"./formControlState-Dq1zat_P.js";import"./useSlot-qvYlW2UV.js";import"./resolveComponentProps-XMphxBSb.js";import"./useForkRef-Dz4aGlyk.js";import"./useFormControl-t_-EwZHM.js";import"./Typography-geKghQyJ.js";import"./index-Df15702_.js";import"./extendSxProp-BZnzVrgj.js";import"./useTheme-CVByJb8R.js";import"./emotion-react.browser.esm-DW5OmoQI.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./Box-BU2WakPM.js";import"./RadioGroup-CGGGzwtq.js";import"./SwitchBase-DpeF2ikX.js";import"./useControlled-DxC1uCGX.js";import"./ButtonBase-DwAEbHIm.js";import"./useTimeout-iC_34LsC.js";import"./useEnhancedEffect-j2zQLnYm.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./TransitionGroupContext-BKlA24Pm.js";import"./isFocusVisible-B8k4qzLc.js";import"./createSvgIcon-DK1T27V3.js";import"./SvgIcon-D3xe1wVB.js";import"./Modal-BSor9Am9.js";import"./ownerWindow-HkKU3E4x.js";import"./ownerDocument-DW-IO8s5.js";import"./Portal-DEVqEMA6.js";import"./useTheme-BGaXYmwe.js";import"./utils-tFrEP9Vf.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./getReactElementRef-Df0xbma2.js";import"./Backdrop-DwIksyfj.js";import"./Fade-rL0rk9XF.js";import"./useId-DIK8ekB4.js";import"./index-CmMPo3bx.js";import"./faCircleArrowRight-B14F5Piw.js";import"./faUser-D77uLSLS.js";import"./faSortDown-BIc1sKK4.js";import"./Select-D8Pqa1Dd.js";import"./Menu-BUIr0rCt.js";import"./index-DyJdRKzS.js";import"./useSlotProps-DT0tbkWv.js";import"./Popover-DNs0CrC2.js";import"./isHostComponent-DVu5iVWx.js";import"./debounce-Be36O1Ab.js";import"./Paper-zGtC1N39.js";import"./List-CN2yCDHK.js";import"./OutlinedInput-ClKDCKpR.js";import"./Stack-2ILcP_1D.js";import"./styled-CsKbBpC_.js";import"./useThemeProps-BNBLMFIr.js";import"./Divider-CE8hkX1u.js";import"./dividerClasses-Cy3VQ5Zc.js";import"./faCheck-BUdkY9Qv.js";import"./Switch-BMNMooHv.js";import"./index-DmY_4rvh.js";import"./Grid2-BbvvmKhh.js";import"./isMuiElement-DiOAsJE6.js";import"./Container-DvcsWH8C.js";import"./index-DJKl12U0.js";import"./index-k9OtkSpc.js";import"./Tooltip-Xh9QA514.js";import"./IconButton-BFylt634.js";import"./CircularProgress-o1goEU37.js";import"./Button-DrYdciQ0.js";import"./LoadingButton-C6EjcQI6.js";const Zo={title:"Form Components/FormUtils/RadioGroup",component:l,tags:["autodocs"]},d={render:a=>o.jsxs(i,{children:[o.jsx(t,{component:"legend",id:"radio-buttons-group-label",children:"Radio Group"}),o.jsxs(l,{"aria-labelledby":"radio-buttons-group-label",defaultValue:"one",name:"radio-buttons-group",...a,children:[o.jsx(r,{value:"one",control:o.jsx(e,{}),label:"Radio One with super extra very looooooooooooooooooooooooooooooooooooooooooong label"}),o.jsx(r,{value:"two",control:o.jsx(e,{}),label:"Radio Two",helpTopicId:"1234"}),o.jsx(r,{value:"three",control:o.jsx(e,{}),label:"Radio Three"})]})]}),args:{}},s={render:a=>o.jsxs(o.Fragment,{children:[o.jsxs(i,{margin:"normal",required:!0,children:[o.jsx(t,{id:"required-radio-buttons-group-label",children:"Required"}),o.jsxs(l,{"aria-labelledby":"required-radio-buttons-group-label",defaultValue:"one",name:"required-radio-buttons-group",...a,children:[o.jsx(r,{value:"one",control:o.jsx(e,{}),label:"Radio One"}),o.jsx(r,{value:"two",control:o.jsx(e,{}),label:"Radio Two",helpTopicId:"1234"}),o.jsx(r,{value:"three",control:o.jsx(e,{}),label:"Radio Three"})]})]}),o.jsxs(i,{margin:"normal",error:!0,children:[o.jsx(t,{id:"error-radio-buttons-group-label",children:"Error"}),o.jsxs(l,{"aria-labelledby":"error-radio-buttons-group-label","aria-describedby":"error-radio-buttons-group-helpertext",name:"error-radio-buttons-group",...a,children:[o.jsx(r,{value:"one",control:o.jsx(e,{}),label:"Radio One"}),o.jsx(r,{value:"two",control:o.jsx(e,{}),label:"Radio Two",helpTopicId:"1234"}),o.jsx(r,{value:"three",control:o.jsx(e,{}),label:"Radio Three"})]}),o.jsx(p,{id:"error-radio-buttons-group-helpertext",children:"Error Message"})]}),o.jsxs(i,{margin:"normal",className:"Mui-readOnly",children:[o.jsx(t,{id:"read-only-radio-buttons-group-label",children:"Read-only"}),o.jsxs(l,{"aria-labelledby":"read-only-radio-buttons-group-label","aria-describedby":"read-only-radio-buttons-group-helpertext",defaultValue:"one",name:"read-only-radio-buttons-group",...a,children:[o.jsx(r,{value:"one",control:o.jsx(e,{readOnly:!0}),label:"Radio One"}),o.jsx(r,{value:"two",control:o.jsx(e,{readOnly:!0}),label:"Radio Two",helpTopicId:"1234"}),o.jsx(r,{value:"three",control:o.jsx(e,{readOnly:!0}),label:"Radio Three"})]}),o.jsx(p,{id:"read-only-radio-buttons-group-helpertext",children:"Read-only state does not exist for non-text fields in HTML. Please review if this pattern can be avoided. If not, selection blocking and other behaviors will need to be recreated to simulate a read-only state."})]}),o.jsxs(i,{margin:"normal",disabled:!0,children:[o.jsx(t,{id:"disabled-radio-buttons-group-label",children:"Disabled"}),o.jsxs(l,{"aria-labelledby":"disabled-radio-buttons-group-label","aria-describedby":"disabled-radio-buttons-group-helpertext",defaultValue:"one",name:"disabled-radio-buttons-group",...a,children:[o.jsx(r,{value:"one",control:o.jsx(e,{}),label:"Radio One"}),o.jsx(r,{value:"two",control:o.jsx(e,{}),label:"Radio Two",helpTopicId:"1234"}),o.jsx(r,{value:"three",control:o.jsx(e,{}),label:"Radio Three"})]}),o.jsx(p,{id:"disabled-radio-buttons-group-helpertext",children:"Error Message"})]})]}),args:{}},n={render:a=>o.jsxs(i,{children:[o.jsx(t,{className:"legend",id:"radio-buttons-group-legend-label",children:"Radio Group"}),o.jsxs(l,{"aria-labelledby":"radio-buttons-group-legend-label",defaultValue:"one",name:"radio-buttons-group",...a,children:[o.jsx(r,{value:"one",control:o.jsx(e,{}),label:"Radio One"}),o.jsx(r,{value:"two",control:o.jsx(e,{}),label:"Radio Two",helpTopicId:"1234"}),o.jsx(r,{value:"three",control:o.jsx(e,{}),label:"Radio Three"})]})]}),args:{}};var u,b,m;d.parameters={...d.parameters,docs:{...(u=d.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: (args: RadioGroupProps) => <FormControl>
+      <FormLabel component="legend" id="radio-buttons-group-label">
+        Radio Group
+      </FormLabel>
+      <RadioGroup aria-labelledby="radio-buttons-group-label" defaultValue="one" name="radio-buttons-group" {...args}>
+        <FormControlLabel value="one" control={<Radio />} label="Radio One with super extra very looooooooooooooooooooooooooooooooooooooooooong label" />
+        <FormControlLabel value="two" control={<Radio />} label="Radio Two" helpTopicId="1234" />
+        <FormControlLabel value="three" control={<Radio />} label="Radio Three" />
+      </RadioGroup>
+    </FormControl>,
+  args: {}
+}`,...(m=(b=d.parameters)==null?void 0:b.docs)==null?void 0:m.source}}};var c,g,x;s.parameters={...s.parameters,docs:{...(c=s.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: (args: RadioGroupProps) => <>
+      <FormControl margin="normal" required>
+        <FormLabel id="required-radio-buttons-group-label">Required</FormLabel>
+        <RadioGroup aria-labelledby="required-radio-buttons-group-label" defaultValue="one" name="required-radio-buttons-group" {...args}>
+          <FormControlLabel value="one" control={<Radio />} label="Radio One" />
+          <FormControlLabel value="two" control={<Radio />} label="Radio Two" helpTopicId="1234" />
+          <FormControlLabel value="three" control={<Radio />} label="Radio Three" />
+        </RadioGroup>
+      </FormControl>
+      <FormControl margin="normal" error>
+        <FormLabel id="error-radio-buttons-group-label">Error</FormLabel>
+        <RadioGroup aria-labelledby="error-radio-buttons-group-label" aria-describedby="error-radio-buttons-group-helpertext" name="error-radio-buttons-group" {...args}>
+          <FormControlLabel value="one" control={<Radio />} label="Radio One" />
+          <FormControlLabel value="two" control={<Radio />} label="Radio Two" helpTopicId="1234" />
+          <FormControlLabel value="three" control={<Radio />} label="Radio Three" />
+        </RadioGroup>
+        <FormHelperText id="error-radio-buttons-group-helpertext">Error Message</FormHelperText>
+      </FormControl>
+      <FormControl margin="normal" className="Mui-readOnly">
+        <FormLabel id="read-only-radio-buttons-group-label">Read-only</FormLabel>
+        <RadioGroup aria-labelledby="read-only-radio-buttons-group-label" aria-describedby="read-only-radio-buttons-group-helpertext" defaultValue="one" name="read-only-radio-buttons-group" {...args}>
+          <FormControlLabel value="one" control={<Radio readOnly />} label="Radio One" />
+          <FormControlLabel value="two" control={<Radio readOnly />} label="Radio Two" helpTopicId="1234" />
+          <FormControlLabel value="three" control={<Radio readOnly />} label="Radio Three" />
+        </RadioGroup>
+        <FormHelperText id="read-only-radio-buttons-group-helpertext">Read-only state does not exist for non-text fields in HTML. Please review if this pattern can be avoided. If not, selection blocking and other behaviors will need to be recreated to simulate a read-only state.</FormHelperText>
+      </FormControl>
+      <FormControl margin="normal" disabled>
+        <FormLabel id="disabled-radio-buttons-group-label">Disabled</FormLabel>
+        <RadioGroup aria-labelledby="disabled-radio-buttons-group-label" aria-describedby="disabled-radio-buttons-group-helpertext" defaultValue="one" name="disabled-radio-buttons-group" {...args}>
+          <FormControlLabel value="one" control={<Radio />} label="Radio One" />
+          <FormControlLabel value="two" control={<Radio />} label="Radio Two" helpTopicId="1234" />
+          <FormControlLabel value="three" control={<Radio />} label="Radio Three" />
+        </RadioGroup>
+        <FormHelperText id="disabled-radio-buttons-group-helpertext">Error Message</FormHelperText>
+      </FormControl>
+    </>,
+  args: {}
+}`,...(x=(g=s.parameters)==null?void 0:g.docs)==null?void 0:x.source}}};var R,h,j,F,y;n.parameters={...n.parameters,docs:{...(R=n.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  render: (args: RadioGroupProps) => <FormControl>
+      <FormLabel className="legend" id="radio-buttons-group-legend-label">
+        Radio Group
+      </FormLabel>
+      <RadioGroup aria-labelledby="radio-buttons-group-legend-label" defaultValue="one" name="radio-buttons-group" {...args}>
+        <FormControlLabel value="one" control={<Radio />} label="Radio One" />
+        <FormControlLabel value="two" control={<Radio />} label="Radio Two" helpTopicId="1234" />
+        <FormControlLabel value="three" control={<Radio />} label="Radio Three" />
+      </RadioGroup>
+    </FormControl>,
+  args: {}
+}`,...(j=(h=n.parameters)==null?void 0:h.docs)==null?void 0:j.source},description:{story:'Legacy styling for the group label can be achieved with the `"legend"` class on the `FormLabel`.',...(y=(F=n.parameters)==null?void 0:F.docs)==null?void 0:y.description}}};const $o=["_RadioGroup","_States","_Legacy"];export{n as _Legacy,d as _RadioGroup,s as _States,$o as __namedExportsOrder,Zo as default};
