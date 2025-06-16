38bd4c72d704d7f6518388e0ebbf19bfaf2d6e6e
Greg Martin <greg.martin@availity.com>
2025-06-16 11:43:54 -0400

    docs(mui-file-selector): remove hardcoded maxTotalSize

--- packages/file-selector/src/lib/FileSelector2.stories.tsx
+++ packages/file-selector/src/lib/FileSelector2.stories.tsx
@@ -25,7 +25,6 @@ const meta: Meta<typeof FileSelector2> = {
     },
     maxFiles: 2,
     maxSize: 1 * 1024 * 1024, // 1MB
-    maxTotalSize: 0.1 * 1024 * 1024, // 2MB
     enableDropArea: true,
     isCloud: true,
     multiple: true,
