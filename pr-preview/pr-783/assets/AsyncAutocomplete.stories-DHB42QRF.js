218c1930900567d152073818262c538bb67bc3c8
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-06-18 19:51:31 +0000

    Deploy preview for PR 783 🛫

---
+++ pr-preview/pr-783/assets/AsyncAutocomplete.stories-DHB42QRF.js
@@ -0,0 +1,128 @@
+import{j as t}from"./jsx-runtime-BTJTZTIL.js";import{C as p}from"./AsyncAutocomplete-CESJeDi9.js";import{B as s}from"./index-k9OtkSpc.js";import{u as d,F as c}from"./index.esm-Cdw3oWqD.js";import{P as b}from"./index-Bd2OKRRh.js";import{T as S}from"./index-ClMemn8c.js";import{G as f}from"./index-DmY_4rvh.js";import{i as F}from"./index-Dl5t6TV_.js";import{A as w,a as V}from"./Types-DoqI4C18.js";import{i as y}from"./suspense-r7UyhJBu.js";import{Q}from"./queryClient-Dd_7vySe.js";import"./index-ChsGqxH_.js";import"./index-zo_jL5tg.js";import"./index-BgYLq7fD.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./index-BREKFP9Y.js";import"./index-YXU10wut.js";import"./index-DJKl12U0.js";import"./index-CmMPo3bx.js";import"./faCircleArrowRight-B14F5Piw.js";import"./faUser-D77uLSLS.js";import"./faSortDown-BIc1sKK4.js";import"./SvgIcon-D3xe1wVB.js";import"./generateUtilityClass-GxznSYtt.js";import"./identifier-CHUoN9pF.js";import"./memoTheme-BLrvrRyo.js";import"./styled-FZfCzBz5.js";import"./DefaultPropsProvider-ArLX9SCD.js";import"./generateUtilityClasses-aotQgNYZ.js";import"./faCheck-BUdkY9Qv.js";import"./Select-D8Pqa1Dd.js";import"./formControlState-Dq1zat_P.js";import"./useFormControl-t_-EwZHM.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./Menu-BUIr0rCt.js";import"./index-DyJdRKzS.js";import"./useSlotProps-DT0tbkWv.js";import"./resolveComponentProps-XMphxBSb.js";import"./useForkRef-Dz4aGlyk.js";import"./Popover-DNs0CrC2.js";import"./isHostComponent-DVu5iVWx.js";import"./useSlot-qvYlW2UV.js";import"./Portal-DEVqEMA6.js";import"./useTheme-BGaXYmwe.js";import"./useTheme-CVByJb8R.js";import"./utils-tFrEP9Vf.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./TransitionGroupContext-BKlA24Pm.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./useTimeout-iC_34LsC.js";import"./useEnhancedEffect-j2zQLnYm.js";import"./getReactElementRef-Df0xbma2.js";import"./ownerDocument-DW-IO8s5.js";import"./ownerWindow-HkKU3E4x.js";import"./debounce-Be36O1Ab.js";import"./Paper-zGtC1N39.js";import"./Modal-BSor9Am9.js";import"./Backdrop-DwIksyfj.js";import"./Fade-rL0rk9XF.js";import"./List-CN2yCDHK.js";import"./OutlinedInput-ClKDCKpR.js";import"./index-Df15702_.js";import"./extendSxProp-BZnzVrgj.js";import"./emotion-react.browser.esm-DW5OmoQI.js";import"./useControlled-DxC1uCGX.js";import"./useId-DIK8ekB4.js";import"./createSvgIcon-DK1T27V3.js";import"./FormControlLabel-NrRuXsuu.js";import"./Typography-geKghQyJ.js";import"./Switch-BMNMooHv.js";import"./SwitchBase-DpeF2ikX.js";import"./ButtonBase-DwAEbHIm.js";import"./isFocusVisible-B8k4qzLc.js";import"./RadioGroup-CGGGzwtq.js";import"./Box-BU2WakPM.js";import"./Divider-CE8hkX1u.js";import"./dividerClasses-Cy3VQ5Zc.js";import"./Stack-2ILcP_1D.js";import"./styled-CsKbBpC_.js";import"./useThemeProps-BNBLMFIr.js";import"./TextField-Dbm3e8rv.js";import"./FormControl-eNKWX08Y.js";import"./isMuiElement-DiOAsJE6.js";import"./Grid2-BbvvmKhh.js";import"./index-DLKRbc4C.js";import"./___vite-browser-external_commonjs-proxy-DDYoOVPM.js";import"./Autocomplete-Dm71EoBJ.js";import"./Close-9bLEWkOn.js";import"./usePreviousProps-DrPGTKoq.js";import"./Tooltip-Xh9QA514.js";import"./Chip-H5O1l7tv.js";import"./IconButton-BFylt634.js";import"./CircularProgress-o1goEU37.js";import"./ListSubheader-BX_8zoPk.js";import"./useInfiniteQuery-CLtpp7_P.js";import"./useBaseQuery-Dm_kC3Jp.js";import"./infiniteQueryBehavior-B_QQvG9O.js";import"./Button-DrYdciQ0.js";import"./LoadingButton-C6EjcQI6.js";import"./Container-DvcsWH8C.js";import"./index-DuRQpI0H.js";import"./_toKey-DFcMYS53.js";const wt={title:"Form Components/Controlled Form/Autocomplete/ControlledAsyncAutocomplete",component:p,tags:["autodocs"],argTypes:{...V,...w}},_=new F({name:"example"}),B=async(r,e)=>{const o=await _.post({offset:r,limit:e},{params:{}});return{totalCount:o.data.totalCount,offset:r,limit:e,options:o.data.options,count:o.data.count}},h=async(r,e)=>{const{options:o,totalCount:i}=await B(r,e);return{options:o,hasMore:r+e<i,offset:r}},g=new Q({defaultOptions:{queries:{refetchOnWindowFocus:!1}}}),n={render:r=>{var o,i,l;const e=d();return t.jsx(y,{client:g,children:t.jsx(c,{...e,children:t.jsxs("form",{onSubmit:e.handleSubmit(a=>a),children:[t.jsx(p,{...r}),t.jsxs(f,{container:!0,direction:"row",justifyContent:"space-between",marginTop:1,children:[t.jsx(s,{disabled:!((o=e==null?void 0:e.formState)!=null&&o.isSubmitSuccessful),children:"Reset",color:"secondary",onClick:()=>e.reset()}),t.jsx(s,{type:"submit",disabled:(i=e==null?void 0:e.formState)==null?void 0:i.isSubmitSuccessful,children:"Submit"})]}),(l=e==null?void 0:e.formState)!=null&&l.isSubmitSuccessful?t.jsxs(b,{sx:{padding:"1.5rem",marginTop:"1.5rem"},children:[t.jsx(S,{variant:"h2",children:"Submitted Values"}),t.jsx("pre",{"data-testid":"result",children:JSON.stringify(e.getValues(),null,2)})]}):null]})})})},args:{name:"controlledAsyncAutocomplete",FieldProps:{label:"Async Select",helperText:"Helper Text",fullWidth:!1,required:!0},getOptionLabel:r=>r.label,loadOptions:h,limit:10,queryKey:"example",rules:{required:"This is required."},defaultToFirstOption:!0,disableClearable:!0}},m={render:r=>{var o,i,l;const e=d({defaultValues:{controlledAsyncAutocomplete:[]}});return t.jsx(y,{client:g,children:t.jsx(c,{...e,children:t.jsxs("form",{onSubmit:e.handleSubmit(a=>a),children:[t.jsx(p,{...r}),t.jsxs(f,{container:!0,direction:"row",justifyContent:"space-between",marginTop:1,children:[t.jsx(s,{disabled:!((o=e==null?void 0:e.formState)!=null&&o.isSubmitSuccessful),children:"Reset",color:"secondary",onClick:()=>e.reset()}),t.jsx(s,{type:"submit",disabled:(i=e==null?void 0:e.formState)==null?void 0:i.isSubmitSuccessful,children:"Submit"})]}),(l=e==null?void 0:e.formState)!=null&&l.isSubmitSuccessful?t.jsxs(b,{sx:{padding:"1.5rem",marginTop:"1.5rem"},children:[t.jsx(S,{variant:"h2",children:"Submitted Values"}),t.jsx("pre",{"data-testid":"result",children:JSON.stringify(e.getValues(),null,2)})]}):null]})})})},args:{name:"controlledAsyncAutocomplete",FieldProps:{label:"Async Select",helperText:"Helper Text",fullWidth:!1,required:!0},getOptionLabel:r=>r.label,isOptionEqualToValue:(r,e)=>r.label===e.label,loadOptions:h,limit:5,queryKey:"example",rules:{required:"This is required."},defaultToFirstOption:!0,disableClearable:!0,multiple:!0,filterSelectedOptions:!0}},u={render:r=>{var o,i,l;const e=d();return t.jsx(y,{client:g,children:t.jsx(c,{...e,children:t.jsxs("form",{onSubmit:e.handleSubmit(a=>a),children:[t.jsx(p,{...r}),t.jsxs(f,{container:!0,direction:"row",justifyContent:"space-between",marginTop:1,children:[t.jsx(s,{disabled:!((o=e==null?void 0:e.formState)!=null&&o.isSubmitSuccessful),children:"Reset",color:"secondary",onClick:()=>e.reset()}),t.jsx(s,{type:"submit",disabled:(i=e==null?void 0:e.formState)==null?void 0:i.isSubmitSuccessful,children:"Submit"})]}),(l=e==null?void 0:e.formState)!=null&&l.isSubmitSuccessful?t.jsxs(b,{sx:{padding:"1.5rem",marginTop:"1.5rem"},children:[t.jsx(S,{variant:"h2",children:"Submitted Values"}),t.jsx("pre",{"data-testid":"result",children:JSON.stringify(e.getValues(),null,2)})]}):null]})})})},args:{name:"controlledAsyncAutocomplete",FieldProps:{label:"Async Select",helperText:"Helper Text",fullWidth:!1,required:!0},getOptionLabel:r=>r.label,loadOptions:h,limit:1,queryKey:"example2",rules:{required:"This is required."},defaultToOnlyOption:!0,disableClearable:!0}};var x,C,A;n.parameters={...n.parameters,docs:{...(x=n.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: args => {
+    const methods = useForm();
+    return <QueryClientProvider client={client}>
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(data => data)}>
+            <ControlledAsyncAutocomplete {...args} />
+            <Grid container direction="row" justifyContent="space-between" marginTop={1}>
+              <Button disabled={!methods?.formState?.isSubmitSuccessful} children="Reset" color="secondary" onClick={() => methods.reset()} />
+              <Button type="submit" disabled={methods?.formState?.isSubmitSuccessful} children="Submit" />
+            </Grid>
+            {methods?.formState?.isSubmitSuccessful ? <Paper sx={{
+            padding: '1.5rem',
+            marginTop: '1.5rem'
+          }}>
+                <Typography variant="h2">Submitted Values</Typography>
+                <pre data-testid="result">{JSON.stringify(methods.getValues(), null, 2)}</pre>
+              </Paper> : null}
+          </form>
+        </FormProvider>
+      </QueryClientProvider>;
+  },
+  args: {
+    name: 'controlledAsyncAutocomplete',
+    FieldProps: {
+      label: 'Async Select',
+      helperText: 'Helper Text',
+      fullWidth: false,
+      required: true
+    },
+    getOptionLabel: (val: Option) => val.label,
+    loadOptions,
+    limit: 10,
+    queryKey: 'example',
+    rules: {
+      required: 'This is required.'
+    },
+    defaultToFirstOption: true,
+    disableClearable: true
+  }
+}`,...(A=(C=n.parameters)==null?void 0:C.docs)==null?void 0:A.source}}};var T,j,O;m.parameters={...m.parameters,docs:{...(T=m.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: args => {
+    const methods = useForm({
+      defaultValues: {
+        controlledAsyncAutocomplete: []
+      }
+    });
+    return <QueryClientProvider client={client}>
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(data => data)}>
+            <ControlledAsyncAutocomplete {...args} />
+            <Grid container direction="row" justifyContent="space-between" marginTop={1}>
+              <Button disabled={!methods?.formState?.isSubmitSuccessful} children="Reset" color="secondary" onClick={() => methods.reset()} />
+              <Button type="submit" disabled={methods?.formState?.isSubmitSuccessful} children="Submit" />
+            </Grid>
+            {methods?.formState?.isSubmitSuccessful ? <Paper sx={{
+            padding: '1.5rem',
+            marginTop: '1.5rem'
+          }}>
+                <Typography variant="h2">Submitted Values</Typography>
+                <pre data-testid="result">{JSON.stringify(methods.getValues(), null, 2)}</pre>
+              </Paper> : null}
+          </form>
+        </FormProvider>
+      </QueryClientProvider>;
+  },
+  args: {
+    name: 'controlledAsyncAutocomplete',
+    FieldProps: {
+      label: 'Async Select',
+      helperText: 'Helper Text',
+      fullWidth: false,
+      required: true
+    },
+    getOptionLabel: (val: Option) => val.label,
+    isOptionEqualToValue: (option, value) => option.label === value.label,
+    loadOptions,
+    limit: 5,
+    queryKey: 'example',
+    rules: {
+      required: 'This is required.'
+    },
+    defaultToFirstOption: true,
+    disableClearable: true,
+    multiple: true,
+    filterSelectedOptions: true
+  }
+}`,...(O=(j=m.parameters)==null?void 0:j.docs)==null?void 0:O.source}}};var P,v,q;u.parameters={...u.parameters,docs:{...(P=u.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  render: args => {
+    const methods = useForm();
+    return <QueryClientProvider client={client}>
+        <FormProvider {...methods}>
+          <form onSubmit={methods.handleSubmit(data => data)}>
+            <ControlledAsyncAutocomplete {...args} />
+            <Grid container direction="row" justifyContent="space-between" marginTop={1}>
+              <Button disabled={!methods?.formState?.isSubmitSuccessful} children="Reset" color="secondary" onClick={() => methods.reset()} />
+              <Button type="submit" disabled={methods?.formState?.isSubmitSuccessful} children="Submit" />
+            </Grid>
+            {methods?.formState?.isSubmitSuccessful ? <Paper sx={{
+            padding: '1.5rem',
+            marginTop: '1.5rem'
+          }}>
+                <Typography variant="h2">Submitted Values</Typography>
+                <pre data-testid="result">{JSON.stringify(methods.getValues(), null, 2)}</pre>
+              </Paper> : null}
+          </form>
+        </FormProvider>
+      </QueryClientProvider>;
+  },
+  args: {
+    name: 'controlledAsyncAutocomplete',
+    FieldProps: {
+      label: 'Async Select',
+      helperText: 'Helper Text',
+      fullWidth: false,
+      required: true
+    },
+    getOptionLabel: (val: Option) => val.label,
+    loadOptions,
+    limit: 1,
+    queryKey: 'example2',
+    rules: {
+      required: 'This is required.'
+    },
+    defaultToOnlyOption: true,
+    disableClearable: true
+  }
+}`,...(q=(v=u.parameters)==null?void 0:v.docs)==null?void 0:q.source}}};const Vt=["_ControlledAsyncAutoComplete","_ControlledAsyncAutoCompleteMultiple","_ControlledAsyncAutoCompleteDefaultToOnlyOption"];export{n as _ControlledAsyncAutoComplete,u as _ControlledAsyncAutoCompleteDefaultToOnlyOption,m as _ControlledAsyncAutoCompleteMultiple,Vt as __namedExportsOrder,wt as default};
