b8734e68d5d49c4faa5176252db0df914b608375
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-06-25 15:04:33 +0000

    Deploy preview for PR 785 🛫

---
+++ pr-preview/pr-785/assets/ListItemButton.stories-V5SwjhJx.js
@@ -0,0 +1,178 @@
+import{j as e}from"./jsx-runtime-BTJTZTIL.js";import{r as N}from"./index-BgYLq7fD.js";import{I as a}from"./index-k9OtkSpc.js";import{S as c}from"./index-zOfo1cSu.js";import{C as f}from"./index-CC6mOWec.js";import{g as $,F as V,D as B,M as m}from"./index-CmMPo3bx.js";import{B as j,G as g}from"./index-DmY_4rvh.js";import{T as S}from"./index-ClMemn8c.js";import{c as i,L as l,a as n,b as s,d,g as p}from"./ListSubheader-Do9RaYCL.js";import"./index-ChsGqxH_.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./Tooltip-Xh9QA514.js";import"./generateUtilityClass-GxznSYtt.js";import"./identifier-CHUoN9pF.js";import"./index-DyJdRKzS.js";import"./useTheme-BGaXYmwe.js";import"./DefaultPropsProvider-ArLX9SCD.js";import"./useTheme-CVByJb8R.js";import"./styled-FZfCzBz5.js";import"./memoTheme-BLrvrRyo.js";import"./useSlot-qvYlW2UV.js";import"./resolveComponentProps-XMphxBSb.js";import"./useForkRef-Dz4aGlyk.js";import"./generateUtilityClasses-aotQgNYZ.js";import"./useTimeout-iC_34LsC.js";import"./useEnhancedEffect-j2zQLnYm.js";import"./useControlled-DxC1uCGX.js";import"./useId-DIK8ekB4.js";import"./getReactElementRef-Df0xbma2.js";import"./Portal-DEVqEMA6.js";import"./utils-tFrEP9Vf.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./TransitionGroupContext-BKlA24Pm.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./ownerDocument-DW-IO8s5.js";import"./useSlotProps-DT0tbkWv.js";import"./isFocusVisible-B8k4qzLc.js";import"./IconButton-BFylt634.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./ButtonBase-DwAEbHIm.js";import"./emotion-react.browser.esm-DW5OmoQI.js";import"./CircularProgress-o1goEU37.js";import"./Button-DrYdciQ0.js";import"./LoadingButton-C6EjcQI6.js";import"./Chip-H5O1l7tv.js";import"./createSvgIcon-DK1T27V3.js";import"./SvgIcon-D3xe1wVB.js";import"./Checkbox-DEN02h0-.js";import"./SwitchBase-DpeF2ikX.js";import"./useFormControl-t_-EwZHM.js";import"./faCircleArrowRight-B14F5Piw.js";import"./faUser-D77uLSLS.js";import"./faSortDown-BIc1sKK4.js";import"./Box-BU2WakPM.js";import"./extendSxProp-BZnzVrgj.js";import"./Grid2-BbvvmKhh.js";import"./styled-CsKbBpC_.js";import"./isMuiElement-DiOAsJE6.js";import"./useThemeProps-BNBLMFIr.js";import"./Stack-2ILcP_1D.js";import"./Container-DvcsWH8C.js";import"./Typography-geKghQyJ.js";import"./index-Df15702_.js";import"./List-CN2yCDHK.js";import"./ListItem-ObWngzl3.js";import"./isHostComponent-DVu5iVWx.js";import"./ListItemText-Dt0OkD4s.js";import"./listItemTextClasses-BE6nMJZB.js";import"./useThemeProps-BADT0iz2.js";import"./ListSubheader-BX_8zoPk.js";const at={title:"Components/List/ListItemButton",component:i,tags:["autodocs"],argTypes:{centerRipple:{table:{disable:!0}},disableRipple:{table:{disable:!0}},disableTouchRipple:{table:{disable:!0}},focusRipple:{table:{disable:!0}},TouchRippleProps:{table:{disable:!0}},touchRippleRef:{table:{disable:!0}}}},x={render:r=>e.jsx(i,{...r}),args:{children:"This text is a child of ListItemButton"}},I={render:()=>e.jsx(j,{children:e.jsxs(l,{children:[e.jsx(n,{disablePadding:!0,children:e.jsx(i,{children:e.jsx(s,{primary:"Default"})})}),e.jsx(n,{disablePadding:!0,children:e.jsxs(i,{selected:!0,children:[e.jsx(d,{children:e.jsx($,{})}),e.jsx(s,{primary:"Selected"})]})}),e.jsx(n,{disablePadding:!0,children:e.jsxs(i,{className:"Mui-focusVisible",children:[e.jsx(d,{children:e.jsx(V,{})}),e.jsx(s,{primary:"Focused"})]})}),e.jsx(n,{disablePadding:!0,children:e.jsx(i,{disabled:!0,children:e.jsx(s,{primary:"Disabled"})})})]})})},u={render:()=>e.jsx(j,{component:"nav","aria-label":"mailbox folders",sx:{width:"100%",maxWidth:360,bgcolor:"background.paper"},children:e.jsxs(l,{children:[e.jsx(n,{disablePadding:!0,children:e.jsxs(i,{children:[e.jsx(d,{children:e.jsx($,{})}),e.jsx(s,{primary:"Inbox"})]})}),e.jsx(n,{disablePadding:!0,children:e.jsxs(i,{children:[e.jsx(d,{children:e.jsx(V,{})}),e.jsx(s,{primary:"Drafts"})]})}),e.jsx(n,{disablePadding:!0,children:e.jsx(i,{children:e.jsx(s,{primary:"Trash"})})}),e.jsx(n,{disablePadding:!0,children:e.jsx(i,{component:"a",href:"#folder-list",children:e.jsx(s,{primary:"Spam"})})})]})})},h={render:()=>{const[r,E]=N.useState([0]),y=t=>()=>{const o=r.indexOf(t),L=[...r];o===-1?L.push(t):L.splice(o,1),E(L)};return e.jsxs(g,{container:!0,spacing:8,children:[e.jsxs(g,{size:{xs:12,sm:6},children:[e.jsx(S,{variant:"h6",component:"div",id:"selectable-default-spacing-header",children:"Default Spacing"}),e.jsx(l,{"aria-labelledby":"selectable-default-spacing-header",children:[0,1,2,3].map(t=>{const o=`checkbox-list-label-${t}`;return e.jsx(n,{disablePadding:!0,secondaryAction:e.jsx(a,{edge:"end",title:"delete",children:e.jsx(B,{fontSize:"xsmall"})}),children:e.jsxs(i,{role:void 0,onClick:y(t),selected:r.indexOf(t)!==-1,children:[e.jsx(d,{children:e.jsx(f,{edge:"start",checked:r.indexOf(t)!==-1,tabIndex:-1,inputProps:{"aria-labelledby":o}})}),e.jsx(s,{id:o,primary:`List item ${t+1}`,secondary:"Secondary"})]})},t)})})]}),e.jsxs(g,{size:{xs:12,sm:6},children:[e.jsx(S,{variant:"h6",component:"div",id:"selectable-dense-spacing-header",children:"Dense Spacing"}),e.jsx(l,{dense:!0,"aria-labelledby":"selectable-dense-spacing-header",children:[4,5,6,7].map(t=>{const o=`checkbox-list-label-${t}`;return e.jsx(n,{disablePadding:!0,secondaryAction:e.jsx(a,{edge:"end",title:"delete",children:e.jsx(B,{fontSize:"xsmall"})}),children:e.jsxs(i,{role:void 0,onClick:y(t),selected:r.indexOf(t)!==-1,children:[e.jsx(d,{children:e.jsx(f,{edge:"start",checked:r.indexOf(t)!==-1,tabIndex:-1,inputProps:{"aria-labelledby":o}})}),e.jsx(s,{id:o,primary:`List item ${t+1}`,secondary:"Secondary"})]})},t)})})]})]})}},b={render:()=>e.jsx(j,{sx:{width:"100%",bgcolor:"background.paper"},children:e.jsxs(l,{"aria-label":"list",children:[e.jsx(p,{disablePadding:!0,color:"success",secondaryAction:e.jsx(a,{title:"More Actions",children:e.jsx(m,{fontSize:"xsmall"})}),children:e.jsxs(i,{children:[e.jsx(s,{children:"Item"}),e.jsx(c,{color:"success",label:"Approved"})]})}),e.jsx(p,{disablePadding:!0,color:"warning",secondaryAction:e.jsx(a,{title:"More Actions",children:e.jsx(m,{fontSize:"xsmall"})}),children:e.jsxs(i,{children:[e.jsx(s,{children:"Item"}),e.jsx(c,{color:"warning",label:"Pending"})]})}),e.jsx(p,{disablePadding:!0,color:"error",secondaryAction:e.jsx(a,{title:"More Actions",children:e.jsx(m,{fontSize:"xsmall"})}),children:e.jsxs(i,{children:[e.jsx(s,{children:"Item"}),e.jsx(c,{color:"error",label:"Denied"})]})}),e.jsx(p,{disablePadding:!0,color:"success",secondaryAction:e.jsx(a,{title:"More Actions",children:e.jsx(m,{fontSize:"xsmall"})}),children:e.jsxs(i,{children:[e.jsx(s,{children:"Item"}),e.jsx(c,{color:"success",label:"Approved"})]})})]})})};var k,P,T;x.parameters={...x.parameters,docs:{...(k=x.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: (args: ListItemButtonProps) => <ListItemButton {...args} />,
+  args: {
+    children: 'This text is a child of ListItemButton'
+  }
+}`,...(T=(P=x.parameters)==null?void 0:P.docs)==null?void 0:T.source}}};var C,v,A;I.parameters={...I.parameters,docs:{...(C=I.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: () => <Box>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemText primary="Default" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton selected>
+            <ListItemIcon>
+              <MailIcon />
+            </ListItemIcon>
+            <ListItemText primary="Selected" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton className="Mui-focusVisible">
+            <ListItemIcon>
+              <FolderIcon />
+            </ListItemIcon>
+            <ListItemText primary="Focused" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton disabled>
+            <ListItemText primary="Disabled" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+}`,...(A=(v=I.parameters)==null?void 0:v.docs)==null?void 0:A.source}}};var M,z,D;u.parameters={...u.parameters,docs:{...(M=u.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  render: () => <Box component="nav" aria-label="mailbox folders" sx={{
+    width: '100%',
+    maxWidth: 360,
+    bgcolor: 'background.paper'
+  }}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <MailIcon />
+            </ListItemIcon>
+            <ListItemText primary="Inbox" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <FolderIcon />
+            </ListItemIcon>
+            <ListItemText primary="Drafts" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemText primary="Trash" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#folder-list">
+            <ListItemText primary="Spam" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+}`,...(D=(z=u.parameters)==null?void 0:z.docs)==null?void 0:D.source}}};var w,_,O;h.parameters={...h.parameters,docs:{...(w=h.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: () => {
+    const [checked, setChecked] = useState([0]);
+    const handleToggle = (value: number) => () => {
+      const currentIndex = checked.indexOf(value);
+      const newChecked = [...checked];
+      if (currentIndex === -1) {
+        newChecked.push(value);
+      } else {
+        newChecked.splice(currentIndex, 1);
+      }
+      setChecked(newChecked);
+    };
+    return <Grid container spacing={8}>
+        <Grid size={{
+        xs: 12,
+        sm: 6
+      }}>
+          <Typography variant="h6" component="div" id="selectable-default-spacing-header">
+            Default Spacing
+          </Typography>
+          <List aria-labelledby="selectable-default-spacing-header">
+            {[0, 1, 2, 3].map(value => {
+            const labelId = \`checkbox-list-label-\${value}\`;
+            return <ListItem key={value} disablePadding secondaryAction={<IconButton edge="end" title="delete">
+                      <DeleteIcon fontSize="xsmall" />
+                    </IconButton>}>
+                  <ListItemButton role={undefined} onClick={handleToggle(value)} selected={checked.indexOf(value) !== -1}>
+                    <ListItemIcon>
+                      <Checkbox edge="start" checked={checked.indexOf(value) !== -1} tabIndex={-1} inputProps={{
+                    'aria-labelledby': labelId
+                  }} />
+                    </ListItemIcon>
+                    <ListItemText id={labelId} primary={\`List item \${value + 1}\`} secondary="Secondary" />
+                  </ListItemButton>
+                </ListItem>;
+          })}
+          </List>
+        </Grid>
+        <Grid size={{
+        xs: 12,
+        sm: 6
+      }}>
+          <Typography variant="h6" component="div" id="selectable-dense-spacing-header">
+            Dense Spacing
+          </Typography>
+          <List dense aria-labelledby="selectable-dense-spacing-header">
+            {[4, 5, 6, 7].map(value => {
+            const labelId = \`checkbox-list-label-\${value}\`;
+            return <ListItem key={value} disablePadding secondaryAction={<IconButton edge="end" title="delete">
+                      <DeleteIcon fontSize="xsmall" />
+                    </IconButton>}>
+                  <ListItemButton role={undefined} onClick={handleToggle(value)} selected={checked.indexOf(value) !== -1}>
+                    <ListItemIcon>
+                      <Checkbox edge="start" checked={checked.indexOf(value) !== -1} tabIndex={-1} inputProps={{
+                    'aria-labelledby': labelId
+                  }} />
+                    </ListItemIcon>
+                    <ListItemText id={labelId} primary={\`List item \${value + 1}\`} secondary="Secondary" />
+                  </ListItemButton>
+                </ListItem>;
+          })}
+          </List>
+        </Grid>
+      </Grid>;
+  }
+}`,...(O=(_=h.parameters)==null?void 0:_.docs)==null?void 0:O.source}}};var F,G,R;b.parameters={...b.parameters,docs:{...(F=b.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: () => <Box sx={{
+    width: '100%',
+    bgcolor: 'background.paper'
+  }}>
+      <List aria-label="list">
+        <ListItemStatusCard disablePadding color="success" secondaryAction={<IconButton title="More Actions">
+              <MoreVerticalIcon fontSize="xsmall" />
+            </IconButton>}>
+          <ListItemButton>
+            <ListItemText>Item</ListItemText>
+            <StatusChip color="success" label="Approved" />
+          </ListItemButton>
+        </ListItemStatusCard>
+        <ListItemStatusCard disablePadding color="warning" secondaryAction={<IconButton title="More Actions">
+              <MoreVerticalIcon fontSize="xsmall" />
+            </IconButton>}>
+          <ListItemButton>
+            <ListItemText>Item</ListItemText>
+            <StatusChip color="warning" label="Pending" />
+          </ListItemButton>
+        </ListItemStatusCard>
+        <ListItemStatusCard disablePadding color="error" secondaryAction={<IconButton title="More Actions">
+              <MoreVerticalIcon fontSize="xsmall" />
+            </IconButton>}>
+          <ListItemButton>
+            <ListItemText>Item</ListItemText>
+            <StatusChip color="error" label="Denied" />
+          </ListItemButton>
+        </ListItemStatusCard>
+        <ListItemStatusCard disablePadding color="success" secondaryAction={<IconButton title="More Actions">
+              <MoreVerticalIcon fontSize="xsmall" />
+            </IconButton>}>
+          <ListItemButton>
+            <ListItemText>Item</ListItemText>
+            <StatusChip color="success" label="Approved" />
+          </ListItemButton>
+        </ListItemStatusCard>
+      </List>
+    </Box>
+}`,...(R=(G=b.parameters)==null?void 0:G.docs)==null?void 0:R.source}}};const dt=["_ListItemButton","_States","_FolderList","_Selectable","_InteractiveStatusCards"];export{u as _FolderList,b as _InteractiveStatusCards,x as _ListItemButton,h as _Selectable,I as _States,dt as __namedExportsOrder,at as default};
---
+++ pr-preview/pr-785/assets/styled-CsKbBpC_.js
@@ -0,0 +1,1 @@
+import{a as t}from"./styled-FZfCzBz5.js";const s=t();export{s};
