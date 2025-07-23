dacff8143dd7a2fa955e009090fbd39eb535b103
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-07-23 19:40:58 +0000

    Deploy preview for PR 793 🛫

---
+++ pr-preview/pr-793/assets/CardHeader.stories-Bhtf8Edz.js
@@ -0,0 +1,57 @@
+import{j as r}from"./jsx-runtime-BTJTZTIL.js";import{C as t}from"./Card-DE4iyS3D.js";import{C as a}from"./CardHeader-GZfWn2kH.js";import{I as f}from"./index-k9OtkSpc.js";import{M as _}from"./index-CmMPo3bx.js";import{A as p}from"./index-BlUXr-HF.js";import{C as I}from"./CardActionArea-DNc57JHI.js";import"./index-ChsGqxH_.js";import"./Card-Ck1iBxwl.js";import"./index-BgYLq7fD.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./generateUtilityClass-GxznSYtt.js";import"./identifier-CHUoN9pF.js";import"./generateUtilityClasses-aotQgNYZ.js";import"./DefaultPropsProvider-ArLX9SCD.js";import"./styled-FZfCzBz5.js";import"./Paper-zGtC1N39.js";import"./useTheme-BGaXYmwe.js";import"./useTheme-CVByJb8R.js";import"./memoTheme-BLrvrRyo.js";import"./index-zOfo1cSu.js";import"./Chip-H5O1l7tv.js";import"./createSvgIcon-DK1T27V3.js";import"./SvgIcon-D3xe1wVB.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./useForkRef-Dz4aGlyk.js";import"./ButtonBase-DwAEbHIm.js";import"./useTimeout-iC_34LsC.js";import"./useEnhancedEffect-j2zQLnYm.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./TransitionGroupContext-BKlA24Pm.js";import"./emotion-react.browser.esm-DW5OmoQI.js";import"./isFocusVisible-B8k4qzLc.js";import"./CardHeader-C1d_JcOR.js";import"./useSlot-qvYlW2UV.js";import"./resolveComponentProps-XMphxBSb.js";import"./Typography-geKghQyJ.js";import"./index-Df15702_.js";import"./extendSxProp-BZnzVrgj.js";import"./Tooltip-Xh9QA514.js";import"./index-DyJdRKzS.js";import"./useControlled-DxC1uCGX.js";import"./useId-DIK8ekB4.js";import"./getReactElementRef-Df0xbma2.js";import"./Portal-DEVqEMA6.js";import"./utils-tFrEP9Vf.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./ownerDocument-DW-IO8s5.js";import"./useSlotProps-DT0tbkWv.js";import"./IconButton-BFylt634.js";import"./CircularProgress-o1goEU37.js";import"./Button-DrYdciQ0.js";import"./LoadingButton-C6EjcQI6.js";import"./faCircleArrowRight-B14F5Piw.js";import"./faUser-D77uLSLS.js";import"./faSortDown-BIc1sKK4.js";import"./index-DdNunzh7.js";import"./index-DLKRbc4C.js";import"./___vite-browser-external_commonjs-proxy-DDYoOVPM.js";import"./index-DuRQpI0H.js";import"./_toKey-DFcMYS53.js";import"./Avatar-BeozFZ7q.js";const wr={title:"Components/Card/CardHeader",component:a,tags:["autodocs"]},o={render:e=>r.jsx(t,{children:r.jsx(a,{...e})}),args:{title:"Card header",subheader:"Subheader"}},s={render:e=>r.jsx(t,{children:r.jsx(a,{...e})}),args:{title:"Card header",subheader:"Subheader",avatar:r.jsx(p,{size:"m"})}},d={render:e=>r.jsx(t,{children:r.jsx(I,{children:r.jsx(a,{...e})})}),args:{title:"Card header",subheader:"Subheader"}},i={render:e=>r.jsx(t,{children:r.jsx(a,{...e})}),args:{title:"Card header",subheader:"Subheader",avatar:r.jsx(p,{size:"m"}),action:r.jsx(f,{title:"More",size:"medium",children:r.jsx(_,{fontSize:"xsmall"})})}},m={render:e=>r.jsx(t,{children:r.jsx(a,{...e})}),args:{title:"Card header",subheader:"Subheader",statusChipProps:{position:"right",label:"Approved",color:"success"},avatar:r.jsx(p,{size:"m"}),action:r.jsx(f,{title:"More",size:"medium",children:r.jsx(_,{fontSize:"xsmall"})})}};var n,c,C;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: (args: CardHeaderProps) => <Card>
+      <CardHeader {...args} />
+    </Card>,
+  args: {
+    title: 'Card header',
+    subheader: 'Subheader'
+  }
+}`,...(C=(c=o.parameters)==null?void 0:c.docs)==null?void 0:C.source}}};var u,l,h;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: (args: CardHeaderProps) => <Card>
+      <CardHeader {...args} />
+    </Card>,
+  args: {
+    title: 'Card header',
+    subheader: 'Subheader',
+    avatar: <Avatar size="m" />
+  }
+}`,...(h=(l=s.parameters)==null?void 0:l.docs)==null?void 0:h.source}}};var g,x,b;d.parameters={...d.parameters,docs:{...(g=d.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: (args: CardHeaderProps) => <Card>
+      <CardActionArea>
+        <CardHeader {...args} />
+      </CardActionArea>
+    </Card>,
+  args: {
+    title: 'Card header',
+    subheader: 'Subheader'
+  }
+}`,...(b=(x=d.parameters)==null?void 0:x.docs)==null?void 0:b.source}}};var H,S,j;i.parameters={...i.parameters,docs:{...(H=i.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  render: (args: CardHeaderProps) => <Card>
+      <CardHeader {...args} />
+    </Card>,
+  args: {
+    title: 'Card header',
+    subheader: 'Subheader',
+    avatar: <Avatar size="m" />,
+    action: <IconButton title="More" size="medium">
+        <MoreVerticalIcon fontSize="xsmall" />
+      </IconButton>
+  }
+}`,...(j=(S=i.parameters)==null?void 0:S.docs)==null?void 0:j.source}}};var A,v,z;m.parameters={...m.parameters,docs:{...(A=m.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  render: (args: CardHeaderProps) => <Card>
+      <CardHeader {...args} />
+    </Card>,
+  args: {
+    title: 'Card header',
+    subheader: 'Subheader',
+    statusChipProps: {
+      position: 'right',
+      label: 'Approved',
+      color: 'success'
+    },
+    avatar: <Avatar size="m" />,
+    action: <IconButton title="More" size="medium">
+        <MoreVerticalIcon fontSize="xsmall" />
+      </IconButton>
+  }
+}`,...(z=(v=m.parameters)==null?void 0:v.docs)==null?void 0:z.source}}};const yr=["_CardHeader","_CardHeaderAvatar","_CardHeaderAvatarClickable","_CardHeaderAction","_CardHeaderStatusChip"];export{o as _CardHeader,i as _CardHeaderAction,s as _CardHeaderAvatar,d as _CardHeaderAvatarClickable,m as _CardHeaderStatusChip,yr as __namedExportsOrder,wr as default};
