0d4c2614eb655b6e118e615bd9b79cb2d5b9e1e5
christieb7007 <christieb7007@users.noreply.github.com>
2025-08-08 18:59:46 +0000

    Deploy preview for PR 797 🛫

---
+++ pr-preview/pr-797/assets/index-DkgxSyG-.js
@@ -0,0 +1,37 @@
+import{r as j}from"./index-BgYLq7fD.js";import{g as O,c as R}from"./generateUtilityClass-GxznSYtt.js";import{m as $}from"./memoTheme-BLrvrRyo.js";import{g as U}from"./generateUtilityClasses-aotQgNYZ.js";import{j as s}from"./jsx-runtime-BTJTZTIL.js";import{u as B}from"./DefaultPropsProvider-ArLX9SCD.js";import{s as I,c as L}from"./styled-FZfCzBz5.js";import{c as y,k as w}from"./emotion-react.browser.esm-DW5OmoQI.js";import{a as M}from"./identifier-CHUoN9pF.js";import{p as N,N as A}from"./index-CmMPo3bx.js";import{L as _}from"./index-BCsFyVyG.js";import{B as E}from"./Breadcrumbs-CJbZRFDL.js";import{T as k}from"./Typography-geKghQyJ.js";function T(t){return String(t).match(/[\d.\-+]*\s*(.*)/)[1]||""}function X(t){return parseFloat(t)}function D(t){return O("MuiSkeleton",t)}U("MuiSkeleton",["root","text","rectangular","rounded","circular","pulse","wave","withChildren","fitContent","heightAuto"]);const H=t=>{const{classes:r,variant:a,animation:e,hasChildren:o,width:i,height:n}=t;return L({root:["root",a,e,o&&"withChildren",o&&!i&&"fitContent",o&&!n&&"heightAuto"]},D,r)},u=w`
+  0% {
+    opacity: 1;
+  }
+
+  50% {
+    opacity: 0.4;
+  }
+
+  100% {
+    opacity: 1;
+  }
+`,v=w`
+  0% {
+    transform: translateX(-100%);
+  }
+
+  50% {
+    /* +0.5s of delay between each loop */
+    transform: translateX(100%);
+  }
+
+  100% {
+    transform: translateX(100%);
+  }
+`,z=typeof u!="string"?y`
+        animation: ${u} 2s ease-in-out 0.5s infinite;
+      `:null,K=typeof v!="string"?y`
+        &::after {
+          animation: ${v} 2s linear 0.5s infinite;
+        }
+      `:null,V=I("span",{name:"MuiSkeleton",slot:"Root",overridesResolver:(t,r)=>{const{ownerState:a}=t;return[r.root,r[a.variant],a.animation!==!1&&r[a.animation],a.hasChildren&&r.withChildren,a.hasChildren&&!a.width&&r.fitContent,a.hasChildren&&!a.height&&r.heightAuto]}})($(({theme:t})=>{const r=T(t.shape.borderRadius)||"px",a=X(t.shape.borderRadius);return{display:"block",backgroundColor:t.vars?t.vars.palette.Skeleton.bg:M(t.palette.text.primary,t.palette.mode==="light"?.11:.13),height:"1.2em",variants:[{props:{variant:"text"},style:{marginTop:0,marginBottom:0,height:"auto",transformOrigin:"0 55%",transform:"scale(1, 0.60)",borderRadius:`${a}${r}/${Math.round(a/.6*10)/10}${r}`,"&:empty:before":{content:'"\\00a0"'}}},{props:{variant:"circular"},style:{borderRadius:"50%"}},{props:{variant:"rounded"},style:{borderRadius:(t.vars||t).shape.borderRadius}},{props:({ownerState:e})=>e.hasChildren,style:{"& > *":{visibility:"hidden"}}},{props:({ownerState:e})=>e.hasChildren&&!e.width,style:{maxWidth:"fit-content"}},{props:({ownerState:e})=>e.hasChildren&&!e.height,style:{height:"auto"}},{props:{animation:"pulse"},style:z||{animation:`${u} 2s ease-in-out 0.5s infinite`}},{props:{animation:"wave"},style:{position:"relative",overflow:"hidden",WebkitMaskImage:"-webkit-radial-gradient(white, black)","&::after":{background:`linear-gradient(
+                90deg,
+                transparent,
+                ${(t.vars||t).palette.action.hover},
+                transparent
+              )`,content:'""',position:"absolute",transform:"translateX(-100%)",bottom:0,left:0,right:0,top:0}}},{props:{animation:"wave"},style:K||{"&::after":{animation:`${v} 2s linear 0.5s infinite`}}}]}})),ct=j.forwardRef(function(r,a){const e=B({props:r,name:"MuiSkeleton"}),{animation:o="pulse",className:i,component:n="span",height:l,style:p,variant:c="text",width:h,...m}=e,g={...e,animation:o,component:n,variant:c,hasChildren:!!m.children},S=H(g);return s.jsx(V,{as:n,ref:a,className:R(S.root,i),ownerState:g,...m,style:{width:h,height:l,...p}})});var W=Object.defineProperty,F=Object.defineProperties,q=Object.getOwnPropertyDescriptors,f=Object.getOwnPropertySymbols,x=Object.prototype.hasOwnProperty,C=Object.prototype.propertyIsEnumerable,b=(t,r,a)=>r in t?W(t,r,{enumerable:!0,configurable:!0,writable:!0,value:a}):t[r]=a,d=(t,r)=>{for(var a in r||(r={}))x.call(r,a)&&b(t,a,r[a]);if(f)for(var a of f(r))C.call(r,a)&&b(t,a,r[a]);return t},P=(t,r)=>F(t,q(r)),G=(t,r)=>{var a={};for(var e in t)x.call(t,e)&&r.indexOf(e)<0&&(a[e]=t[e]);if(t!=null&&f)for(var e of f(t))r.indexOf(e)<0&&C.call(t,e)&&(a[e]=t[e]);return a},J=({name:t,url:r,target:a="_top",LinkProps:e})=>{const o={"aria-label":t,children:t};return r?s.jsx(_,P(d(d({},o),e),{href:r,target:a})):s.jsx(k,d({},o))},mt=t=>{var r=t,{active:a,children:e,crumbs:o,emptyState:i="...",homeUrl:n="/public/apps/dashboard",LinkProps:l}=r,p=G(r,["active","children","crumbs","emptyState","homeUrl","LinkProps"]);return s.jsxs(E,P(d({},p),{separator:s.jsx(A,{fontSize:"xsmall"}),slotProps:{collapsedIcon:{className:"breadcrumbs__collapsed-icon"}},slots:{CollapsedIcon:N},"aria-label":p["aria-label"]||"breadcrumbs",children:[s.jsx(_,{"aria-label":"Home",href:n,loadApp:!1,children:"Home"}),o&&o.length>0&&o.map(({name:c=i,url:h,target:m})=>s.jsx(J,{name:c,url:h,target:m,LinkProps:l},c)),e,s.jsx(k,{children:a||i})]}))};export{mt as B,ct as S};
