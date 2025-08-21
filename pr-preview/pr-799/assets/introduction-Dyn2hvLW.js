fdf0bc26cb933b82710c229231630eab2c1b8164
christieb7007 <christieb7007@users.noreply.github.com>
2025-08-21 01:18:06 +0000

    Deploy preview for PR 799 🛫

---
+++ pr-preview/pr-799/assets/introduction-Dyn2hvLW.js
@@ -0,0 +1,285 @@
+import{j as t}from"./jsx-runtime-BTJTZTIL.js";import{useMDXComponents as o}from"./index-XODwqvNS.js";import{M as u,c as n}from"./index-ChdLjWan.js";import"./DocsRenderer-CFRXHY34-BwdMTffU.js";import"./index-ChsGqxH_.js";import"./index-BgYLq7fD.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./iframe-COCb5AQ3.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./client-DQNj1uEJ.js";const m=`# @availity/mui-block-ui
+
+> Availity MUI BlockUi component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-block-ui.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-block-ui)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-block-ui.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-block-ui)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-block-ui?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-block-ui/package.json)
+
+## Documentation
+
+This package extends the MUI BlockUi component: [MUI BlockUi Docs](https://mui.com/components/block-ui/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-block-ui-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://design.availity.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+_This package has a few peer dependencies. Add \`@mui/material\` & \`@emotion/react\` to your project if not already installed._
+
+\`\`\`bash
+npm install @availity/mui-block-ui
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-block-ui
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { BlockUi } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { BlockUi } from '@availity/mui-block-ui';
+\`\`\`
+`,l=`# Changelog
+
+This file was generated using [@jscutlery/semver](https://github.com/jscutlery/semver).
+
+## [1.0.6](https://github.com/Availity/element/compare/@availity/mui-block-ui@1.0.5...@availity/mui-block-ui@1.0.6) (2025-04-30)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`1.0.5\`
+## [1.0.5](https://github.com/Availity/element/compare/@availity/mui-block-ui@1.0.4...@availity/mui-block-ui@1.0.5) (2025-04-21)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`1.0.4\`
+## [1.0.4](https://github.com/Availity/element/compare/@availity/mui-block-ui@1.0.3...@availity/mui-block-ui@1.0.4) (2025-04-14)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`1.0.3\`
+## [1.0.3](https://github.com/Availity/element/compare/@availity/mui-block-ui@1.0.2...@availity/mui-block-ui@1.0.3) (2025-04-11)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`1.0.2\`
+* \`mui-paper\` updated to version \`1.0.2\`
+* \`mui-typography\` updated to version \`1.0.2\`
+## [1.0.2](https://github.com/Availity/element/compare/@availity/mui-block-ui@1.0.1...@availity/mui-block-ui@1.0.2) (2025-04-08)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`1.0.1\`
+## [1.0.1](https://github.com/Availity/element/compare/@availity/mui-block-ui@1.0.0...@availity/mui-block-ui@1.0.1) (2025-03-07)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`1.0.0\`
+* \`mui-paper\` updated to version \`1.0.0\`
+* \`mui-typography\` updated to version \`1.0.0\`
+## [1.0.0](https://github.com/Availity/element/compare/@availity/mui-block-ui@1.0.0-alpha.0...@availity/mui-block-ui@1.0.0) (2025-02-25)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.3.0\`
+* \`mui-paper\` updated to version \`0.3.0\`
+* \`mui-typography\` updated to version \`0.3.0\`
+
+### ⚠ BREAKING CHANGES
+
+* upgrade to @mui/material v6
+
+### Miscellaneous Chores
+
+* element v1 release ([a6e3567](https://github.com/Availity/element/commit/a6e35671185b9f13d25c7a39c4488ecb8774633e))
+
+## [1.0.0-alpha.0](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.3.0...@availity/mui-block-ui@1.0.0-alpha.0) (2025-02-24)
+
+
+### ⚠ BREAKING CHANGES
+
+* upgraded to @mui/material v6
+* **element:** upgraded to @mui/material v6
+
+### Features
+
+* **element:** upgrade to @mui/material v6 ([cb958bb](https://github.com/Availity/element/commit/cb958bba99a4f1ee6dab323f0ff54b69e6fd3493))
+* upgrade @mui/material ([571453a](https://github.com/Availity/element/commit/571453a34b21c344594ab4c03bc497d19aba942b))
+* upgrade to MUI v6 ([7febd6f](https://github.com/Availity/element/commit/7febd6fd4fd58e87e1c97a832cea3b4595a35d58))
+
+## [0.3.0](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.2.1...@availity/mui-block-ui@0.3.0) (2025-02-04)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.2.1\`
+* \`mui-paper\` updated to version \`0.2.1\`
+* \`mui-typography\` updated to version \`0.2.1\`
+
+### Features
+
+* configure package.json to allow esm to be used ([5a5393d](https://github.com/Availity/element/commit/5a5393de761f52608e714dd94a05106937dd95db))
+
+## [0.2.1](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.2.0...@availity/mui-block-ui@0.2.1) (2025-01-27)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.2.0\`
+## [0.2.0](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.19...@availity/mui-block-ui@0.2.0) (2025-01-23)
+
+
+### Features
+
+* **mui-block-ui:** backdrop colors ([dfcbffe](https://github.com/Availity/element/commit/dfcbffebb724f8787b16c5ad779d210e623ca2c7))
+
+## [0.1.19](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.18...@availity/mui-block-ui@0.1.19) (2025-01-22)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.1.18\`
+## [0.1.18](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.17...@availity/mui-block-ui@0.1.18) (2025-01-06)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.1.17\`
+## [0.1.17](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.16...@availity/mui-block-ui@0.1.17) (2025-01-03)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.1.16\`
+## [0.1.16](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.15...@availity/mui-block-ui@0.1.16) (2024-12-09)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.1.15\`
+## [0.1.15](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.14...@availity/mui-block-ui@0.1.15) (2024-10-31)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.1.14\`
+* \`mui-typography\` updated to version \`0.1.14\`
+## [0.1.14](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.13...@availity/mui-block-ui@0.1.14) (2024-08-08)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.1.13\`
+## [0.1.13](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.12...@availity/mui-block-ui@0.1.13) (2024-08-05)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.1.12\`
+## [0.1.12](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.11...@availity/mui-block-ui@0.1.12) (2024-07-22)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.1.11\`
+## [0.1.11](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.10...@availity/mui-block-ui@0.1.11) (2024-06-14)
+
+### Dependency Updates
+
+* \`mui-typography\` updated to version \`0.1.10\`
+## [0.1.10](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.9...@availity/mui-block-ui@0.1.10) (2024-04-19)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.1.9\`
+* \`mui-paper\` updated to version \`0.1.9\`
+* \`mui-typography\` updated to version \`0.1.9\`
+## [0.1.9](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.8...@availity/mui-block-ui@0.1.9) (2024-04-16)
+
+
+### Bug Fixes
+
+* **mui-block-ui:** update color ([fdf5197](https://github.com/Availity/element/commit/fdf51971de9e1858cd306b0970fac129bcf13db4))
+
+## [0.1.8](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.7...@availity/mui-block-ui@0.1.8) (2024-04-15)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.6.6\`
+## [0.1.7](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.6...@availity/mui-block-ui@0.1.7) (2024-04-04)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.6.5\`
+* \`mui-paper\` updated to version \`0.1.8\`
+* \`mui-typography\` updated to version \`0.1.7\`
+## [0.1.6](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.5...@availity/mui-block-ui@0.1.6) (2024-03-29)
+
+
+### Bug Fixes
+
+* **mui-block-ui:** pass className ([93da27e](https://github.com/Availity/element/commit/93da27ea90ec7605d384c6b1c25940d04be4a421))
+
+## [0.1.5](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.4...@availity/mui-block-ui@0.1.5) (2024-03-15)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.6.4\`
+## [0.1.4](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.3...@availity/mui-block-ui@0.1.4) (2024-03-05)
+
+
+### Bug Fixes
+
+* **mui-block-ui:** update unit test ([cfd54d4](https://github.com/Availity/element/commit/cfd54d454e131a12d765d364fc3638b0acb61501))
+* **mui-block-ui:** update unit test ([fbddccc](https://github.com/Availity/element/commit/fbddcccd3dce983d175e6939c951a77d54137ab0))
+* **mui-block-ui:** use src instead of dist ([035b418](https://github.com/Availity/element/commit/035b418b84d89c43c78fcd2405eadf2a3df6b0d5))
+
+## [0.1.3](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.2...@availity/mui-block-ui@0.1.3) (2024-02-22)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.6.3\`
+## [0.1.2](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.1...@availity/mui-block-ui@0.1.2) (2024-02-20)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.6.2\`
+* \`mui-paper\` updated to version \`0.1.7\`
+* \`mui-typography\` updated to version \`0.1.6\`
+## [0.1.1](https://github.com/Availity/element/compare/@availity/mui-block-ui@0.1.0...@availity/mui-block-ui@0.1.1) (2024-01-12)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.6.1\`
+## 0.1.0 (2023-12-22)
+
+### Dependency Updates
+
+* \`mui-button\` updated to version \`0.6.0\`
+* \`mui-paper\` updated to version \`0.1.6\`
+* \`mui-typography\` updated to version \`0.1.5\`
+
+### Features
+
+* **mui-block-ui:** add BlockUi component ([91f6103](https://github.com/Availity/element/commit/91f61039e74e4a2120988e4d2e72f0c27e82f686))
+* **mui-block-ui:** add more text and button to story ([23d24f5](https://github.com/Availity/element/commit/23d24f53900a713d43a069711180dd1c386c620a))
+
+
+### Bug Fixes
+
+* **mui-block-ui:** add text to better test keepInView ([51d391f](https://github.com/Availity/element/commit/51d391f09e777c8b505838fc4bf50967cd47b928))
+* **mui-block-ui:** make background darker and add to form story ([d923b89](https://github.com/Availity/element/commit/d923b89d07f406afc2964006f8d32551fe582d40))
+* **mui-block-ui:** update colors ([72aef04](https://github.com/Availity/element/commit/72aef0480595f61c1766be0cf523b7848fd4ef4b))
+`;function a(i){return t.jsxs(t.Fragment,{children:[t.jsx(u,{title:"Components/BlockUi/Introduction"}),`
+`,t.jsx(n,{children:m}),`
+`,t.jsx(n,{children:l})]})}function U(i={}){const{wrapper:e}={...o(),...i.components};return e?t.jsx(e,{...i,children:t.jsx(a,{...i})}):a()}export{U as default};
