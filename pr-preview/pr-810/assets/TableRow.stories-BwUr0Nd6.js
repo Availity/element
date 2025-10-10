0e4f6441174520dbd9b0bfc30a4d335f62322566
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-10-10 19:33:43 +0000

    Deploy preview for PR 810 🛫

---
+++ pr-preview/pr-810/assets/TableRow.stories-BwUr0Nd6.js
@@ -0,0 +1,90 @@
+import{j as r}from"./jsx-runtime-BTJTZTIL.js";import{T as l,a as h}from"./TableRow-D5lAztty.js";import"./TablePagination-BcCmf2ou.js";import{T as t}from"./TableCell-BcTZw5PM.js";import"./TableContainer-ClzDj8v-.js";import"./TableSortLabel-BJpxS7DP.js";import{r as g}from"./index-BgYLq7fD.js";import{I as T}from"./index-k9OtkSpc.js";import{E as u,n as j}from"./index-CmMPo3bx.js";import{B as C,G as i}from"./index-DmY_4rvh.js";import{C as w}from"./index-C8Ap_n0-.js";import{T as o}from"./index-ClMemn8c.js";import"./index-ChsGqxH_.js";import"./TableRow-CYkSGJKZ.js";import"./generateUtilityClass-GxznSYtt.js";import"./identifier-CHUoN9pF.js";import"./TableCell-DT8dV3s1.js";import"./memoTheme-BLrvrRyo.js";import"./styled-FZfCzBz5.js";import"./DefaultPropsProvider-ArLX9SCD.js";import"./generateUtilityClasses-aotQgNYZ.js";import"./TableHead-oMlxqc4e.js";import"./index-K0C69Pun.js";import"./index-DyJdRKzS.js";import"./KeyboardArrowRight-DGijcEGn.js";import"./createSvgIcon-DK1T27V3.js";import"./SvgIcon-D3xe1wVB.js";import"./PaginationItem-flpc0RTc.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./useSlot-qvYlW2UV.js";import"./resolveComponentProps-XMphxBSb.js";import"./useForkRef-Dz4aGlyk.js";import"./ButtonBase-DwAEbHIm.js";import"./useTimeout-iC_34LsC.js";import"./useEnhancedEffect-j2zQLnYm.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./TransitionGroupContext-BKlA24Pm.js";import"./emotion-react.browser.esm-DW5OmoQI.js";import"./isFocusVisible-B8k4qzLc.js";import"./IconButton-BFylt634.js";import"./useId-DIK8ekB4.js";import"./CircularProgress-o1goEU37.js";import"./OutlinedInput-ClKDCKpR.js";import"./useFormControl-t_-EwZHM.js";import"./formControlState-Dq1zat_P.js";import"./isHostComponent-DVu5iVWx.js";import"./index-Df15702_.js";import"./extendSxProp-BZnzVrgj.js";import"./useTheme-CVByJb8R.js";import"./ownerWindow-HkKU3E4x.js";import"./ownerDocument-DW-IO8s5.js";import"./debounce-Be36O1Ab.js";import"./MenuItem-B0sbJV79.js";import"./List-CN2yCDHK.js";import"./listItemTextClasses-BE6nMJZB.js";import"./dividerClasses-Cy3VQ5Zc.js";import"./Select-D8Pqa1Dd.js";import"./Menu-BUIr0rCt.js";import"./useSlotProps-DT0tbkWv.js";import"./Popover-DNs0CrC2.js";import"./Portal-DEVqEMA6.js";import"./useTheme-BGaXYmwe.js";import"./utils-tFrEP9Vf.js";import"./index-Bxu_udwM.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./index-DdXvKv-Q.js";import"./getReactElementRef-Df0xbma2.js";import"./Paper-zGtC1N39.js";import"./Modal-BSor9Am9.js";import"./Backdrop-DwIksyfj.js";import"./Fade-rL0rk9XF.js";import"./useControlled-DxC1uCGX.js";import"./Pagination-BT2C5VbO.js";import"./index-CkWMovvl.js";import"./index-DJKl12U0.js";import"./faCheck-BUdkY9Qv.js";import"./FormControlLabel-NrRuXsuu.js";import"./Typography-geKghQyJ.js";import"./Switch-B_HzHwQu.js";import"./SwitchBase-DpeF2ikX.js";import"./RadioGroup-CGGGzwtq.js";import"./Box-BU2WakPM.js";import"./Divider-CE8hkX1u.js";import"./Stack-2ILcP_1D.js";import"./styled-CsKbBpC_.js";import"./useThemeProps-BNBLMFIr.js";import"./visuallyHidden-Dan1xhjv.js";import"./TableContainer-BQmYc0Cb.js";import"./TableSortLabel-DaBUob62.js";import"./Tooltip-Xh9QA514.js";import"./Button-DrYdciQ0.js";import"./LoadingButton-C6EjcQI6.js";import"./faCircleArrowRight-B14F5Piw.js";import"./faUser-D77uLSLS.js";import"./faSortDown-BIc1sKK4.js";import"./Grid2-BbvvmKhh.js";import"./isMuiElement-DiOAsJE6.js";import"./Container-DvcsWH8C.js";import"./Collapse-RuyvmUHH.js";const ho={title:"Components/Table/TableRow",component:l,tags:["autodocs"],args:{children:"This text is a child of TableRow"}},e={render:a=>r.jsx(h,{children:r.jsxs(l,{...a,children:[r.jsx(t,{children:"Cell 1"}),r.jsx(t,{children:"Cell 2"}),r.jsx(t,{children:"Cell 3"})]})})},p={render:a=>{const[s,b]=g.useState(!1);return r.jsxs(h,{children:[r.jsxs(l,{...a,children:[r.jsx(t,{padding:"checkbox",children:r.jsx(T,{title:"expand row",size:"medium",onClick:()=>b(!s),children:s?r.jsx(u,{}):r.jsx(j,{})})}),r.jsx(t,{children:"Cell 1"}),r.jsx(t,{children:"Cell 2"}),r.jsx(t,{children:"Cell 3"})]}),r.jsx(l,{...a,children:r.jsx(t,{style:{padding:0,paddingLeft:"32px"},colSpan:12,children:r.jsx(w,{in:s,timeout:"auto",unmountOnExit:!0,children:r.jsxs(C,{sx:{padding:2},children:[r.jsxs(i,{container:!0,spacing:2,wrap:"wrap",children:[r.jsxs(i,{size:"grow",children:[r.jsx(o,{variant:"body2",sx:{fontWeight:"bold"},children:"Extra Data 1"}),r.jsx(o,{variant:"body2",children:"Lorem Ipsum"})]}),r.jsxs(i,{size:"grow",children:[r.jsx(o,{variant:"body2",sx:{fontWeight:"bold"},children:"Extra Data 2"}),r.jsx(o,{variant:"body2",children:"Lorem Ipsum"})]}),r.jsxs(i,{size:"grow",children:[r.jsx(o,{variant:"body2",sx:{fontWeight:"bold"},children:"Extra Data 3"}),r.jsx(o,{variant:"body2",children:"Lorem Ipsum"})]})]}),r.jsxs(i,{container:!0,spacing:2,wrap:"wrap",children:[r.jsxs(i,{size:"grow",children:[r.jsx(o,{variant:"body2",sx:{fontWeight:"bold"},children:"Extra Data 4"}),r.jsx(o,{variant:"body2",children:"Lorem Ipsum"})]}),r.jsxs(i,{size:"grow",children:[r.jsx(o,{variant:"body2",sx:{fontWeight:"bold"},children:"Extra Data 5"}),r.jsx(o,{variant:"body2",children:"Lorem Ipsum"})]}),r.jsxs(i,{size:"grow",children:[r.jsx(o,{variant:"body2",sx:{fontWeight:"bold"},children:"Extra Data 6"}),r.jsx(o,{variant:"body2",children:"Lorem Ipsum"})]})]})]})})})})]})}};var n,m,d;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: (args: TableRowProps) => <TableBody>
+      <TableRow {...args}>
+        <TableCell>Cell 1</TableCell>
+        <TableCell>Cell 2</TableCell>
+        <TableCell>Cell 3</TableCell>
+      </TableRow>
+    </TableBody>
+}`,...(d=(m=e.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var x,c,y;p.parameters={...p.parameters,docs:{...(x=p.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: (args: TableRowProps) => {
+    const [open, setOpen] = useState(false);
+    return <TableBody>
+        <TableRow {...args}>
+          <TableCell padding="checkbox">
+            <IconButton title="expand row" size="medium" onClick={() => setOpen(!open)}>
+              {open ? <ExpandIcon /> : <CollapseIcon />}
+            </IconButton>
+          </TableCell>
+          <TableCell>Cell 1</TableCell>
+          <TableCell>Cell 2</TableCell>
+          <TableCell>Cell 3</TableCell>
+        </TableRow>
+        <TableRow {...args}>
+          <TableCell style={{
+          padding: 0,
+          paddingLeft: '32px'
+        }} colSpan={12}>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <Box sx={{
+              padding: 2
+            }}>
+                <Grid container spacing={2} wrap="wrap">
+                  <Grid size="grow">
+                    <Typography variant="body2" sx={{
+                    fontWeight: 'bold'
+                  }}>
+                      Extra Data 1
+                    </Typography>
+                    <Typography variant="body2">Lorem Ipsum</Typography>
+                  </Grid>
+                  <Grid size="grow">
+                    <Typography variant="body2" sx={{
+                    fontWeight: 'bold'
+                  }}>
+                      Extra Data 2
+                    </Typography>
+                    <Typography variant="body2">Lorem Ipsum</Typography>
+                  </Grid>
+                  <Grid size="grow">
+                    <Typography variant="body2" sx={{
+                    fontWeight: 'bold'
+                  }}>
+                      Extra Data 3
+                    </Typography>
+                    <Typography variant="body2">Lorem Ipsum</Typography>
+                  </Grid>
+                </Grid>
+                <Grid container spacing={2} wrap="wrap">
+                  <Grid size="grow">
+                    <Typography variant="body2" sx={{
+                    fontWeight: 'bold'
+                  }}>
+                      Extra Data 4
+                    </Typography>
+                    <Typography variant="body2">Lorem Ipsum</Typography>
+                  </Grid>
+                  <Grid size="grow">
+                    <Typography variant="body2" sx={{
+                    fontWeight: 'bold'
+                  }}>
+                      Extra Data 5
+                    </Typography>
+                    <Typography variant="body2">Lorem Ipsum</Typography>
+                  </Grid>
+                  <Grid size="grow">
+                    <Typography variant="body2" sx={{
+                    fontWeight: 'bold'
+                  }}>
+                      Extra Data 6
+                    </Typography>
+                    <Typography variant="body2">Lorem Ipsum</Typography>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Collapse>
+          </TableCell>
+        </TableRow>
+      </TableBody>;
+  }
+}`,...(y=(c=p.parameters)==null?void 0:c.docs)==null?void 0:y.source}}};const bo=["_TableRow","_CollapsibleRow"];export{p as _CollapsibleRow,e as _TableRow,bo as __namedExportsOrder,ho as default};
