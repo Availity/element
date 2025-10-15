fed6898bc787b0f3a048653c085176d56a213ccd
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-10-15 18:38:46 +0000

    Deploy preview for PR 813 🛫

---
+++ pr-preview/pr-813/assets/TabPanel.stories-CRuPSza1.js
@@ -0,0 +1,14 @@
+import{j as r}from"./jsx-runtime-BTJTZTIL.js";import{T as m,a as p,b as s}from"./TabPanel-CTvIue5r.js";import{D as l}from"./index-CUusWYzA.js";import{T as n}from"./Tab-DgDtXouu.js";import"./index-ChsGqxH_.js";import"./TabPanel-BQDHBiA1.js";import"./index-BgYLq7fD.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./Tabs-DA3CdUG8.js";import"./generateUtilityClass-GxznSYtt.js";import"./identifier-CHUoN9pF.js";import"./memoTheme-BLrvrRyo.js";import"./styled-FZfCzBz5.js";import"./DefaultPropsProvider-ArLX9SCD.js";import"./generateUtilityClasses-aotQgNYZ.js";import"./ButtonBase-DwAEbHIm.js";import"./useTimeout-iC_34LsC.js";import"./useEnhancedEffect-j2zQLnYm.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./TransitionGroupContext-BKlA24Pm.js";import"./emotion-react.browser.esm-DW5OmoQI.js";import"./useForkRef-Dz4aGlyk.js";import"./isFocusVisible-B8k4qzLc.js";import"./index-DyJdRKzS.js";import"./useTheme-BGaXYmwe.js";import"./useTheme-CVByJb8R.js";import"./debounce-Be36O1Ab.js";import"./ownerWindow-HkKU3E4x.js";import"./ownerDocument-DW-IO8s5.js";import"./KeyboardArrowRight-DGijcEGn.js";import"./createSvgIcon-DK1T27V3.js";import"./SvgIcon-D3xe1wVB.js";import"./useSlotProps-DT0tbkWv.js";import"./resolveComponentProps-XMphxBSb.js";import"./useThemeProps-BADT0iz2.js";import"./useThemeProps-BNBLMFIr.js";import"./Divider-CE8hkX1u.js";import"./dividerClasses-Cy3VQ5Zc.js";const W={title:"Components/Tabs/TabPanel",component:m,tags:["autodocs"]},t={render:i=>r.jsxs(m,{value:"1",children:[r.jsx(p,{"aria-label":"lab API tabs example",children:r.jsx(n,{label:"Item One",value:"1"})}),r.jsx(l,{}),r.jsx(s,{...i,value:"1"})]}),args:{children:"Item One"}};var a,o,e;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: (args: TabPanelProps) => {
+    return <TabContext value="1">
+        <TabList aria-label="lab API tabs example">
+          <Tab label="Item One" value="1" />
+        </TabList>
+        <Divider />
+        <TabPanel {...args} value="1" />
+      </TabContext>;
+  },
+  args: {
+    children: 'Item One'
+  }
+}`,...(e=(o=t.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const X=["_TabPanel"];export{t as _TabPanel,X as __namedExportsOrder,W as default};
---
+++ pr-preview/pr-813/assets/introduction-B7ErvlIa.js
@@ -0,0 +1,208 @@
+import{j as t}from"./jsx-runtime-BTJTZTIL.js";import{useMDXComponents as a}from"./index-XODwqvNS.js";import{M as m,c as i}from"./index-6BYaPo8k.js";import"./DocsRenderer-CFRXHY34-Byt-u5gb.js";import"./index-ChsGqxH_.js";import"./index-BgYLq7fD.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./iframe-0nk9evyf.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./client-DQNj1uEJ.js";const l=`# @availity/mui-toggle-button
+
+> Availity MUI ToggleButton component to be used with @availity/element design system.
+
+[![Version](https://img.shields.io/npm/v/@availity/mui-toggle-button.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-toggle-button)
+[![NPM Downloads](https://img.shields.io/npm/dt/@availity/mui-toggle-button.svg?style=for-the-badge)](https://www.npmjs.com/package/@availity/mui-toggle-button)
+[![Dependency Status](https://img.shields.io/librariesio/release/npm/@availity/mui-toggle-button?style=for-the-badge)](https://github.com/Availity/element/blob/main/packages/mui-toggle-button/package.json)
+
+## Documentation
+
+This package extends the MUI ToggleButton component: [MUI ToggleButton Docs](https://mui.com/components/toggle-button/)
+
+Live demo and documentation in our [Storybook](https://availity.github.io/element/?path=/docs/components-togglebutton-introduction--docs)
+
+Availity standards for design and usage can be found in the [Availity Design Guide](https://design.availity.com/2e36e50c7)
+
+## Installation
+
+### Import Through @availity/element (Recommended)
+
+#### NPM
+
+\`\`\`bash
+npm install @availity/element
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/element
+\`\`\`
+
+### Direct Import
+
+#### NPM
+
+_This package has a few peer dependencies. Add \`@mui/material\` & \`@emotion/react\` to your project if not already installed._
+
+\`\`\`bash
+npm install @availity/mui-toggle-button
+\`\`\`
+
+#### Yarn
+
+\`\`\`bash
+yarn add @availity/mui-toggle-button
+\`\`\`
+
+### Usage
+
+#### Import through @availity/element
+
+\`\`\`tsx
+import { ToggleButton } from '@availity/element';
+\`\`\`
+
+#### Direct import
+
+\`\`\`tsx
+import { ToggleButton } from '@availity/mui-toggle-button';
+\`\`\`
+`,u=`# Changelog
+
+This file was generated using [@jscutlery/semver](https://github.com/jscutlery/semver).
+
+## [1.0.3](https://github.com/Availity/element/compare/@availity/mui-toggle-button@1.0.2...@availity/mui-toggle-button@1.0.3) (2025-04-30)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`1.0.2\`
+## [1.0.2](https://github.com/Availity/element/compare/@availity/mui-toggle-button@1.0.1...@availity/mui-toggle-button@1.0.2) (2025-04-11)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`1.0.1\`
+## [1.0.1](https://github.com/Availity/element/compare/@availity/mui-toggle-button@1.0.0...@availity/mui-toggle-button@1.0.1) (2025-03-07)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`1.0.0\`
+## [1.0.0](https://github.com/Availity/element/compare/@availity/mui-toggle-button@1.0.0-alpha.0...@availity/mui-toggle-button@1.0.0) (2025-02-25)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.2.0\`
+
+### ⚠ BREAKING CHANGES
+
+* upgrade to @mui/material v6
+
+### Miscellaneous Chores
+
+* element v1 release ([a6e3567](https://github.com/Availity/element/commit/a6e35671185b9f13d25c7a39c4488ecb8774633e))
+
+## [1.0.0-alpha.0](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.2.0...@availity/mui-toggle-button@1.0.0-alpha.0) (2025-02-24)
+
+
+### ⚠ BREAKING CHANGES
+
+* upgraded to @mui/material v6
+* **element:** upgraded to @mui/material v6
+
+### Features
+
+* **element:** upgrade to @mui/material v6 ([cb958bb](https://github.com/Availity/element/commit/cb958bba99a4f1ee6dab323f0ff54b69e6fd3493))
+* upgrade @mui/material ([571453a](https://github.com/Availity/element/commit/571453a34b21c344594ab4c03bc497d19aba942b))
+* upgrade to MUI v6 ([7febd6f](https://github.com/Availity/element/commit/7febd6fd4fd58e87e1c97a832cea3b4595a35d58))
+
+## [0.2.0](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.19...@availity/mui-toggle-button@0.2.0) (2025-02-04)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.1.19\`
+
+### Features
+
+* configure package.json to allow esm to be used ([5a5393d](https://github.com/Availity/element/commit/5a5393de761f52608e714dd94a05106937dd95db))
+
+## [0.1.19](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.18...@availity/mui-toggle-button@0.1.19) (2025-01-27)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.1.18\`
+## [0.1.18](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.17...@availity/mui-toggle-button@0.1.18) (2025-01-22)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.1.17\`
+## [0.1.17](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.16...@availity/mui-toggle-button@0.1.17) (2025-01-06)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.1.16\`
+## [0.1.16](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.15...@availity/mui-toggle-button@0.1.16) (2025-01-03)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.1.15\`
+## [0.1.15](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.14...@availity/mui-toggle-button@0.1.15) (2024-12-09)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.1.14\`
+## [0.1.14](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.13...@availity/mui-toggle-button@0.1.14) (2024-10-31)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.1.13\`
+## [0.1.13](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.12...@availity/mui-toggle-button@0.1.13) (2024-08-08)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.1.12\`
+## [0.1.12](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.11...@availity/mui-toggle-button@0.1.12) (2024-08-05)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.1.11\`
+## [0.1.11](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.10...@availity/mui-toggle-button@0.1.11) (2024-07-22)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.1.10\`
+## [0.1.10](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.9...@availity/mui-toggle-button@0.1.10) (2024-04-19)
+
+### Dependency Updates
+
+* \`mui-icon\` updated to version \`0.1.9\`
+## [0.1.9](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.8...@availity/mui-toggle-button@0.1.9) (2024-04-04)
+
+
+### Performance Improvements
+
+* **mui-toggle-button:** use path imports for material deps ([1e9bf41](https://github.com/Availity/element/commit/1e9bf418541859cd6162950b22a0b22a1dff6b9a))
+
+## [0.1.8](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.7...@availity/mui-toggle-button@0.1.8) (2024-02-20)
+
+## [0.1.7](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.6...@availity/mui-toggle-button@0.1.7) (2023-11-21)
+
+## [0.1.6](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.5...@availity/mui-toggle-button@0.1.6) (2023-11-07)
+
+## [0.1.5](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.4...@availity/mui-toggle-button@0.1.5) (2023-10-06)
+
+## [0.1.4](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.3...@availity/mui-toggle-button@0.1.4) (2023-08-22)
+
+### Bug Fixes
+
+- **mui-toggle-button:** export subcomponents ([49228f4](https://github.com/Availity/element/commit/49228f43d8f90bef8c1d31f7a95a9af2ce631592))
+
+## [0.1.3](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.2...@availity/mui-toggle-button@0.1.3) (2023-08-21)
+
+### Bug Fixes
+
+- **mui-toggle-button:** fix color contrast issues for 508 compliance ([9e6ec3e](https://github.com/Availity/element/commit/9e6ec3e6451efa9638c10a427bba8aac87b3c419))
+
+## [0.1.2](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.1...@availity/mui-toggle-button@0.1.2) (2023-08-11)
+
+## [0.1.1](https://github.com/Availity/element/compare/@availity/mui-toggle-button@0.1.0...@availity/mui-toggle-button@0.1.1) (2023-08-11)
+
+## 0.1.0 (2023-08-11)
+
+### Features
+
+- **mui-toggle-button:** init commit ([df6c00f](https://github.com/Availity/element/commit/df6c00fb753ea5f77ed7d5055a0289e2858d2235))
+- **mui-toggle-button:** toggle-button-group init commit ([4992bba](https://github.com/Availity/element/commit/4992bbaf1eef8e42fa7b16bb5d7a3d7d50c40dfd))
+`;function o(e){return t.jsxs(t.Fragment,{children:[t.jsx(m,{title:"Components/ToggleButton/Introduction"}),`
+`,t.jsx(i,{children:l}),`
+`,t.jsx(i,{children:u})]})}function U(e={}){const{wrapper:n}={...a(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(o,{...e})}):o()}export{U as default};
