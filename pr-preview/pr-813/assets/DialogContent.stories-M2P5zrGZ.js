877115136ef84d5014f4222f10f5513d69982ec7
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-10-15 19:47:09 +0000

    Remove preview for PR 813 🛬

--- pr-preview/pr-813/assets/DialogContent.stories-M2P5zrGZ.js
+++
@@ -1,6 +0,0 @@
-import{j as s}from"./jsx-runtime-BTJTZTIL.js";import{D as e}from"./DialogContent-CltZZPgq.js";import"./index-ChsGqxH_.js";import"./DialogContent-Bqwt87pB.js";import"./index-BgYLq7fD.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./generateUtilityClass-GxznSYtt.js";import"./identifier-CHUoN9pF.js";import"./memoTheme-BLrvrRyo.js";import"./styled-FZfCzBz5.js";import"./DefaultPropsProvider-ArLX9SCD.js";import"./generateUtilityClasses-aotQgNYZ.js";import"./dialogTitleClasses-wn6hUYL7.js";const j={title:"Components/Dialog/DialogContent",component:e,tags:["autodocs"]},o={render:a=>s.jsx(e,{...a}),args:{children:"Modal Content Body"}};var t,r,n;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  render: (args: DialogContentProps) => <DialogContent {...args} />,
-  args: {
-    children: 'Modal Content Body'
-  }
-}`,...(n=(r=o.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const h=["_DialogContent"];export{o as _DialogContent,h as __namedExportsOrder,j as default};
