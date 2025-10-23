bef8a1e9666598656b7ce169fbee2c5088e2d6d3
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-10-23 20:53:12 +0000

    Deploy preview for PR 816 🛫

---
+++ pr-preview/pr-816/assets/PageHeader.stories-CA0o3ZHs.js
@@ -0,0 +1,117 @@
+import{j as e}from"./jsx-runtime-BTJTZTIL.js";import{r as ae}from"./index-BgYLq7fD.js";import{F as te,S as T,b as ie,c as oe,a as x,d as P,e as U}from"./index-Doc0UVUi.js";import{D as se}from"./index-CUusWYzA.js";import{T as v}from"./index-ClMemn8c.js";import{B as ne,S as H}from"./index-BAK8aEmj.js";import{L as pe}from"./index-BCsFyVyG.js";import{B as de}from"./index-k9OtkSpc.js";import{G as t,B as me}from"./index-DmY_4rvh.js";import{i as X}from"./suspense-r7UyhJBu.js";import{Q as le}from"./queryClient-D8s2fCXB.js";import"./index-ChsGqxH_.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./index-CXGBBhzw.js";import"./Popover-DNs0CrC2.js";import"./generateUtilityClass-GxznSYtt.js";import"./identifier-CHUoN9pF.js";import"./isHostComponent-DVu5iVWx.js";import"./generateUtilityClasses-aotQgNYZ.js";import"./useSlot-qvYlW2UV.js";import"./resolveComponentProps-XMphxBSb.js";import"./useForkRef-Dz4aGlyk.js";import"./DefaultPropsProvider-ArLX9SCD.js";import"./Portal-DEVqEMA6.js";import"./useTheme-BGaXYmwe.js";import"./useTheme-CVByJb8R.js";import"./utils-tFrEP9Vf.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./TransitionGroupContext-BKlA24Pm.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./useTimeout-iC_34LsC.js";import"./useEnhancedEffect-j2zQLnYm.js";import"./getReactElementRef-Df0xbma2.js";import"./ownerDocument-DW-IO8s5.js";import"./ownerWindow-HkKU3E4x.js";import"./debounce-Be36O1Ab.js";import"./styled-FZfCzBz5.js";import"./Paper-zGtC1N39.js";import"./memoTheme-BLrvrRyo.js";import"./Modal-BSor9Am9.js";import"./Backdrop-DwIksyfj.js";import"./Fade-rL0rk9XF.js";import"./ToggleButtonGroup-vco19Q6_.js";import"./ToggleButton-C2t_akXR.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./ButtonBase-DwAEbHIm.js";import"./emotion-react.browser.esm-DW5OmoQI.js";import"./isFocusVisible-B8k4qzLc.js";import"./getValidReactChildren-DPCFFY5_.js";import"./index-DdNunzh7.js";import"./index-DLKRbc4C.js";import"./___vite-browser-external_commonjs-proxy-DDYoOVPM.js";import"./index-DuRQpI0H.js";import"./_toKey-DFcMYS53.js";import"./index-DbVu_i9b.js";import"./index-CkWMovvl.js";import"./index-DJKl12U0.js";import"./index-CmMPo3bx.js";import"./faCircleArrowRight-B14F5Piw.js";import"./faUser-D77uLSLS.js";import"./faSortDown-BIc1sKK4.js";import"./SvgIcon-D3xe1wVB.js";import"./faCheck-BUdkY9Qv.js";import"./Select-D8Pqa1Dd.js";import"./formControlState-Dq1zat_P.js";import"./useFormControl-t_-EwZHM.js";import"./Menu-BUIr0rCt.js";import"./index-DyJdRKzS.js";import"./useSlotProps-DT0tbkWv.js";import"./List-CN2yCDHK.js";import"./OutlinedInput-ClKDCKpR.js";import"./index-Df15702_.js";import"./extendSxProp-BZnzVrgj.js";import"./useControlled-DxC1uCGX.js";import"./useId-DIK8ekB4.js";import"./createSvgIcon-DK1T27V3.js";import"./FormControlLabel-NrRuXsuu.js";import"./Typography-geKghQyJ.js";import"./Switch-B_HzHwQu.js";import"./SwitchBase-DpeF2ikX.js";import"./RadioGroup-CGGGzwtq.js";import"./Box-BU2WakPM.js";import"./Divider-CE8hkX1u.js";import"./dividerClasses-Cy3VQ5Zc.js";import"./Stack-2ILcP_1D.js";import"./styled-CsKbBpC_.js";import"./useThemeProps-BNBLMFIr.js";import"./TextField-Dbm3e8rv.js";import"./FormControl-eNKWX08Y.js";import"./isMuiElement-DiOAsJE6.js";import"./Grid2-BbvvmKhh.js";import"./index.esm-Cdw3oWqD.js";import"./Container-DvcsWH8C.js";import"./index-g8KIT1P4.js";import"./index-rh78dY07.js";import"./Alert-BSC4cdjV.js";import"./Close-9bLEWkOn.js";import"./IconButton-BFylt634.js";import"./CircularProgress-o1goEU37.js";import"./AlertTitle-Bln1cCcI.js";import"./Dialog-BkyyFY3z.js";import"./DialogContext-Dy-mxFGl.js";import"./DialogTitle-DfOn25XU.js";import"./dialogTitleClasses-wn6hUYL7.js";import"./DialogActions-BLu5LWbN.js";import"./DialogContent-Bqwt87pB.js";import"./DialogContentText-BLvIXTV7.js";import"./dayjs.min-CfWDS3UC.js";import"./Img-B45hvfNA.js";import"./index-Chjiymov.js";import"./index-D95KORoo.js";import"./LinearProgress-DRbxDwO3.js";import"./index-DATHDNip.js";import"./Tooltip-Xh9QA514.js";import"./index-DexkmQxl.js";import"./ListItem-ObWngzl3.js";import"./ListItemText-Dt0OkD4s.js";import"./listItemTextClasses-BE6nMJZB.js";import"./useThemeProps-BADT0iz2.js";import"./index-B7xDH96f.js";import"./Link-rLN5tdDv.js";import"./TabPanel-BQDHBiA1.js";import"./Tabs-DA3CdUG8.js";import"./KeyboardArrowRight-DGijcEGn.js";import"./Breadcrumbs-CJbZRFDL.js";import"./Button-DrYdciQ0.js";import"./LoadingButton-C6EjcQI6.js";import"./infiniteQueryBehavior-B_QQvG9O.js";const ce=r=>r.spaceId?e.jsx(T,{imageType:"images.logo",spaceId:r.spaceId,Loader:({id:a})=>e.jsx(H,{id:a,height:"60px",width:"234px"})}):r.payerId?e.jsx(T,{imageType:"images.logo",payerId:r.payerId,Loader:({id:a})=>e.jsx(H,{id:a,height:"60px",width:"234px"})}):null,i=({breadcrumbs:r,buttons:a,feedback:b=!1,headerText:d,logo:s,help:o,divider:Z=e.jsx(se,{})})=>e.jsxs(t,{component:me,container:!0,direction:"column",sx:{marginTop:"1rem",marginBottom:"1.25rem",paddingLeft:3,paddingRight:3},children:[r||s||o?e.jsxs(t,{direction:"row",container:!0,sx:{justifyContent:"space-between",marginBottom:4},spacing:2,children:[r?e.jsx(t,{children:e.jsx(ne,{...r})}):e.jsx(t,{sx:{display:{xs:"none",sm:"block"}}}),(s||o)&&e.jsxs(t,{direction:"column",container:!0,spacing:2,children:[o&&e.jsx(t,{children:e.jsxs(v,{variant:"body1",children:["Need help?"," ",e.jsx(pe,{href:o.url,target:"_blank",loadApp:!1,children:o.helpAppName?"Watch a demo":"Learn More"})," ",o.helpAppName?` for ${o.helpAppName}`:null]})}),s&&e.jsx(t,{container:!0,justifyContent:"end",children:e.jsx(ce,{...s})})]})]}):null,e.jsxs(t,{direction:"row",container:!0,sx:{marginBottom:2,alignItems:"center",justifyContent:"space-between"},spacing:2,children:[e.jsx(t,{children:e.jsx(v,{variant:"h1",children:d})}),e.jsxs(t,{container:!0,sx:{width:"auto"},spacing:2,children:[a&&a.length>0&&(a==null?void 0:a.map(({key:ee,...re})=>e.jsx(t,{height:"100%",children:e.jsx(de,{...re,size:"large",color:"secondary"})},ee))),b?e.jsx(t,{sx:{height:"100%"},children:e.jsx(te,{appName:d})}):null]})]}),Z]});try{i.displayName="PageHeader",i.__docgenInfo={description:"",displayName:"PageHeader",props:{breadcrumbs:{defaultValue:null,description:"Render breadcrumbs above the header",name:"breadcrumbs",required:!1,type:{name:"BreadcrumbsProps"}},buttons:{defaultValue:null,description:"Renders buttons in the right side of the header",name:"buttons",required:!1,type:{name:"ButtonsProps[]"}},feedback:{defaultValue:{value:"false"},description:"If true, the Feedback button displays",name:"feedback",required:!1,type:{name:"boolean"}},headerText:{defaultValue:null,description:"The text that displays in the header",name:"headerText",required:!0,type:{name:"string"}},help:{defaultValue:null,description:"Help attributes",name:"help",required:!1,type:{name:"Help"}},logo:{defaultValue:null,description:"Logo attributes",name:"logo",required:!1,type:{name:"LogoSpaceId | LogoPayerId"}},divider:{defaultValue:{value:"<Divider />"},description:"The divider below the header, defaults to `<Divider />`. Can be replaced with `<TabList>`.",name:"divider",required:!1,type:{name:"Element"}}}}}catch{}const ka={title:"Components/PageHeader/PageHeader",component:i,tags:["autodocs"]},m={render:r=>e.jsx(i,{...r}),args:{headerText:"This text is a child of PageHeader",breadcrumbs:{active:"This Page"}}},l={render:r=>e.jsx(i,{...r}),args:{headerText:"This text is a child of PageHeader",breadcrumbs:{active:"This Page"},help:{helpAppName:void 0,url:"https://www.availity.com"}}},n={render:r=>e.jsx(i,{...r}),args:{headerText:"This text is a child of PageHeader",breadcrumbs:{active:"This Page"},help:{helpAppName:"This App",url:"https://www.availity.com"}}},c={render:r=>e.jsx(i,{...r}),args:{headerText:"This text is a child of PageHeader",breadcrumbs:{active:"This Page"},buttons:[{children:"Button 1",key:"button1"}]}},u={render:r=>e.jsx(i,{...r}),args:{headerText:"This text is a child of PageHeader",breadcrumbs:{active:"This Page"},buttons:[{children:"Button 1",key:"button1"},{children:"Button 2",key:"button2"}]}},p={render:r=>{const[a,b]=ae.useState("1"),d=(s,o)=>{b(o)};return e.jsxs(ie,{value:a,children:[e.jsx(i,{divider:e.jsxs(oe,{...r,onChange:d,"aria-label":"pageheader tabs example",children:[e.jsx(x,{label:"Tab One",value:"1"}),e.jsx(x,{label:"Tab Two",value:"2"}),e.jsx(x,{label:"Tab Three",value:"3"})]}),...r}),e.jsx(P,{value:"1",children:`Hello from Panel ${a}`}),e.jsx(P,{value:"2",children:`Hello from Panel ${a}`}),e.jsx(P,{value:"3",children:`Hello from Panel ${a}`})]})},args:{headerText:"This text is a child of PageHeader",breadcrumbs:{active:"This Page"}}},Y=new le,h={render:r=>e.jsx(X,{client:Y,children:e.jsx(U,{spaceIds:["11","22","33"],children:e.jsx(i,{...r})})}),args:{headerText:"App Name",breadcrumbs:{active:"This Page"},logo:{spaceId:"11"}}},g={render:r=>e.jsx(X,{client:Y,children:e.jsx(U,{spaceIds:["11","22","33"],children:e.jsx(i,{...r})})}),args:{headerText:"App Name",breadcrumbs:{active:"This Page"},help:{helpAppName:"This App",url:"https://www.availity.com"},logo:{spaceId:"11"}}};var f,y,j;m.parameters={...m.parameters,docs:{...(f=m.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: (args: PageHeaderProps) => <PageHeader {...args} />,
+  args: {
+    headerText: 'This text is a child of PageHeader',
+    breadcrumbs: {
+      active: 'This Page'
+    }
+  }
+}`,...(j=(y=m.parameters)==null?void 0:y.docs)==null?void 0:j.source}}};var w,_,S;l.parameters={...l.parameters,docs:{...(w=l.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: (args: PageHeaderProps) => <PageHeader {...args} />,
+  args: {
+    headerText: 'This text is a child of PageHeader',
+    breadcrumbs: {
+      active: 'This Page'
+    },
+    help: {
+      helpAppName: undefined,
+      url: 'https://www.availity.com'
+    }
+  }
+}`,...(S=(_=l.parameters)==null?void 0:_.docs)==null?void 0:S.source}}};var C,A,B,I,L;n.parameters={...n.parameters,docs:{...(C=n.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: (args: PageHeaderProps) => <PageHeader {...args} />,
+  args: {
+    headerText: 'This text is a child of PageHeader',
+    breadcrumbs: {
+      active: 'This Page'
+    },
+    help: {
+      helpAppName: 'This App',
+      url: 'https://www.availity.com'
+    }
+  }
+}`,...(B=(A=n.parameters)==null?void 0:A.docs)==null?void 0:B.source},description:{story:"`helpAppName` should be used when the help link leads to a demo video.",...(L=(I=n.parameters)==null?void 0:I.docs)==null?void 0:L.description}}};var N,k,V;c.parameters={...c.parameters,docs:{...(N=c.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  render: (args: PageHeaderProps) => <PageHeader {...args} />,
+  args: {
+    headerText: 'This text is a child of PageHeader',
+    breadcrumbs: {
+      active: 'This Page'
+    },
+    buttons: [{
+      children: 'Button 1',
+      key: 'button1'
+    }]
+  }
+}`,...(V=(k=c.parameters)==null?void 0:k.docs)==null?void 0:V.source}}};var q,D,Q;u.parameters={...u.parameters,docs:{...(q=u.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  render: (args: PageHeaderProps) => <PageHeader {...args} />,
+  args: {
+    headerText: 'This text is a child of PageHeader',
+    breadcrumbs: {
+      active: 'This Page'
+    },
+    buttons: [{
+      children: 'Button 1',
+      key: 'button1'
+    }, {
+      children: 'Button 2',
+      key: 'button2'
+    }]
+  }
+}`,...(Q=(D=u.parameters)==null?void 0:D.docs)==null?void 0:Q.source}}};var $,E,R,F,O;p.parameters={...p.parameters,docs:{...($=p.parameters)==null?void 0:$.docs,source:{originalSource:`{
+  render: (args: PageHeaderProps) => {
+    const [value, setValue] = useState('1');
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+      setValue(newValue);
+    };
+    return <TabContext value={value}>
+        <PageHeader divider={<TabList {...args} onChange={handleChange} aria-label="pageheader tabs example">
+              <Tab label="Tab One" value="1" />
+              <Tab label="Tab Two" value="2" />
+              <Tab label="Tab Three" value="3" />
+            </TabList>} {...args} />
+        <TabPanel value="1" children={\`Hello from Panel \${value}\`} />
+        <TabPanel value="2" children={\`Hello from Panel \${value}\`} />
+        <TabPanel value="3" children={\`Hello from Panel \${value}\`} />
+      </TabContext>;
+  },
+  args: {
+    headerText: 'This text is a child of PageHeader',
+    breadcrumbs: {
+      active: 'This Page'
+    }
+  }
+}`,...(R=(E=p.parameters)==null?void 0:E.docs)==null?void 0:R.source},description:{story:"Divider can be replaced with primary style `<TabList>`",...(O=(F=p.parameters)==null?void 0:F.docs)==null?void 0:O.description}}};var G,z,M;h.parameters={...h.parameters,docs:{...(G=h.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  render: (args: PageHeaderProps) => <QueryClientProvider client={queryClient}>
+      <Spaces spaceIds={['11', '22', '33']}>
+        <PageHeader {...args} />
+      </Spaces>
+    </QueryClientProvider>,
+  args: {
+    headerText: 'App Name',
+    breadcrumbs: {
+      active: 'This Page'
+    },
+    logo: {
+      spaceId: '11'
+    }
+  }
+}`,...(M=(z=h.parameters)==null?void 0:z.docs)==null?void 0:M.source}}};var W,J,K;g.parameters={...g.parameters,docs:{...(W=g.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  render: (args: PageHeaderProps) => <QueryClientProvider client={queryClient}>
+      <Spaces spaceIds={['11', '22', '33']}>
+        <PageHeader {...args} />
+      </Spaces>
+    </QueryClientProvider>,
+  args: {
+    headerText: 'App Name',
+    breadcrumbs: {
+      active: 'This Page'
+    },
+    help: {
+      helpAppName: 'This App',
+      url: 'https://www.availity.com'
+    },
+    logo: {
+      spaceId: '11'
+    }
+  }
+}`,...(K=(J=g.parameters)==null?void 0:J.docs)==null?void 0:K.source}}};const Va=["_PageHeader","_PageHeaderHelp","_PageHeaderHelpDemo","_PageHeaderSingleButton","_PageHeaderDoubleButtons","_PageHeaderTabs","_PageHeaderLogo","_PageHeaderLogoAndHelp"];export{m as _PageHeader,u as _PageHeaderDoubleButtons,l as _PageHeaderHelp,n as _PageHeaderHelpDemo,h as _PageHeaderLogo,g as _PageHeaderLogoAndHelp,c as _PageHeaderSingleButton,p as _PageHeaderTabs,Va as __namedExportsOrder,ka as default};
