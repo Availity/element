62931c7331310bb6df80c4f9c2b7dd9483bdb7af
DavidCritt <DavidCritt@users.noreply.github.com>
2025-10-30 20:59:44 +0000

    Deploy preview for PR 822 🛫

---
+++ pr-preview/pr-822/assets/PaginationItem.stories-BSP-frSx.js
@@ -0,0 +1,72 @@
+import{j as t}from"./jsx-runtime-BTJTZTIL.js";import{G as i}from"./index-CQspzllj.js";import{P as e}from"./PaginationItem-CSU55jZM.js";import"./index-ChsGqxH_.js";import"./index-BgYLq7fD.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./Box-Dv31mJKI.js";import"./identifier-CudAVvBZ.js";import"./generateUtilityClasses-whvyra8-.js";import"./generateUtilityClass-BeVSWKCg.js";import"./useTheme-Ib01SJ3a.js";import"./extendSxProp-BY7TjwKM.js";import"./Grid2-JMPt1-k0.js";import"./useTheme-Cx1wRXKO.js";import"./DefaultPropsProvider-CWkNlXkE.js";import"./styled-Dbr83ACF.js";import"./styled-CKuRQFSa.js";import"./isMuiElement-DiOAsJE6.js";import"./useThemeProps-Do5o_sjG.js";import"./Stack-qbqOHiMo.js";import"./Container-DVbvSSVb.js";import"./index-CIzqi3uD.js";import"./faCircleArrowRight-DSzUu1Qy.js";import"./faUser-D77uLSLS.js";import"./SvgIcon-CiBNlF9H.js";import"./memoTheme-CpoNRPWX.js";import"./PaginationItem-BfPGWPut.js";import"./index-DyJdRKzS.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./createSvgIcon-BWWwXn7v.js";import"./useSlot-1VRyIerd.js";import"./resolveComponentProps-CCNxqvxI.js";import"./useForkRef-Dz4aGlyk.js";import"./ButtonBase-D97P5WV0.js";import"./useTimeout-iC_34LsC.js";import"./useEnhancedEffect-j2zQLnYm.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./TransitionGroupContext-BKlA24Pm.js";import"./emotion-react.browser.esm-v7Jm8XFw.js";import"./isFocusVisible-B8k4qzLc.js";const Z={title:"Components/Pagination/PaginationItem",component:e,tags:["autodocs"],args:{type:"next"}},n={render:m=>t.jsx(e,{...m})},r={render:()=>t.jsxs(i,{container:!0,spacing:1,children:[t.jsx(i,{size:{xs:3},children:"Text"}),t.jsx(i,{size:{xs:9},children:"Outlined"}),t.jsx(i,{size:{xs:3},children:t.jsx(e,{variant:"text",type:"page",page:"1",selected:!0})}),t.jsx(i,{size:{xs:9},children:t.jsx(e,{variant:"outlined",type:"page",page:"1",selected:!0})}),t.jsx(i,{size:{xs:3},children:t.jsx(e,{variant:"text",type:"page",page:"1"})}),t.jsx(i,{size:{xs:9},children:t.jsx(e,{variant:"outlined",type:"page",page:"1"})}),t.jsx(i,{size:{xs:3},children:t.jsx(e,{variant:"text",type:"first"})}),t.jsx(i,{size:{xs:9},children:t.jsx(e,{variant:"outlined",type:"first"})}),t.jsx(i,{size:{xs:3},children:t.jsx(e,{variant:"text",type:"previous"})}),t.jsx(i,{size:{xs:9},children:t.jsx(e,{variant:"outlined",type:"previous"})}),t.jsx(i,{size:{xs:3},children:t.jsx(e,{variant:"text",type:"next"})}),t.jsx(i,{size:{xs:9},children:t.jsx(e,{variant:"outlined",type:"next"})}),t.jsx(i,{size:{xs:3},children:t.jsx(e,{variant:"text",type:"last"})}),t.jsx(i,{size:{xs:9},children:t.jsx(e,{variant:"outlined",type:"last"})})]})};var s,a,o;n.parameters={...n.parameters,docs:{...(s=n.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: (args: PaginationItemProps) => <PaginationItem {...args} />
+}`,...(o=(a=n.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};var p,x,d;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => <Grid container spacing={1}>
+      <Grid size={{
+      xs: 3
+    }}>Text</Grid>
+      <Grid size={{
+      xs: 9
+    }}>Outlined</Grid>
+      <Grid size={{
+      xs: 3
+    }}>
+        <PaginationItem variant="text" type="page" page="1" selected />
+      </Grid>
+      <Grid size={{
+      xs: 9
+    }}>
+        <PaginationItem variant="outlined" type="page" page="1" selected />
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>
+        <PaginationItem variant="text" type="page" page="1" />
+      </Grid>
+      <Grid size={{
+      xs: 9
+    }}>
+        <PaginationItem variant="outlined" type="page" page="1" />
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>
+        <PaginationItem variant="text" type="first" />
+      </Grid>
+      <Grid size={{
+      xs: 9
+    }}>
+        <PaginationItem variant="outlined" type="first" />
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>
+        <PaginationItem variant="text" type="previous" />
+      </Grid>
+      <Grid size={{
+      xs: 9
+    }}>
+        <PaginationItem variant="outlined" type="previous" />
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>
+        <PaginationItem variant="text" type="next" />
+      </Grid>
+      <Grid size={{
+      xs: 9
+    }}>
+        <PaginationItem variant="outlined" type="next" />
+      </Grid>
+      <Grid size={{
+      xs: 3
+    }}>
+        <PaginationItem variant="text" type="last" />
+      </Grid>
+      <Grid size={{
+      xs: 9
+    }}>
+        <PaginationItem variant="outlined" type="last" />
+      </Grid>
+    </Grid>
+}`,...(d=(x=r.parameters)==null?void 0:x.docs)==null?void 0:d.source}}};const $=["_PaginationItem","_Types"];export{n as _PaginationItem,r as _Types,$ as __namedExportsOrder,Z as default};
---
+++ pr-preview/pr-822/assets/TableHead.stories-ZKAxbRBo.js
@@ -0,0 +1,9 @@
+import{j as e}from"./jsx-runtime-BTJTZTIL.js";import{T as s}from"./TableHead-C_rTWG7Z.js";import{T as d}from"./TableRow-B9EZR1AL.js";import{T as a}from"./TableCell-CTNndg7j.js";import"./index-ChsGqxH_.js";import"./TableHead-CVGF_Mp6.js";import"./index-BgYLq7fD.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./generateUtilityClass-BeVSWKCg.js";import"./identifier-CudAVvBZ.js";import"./TableCell-Dihxu9Xc.js";import"./memoTheme-CpoNRPWX.js";import"./styled-CKuRQFSa.js";import"./DefaultPropsProvider-CWkNlXkE.js";import"./generateUtilityClasses-whvyra8-.js";import"./TableRow-D6CVZBx2.js";import"./visuallyHidden-Dan1xhjv.js";const E={title:"Components/Table/TableHead",component:s,tags:["autodocs"],args:{}},r={render:m=>e.jsx(s,{...m,children:e.jsxs(d,{children:[e.jsx(a,{children:"Header 1"}),e.jsx(a,{children:"Header 2"}),e.jsx(a,{children:"Header 3"})]})})};var o,l,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: (args: TableHeadProps) => <TableHead {...args}>
+      <TableRow>
+        <TableCell>Header 1</TableCell>
+        <TableCell>Header 2</TableCell>
+        <TableCell>Header 3</TableCell>
+      </TableRow>
+    </TableHead>
+}`,...(t=(l=r.parameters)==null?void 0:l.docs)==null?void 0:t.source}}};const O=["_TableHead"];export{r as _TableHead,O as __namedExportsOrder,E as default};
