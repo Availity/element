3575f8a11126290c2a3ad3af0ebb1415ac177dff
gregmartDOTin <gregmartDOTin@users.noreply.github.com>
2025-11-06 15:54:51 +0000

    deploy: e23024aba55f2a4e1bfeceed6aff361d069d35ca

---
+++ pr-preview/pr-828/assets/RadioGroup.stories-eEZokkyJ.js
@@ -0,0 +1,50 @@
+import{j as o}from"./jsx-runtime-BTJTZTIL.js";import{r as y}from"./index-BgYLq7fD.js";import{R as T}from"./RadioGroup-bZEe10Dj.js";import{R as e}from"./Radio-BKZST-Is.js";import{F as d}from"./FormControl-BpN01e6f.js";import{F as s}from"./FormLabel-C4UB19VU.js";import{F as r}from"./FormControlLabel-BnmDY0zq.js";import{F as C}from"./FormHelperText-ChEHL-32.js";import"./index-ChsGqxH_.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./generateUtilityClass-BeVSWKCg.js";import"./identifier-CudAVvBZ.js";import"./generateUtilityClasses-whvyra8-.js";import"./Radio-I3N--tLc.js";import"./SwitchBase-CYtmyT40.js";import"./styled-CKuRQFSa.js";import"./DefaultPropsProvider-CWkNlXkE.js";import"./useFormControl-t_-EwZHM.js";import"./useControlled-DxC1uCGX.js";import"./ButtonBase-D97P5WV0.js";import"./useTimeout-iC_34LsC.js";import"./useEnhancedEffect-j2zQLnYm.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./TransitionGroupContext-BKlA24Pm.js";import"./emotion-react.browser.esm-v7Jm8XFw.js";import"./useForkRef-Dz4aGlyk.js";import"./isFocusVisible-B8k4qzLc.js";import"./createSvgIcon-BWWwXn7v.js";import"./SvgIcon-CiBNlF9H.js";import"./memoTheme-CpoNRPWX.js";import"./createSimplePaletteValueFilter-bm0fmN_7.js";import"./createChainedFunction-BO_9K8Jh.js";import"./useId-DIK8ekB4.js";import"./FormGroup-GBcTM3pP.js";import"./formControlState-Dq1zat_P.js";import"./utils-DoM3o7-Q.js";import"./isMuiElement-DiOAsJE6.js";import"./FieldHelpIcon-CaoA1T49.js";import"./index-DJKl12U0.js";import"./index-CIzqi3uD.js";import"./faCircleArrowRight-DSzUu1Qy.js";import"./faUser-D77uLSLS.js";import"./index-DnNfvsmq.js";import"./Tooltip-CIyLwlBA.js";import"./index-DyJdRKzS.js";import"./useTheme-Cx1wRXKO.js";import"./useTheme-Ib01SJ3a.js";import"./useSlot-1VRyIerd.js";import"./resolveComponentProps-CCNxqvxI.js";import"./getReactElementRef-Df0xbma2.js";import"./Portal-C2J4q3Ji.js";import"./utils-tFrEP9Vf.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./ownerDocument-DW-IO8s5.js";import"./useSlotProps-BmqT0tgx.js";import"./IconButton-D_kG6RhR.js";import"./CircularProgress-fcRSDrpN.js";import"./Button-yLAd6_L_.js";import"./LoadingButton-Cl76w144.js";import"./FormLabel-BDxB3zbH.js";import"./FormControlLabel-BzV1MzKU.js";import"./Typography-COSof5sR.js";import"./index-DfraP8LW.js";import"./extendSxProp-BY7TjwKM.js";import"./Box-Dv31mJKI.js";import"./FormHelperText-DrOxEAeh.js";const l=y.forwardRef(({children:a,...F},j)=>o.jsx(T,{...F,ref:j,children:a}));try{l.displayName="RadioGroup",l.__docgenInfo={description:"",displayName:"RadioGroup",props:{children:{defaultValue:null,description:"The content of the component.",name:"children",required:!1,type:{name:"ReactNode"}},classes:{defaultValue:null,description:"Override or extend the styles applied to the component.",name:"classes",required:!1,type:{name:"Partial<FormGroupClasses>"}},sx:{defaultValue:null,description:"The system prop that allows defining system overrides as well as additional CSS styles.",name:"sx",required:!1,type:{name:"SxProps<Theme>"}},defaultValue:{defaultValue:null,description:"The default value. Use when the component is not controlled.",name:"defaultValue",required:!1,type:{name:"any"}},onChange:{defaultValue:null,description:`Callback fired when a radio button is selected.
+@param event The event source of the callback.
+@param value The value of the selected radio button.
+You can pull out the new value by accessing \`event.target.value\` (string).`,name:"onChange",required:!1,type:{name:"(event: ChangeEvent<HTMLInputElement>, value: string) => void"}},row:{defaultValue:{value:"false"},description:"Display group of elements in a compact row.",name:"row",required:!1,type:{name:"boolean"}},name:{defaultValue:null,description:`The name used to reference the value of the control.
+If you don't provide this prop, it falls back to a randomly generated name.`,name:"name",required:!1,type:{name:"string"}},value:{defaultValue:null,description:"Value of the selected radio button. The DOM API casts this to a string.",name:"value",required:!1,type:{name:"any"}}}}}catch{}const ko={title:"Form Components/FormUtils/RadioGroup",component:l,tags:["autodocs"]},n={render:a=>o.jsxs(d,{children:[o.jsx(s,{component:"legend",id:"radio-buttons-group-label",children:"Radio Group"}),o.jsxs(l,{"aria-labelledby":"radio-buttons-group-label",defaultValue:"one",name:"radio-buttons-group",...a,children:[o.jsx(r,{value:"one",control:o.jsx(e,{}),label:"Radio One"}),o.jsx(r,{value:"two",control:o.jsx(e,{}),label:"Radio Two",helpTopicId:"1234"}),o.jsx(r,{value:"three",control:o.jsx(e,{}),label:"Radio Three"})]})]}),args:{}},i={render:a=>o.jsxs(o.Fragment,{children:[o.jsxs(d,{margin:"normal",required:!0,children:[o.jsx(s,{id:"required-radio-buttons-group-label",children:"Required"}),o.jsxs(l,{"aria-labelledby":"required-radio-buttons-group-label",defaultValue:"one",name:"required-radio-buttons-group",...a,children:[o.jsx(r,{value:"one",control:o.jsx(e,{}),label:"Radio One"}),o.jsx(r,{value:"two",control:o.jsx(e,{}),label:"Radio Two",helpTopicId:"1234"}),o.jsx(r,{value:"three",control:o.jsx(e,{}),label:"Radio Three"})]})]}),o.jsxs(d,{margin:"normal",error:!0,children:[o.jsx(s,{id:"error-radio-buttons-group-label",children:"Error"}),o.jsxs(l,{"aria-labelledby":"error-radio-buttons-group-label","aria-describedby":"error-radio-buttons-group-helpertext",name:"error-radio-buttons-group",...a,children:[o.jsx(r,{value:"one",control:o.jsx(e,{}),label:"Radio One"}),o.jsx(r,{value:"two",control:o.jsx(e,{}),label:"Radio Two",helpTopicId:"1234"}),o.jsx(r,{value:"three",control:o.jsx(e,{}),label:"Radio Three"})]}),o.jsx(C,{id:"error-radio-buttons-group-helpertext",children:"Error Message"})]})]}),args:{}},t={render:a=>o.jsxs(d,{children:[o.jsx(s,{className:"legend",id:"radio-buttons-group-legend-label",children:"Radio Group"}),o.jsxs(l,{"aria-labelledby":"radio-buttons-group-legend-label",defaultValue:"one",name:"radio-buttons-group",...a,children:[o.jsx(r,{value:"one",control:o.jsx(e,{}),label:"Radio One"}),o.jsx(r,{value:"two",control:o.jsx(e,{}),label:"Radio Two",helpTopicId:"1234"}),o.jsx(r,{value:"three",control:o.jsx(e,{}),label:"Radio Three"})]})]}),args:{}};var p,u,m;n.parameters={...n.parameters,docs:{...(p=n.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: (args: RadioGroupProps) => <FormControl>
+      <FormLabel component="legend" id="radio-buttons-group-label">
+        Radio Group
+      </FormLabel>
+      <RadioGroup aria-labelledby="radio-buttons-group-label" defaultValue="one" name="radio-buttons-group" {...args}>
+        <FormControlLabel value="one" control={<Radio />} label="Radio One" />
+        <FormControlLabel value="two" control={<Radio />} label="Radio Two" helpTopicId="1234" />
+        <FormControlLabel value="three" control={<Radio />} label="Radio Three" />
+      </RadioGroup>
+    </FormControl>,
+  args: {}
+}`,...(m=(u=n.parameters)==null?void 0:u.docs)==null?void 0:m.source}}};var c,b,g;i.parameters={...i.parameters,docs:{...(c=i.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: (args: RadioGroupProps) => <>
+      <FormControl margin="normal" required>
+        <FormLabel id="required-radio-buttons-group-label">Required</FormLabel>
+        <RadioGroup aria-labelledby="required-radio-buttons-group-label" defaultValue="one" name="required-radio-buttons-group" {...args}>
+          <FormControlLabel value="one" control={<Radio />} label="Radio One" />
+          <FormControlLabel value="two" control={<Radio />} label="Radio Two" helpTopicId="1234" />
+          <FormControlLabel value="three" control={<Radio />} label="Radio Three" />
+        </RadioGroup>
+      </FormControl>
+      <FormControl margin="normal" error>
+        <FormLabel id="error-radio-buttons-group-label">Error</FormLabel>
+        <RadioGroup aria-labelledby="error-radio-buttons-group-label" aria-describedby="error-radio-buttons-group-helpertext" name="error-radio-buttons-group" {...args}>
+          <FormControlLabel value="one" control={<Radio />} label="Radio One" />
+          <FormControlLabel value="two" control={<Radio />} label="Radio Two" helpTopicId="1234" />
+          <FormControlLabel value="three" control={<Radio />} label="Radio Three" />
+        </RadioGroup>
+        <FormHelperText id="error-radio-buttons-group-helpertext">Error Message</FormHelperText>
+      </FormControl>
+    </>,
+  args: {}
+}`,...(g=(b=i.parameters)==null?void 0:b.docs)==null?void 0:g.source}}};var h,R,x,f,v;t.parameters={...t.parameters,docs:{...(h=t.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: (args: RadioGroupProps) => <FormControl>
+      <FormLabel className="legend" id="radio-buttons-group-legend-label">
+        Radio Group
+      </FormLabel>
+      <RadioGroup aria-labelledby="radio-buttons-group-legend-label" defaultValue="one" name="radio-buttons-group" {...args}>
+        <FormControlLabel value="one" control={<Radio />} label="Radio One" />
+        <FormControlLabel value="two" control={<Radio />} label="Radio Two" helpTopicId="1234" />
+        <FormControlLabel value="three" control={<Radio />} label="Radio Three" />
+      </RadioGroup>
+    </FormControl>,
+  args: {}
+}`,...(x=(R=t.parameters)==null?void 0:R.docs)==null?void 0:x.source},description:{story:'Legacy styling for the group label can be achieved with the `"legend"` class on the `FormLabel`.',...(v=(f=t.parameters)==null?void 0:f.docs)==null?void 0:v.description}}};const Do=["_RadioGroup","_States","_Legacy"];export{t as _Legacy,n as _RadioGroup,i as _States,Do as __namedExportsOrder,ko as default};
